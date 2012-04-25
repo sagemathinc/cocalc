0cc65abdb83fe24911cb590da983d3340322dada
William Stein <wstein@gmail.com>
2012-04-24 22:19:09 -0700

    Added some demos for AJAX support.

---
+++ workspace_server.py
@@ -0,0 +1,12 @@
+import sys
+
+from flask import Flask
+app = Flask(__name__)
+from flask import render_template
+
+@app.route('/demo1.html')
+def demo1():
+    return render_template('demo1.html')
+
+if __name__ == '__main__':
+    app.run(port=int(sys.argv[1]), debug=True)
