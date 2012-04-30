f8d6d474e21940300b375dbd8eeb3cf7a380a1b8
William Stein <wstein@gmail.com>
2012-04-29 19:28:30 -0700

    writing javascript client library: create a .js file in the static directory and include in a testing html page.

---
+++ static/sagews.js
@@ -0,0 +1,13 @@
+/* 
+
+sagews -- basic javascript client library for the sage workspace server
+
+AUTHOR: 
+   - William Stein
+
+*/
+
+
+test = function (mesg) {
+    alert(mesg);
+}
--- workspace_server.py
+++ workspace_server.py
@@ -8,5 +8,10 @@ from flask import render_template
 def demo1():
     return render_template('demo1.html')
 
+@app.route('/client')
+def client():
+    return render_template('client.html')
+
+
 if __name__ == '__main__':
     app.run(port=int(sys.argv[1]), debug=True)
