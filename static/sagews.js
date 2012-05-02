7eec8af62d4d903045d0d7b12daed631542a4b71
William Stein <wstein@gmail.com>
2012-05-02 09:02:13 -0700

    refactored demo1

--- static/sagews.js
+++ static/sagews.js
@@ -16,82 +16,152 @@ sagews.test = function (mesg) {
 
 sagews.Client = function(server) {
 
-  get = function(url, callback) {
-      $.getJSON(server + '/' + url, '', function(response, status) { callback(response); });
-  }
-
-  post = function(url, vars, callback) {
-      $.post(server + '/' + url, vars, function(response, status) { callback(response); }, 'json');
-  }
-
-  return {
-
-  /* All Sessions */
-
-    /* kill processes and delete everything about all sessions */
-    close_all_sessions: function(callback) { get('close_all_sessions', callback); }, 
-
-    /* much information about all sessions */
-    sessions: function(callback) { get('sessions', callback); }, 
+    get = function(url, callback) {
+	$.getJSON(server + '/' + url, '', function(response, status) { callback(response); });
+    }
     
-  /* Session Management: creating, interrupting, killing and deleting */
+    post = function(url, vars, callback) {
+	$.post(server + '/' + url, vars, function(response, status) { callback(response); }, 'json');
+    }
 
-    /* create a new session */
-    new_session: function(callback) { get('new_session', callback); },
+    /* Using exponentially backed off polling to obtain and report output. */
+    /* We support *both* polling and socket.io push, with strong preference for the latter. */
+    report_output_using_polling = function(client, session_id, cell_id, number, interval, callback) {
+	client.output_messages(session_id, cell_id, number, function(m) {
+	    var i, o, n=m.data.length, done=false;
+	    if (n === 0) { 		/* received no new messages */
+		/* exponentially reduce polling frequency */
+		interval *= client.polling.backoff;  
+		if (interval > client.polling.max_interval) {
+		    interval = client.polling.max_interval;
+		}
+	    } else { /* received new messages */
+		interval = client.min_polling_interval;
+		for (i = 0; i < n; i++) {
+		    o = m.data[i].output;
+		    number = m.data[i].number;
+		    callback(m.data[i]);
+		    if (m.data[i].done) { done = true; }
+		}
+		/* increment message number so next time we ask for next message */
+		number += 1;
+	    }
+	    if (!done) {
+		setTimeout(function() { report_output_using_polling(
+                      client, session_id, cell_id, number, interval, callback); }, interval);
+	    }
+	});
+    }
 
-    /* send interrupt signal to session with given id */
-    sigint: function(session_id, callback) { get('sigint/' + session_id, callback); },
 
-    /* kill session with given id */
-    sigkill: function(session_id, callback) { get('sigkill/' + session_id, callback); },
-
-    /* kill process and remove all files for session with given id */
-    close_session: function(session_id, callback) { get('close_session/' + session_id, callback); },
-
-  /* Session Information: */
-
-    /* extensive information about a given session */
-    session: function(session_id, callback) { get('session/' + session_id, callback); },
     
-    /* get status of session with given id: 'ready', 'running', 'dead' */
-    status: function(session_id, callback) { get('status/' + session_id, callback); },
+    return {
 
-    /* list of cells in a given session */
-    cells: function(session_id, callback) { get('cells/' + session_id, callback); },
+	polling:{'min_interval':50,  /* when using polling to report output, this is the minimal interval */
+		 'backoff':1.1, /* factor to backoff when polling and get no results */
+		 'max_interval':1000 /* until we get to this many milliseconds */
+		},
 
-  /* Code execution */
+	use_sockets:false,   /* not implemented yet */
 
-    /* execute block of code */    
-    execute: function(session_id, code, callback) { post('execute/' + session_id, {'code':code}, callback); },
+	/****************/
+	/* All Sessions */
+	/****************/
+	/* kill processes and delete everything about all sessions */
+	close_all_sessions: function(callback) { get('close_all_sessions', callback); }, 
+	
+	/* much information about all sessions */
+	sessions: function(callback) { get('sessions', callback); }, 
+	
+	/********************************************************************/
+	/* Session Management: creating, interrupting, killing and deleting */
+	/********************************************************************/
+	
+	/* create a new session */
+	new_session: function(callback) { get('new_session', callback); },
+	
+	/* send interrupt signal to session with given id */
+	sigint: function(session_id, callback) { get('sigint/' + session_id, callback); },
+	
+	/* kill session with given id */
+	sigkill: function(session_id, callback) { get('sigkill/' + session_id, callback); },
+	
+	/* kill process and remove all files for session with given id */
+	close_session: function(session_id, callback) { get('close_session/' + session_id, callback); },
+	
+	/************************/
+	/* Session Information: */
+	/************************/
+	
+	/* extensive information about a given session */
+	session: function(session_id, callback) { get('session/' + session_id, callback); },
+	
+	/* get status of session with given id: 'ready', 'running', 'dead' */
+	status: function(session_id, callback) { get('status/' + session_id, callback); },
+	
+	/* list of cells in a given session */
+	cells: function(session_id, callback) { get('cells/' + session_id, callback); },
+	
+	/******************/
+	/* Code execution */
+	/******************/
+	
+	/* execute block of code
+	   
+	   The callback function gets called with a message object m
+           as input with a message for each generated output message,
+           until a final message m with m.done=true. */    
+	execute: function(session_id, code, callback) { 
+	    var client = this;  /* since this changes inside post callback below */
+	    post('execute/' + session_id, {'code':code}, function (m) {
+		if (m.status == 'error') {
+		    callback({'status':'error', 'data':m.data, 'done':true});
+		} else if (client.use_sockets) { 
+		    /* best option -- register callback, etc. */
+		    alert('sockets not implemented!');
+		} else {
+		    /* fallback to horrible polling option */
+		    report_output_using_polling(client, session_id,
+                              m.cell_id, 0, client.polling.min_interval, callback);
+		}
+	    }); 
+	},
+	
+	/* incremental output produced when executing code -- clients
+	 *should* receipt socket.io messages instead of using this.
+	 This is used mainly for testing and development purposes.  */
+	output_messages: function(session_id, cell_id, number, callback) { 
+            get('output_messages/' + session_id + '/' + cell_id + '/' + number, callback);
+	}, 
+	
+	/*******************************************************/
+	/* Files: uploading, downloading, deleting and listing */
+	/*******************************************************/
+	
+	/* return list of all files in the given session */
+	files: function(session_id, callback) { get('files/' + session_id, callback); }, 
+	
+	/* put a file or files into the given session */
+	put_file: function(session_id, files, callback) { put('put_file/' + session_id, files, callback); },
+	
+	/* get a file from the given session */
+	get_file: function(session_id, path, callback) { get('get_file/' + session_id + '/' + path, callback); },
+	
+	/* delete a file from the session */
+	delete_file: function(session_id, path, callback) { get('delete_file/' + session_id + '/' + path, callback); },
+	
+	/* wait -- use this only for testing purposes. */
+	wait: function(session_id, callback) { 
+            /* TODO: do a proper implementation!!!! */
+            setTimeout(2000, callback); 
+	},
+	
+	/*********************************************************/
+	/* More complicated functionality specific to Javascript */
+	/*********************************************************/
 
-    /* incremental output produced when executing code -- clients
-       *should* receipt socket.io messages instead of using this.
-       This is used mainly for testing and development purposes.  */
-    output_messages: function(session_id, cell_id, number, callback) { 
-        get('output_messages/' + session_id + '/' + cell_id + '/' + number, callback);
-    }, 
-
-  /* Files: uploading, downloading, deleting and listing */
-
-    /* return list of all files in the given session */
-    files: function(session_id, callback) { get('files/' + session_id, callback); }, 
-
-    /* put a file or files into the given session */
-    put_file: function(session_id, files, callback) { put('put_file/' + session_id, files, callback); },
-
-    /* get a file from the given session */
-    get_file: function(session_id, path, callback) { get('get_file/' + session_id + '/' + path, callback); },
-
-    /* delete a file from the session */
-    delete_file: function(session_id, path, callback) { get('delete_file/' + session_id + '/' + path, callback); },
-
-  /* wait -- use this only for testing purposes. */
-    wait: function(session_id, callback) { 
-        /* TODO: do a proper implementation!!!! */
-        setTimeout(2000, callback); 
-    },
-
-  };
+    /* end object creation */
+    };
 }
 
 /* Run tests on the Client object pointed at the given server. 
