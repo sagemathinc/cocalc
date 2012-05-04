34cc559f26eb9efc074b93b7bf10ba928536e163
William Stein <wstein@gmail.com>
2012-05-03 18:35:53 -0700

    update README.

--- demos/gevent_websocket/gevent_websocket.py
+++ demos/gevent_websocket/gevent_websocket.py
@@ -18,5 +18,5 @@ def api():
     return
 
 if __name__ == '__main__':
-    http_server = WSGIServer(('',5000), app, handler_class=WebSocketHandler)
+    http_server = WSGIServer(('0.0.0.0',5000), app, handler_class=WebSocketHandler)
     http_server.serve_forever()
