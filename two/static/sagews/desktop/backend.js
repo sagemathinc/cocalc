aea46260bb5a6951bfb9931ea423f0108a420f3b
William Stein <wstein@gmail.com>
2012-06-01 07:53:13 -0700

    removed benchmark code (to deleted)

--- two/static/sagews/desktop/backend.js
+++ two/static/sagews/desktop/backend.js
@@ -4,41 +4,9 @@ desktop/backend -- javascript client for desktop backend client
 
 */
 
-var callback = 0;
-var t = 0;
-var last_id = -1;
-
 var socket = sagews.socket({
-    'recv':function(mesg) { test_callback(mesg); },
-    'connect':function() { /* console.log("connected.");*/ },
-    'disconnect':function() { /*console.log("disconnected.");*/ },
+    'recv':function(mesg) { console.log(mesg); },
+    'connect':function() {  console.log("connected."); },
+    'disconnect':function() { console.log("disconnected."); },
 });
-
-var n = 1000;
-var msg_count;
-function test_callback(mesg) {
-    msg_count += 1;
-    if (typeof mesg.stdout == 'undefined') {
-	return;
-    }
-    if(mesg.stdout[0] == 'a') {
-	callback(n + ' tests; took ' + (sagews.walltime()-t)/n + ' ms each on average, with ' + msg_count + ' messages received.');
-    } else {
-	callback(mesg.stdout);
-    }
-}
-
-socket.new_session( function(id) { last_id = id; /*console.log("new session: "+ id);*/ } );
-
-function test(num, f) {
-    n = parseInt(num); 
-    msg_count = 0;
-    callback = f;
-    t = sagews.walltime();
-    callback('start...');
-    for(i=0;i<n-1;i++) {
-	socket.session_send(last_id, {'cmd':'execute', 'code':'2*'+i});
-    }
-    socket.session_send(last_id, {'cmd':'execute', 'code':'print "a"'});
-}
 
