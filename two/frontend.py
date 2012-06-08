3d94524688fb0b9123fe509f29fce03b04e96079
William Stein <wstein@gmail.com>
2012-06-08 12:32:51 -0700

    working on implementing the frontend.

--- two/frontend.py
+++ two/frontend.py
@@ -3,8 +3,48 @@ Frontend
 
 """
 
+import logging
+from tornado import web, ioloop
 
-####################################################
-# WEB SERVER
-# Implementation: Tornado
-####################################################
+class ManageHandler(web.RequestHandler):
+    def get(self):
+        self.render('static/sagews/desktop/manage.html')
+
+routes = [
+    (r"/manage", ManageHandler),
+    (r"/static/(.*)", web.StaticFileHandler, {'path':'static'})
+]
+
+def run(port, address, debug, secure):
+    print "Launching frontend%s: http%s://%s:%s"%(
+        ' in debug mode' if debug else ' in production mode',
+        's' if secure else '',
+        address if address else '*', port)
+
+    if debug:
+        logging.getLogger().setLevel(logging.DEBUG)
+
+    if secure:  # todo
+        raise NotImplementedError
+    
+    app = web.Application(routes)
+    app.listen(port=port, address=address)
+    ioloop.IOLoop.instance().start()
+
+if __name__ == '__main__':
+    import argparse
+    
+    parser = argparse.ArgumentParser(description="Run a frontend instance")
+    parser.add_argument("-p", dest='port', type=int, default=8080,
+                        help="port the frontend listens on (default: 8080)")
+    parser.add_argument("-a", dest="address", type=str, default="",
+                        help="address the frontend listens on (default: '')")
+    parser.add_argument("-d", dest="debug", action='store_const', const=True,
+                        help="debug mode (default: False)", default=False)
+    parser.add_argument("-s", dest="secure", action='store_const', const=True,
+                        help="SSL secure mode (default: False)", default=False)
+    
+    args = parser.parse_args()
+    run(args.port, args.address, args.debug, args.secure)
+
+
