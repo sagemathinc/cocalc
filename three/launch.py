0ef34898c26712eb3e8138ff36064f0d7588334f
William Stein <wstein@gmail.com>
2012-07-09 23:09:16 -0700

    Add launching of postgres.

--- three/launch.py
+++ three/launch.py
@@ -4,7 +4,13 @@
 Launch control
 """
 
-import logging, os, stat
+import logging, os, stat, time
+
+log_files = {'postgresql':'postgres.log',
+             }
+
+ports = {'postgresql':5432,  # also coded into postgresql.conf
+         }  
 
 # Enable logging
 logging.basicConfig()
@@ -13,14 +19,22 @@ log.setLevel(logging.DEBUG)   # WARNING, INFO
 
 
 DATA = os.path.abspath('data')
+LOGS = os.path.join(DATA, 'logs')
 def init_data_directory():
     log.info("ensuring that the data directory exist")
     if not os.path.exists(DATA):
         os.makedirs(DATA)
 
+    if not os.path.exists(LOGS):
+        os.makedirs(LOGS)
+
     log.info("ensuring that the data directory has restrictive permissions")
     if os.stat(DATA)[stat.ST_MODE] != 0o40700:
         os.chmod(DATA, 0o40700)
+
+    os.environ['PATH'] = os.path.join(DATA, 'local/bin/') + ':' + os.environ['PATH']
+
+DATABASE = os.path.join(DATA, 'db')
 
 def read_configuration_file():
     log.info('reading configuration file')
@@ -41,6 +55,30 @@ def launch_nginx_servers():
 def launch_haproxy_servers():
     log.info('launching haproxy servers')
     cmd('sudo haproxy -f haproxy.conf', '')
+
+def start_postgresql():
+    cmd('pg_ctl start -D "%s" -l data/logs/%s'%(DATABASE, log_files['postgresql']), '')
+    
+def initialize_postgresql_database():    
+    # on OS X this initdb can fail.  The fix (see http://willbryant.net/software/mac_os_x/postgres_initdb_fatal_shared_memory_error_on_leopard) is to type "sudo sysctl -w kern.sysv.shmall=65536" and also create /etc/sysctl.conf with content "kern.sysv.shmall=65536".
+    cmd('initdb -D "%s"'%DATABASE, '')
+    cmd('rm postgresql.conf', 'data/db')
+    cmd('ln -s ../../postgresql.conf .', 'data/db')
+    start_postgresql()
+    for i in range(5):
+        time.sleep(0.5)
+        try:
+            cmd('createdb -p %s sagews'%ports['postgresql'])
+            break
+        except:
+            pass
+
+def launch_postgresql_servers():
+    log.info('launching postgresql servers')
+    if not os.path.exists(DATABASE):
+        initialize_postgresql_database()
+    else:
+        start_postgresql()
 
 def launch_database_servers():
     log.info('launching database servers')        
@@ -81,6 +119,9 @@ if __name__ == "__main__":
     parser.add_argument('--launch_haproxy', dest='launch_haproxy', action='store_const', const=True, default=False,
                         help="launch the haproxy server")
 
+    parser.add_argument('--launch_postgresql', dest='launch_postgresql', action='store_const', const=True, default=False,
+                        help="launch the postgresql database server")
+
     args = parser.parse_args()
     
     init_data_directory()
@@ -92,3 +133,5 @@ if __name__ == "__main__":
     if args.launch_haproxy:
         launch_haproxy_servers()
 
+    if args.launch_postgresql:
+        launch_postgresql_servers()
