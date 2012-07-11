ab1e18f0e4e106cd2cc5317eb20a124133d68584
William Stein <wstein@gmail.com>
2012-07-11 15:59:38 -0700

    simple tornado+sockjs+haproxy demo; reorganize todo list.

--- three/backend.py
+++ three/backend.py
@@ -1,13 +1,56 @@
-from tornado import web, ioloop
-from sockjs.tornado import SockJSRouter, SockJSConnection
+# -*- coding: utf-8 -*-
+"""
+    Simple sockjs-tornado chat application. By default will listen on port 8080.
+"""
+import tornado.ioloop
+import tornado.web
 
-class EchoConnection(SockJSConnection):
-    def on_message(self, msg):
-        self.send(msg)
+import sockjs.tornado
 
-if __name__ == '__main__':
-    EchoRouter = SockJSRouter(EchoConnection, '/echo')
 
-    app = web.Application(EchoRouter.urls)
-    app.listen(9999)
-    ioloop.IOLoop.instance().start()
+class IndexHandler(tornado.web.RequestHandler):
+    """Regular HTTP handler to serve the chatroom page"""
+    def get(self):
+        self.render('static/chat.html')
+
+
+class ChatConnection(sockjs.tornado.SockJSConnection):
+    """Chat connection implementation"""
+    # Class level variable
+    participants = set()
+
+    def on_open(self, info):
+        # Send that someone joined
+        self.broadcast(self.participants, "Someone joined.")
+
+        # Add client to the clients list
+        self.participants.add(self)
+
+    def on_message(self, message):
+        # Broadcast message
+        self.broadcast(self.participants, message)
+
+    def on_close(self):
+        # Remove client from the clients list and broadcast leave message
+        self.participants.remove(self)
+
+        self.broadcast(self.participants, "Someone left.")
+
+if __name__ == "__main__":
+    import logging
+    logging.getLogger().setLevel(logging.DEBUG)
+
+    # 1. Create chat router
+    ChatRouter = sockjs.tornado.SockJSRouter(ChatConnection, '/backend/chat')
+
+    # 2. Create Tornado application
+    app = tornado.web.Application(
+            [(r"/backend", IndexHandler)] + ChatRouter.urls,
+            debug=True
+    )
+
+    # 3. Make Tornado app listen on port 8080
+    app.listen(9000)
+
+    # 4. Start IOLoop
+    tornado.ioloop.IOLoop.instance().start()
