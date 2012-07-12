4f8e749e76761aa98eb428cf91f77e10592a4818
William Stein <wstein@gmail.com>
2012-07-11 23:28:59 -0700

    move the one static file from backend to nginx instead of tornado

--- three/backend.py
+++ three/backend.py
@@ -10,54 +10,26 @@ import tornado.web
 import sockjs.tornado
 
 
-class IndexHandler(tornado.web.RequestHandler):
-    """Regular HTTP handler to serve the chatroom page"""
-    def get(self):
-        self.render('static/chat.html')
-
-
-class ChatConnection(sockjs.tornado.SockJSConnection):
-    """Chat connection implementation"""
-    # Class level variable
+class Connection(sockjs.tornado.SockJSConnection):
     participants = set()
 
     def on_open(self, info):
-        # Send that someone joined
-        self.broadcast(self.participants, "Someone joined.")
-
-        # Add client to the clients list
+        self.broadcast(self.participants, "User connected.")
         self.participants.add(self)
         self.broadcast(self.participants, sys.argv[1] if len(sys.argv)>=2 else '')
 
     def on_message(self, message):
-        # Broadcast message
-        print message
         self.broadcast(self.participants, message)
 
     def on_close(self):
-        # Remove client from the clients list and broadcast leave message
         self.participants.remove(self)
-
-        self.broadcast(self.participants, "Someone left.")
+        self.broadcast(self.participants, "User disconnected.")
 
 if __name__ == "__main__":
     import logging
     logging.getLogger().setLevel(logging.DEBUG)
-
-    # 1. Create chat router
-    ChatRouter = sockjs.tornado.SockJSRouter(ChatConnection, '/backend/chat')
-
-    # 2. Create Tornado application
-    app = tornado.web.Application(
-            [(r"/backend", IndexHandler)] + ChatRouter.urls,
-            debug=True
-    )
-
-    # 3. Make Tornado app listen 
-    if len(sys.argv) == 2:
-        app.listen(int(sys.argv[1]))
-    else:
-        app.listen(9000)
-
-    # 4. Start IOLoop
+    
+    Router = sockjs.tornado.SockJSRouter(Connection, '/backend')
+    app = tornado.web.Application(Router.urls, debug=True)
+    app.listen(int(sys.argv[1]))
     tornado.ioloop.IOLoop.instance().start()
