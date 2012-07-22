68f5f0dd1b84ca56067bece5482a8238b0e8c838
William Stein <wstein@gmail.com>
2012-07-21 18:51:21 -0700

    Started work on some restyling and authentication.  I know what I want now.

--- three/authdemo.py
+++ three/authdemo.py
@@ -36,24 +36,6 @@ class LogoutHandler(BaseHandler):
         self.clear_cookie('user')
         self.redirect('/')
 
-## class FacebookLoginHandler(BaseHandler, tornado.auth.FacebookGraphMixin):
-##     @tornado.web.asynchronous
-##     def get(self):
-##         if self.get_argument("code", False):
-##             self.get_authenticated_user(
-##                 redirect_uri  = '/auth/facebookgraph/',
-##                 client_id     = self.settings["facebook_api_key"],
-##                 client_secret = self.settings["facebook_secret"],
-##                 code          = self.get_argument("code"),
-##                 callback      = self.async_callback(self._on_login))
-##             return
-##         self.authorize_redirect(redirect_uri = '/auth/facebookgraph/',
-##                                 client_id    = self.settings["facebook_api_key"],
-##                                 extra_params = {"scope": "read_stream,offline_access"})
-##     def _on_login(self, user):
-##         print "user = ", user
-##         self.finish()
-
 class FacebookLoginHandler(BaseHandler, tornado.auth.FacebookGraphMixin):
     @tornado.web.asynchronous
     def get(self):
