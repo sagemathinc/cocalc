be8b6a78077769b1bdc7ff0d25a3fb4b7b74bf3e
William Stein <wstein@gmail.com>
2012-07-25 18:36:46 -0700

    delete a bunch of old static/ html/js -- need to start cleaner

--- three/static/mobile/index.js
+++ three/static/mobile/index.js
@@ -1,12 +1,12 @@
 $(function(){
 
-    $("#connection_status").html("connecting..."); 
+   $("#connection_status").html("connecting..."); 
 
     var backend = sagews.Backend(
 	{'onopen':function(protocol) { $("#connection_status").html("connected ("+protocol+")"); },
 	 'onclose':function() { $("#connection_status").html("reconnecting..."); }});
 
-    $("#execute").click(function(e) { 
+    function execute_code() {
 	$("#output").val("");
 	$("#run_status").html("running");
 	backend.execute($("#input").val(), 
@@ -16,9 +16,11 @@ $(function(){
 		    if (mesg.output.stderr) {
 			o.val(o.val() + "\n!!!!!!!!!!!!!!\n" + mesg.output.stderr + "\n!!!!!!!!!!!!!\n");
 		    }
-		    $("#run_status").html(mesg.output.done?"done":"running...");
+		    $("#run_status").html(mesg.done?"done":"running...");
 		});
-    });
+    }
+
+    $("#execute").click(function(e) { execute_code(); });
 
 })
 
