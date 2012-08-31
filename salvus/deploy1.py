e64b05a9d50623d3b05a92841bcf312c72e033a6
William Stein <wstein@gmail.com>
2012-08-31 12:11:55 -0700

    fix all the bugs in admin.py as it is now so that it fully works for deploy/start/stop/restart each component locally.

---
+++ salvus/deploy1.py
@@ -0,0 +1,14 @@
+
+# import admin, config2; reload(admin); reload(config2); h = config2.hosts
+
+import admin
+
+hosts = admin.Hosts('conf/deploy1/hosts')
+
+services = admin.Services('conf/deploy1/')
+
+
+
+        
+    
+    
