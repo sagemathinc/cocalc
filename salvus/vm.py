1bb1a18b3f848f7c64662536d0644da40359ab62
William Stein <wstein@gmail.com>
2012-09-28 21:42:36 -0700

    finished first usable version of kvm virtual machine launch daemon

--- salvus/vm.py
+++ salvus/vm.py
@@ -5,11 +5,6 @@ vm.py -- create and run a virtual machine based on the standard
          the vm to our tinc VPN.  When this script terminates, the vm
          is destroyed, undefined, and the image file associated with
          it is deleted.
-
-TODO:
-  - implement log file in daemon mode!
-  - daemon mode fails to work for unknown reason
-  - killing pid kills it too quickly, so cleanup doesn't happen -- need better except...
 """
 
 #######################################################################
@@ -17,14 +12,20 @@ TODO:
 # assigned to University of Washington.
 #######################################################################
 
-import os, shutil, socket, tempfile, time
+import logging, os, shutil, socket, tempfile, time
 
 import daemon
 
-from admin import run, sh, log
+from admin import run, sh
+
+conf_path = os.path.join(os.path.split(os.path.realpath(__file__))[0], 'conf')
+
+###########################################
+# kvm -- via libvirt
+###########################################
 
 def virsh(command, name):
-    return sh['virsh', '--connect', 'qemu:///session', command, name].strip()
+    return run(['virsh', '--connect', 'qemu:///session', command, name], verbose=False).strip()
 
 def run_kvm(ip_address, hostname, vcpus, ram):
     #################################
@@ -34,68 +35,76 @@ def run_kvm(ip_address, hostname, vcpus, ram):
     vm_path = os.path.join(os.environ['HOME'], 'vm', 'images')
     new_img = os.path.join(vm_path, ip_address + '.img')
     base_img = os.path.join(vm_path, 'salvus_base.img')
-    sh['qemu-img', 'create', '-b', base_img, '-f', 'qcow2', new_img]
-    log.info("created %s in %s seconds", new_img, time.time()-t); t = time.time()
-
-    #################################
-    # configure the vm's image
-    #################################
-    # - mount the image in a temp directory
-    tmp_path = tempfile.mkdtemp()
-    run(['guestmount', '-i', '-a', new_img, '--rw', tmp_path], maxtime=60)
-
-    #### hostname ####
-    hostname_file = os.path.join(tmp_path,'etc/hostname')
-    os.unlink(hostname_file)
-    open(hostname_file,'w').write(hostname)
-    hosts_file = os.path.join(tmp_path, 'etc/hosts')
-    hosts = open(hosts_file).read()
-    os.unlink(hosts_file)
-    open(hosts_file,'w').write("%s\n127.0.1.1  %s\n"%(hosts, hostname))
-
-    #### tinc vpn ####
-
-    tinc_path = os.path.join(tmp_path, 'home/salvus/salvus/salvus/data/local/etc/tinc/')
-    open(os.path.join(tinc_path, 'tinc-up'),'w').write(
-        "#!/bin/sh\nifconfig $INTERFACE %s netmask 255.255.0.0"%ip_address)
-    open(os.path.join(tinc_path, 'tinc.conf'),'w').write(
-        "Name = %s\nConnectTo = %s"%(hostname, socket.gethostname()))
-    rsa_key_priv = os.path.join(tinc_path, 'rsa_key.priv')
-    rsa_key_pub = os.path.join(tinc_path, 'hosts', hostname)
-    if os.path.exists(rsa_key_priv): os.unlink(rsa_key_priv)
-    if os.path.exists(rsa_key_pub): os.unlink(rsa_key_pub)
-    sh['tincd', '--config', tinc_path, '-K']
-    host_file = os.path.join(tinc_path, 'hosts', hostname)
-    public_key = open(rsa_key_pub).read().strip()
-    open(host_file,'w').write("Subnet = %s/32\n%s"%(ip_address, public_key))
-    # put the tinc public key in our local db, so that the vm can connect to host.
-    shutil.copyfile(host_file, os.path.join(os.path.split(os.path.realpath(__file__))[0],
-                                            'conf', 'tinc_hosts', hostname))
-    # - unmount image and remove tmp_path
-    sh['fusermount', '-u', tmp_path]
-    shutil.rmtree(tmp_path)
-
-    log.info("configured image in %s seconds", time.time()-t); t = time.time()
-
-    #################################
-    # create and start the vm itself
-    #################################
-    run(['virt-install', '--cpu', 'host', '--network', 'user,model=virtio', '--name',
-       ip_address, '--vcpus', vcpus, '--ram', 1024*ram, '--import', '--disk',
-       new_img + ',device=disk,bus=virtio,format=qcow2', '--noautoconsole'], maxtime=60)
-
-    log.info("created new virtual machine in %s seconds", time.time()-t); t = time.time()
-
-    ##########################################################################
-    # - run until vm terminates or we receive term signal, undefined, destroy
-    ##########################################################################
     try:
-        while virsh('domstate', ip_address) == 'running':
-            time.sleep(1)
-    except:
-        # clean up
-        virsh('destroy', ip_address)
-        virsh('undefine', ip_address)
+        sh['qemu-img', 'create', '-b', base_img, '-f', 'qcow2', new_img]
+        log.info("created %s in %s seconds", new_img, time.time()-t); t = time.time()
+
+        #################################
+        # configure the vm's image
+        #################################
+        # - mount the image in a temp directory
+        try:
+            tmp_path = tempfile.mkdtemp()
+            try:
+                run(['guestmount', '-i', '-a', new_img, '--rw', tmp_path], maxtime=60)
+
+                #### hostname ####
+                hostname_file = os.path.join(tmp_path,'etc/hostname')
+                os.unlink(hostname_file)
+                open(hostname_file,'w').write(hostname)
+                hosts_file = os.path.join(tmp_path, 'etc/hosts')
+                hosts = open(hosts_file).read()
+                os.unlink(hosts_file)
+                open(hosts_file,'w').write("%s\n127.0.1.1  %s\n"%(hosts, hostname))
+
+                #### tinc vpn ####
+
+                tinc_path = os.path.join(tmp_path, 'home/salvus/salvus/salvus/data/local/etc/tinc/')
+                open(os.path.join(tinc_path, 'tinc-up'),'w').write(
+                    "#!/bin/sh\nifconfig $INTERFACE %s netmask 255.255.0.0"%ip_address)
+                open(os.path.join(tinc_path, 'tinc.conf'),'w').write(
+                    "Name = %s\nConnectTo = %s"%(hostname, socket.gethostname()))
+                rsa_key_priv = os.path.join(tinc_path, 'rsa_key.priv')
+                rsa_key_pub = os.path.join(tinc_path, 'hosts', hostname)
+                if os.path.exists(rsa_key_priv): os.unlink(rsa_key_priv)
+                if os.path.exists(rsa_key_pub): os.unlink(rsa_key_pub)
+                sh['tincd', '--config', tinc_path, '-K']
+                host_file = os.path.join(tinc_path, 'hosts', hostname)
+                public_key = open(rsa_key_pub).read().strip()
+                open(host_file,'w').write("Subnet = %s/32\n%s"%(ip_address, public_key))
+                # put the tinc public key in our local db, so that the vm can connect to host.
+                shutil.copyfile(host_file, os.path.join(conf_path, 'tinc_hosts', hostname))
+            finally:
+                # - unmount image and remove tmp_path
+                sh['fusermount', '-u', tmp_path]
+        finally:
+            shutil.rmtree(tmp_path)
+
+        log.info("configured image in %s seconds", time.time()-t); t = time.time()
+
+        #################################
+        # create and start the vm itself
+        #################################
+        try:
+            run(['virt-install', '--cpu', 'host', '--network', 'user,model=virtio', '--name',
+               ip_address, '--vcpus', vcpus, '--ram', 1024*ram, '--import', '--disk',
+               new_img + ',device=disk,bus=virtio,format=qcow2', '--noautoconsole'], maxtime=60)
+
+            log.info("created new virtual machine in %s seconds -- now running", time.time()-t); t = time.time()
+
+            ##########################################################################
+            # - run until vm terminates or we receive term signal, undefined, destroy
+            ##########################################################################
+            while virsh('domstate', ip_address) == 'running':
+                time.sleep(1)
+        finally:
+            # clean up
+            virsh('undefine', ip_address)
+            virsh('destroy', ip_address)
+            
+    finally:
+        try: os.unlink(os.path.join(conf_path, 'tinc_hosts', hostname))
+        except: pass
         os.unlink(new_img)
 
 def run_virtualbox(ip_address, hostname, vcpus, ram):
@@ -119,12 +128,48 @@ if __name__ == "__main__":
                         help="Gigabytes of ram")
     parser.add_argument("--pidfile", dest="pidfile", type=str, default='',
                         help="store pid in this file")
+    parser.add_argument("-l", dest='log_level', type=str, default='INFO',
+                        help="log level (default: INFO) useful options include WARNING and DEBUG")
+    parser.add_argument("--logfile", dest="logfile", type=str, default='',
+                        help="store log in this file (default: '' = don't log to a file)")
 
     args = parser.parse_args()
     
-    args.hostname = args.hostname if args.hostname else args.ip_address.replace('.','x')
+    if args.logfile:
+        args.logfile = os.path.abspath(args.logfile)
+    if args.pidfile:
+        args.pidfile = os.path.abspath(args.pidfile)
+    if args.ip_address.count('.') == 0:
+        args.ip_address = '10.38.1.' + args.ip_address
+    elif args.ip_address.count('.') == 1:
+        args.ip_address = '10.38.' + args.ip_address
+
+    assert args.ip_address.startswith('10.38.'), "ip address must belong to the class B network 10.38."
+
+    args.hostname = args.hostname if args.hostname else args.ip_address.replace('.','dot')
 
     def main():
+        global log
+
+        logging.basicConfig()
+        log = logging.getLogger('vm')
+        log.setLevel(logging.INFO)
+
+        if args.log_level:
+            level = getattr(logging, args.log_level.upper())
+            log.setLevel(level)
+
+        if args.logfile:
+            log.addHandler(logging.FileHandler(args.logfile))
+
+        import admin   # take over the admin logger
+        admin.log = log
+
+        log.info("logger started")
+
+        if args.pidfile:
+            open(args.pidfile,'w').write(str(os.getpid()))
+
         if args.vm_type == 'kvm':
             run_kvm(args.ip_address, args.hostname, args.vcpus, args.ram)
         elif args.vm_type == 'virtualbox':
@@ -132,11 +177,9 @@ if __name__ == "__main__":
         else:
             print "Unknown vm_type '%s'"%args.vm_type
             sys.exit(1)
+
     try:
-        if args.pidfile:
-            open(args.pidfile,'w').write(str(os.getpid()))
         if args.daemon:
-            import daemon
             with daemon.DaemonContext():
                 main()
         else:
