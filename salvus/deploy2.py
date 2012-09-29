5cd27d133efa0d974b42646a86344bdb8206694b
William Stein <wstein@gmail.com>
2012-09-28 22:26:24 -0700

    working on remote vm management

---
+++ salvus/deploy2.py
@@ -0,0 +1,14 @@
+
+# import admin, deploy2; reload(admin); reload(deploy2); h = deploy2.hosts; s=deploy2.services
+
+import admin
+
+hosts = admin.Hosts('conf/deploy2/hosts')
+
+services = admin.Services('conf/deploy2/')
+
+
+
+        
+    
+    
