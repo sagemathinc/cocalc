46b8afb07e30e376d190079950aa1eb1f19b7b1c
William Stein <wstein@gmail.com>
2012-10-03 20:47:40 -0700

    script to restart tincd

--- salvus/scripts/update_git.py
+++ salvus/scripts/update_git.py
@@ -9,7 +9,6 @@ user = os.environ['USER']
 import misc
 
 for hostname in hosts.persistent_hosts:
-    # the u below means that this won't overwrite newer files on destination, which could happen by accident if we were careless.
     cmd = 'ssh salvus@%s "cd salvus; git pull %s@%s:salvus/"'%(hostname, user, misc.local_ip_address(hostname))
     print cmd
     os.system(cmd)
