70ce27bde91bb966f773d076bc248bf988933d45
William Stein <wstein@gmail.com>
2012-10-03 20:45:13 -0700

    working on update git script and pushing vm images

--- salvus/scripts/push_vm_images_base.py
+++ salvus/scripts/push_vm_images_base.py
@@ -2,7 +2,7 @@
 
 import hosts, os
 
-BASE = 'vm/images/base/'
+BASE = '~/vm/images/base/'
 
 for hostname in hosts.vm_hosts:
     # the u below means that this won't overwrite newer files on destination, which could happen by accident if we were careless.
--- salvus/scripts/update_git.py
+++ salvus/scripts/update_git.py
@@ -10,6 +10,10 @@ import misc
 
 for hostname in hosts.persistent_hosts:
     # the u below means that this won't overwrite newer files on destination, which could happen by accident if we were careless.
-    cmd = 'ssh salvus@%s "cd salvus; git pull %s@%s:salvus/"'%(hostname, user, misc.local_ip_address(hostname))
+    ip = misc.local_ip_address(hostname)
+    if ip.startswith('127'): continue
+    cmd = 'ssh salvus@%s "cd salvus; git pull %s@%s:salvus/"'%(hostname, user, ip)
     print cmd
     os.system(cmd)
+
+print "Deal with these manually: ", hosts.unsafe_hosts
