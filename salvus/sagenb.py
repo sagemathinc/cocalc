985852c07cf743902023c0d0b3ff67f09f37ebac
William Stein <wstein@gmail.com>
2012-10-09 22:43:06 -0700

    working on sagenb.py

--- salvus/sagenb.py
+++ salvus/sagenb.py
@@ -11,16 +11,69 @@ sagenb.py -- start a sage notebook server
 #                  http://www.gnu.org/licenses/
 #########################################################################################
 
-import logging, sys
+import logging, os, shutil, subprocess, sys
 
 # configure logging
 logging.basicConfig()
 log = logging.getLogger('sage_server')
 log.setLevel(logging.INFO)
 
-def serve(path, port, address):
+def drop_privileges(id, home):        
+    gid = id
+    uid = id
+    os.chown(home, uid, gid)
+    os.setgid(gid)
+    os.setuid(uid)
+    os.environ['DOT_SAGE'] = home
+    os.environ['IPYTHON_DIR'] = home
+    os.chdir(home)
+
+def run(args):
+    log.info(' '.join(args))
+    p = subprocess.Popen(args, stdin=subprocess.PIPE, stdout = subprocess.PIPE,
+                                stderr=subprocess.PIPE)
+    p.wait()
+    log.info('output = ', p.stdout.read())
+    log.info('err = ', p.stderr.read())
+    return p.returncode
+
+def create_user(basename):
+    i = 0
+    name = basename + str(i)
+    while run(['useradd', '-m', name]):
+        i += 1
+        name = basename + str(i)
+    return name
+
+def serve(path, port, address, timeout):
     log.info("served")
 
+    # create two new unix users: 'server' and 'user'
+    server = create_user('server')
+    try:
+        user = create_user('user')
+        try:
+            # make 'server' user own path
+            run(['chown', '-R', server + '.', path])
+            
+            # make it so server can ssh without a password to be user
+            run(['su', server, '-c', '"ssh-keygen -b 2048 -N \'\' -f ~/.ssh/id_rsa"'])  # generate ssh key for server
+            run(['su', user, '-c', '"ssh-keygen -b 2048 -N \'\' -f ~/.ssh/id_rsa"'])  # generate ssh key for user
+            shutil.copyfile(os.path.join('/home', server, '.ssh/id_rsa.pub'),
+                            os.path.join('/home', user, '.ssh/authorized_keys'))
+            run(['chown', user+'.', os.path.join('/home', user, '.ssh/authorized_keys')])
+            run(['su', server, '-c', '"ssh -o \'StrictHostKeyChecking no\' %s@localhost ls"'%user]) # host key verification...
+            
+            # launch sage notebook server
+            # TODO -- pass options!!!!
+            run(['su', server, '-c', '"sage --notebook directory=%s port=%s interface=%s accounts=True open_viewer=False --timeout=%s --server_pool=[\"%s@localhost\"]"'%(path, port, address, timeout, user)])
+            
+        finally:
+            run(['deluser', '--remove-all-files', 'user'])
+
+    finally:
+        # delete users
+        run(['deluser', '--remove-home',  server])
             
 def run_sagenb(path, port, address, pidfile, logfile):
     if pidfile:
@@ -29,7 +82,7 @@ def run_sagenb(path, port, address, pidfile, logfile):
         log.addHandler(logging.FileHandler(logfile))
     log.info("port=%s, address=%s, pidfile='%s', logfile='%s'", port, address, pidfile, logfile)
     try:
-        serve(path, port, address)
+        serve(path, port, address, timeout=3600)
     finally:
         if pidfile:
             os.unlink(pidfile)
@@ -53,6 +106,11 @@ if __name__ == "__main__":
                         help="log level (default: INFO) useful options include WARNING and DEBUG")
 
     args = parser.parse_args()
+
+    if os.environ['USER'] != 'root':
+        print "%s: this script must be run as root"%sys.argv[0]
+        sys.exit(1)
+        
 
     if not args.address:
         print "%s: must specify address to bind to"%sys.argv[0]
