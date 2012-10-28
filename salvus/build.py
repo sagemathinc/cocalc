8e8edf26dfd7cc323082bee8fc66bed2b90ff944
William Stein <wstein@gmail.com>
2012-10-28 12:26:42 -0700

    add more node build package dependencies

--- salvus/build.py
+++ salvus/build.py
@@ -35,7 +35,8 @@ TARGET = os.path.abspath(os.path.join(DATA, 'local'))
 
 NODEJS_PACKAGES = [
     'commander', 'start-stop-daemon', 'winston', 'sockjs', 'helenus',
-    'sockjs-client-ws', 'coffee-script', 'node-uuid', 'browserify', 'uglify-js2'
+    'sockjs-client-ws', 'coffee-script', 'node-uuid', 'browserify', 'uglify-js2',
+    'passport', 'passport-github', 'express'
     ]
 
 PYTHON_PACKAGES = [
