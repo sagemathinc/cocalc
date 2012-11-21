70fbac6b449b90762f98bc07f46a494fc175ce74
William Stein <wstein@gmail.com>
2012-11-20 23:43:38 -0800

    get rid of "assets" subdirectory.

--- salvus/static/mercury/js/mercury_loader.js
+++ salvus/static/mercury/js/mercury_loader.js
@@ -25,7 +25,7 @@
 // script.  These, or your own packages can be specified to the loader in query params (read below for details).
 if (!window.mercuryPackages) window.mercuryPackages = {
   development: {javascripts: 'mercury.js', stylesheets: 'mercury.css'},
-  bundled: {javascripts: 'javascripts/mercury.min.js,javascripts/mercury_dialogs.js', stylesheets: 'stylesheets/mercury.bundle.css'}
+  bundled: {javascripts: 'js/mercury.min.js,js/mercury_dialogs.js', stylesheets: 'css/mercury.bundle.css'}
 };
 
 
@@ -58,7 +58,7 @@ if (!window.mercuryPackages) window.mercuryPackages = {
   // created, so for instance you could put `visible=false`, and the editor be hidden after it's created.
   var options = {
     // A path or url from which the javascripts and css should be loaded.
-    src: '/assets',
+    src: '/',
     // A value defined in the packages above.  Development is used by default.  If you want to provide your own package
     // you can just define one before including this script.
     pack: 'development'
