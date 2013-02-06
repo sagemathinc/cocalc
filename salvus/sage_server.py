094b8b42a0d5c3dcbad4b0f9598aee0d27c24b09
William Stein <wstein@gmail.com>
2013-02-06 06:03:51 -0800

    (0:15) sage_server: killing hub makes python process go 100%!

--- salvus/sage_server.py
+++ salvus/sage_server.py
@@ -604,20 +604,17 @@ def session(conn, home, username, cputime, numfiles, vmem, uid, transient):
     import time; import random; random.seed(time.time())
 
     while True:
-        try:
-            typ, mesg = conn.recv()
-            #print 'INFO:child%s: received message "%s"'%(pid, mesg)
-            event = mesg['event']
-            if event == 'terminate_session':
-                return
-            elif event == 'execute_code':
-                execute(conn=conn, id=mesg['id'], code=mesg['code'], data=mesg.get('data',None), preparse=mesg['preparse'])
-            elif event == 'introspect':
-                introspect(conn=conn, id=mesg['id'], line=mesg['line'], preparse=mesg['preparse'])
-            else:
-                raise RuntimeError("invalid message '%s'"%mesg)
-        except: # KeyboardInterrupt:
-            pass
+        typ, mesg = conn.recv()
+        #print 'INFO:child%s: received message "%s"'%(pid, mesg)
+        event = mesg['event']
+        if event == 'terminate_session':
+            return
+        elif event == 'execute_code':
+            execute(conn=conn, id=mesg['id'], code=mesg['code'], data=mesg.get('data',None), preparse=mesg['preparse'])
+        elif event == 'introspect':
+            introspect(conn=conn, id=mesg['id'], line=mesg['line'], preparse=mesg['preparse'])
+        else:
+            raise RuntimeError("invalid message '%s'"%mesg)
 
 def introspect(conn, id, line, preparse):
     salvus = Salvus(conn=conn, id=id) # so salvus.[tab] works -- note that Salvus(...) modifies namespace.
