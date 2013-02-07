c390bda1583e916103e4f6dad2efe0c7414ac392
William Stein <wstein@gmail.com>
2013-02-06 19:43:01 -0800

    (0:20) salvus.[tab] bonus create a salvus.notify feature that pops up a pnotify with same options

--- salvus/sage_server.py
+++ salvus/sage_server.py
@@ -403,7 +403,7 @@ class Salvus(object):
         - salvus.stdout, salvus.stderr, salvus.html, salvus.tex -- all
           allow you to write additional output to the cell
         - worksheet - jQuery wrapper around the current worksheet DOM object
-
+        - obj -- the optional obj argument, which is passed via JSON serialization
         """
         if obj is None:
             obj = {}
@@ -416,6 +416,46 @@ class Salvus(object):
         """
         kwds['coffeescript'] = True
         return self.javascript(*args, **kwds)
+
+    def notify(self, **kwds):
+        """
+        Display a graphical notification using the pnotify Javascript library.
+
+        INPUTS:
+
+        - `title: false` - The notice's title.
+        - `title_escape: false` - Whether to escape the content of the title. (Not allow HTML.)
+        - `text: false` - The notice's text.
+        - `text_escape: false` - Whether to escape the content of the text. (Not allow HTML.)
+        - `styling: "bootstrap"` - What styling classes to use. (Can be either jqueryui or bootstrap.)
+        - `addclass: ""` - Additional classes to be added to the notice. (For custom styling.)
+        - `cornerclass: ""` - Class to be added to the notice for corner styling.
+        - `nonblock: false` - Create a non-blocking notice. It lets the user click elements underneath it.
+        - `nonblock_opacity: .2` - The opacity of the notice (if it's non-blocking) when the mouse is over it.
+        - `history: true` - Display a pull down menu to redisplay previous notices, and place the notice in the history.
+        - `auto_display: true` - Display the notice when it is created. Turn this off to add notifications to the history without displaying them.
+        - `width: "300px"` - Width of the notice.
+        - `min_height: "16px"` - Minimum height of the notice. It will expand to fit content.
+        - `type: "notice"` - Type of the notice. "notice", "info", "success", or "error".
+        - `icon: true` - Set icon to true to use the default icon for the selected style/type, false for no icon, or a string for your own icon class.
+        - `animation: "fade"` - The animation to use when displaying and hiding the notice. "none", "show", "fade", and "slide" are built in to jQuery. Others require jQuery UI. Use an object with effect_in and effect_out to use different effects.
+        - `animate_speed: "slow"` - Speed at which the notice animates in and out. "slow", "def" or "normal", "fast" or number of milliseconds.
+        - `opacity: 1` - Opacity of the notice.
+        - `shadow: true` - Display a drop shadow.
+        - `closer: true` - Provide a button for the user to manually close the notice.
+        - `closer_hover: true` - Only show the closer button on hover.
+        - `sticker: true` - Provide a button for the user to manually stick the notice.
+        - `sticker_hover: true` - Only show the sticker button on hover.
+        - `hide: true` - After a delay, remove the notice.
+        - `delay: 8000` - Delay in milliseconds before the notice is removed.
+        - `mouse_reset: true` - Reset the hide timer if the mouse moves over the notice.
+        - `remove: true` - Remove the notice's elements from the DOM after it is removed.
+        - `insert_brs: true` - Change new lines to br tags.
+        """
+        obj = {}
+        for k, v in kwds.iteritems():
+            obj[k] = sage_salvus.jsonable(v)
+        self.javascript("$.pnotify(obj)", obj=obj)
 
     def execute_javascript(self, code, coffeescript=False, data=None):
         """
@@ -612,7 +652,10 @@ def session(conn, home, username, cputime, numfiles, vmem, uid, transient):
         elif event == 'execute_code':
             execute(conn=conn, id=mesg['id'], code=mesg['code'], data=mesg.get('data',None), preparse=mesg['preparse'])
         elif event == 'introspect':
-            introspect(conn=conn, id=mesg['id'], line=mesg['line'], preparse=mesg['preparse'])
+            try:
+                introspect(conn=conn, id=mesg['id'], line=mesg['line'], preparse=mesg['preparse'])
+            except KeyboardInterrupt:
+                pass
         else:
             raise RuntimeError("invalid message '%s'"%mesg)
 
