e98fe20560110bc8962e46bd50e8b0ab0c030133
William Stein <wstein@gmail.com>
2013-05-02 08:56:48 -0700

    Script to create a new VM image more easily (instead of pasting lines into the console).

---
+++ salvus/scripts/new_vm_image.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+import argparse, os, sys, time 
+VM_PATH = os.path.join(os.environ['HOME'], 'vm/images/base/')
+
+def cmd(s):
+    print s
+    if os.system(s):
+        raise RuntimeError("error executing '%s'"%s)
+
+cmd("ls -lth %s|head -5"%VM_PATH)
+                                                                                                                                        
+parser = argparse.ArgumentParser(description="Create a new VM image.")                                                         
+parser.add_argument("--prev", dest="prev", type=str, help="previous vm image name", default="")
+parser.add_argument("--next", dest="next",  type=str, help="new vm image name", default="")                                     
+args = parser.parse_args()                                                                                                              
+
+prev = args.prev
+next = args.next
+
+if prev == "":
+   # make it the most recent image
+   prev = os.popen("ls -1t ~/vm/images/base/|head -1").read().strip().rstrip('.img')
+
+if next == "":
+   # make image name salvus-date   
+   next = time.strftime("salvus-%Y-%m-%d")
+
+prev_path = os.path.join(VM_PATH, prev + '.img')
+next_path = os.path.join(VM_PATH, next + '.img')
+
+if not os.path.exists(prev_path):
+    raise ValueError("previous vm image doesn't exist -- " + prev_path)
+
+if os.path.exists(next_path):
+    raise ValueError("next vm image already exists -- " + next_path)
+
+cmd("qemu-img create -b %s -f qcow2 %s"%(prev_path, next_path))
+
+cmd("virt-install --cpu host --network user,model=virtio --name %s --vcpus=16 --ram 32768 --import --disk %s,device=disk,bus=virtio,format=qcow2,cache=writeback --noautoconsole"%(next,next_path))
+
+cmd("virsh -c qemu:///session qemu-monitor-command --hmp %s 'hostfwd_add ::2222-:22'"%next)
+
+cmd("ssh localhost -p 2222")
+
+print """
+You probably want to do something like this:
+
+    cd salvus/salvus
+    . salvus-env
+    git pull
+    ./make_coffee
+    # fix /etc/ssh/ssh_config
+    sudo su
+    apt-get update; apt-get upgrade
+    reboot -h now
+    sudo shutdown -h now
+
+ Then
+
+    virsh_undefine %s 
+    cd vm/images/base/
+    ./push
+
+"""%next
