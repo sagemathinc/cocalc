33b833fb1d0e681997db6dedac46aa9db4392ca4
William Stein <wstein@gmail.com>
2013-12-18 18:48:22 +0000

    storage: adding various functionality for dealing with snapshots, etc.

--- salvus/scripts/storage.py
+++ salvus/scripts/storage.py
@@ -4,6 +4,9 @@ import argparse, cPickle, hashlib, json, logging, os, sys, time, random
 from uuid import UUID
 from subprocess import Popen, PIPE
 
+def print_json(s):
+    print json.dumps(s, separators=(',',':'))
+
 
 log = None
 
@@ -73,7 +76,7 @@ def migrate_project_to_storage(src, new_only):
     cmd("zfs set snapdir=hidden %s"%dataset)
     cmd("chown %s. -R /%s"%(projectid, home))   # chown with snapdir visible doesn't work; can cause other problems too.
 
-    cmd("zfs snapshot %s@%s"%(dataset, time.strftime('%Y-%m-%dT%H:%M:%S')))
+    snapshot(project_id)
     cmd("zfs list %s"%dataset)
 
 def create_dataset(project_id):
@@ -104,10 +107,60 @@ def create_user(project_id): # safe to call repeatedly even if user exists
     cmd("groupadd -g %s -o %s"%(id, name))
     cmd("useradd -u %s -g %s -o -d %s %s"%(id, id, home, name))
 
-def newest_snapshot(project_id, host=None):
+def list_snapshots(project_id, host=''):
+    """
+    Return sorted list of snapshots of the given project available on the given host.
+
+    This is very fast if host=None, and the cost of ssh if host isn't.
+    """
+    # see discussion about zfs list being really slow.
+    c = "sudo /bin/ls -1 '%s'"%os.path.join(path_to_project(project_id), '.zfs/snapshot')
+    try:
+        if not host:
+            v = cmd(c)
+        else:
+            v = cmd('ssh %s %s'%(host, c))
+    except RuntimeError, msg:
+        if 'No such file or directory' in str(msg):
+            # project isn't available here
+            return []
+        raise # something else went wrong
+    v = v.strip().split()
+    v.sort()
+    return v
+
+def snapshot(project_id, name=''):
+    """
+    Create a new snapshot right now with current ISO timestamp at start of name.
+    """
+    cmd("sudo zfs snapshot %s@%s%s"%(dataset_name(project_id), time.strftime('%Y-%m-%dT%H:%M:%S'), name))
+
+def newest_snapshot(project_id, host=''):
     """
     Return most recent snapshot or empty string if none.
     If host is given, does this on a remote host.
+
+    We *assume* snapshots start with an ISO-formatted name!!!
+    You can put anything after the name.
+    """
+    v = list_snapshots(project_id=project_id, host=host)
+    if len(v) == 0:
+        return ''
+    else:
+        return v[-1]
+
+def newest_snapshot_zfslist(project_id, host=None):
+    """
+    Return most recent snapshot or empty string if none.
+    If host is given, does this on a remote host.
+
+    This uses the zfs list command, which is REALLY, REALLY slow as
+    soon as there are more than a few thousand datasets.  There
+    are several bug reports about zfs list being slow online,
+    so this may get resolved at some point.  However, just using
+    ls on the .zfs/snapshot directory is super fast, and works
+    for our purposes (see above).  The timestamps of the snapshot
+    directories don't seem useful, but the names are enough.
     """
     c = "sudo zfs list -r -t snapshot -o name -s creation %s|tail -1"%dataset_name(project_id)
     if host is None:
@@ -118,19 +171,19 @@ def newest_snapshot(project_id, host=None):
         return ''
     return v.strip()
 
-def send(project_id, dest):
+def send(project_id, dest, snap_src):
     log.info("sending %s to %s", project_id, dest)
 
-    snap_src  = newest_snapshot(project_id)
+    snap_src  = newest_snapshot(args.project_id)
     if not snap_src:
-        raise RuntimeError("no local dataset '%s'"%project_id)
+        log.warning("send: no local snapshot for '%s'"%project_id)
+        return
 
     snap_dest = newest_snapshot(project_id, dest)
-
     log.debug("src: %s, dest: %s", snap_src, snap_dest)
 
     if snap_src == snap_dest:
-        log.info("already up to date")
+        log.info("send %s -- already up to date", project_id)
         return
 
     dataset = dataset_name(project_id)
@@ -176,14 +229,32 @@ if __name__ == "__main__":
     parser_migrate.add_argument("src", help="the current project home directory", type=str, nargs="+")
     parser_migrate.set_defaults(func=migrate)
 
-    def send_projects(args):
-        for project_id in args.project_id:
-            send(project_id=project_id, dest=args.dest)
+    def _send(args):
+        for dest in args.dest:
+            send(project_id=args.project_id, dest=dest)
 
-    parser_send = subparsers.add_parser('send', help='send project(s) to remote compute vm using ZFS replication; this overwrites any data in the targets, so be careful!')
-    parser_send.add_argument("dest", help="ip address of destination", type=str)
-    parser_send.add_argument("project_id", help="project id", type=str, nargs="+")
-    parser_send.set_defaults(func=send_projects)
+    parser_send = subparsers.add_parser('send', help='send latest UTC iso date formated snapshot of project to remote storage servers (this overwrites any changes to the targets)')
+    parser_send.add_argument("project_id", help="project id", type=str)
+    parser_send.add_argument("dest", help="ip address of destination", type=str, nargs="+")
+    parser_send.set_defaults(func=_send)
+
+    def _snapshot(args):
+        snapshot(project_id=args.project_id, name=args.name)
+
+    parser_snapshot = subparsers.add_parser('snapshot', help='make a new snapshot of the given project')
+    parser_snapshot.add_argument("project_id", help="project id", type=str)
+    parser_snapshot.add_argument("name", help="appended to end of snapshot timestamp", type=str, default='', nargs='?')
+    parser_snapshot.set_defaults(func=_snapshot)
+
+    def _snapshots(args):
+        print_json({'project_id':args.project_id, 'snapshots':list_snapshots(project_id=args.project_id, host=args.host)})
+
+    parser_snapshots = subparsers.add_parser('snapshots', help='list in order the snapshots of the given project')
+    parser_snapshots.add_argument("project_id", help="project id", type=str)
+    parser_snapshots.add_argument("host", help="ip address of host (default: ''=localhost)", type=str, default='', nargs='?')
+    parser_snapshots.set_defaults(func=_snapshots)
+
+
 
     args = parser.parse_args()
 
