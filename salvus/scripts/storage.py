ca15dc0540b001fe812bcd4b98f1065c245e59d9
William Stein <wstein@gmail.com>
2013-12-18 00:59:25 +0000

    storage: starting to work on zfs send

--- salvus/scripts/storage.py
+++ salvus/scripts/storage.py
@@ -37,11 +37,17 @@ def users():
     global _users
     if _users is not None:
         return _users
-    _users = set([x.split(":")[0].strip() for x in open("/etc/passwd").readlines()])
+    _users = {}
+    for x in open("/etc/passwd").readlines():
+        v = x.split(':')
+        _users[v[0].strip()] = int(v[2].strip())  # user to uid
     return _users
 
 def path_to_project(project_id):
     return os.path.join('/projects', project_id)
+
+def dataset_name(project_id):
+    return 'projects/%s'%project_id
 
 def migrate_project_to_storage(src, new_only):
     info_json = os.path.join(src, '.sagemathcloud', 'info.json')
@@ -52,33 +58,68 @@ def migrate_project_to_storage(src, new_only):
     project_id = json.loads(open(info_json).read())['project_id']
     projectid = project_id.replace('-','')
     home = path_to_project(project_id)
-    dataset = home.lstrip('/')
+    dataset = dataset_name(project_id)
     if new_only and os.path.exists(home):
         log.info("skipping %s (%s) since it already exists (and new_only=True)"%(src, project_id))
         return
     if not os.path.exists(home):
         create_dataset(project_id)
+    create_user(project_id)
 
     # rsync data over
     cmd("rsync -Hax --delete --exclude .forever --exclude .bup --exclude .zfs %s/ %s/"%(src, home))
     id = uid(project_id)
-    cmd("chown %s:%s -R /%s"%(id, id, home))
+
+    cmd("zfs set snapdir=hidden %s"%dataset)
+    cmd("chown %s. -R /%s"%(projectid, home))   # chown with snapdir visible doesn't work; can cause other problems too.
+
     cmd("zfs snapshot %s@%s"%(dataset, time.strftime('%Y-%m-%dT%H:%M:%S')))
     cmd("zfs list %s"%dataset)
 
 def create_dataset(project_id):
     check_uuid(project_id)
-    id = uid(project_id)
     home = path_to_project(project_id)
     dataset = home.lstrip('/')
     cmd('zfs create %s'%dataset)
     cmd('zfs set snapdir=visible %s'%dataset)
     cmd('zfs set quota=10G %s'%dataset)
-    projectid = project_id.replace('-','')
+
+def create_user(project_id): # safe to call repeatedly even if user exists
+    name = project_id.replace('-','')
+    id = uid(project_id)
+    u = users().get(name, 0)
+    if u == id:
+        # user already exists and has correct id
+        return
+    if u != 0:
+        return # for now -- otherwise we would mess up the user
+               # after the transition is complete, we'll go through all compute
+               # vm's and run this again, which will fix the uid'.s
+        # there's the username but with wrong id
+        cmd("groupdel %s"%name)
+        cmd("userdel %s"%name)
+
+    # Now make correct user.
     # the -o makes it so in the incredibly unlikely event of a collision, no big deal.
-    if projectid not in users():
-        cmd("groupadd -g %s -o %s"%(id, projectid))
-        cmd("useradd -u %s -g %s -o -d %s  %s"%(id, id, home, projectid))
+    cmd("groupadd -g %s -o %s"%(id, name))
+    cmd("useradd -u %s -g %s -o -d %s %s"%(id, id, home, name))
+
+
+def send(project_id, dest):
+    log.info("sending %s to %s", project_id, dest)
+    dataset = dataset_name(project_id)
+
+    v = cmd("/bin/ls -t " + os.path.join(path_to_project(project_id), '.zfs', 'snapshot')).split()
+    if len(v) == 0:
+        log.warning("no snapshots of %s", project_id)
+        return
+    s = v[-1]  # most recent snapshot
+    t = time.time()
+    c = "zfs send -RD %s@%s | ssh %s zfs recv -F %s"%(dataset, s, dest, dataset)
+    print c
+    os.system(c)
+    log.info("sent -- time=%s seconds", time.time()-t)
+
 
 def setup_log(loglevel='DEBUG', logfile=''):
     logging.basicConfig()
@@ -97,7 +138,7 @@ def setup_log(loglevel='DEBUG', logfile=''):
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="ZFS based project storage")
-    parser.add_argument("--loglevel", dest='loglevel', type=str, default='INFO',
+    parser.add_argument("--loglevel", dest='loglevel', type=str, default='DEBUG',
                            help="log level: useful options include INFO, WARNING and DEBUG")
     parser.add_argument("--logfile", dest="logfile", type=str, default='',
                         help="store log in this file (default: '' = don't log to a file)")
@@ -115,7 +156,14 @@ if __name__ == "__main__":
     parser_migrate.add_argument("src", help="the current project home directory", type=str, nargs="+")
     parser_migrate.set_defaults(func=migrate)
 
+    def send_projects(args):
+        for project_id in args.project_id:
+            send(project_id=project_id, dest=args.dest)
 
+    parser_send = subparsers.add_parser('send', help='send project(s) to remote compute vm using ZFS replication; this overwrites any data in the targets, so be careful!')
+    parser_send.add_argument("dest", help="ip address of destination", type=str)
+    parser_send.add_argument("project_id", help="project id", type=str, nargs="+")
+    parser_send.set_defaults(func=send_projects)
 
     args = parser.parse_args()
 
