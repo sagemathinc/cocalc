a9e2510bad28c4e7e2bfbdae5339c84f1218afb8
William Stein <wstein@gmail.com>
2013-12-25 16:58:44 +0000

    storage2: improved the create project user script further to automatically repair ~/.ssh

--- salvus/scripts/create_project_user.py
+++ salvus/scripts/create_project_user.py
@@ -30,12 +30,18 @@ def cmd(s):
         raise RuntimeError(x)
     return x
 
+def home(project_id):
+    return os.path.join('/projects', project_id)
+
+def username(project_id):
+    return project_id.replace('-','')
+
 def create_user(project_id):
     """
     Create the user the contains the given project data.   It is safe to
     call this function even if the user already exists.
     """
-    name = project_id.replace('-','')
+    name = username(project_id)
     id = uid(project_id)
     r = open('/etc/passwd').read()
     i = r.find(name)
@@ -55,7 +61,7 @@ def create_user(project_id):
     # Now make the correct user.  The -o makes it so in the incredibly unlikely
     # event of a collision, no big deal.
     cmd("groupadd -g %s -o %s"%(id, name))
-    cmd("useradd -u %s -g %s -o -d %s %s"%(id, id, os.path.join('/projects', project_id), name))
+    cmd("useradd -u %s -g %s -o -d %s %s"%(id, id, home(project_id), name))
 
     # Save account info so it persists through reboots/upgrades/etc. that replaces the ephemeral root fs.
     if os.path.exists("/mnt/home/etc/"): # UW nodes
@@ -63,10 +69,30 @@ def create_user(project_id):
     if os.path.exists("/mnt/conf/etc/"): # GCE nodes
         cmd("cp /etc/passwd /etc/shadow /etc/group /mnt/conf/etc/")
 
+def ensure_ssh_access(project_id):
+    # If possible, make some attempts to ensure ssh access to this account.
+    h = home(project_id)
+    if not os.path.exists(h):
+        # there is nothing we can possibly do yet -- filesystem not mounted
+        return
+    ssh_path = os.path.join(h, '.ssh')
+    authorized_keys2 = os.path.join(ssh_path, 'authorized_keys2')
+    public_key = open('/home/salvus/.ssh/id_rsa.pub').read().strip()
+    add_public_key = '\n#Added by SageMath Cloud\n' + public_key + '\n'
+    if not os.path.exists(ssh_path):
+        os.makedirs(ssh_path)
+    if not os.path.exists(authorized_keys2):
+        open(authorized_keys2,'w').write(add_public_key)
+    elif public_key not in open(authorized_keys2).read():
+        open(authorized_keys2,'a').write(add_public_key)
+    os.system('chown -R %s. %s'%(username(project_id), ssh_path))
+    os.system('chmod og-rwx -R %s'%ssh_path)
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="create project user")
     parser.add_argument("project_id", help="the uuid of the project", type=str)
     args = parser.parse_args()
     create_user(args.project_id)
+    ensure_ssh_access(args.project_id)
 
 
