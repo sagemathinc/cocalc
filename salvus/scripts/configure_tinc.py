db8ede2628969ae4632c5a1b2795516a5d6b54bc
William Stein <wstein@gmail.com>
2014-05-11 04:24:55 +0000

    working on better tinc configuration.

--- salvus/scripts/configure_tinc.py
+++ salvus/scripts/configure_tinc.py
@@ -4,40 +4,55 @@
 
 import json, os, socket, sys
 
-external_address = sys.argv[1]
-tinc_address     = sys.argv[2]
-tincname         = sys.argv[3]
-connect_to       = "\n".join(["ConnectTo = cloud%s"%i for i in range(1,8) + range(10, 22)])
-tinc_path        = '/home/salvus/salvus/salvus/data/local/etc/tinc/'
+tinc_path = '/home/salvus/salvus/salvus/data/local/etc/tinc/'
+tinc_conf = os.path.join(tinc_path, 'tinc.conf')
 
-if not os.path.exists(tinc_path):
-    os.makedirs(tinc_path)
+def init():
+    external_address = sys.argv[2]
+    tinc_address     = sys.argv[3]
+    tincname         = sys.argv[4]
 
-run_path = '/home/salvus/salvus/salvus/data/local/var/run/'
-if not os.path.exists(run_path):
-    os.makedirs(run_path)
+    if not os.path.exists(tinc_path):
+        os.makedirs(tinc_path)
 
-tinc_up = os.path.join(tinc_path, 'tinc-up')
+    run_path = '/home/salvus/salvus/salvus/data/local/var/run/'
+    if not os.path.exists(run_path):
+        os.makedirs(run_path)
 
-open(tinc_up,'w').write(
-      "#!/bin/sh\nifconfig $INTERFACE %s netmask 255.192.0.0 txqueuelen 10000"%tinc_address)
+    tinc_up = os.path.join(tinc_path, 'tinc-up')
 
-os.popen("chmod a+rx %s"%tinc_up)
+    open(tinc_up,'w').write(
+          "#!/bin/sh\nifconfig $INTERFACE %s netmask 255.192.0.0 txqueuelen 10000"%tinc_address)
 
-open(os.path.join(tinc_path, 'tinc.conf'),'w').write(
-     "Name = %s\nKeyExpire = 2592000\n%s"%(tincname, connect_to))
+    os.popen("chmod a+rx %s"%tinc_up)
 
-rsa_key_priv = os.path.join(tinc_path, 'rsa_key.priv')
-rsa_key_pub  = os.path.join(tinc_path, 'hosts', tincname)
+    open(tinc_conf,'w').write("Name = %s\nKeyExpire = 2592000\n"%tincname)
 
-if os.path.exists(rsa_key_priv): os.unlink(rsa_key_priv)
-if os.path.exists(rsa_key_pub): os.unlink(rsa_key_pub)
+    rsa_key_priv = os.path.join(tinc_path, 'rsa_key.priv')
+    rsa_key_pub  = os.path.join(tinc_path, 'hosts', tincname)
 
-os.popen("tincd --config %s -K"%tinc_path).read()
+    if os.path.exists(rsa_key_priv): os.unlink(rsa_key_priv)
+    if os.path.exists(rsa_key_pub): os.unlink(rsa_key_pub)
 
-host_file  = os.path.join(tinc_path, 'hosts', tincname)
-public_key = open(rsa_key_pub).read().strip()
+    os.popen("tincd --config %s -K"%tinc_path).read()
 
-open(host_file,'w').write("Address = %s\nTCPonly=yes\nCompression=10\nCipher = aes-128-cbc\nSubnet = %s/32\n%s"%(external_address, tinc_address, public_key))
+    host_file  = os.path.join(tinc_path, 'hosts', tincname)
+    public_key = open(rsa_key_pub).read().strip()
 
-print json.dumps({"tincname":tincname, "host_file":open(host_file).read()}, separators=(',',':'))
+    open(host_file,'w').write("Address = %s\nTCPonly=yes\nCompression=10\nCipher = aes-128-cbc\nSubnet = %s\n%s"%(external_address, tinc_address, public_key))
+
+    print json.dumps({"tincname":tincname, "host_file":open(host_file).read()}, separators=(',',':'))
+
+def connect_to():
+    s = '\n' + '\n'.join(["ConnectTo = %s"%host for host in sys.argv[2:]]) + '\n'
+    open(tinc_conf,'a').write(s)
+
+
+command = sys.argv[1]
+if command == "init":
+    init()
+elif command == "connect_to":
+    connect_to()
+else:
+    raise RuntimeError("unknown command '%s'"%command)
+
