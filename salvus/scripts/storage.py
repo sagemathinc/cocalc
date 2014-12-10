1e75312c82bc84e46cbd21ebb283a8bc5073abf5
William Stein <wstein@uw.edu>
2014-12-10 00:51:23 +0000

    add GPL headers to most source files and the GPL license file, as required by NSF grants 1147802 and 1161226

--- salvus/scripts/storage.py
+++ salvus/scripts/storage.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python
+###############################################################################
+#
+# SageMathCloud: A collaborative web-based interface to Sage, IPython, LaTeX and the Terminal.
+#
+#    Copyright (C) 2014, William Stein
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+
+
 
 # NOTE: I'm very unhappy with the multiprocessing issues "messing up" this code.
 # Hence I've basically abandoned it for storage.coffee
