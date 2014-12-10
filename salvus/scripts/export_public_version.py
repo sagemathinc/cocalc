0ff1f8d99324f406d07ccd72eae1dcc998d2b7d7
William Stein <wstein@uw.edu>
2014-12-10 06:02:29 +0000

    support for open sourcing

---
+++ salvus/scripts/export_public_version.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import os
+
+def cmd(s):
+    print s
+    if os.system(s):
+        raise RuntimeError("error running '%s'"%s)
+
+if __name__ == '__main__':
+    os.chdir("%s/tmp"%os.environ['HOME'])
+    cmd("rm -rf cloud-public")
+    cmd("git clone git@github.com:sagemath/cloud.git cloud-public && rm -rf cloud-public/*")
+    cmd("git clone ~/devel/william cloud-private")
+    cmd("rsync -axvH cloud-private/salvus/ cloud-public/")
+    cmd("cd cloud-public && rm -r conf/deploy*  conf/tinc_* && git add --all . && git commit -a -m 'Public release of SageMathCloud'")
+    #cmd("rm -rf cloud-private")
