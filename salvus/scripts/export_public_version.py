f421fdcf0c8f3e510f2023d354305efdbb68bb9a
William Stein <wstein@uw.edu>
2014-12-10 13:48:40 +0000

    fix issues with public export script

--- salvus/scripts/export_public_version.py
+++ salvus/scripts/export_public_version.py
@@ -9,9 +9,10 @@ def cmd(s):
 
 if __name__ == '__main__':
     os.chdir("%s/tmp"%os.environ['HOME'])
-    cmd("rm -rf cloud-public")
+    cmd("rm -rf cloud-public cloud-private")
     cmd("git clone git@github.com:sagemath/cloud.git cloud-public && rm -rf cloud-public/*")
     cmd("git clone ~/devel/william cloud-private")
     cmd("rsync -axvH cloud-private/salvus/ cloud-public/")
-    cmd("cd cloud-public && rm -r conf/deploy*  conf/tinc_* && git add --all . && git commit -a -m 'Public release of SageMathCloud'")
+    cmd("cd cloud-public && rm -r conf/deploy*  conf/tinc_* && git add --all .")
+    print "See %s/tmp/cloud-public"%os.environ['HOME']
     #cmd("rm -rf cloud-private")
