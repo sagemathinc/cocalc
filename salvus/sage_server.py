c404714215acd0175aedcd12b91d1715769ab96f
William Stein <wstein@uw.edu>
2015-01-05 18:43:07 +0000

    add %mediawiki magic.

--- salvus/sage_server.py
+++ salvus/sage_server.py
@@ -1557,7 +1557,7 @@ def serve(port, host):
                      'script', 'python', 'python3', 'perl', 'ruby', 'sh', 'prun', 'show', 'auto',
                      'hide', 'hideall', 'cell', 'fork', 'exercise', 'dynamic', 'var',
                      'reset', 'restore', 'md', 'load', 'runfile', 'typeset_mode', 'default_mode',
-                     'sage_chat', 'fortran', 'magics', 'go', 'julia', 'pandoc', 'wiki']:
+                     'sage_chat', 'fortran', 'magics', 'go', 'julia', 'pandoc', 'wiki', 'mediawiki']:
             namespace[name] = getattr(sage_salvus, name)
 
         # alias pretty_print_default to typeset_mode, since sagenb has/uses that.
