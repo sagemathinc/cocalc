1421030e45365a15db9a7a2f4fe9b49df7c2a3b8
William Stein <wstein@sagemath.com>
2015-02-20 00:26:06 +0000

    write new cassandra backup system using rsync

--- salvus/scripts/cassandra/backup_cassandra.py
+++ salvus/scripts/cassandra/backup_cassandra.py
@@ -39,4 +39,8 @@ def backup_cassandra(dc, hosts):
         paths = ' '.join(b)
 
         cmd2("time bup on %s index %s"%(host, paths))
-        cmd2("time bup on %s save --bwlimit=2000k -n %s %s"%(host, host, paths))
+        try:
+            cmd2("time bup on %s save -n %s %s"%(host, host, paths))
+        except Exception, msg:
+            # This is expected to happen sometimes due to  files vanishing, changing, etc.
+            print msg
