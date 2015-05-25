f0b216461e2af31cd4c35e3c76bb576f17c1821b
William Stein <wstein@gmail.com>
2015-05-25 17:13:09 +0000

    oops -- need to make this setup more automatic

--- salvus/scripts/smc_compute.py
+++ salvus/scripts/smc_compute.py
@@ -1668,8 +1668,8 @@ if __name__ == "__main__":
                         dest='bucket', default=os.environ.get("SMC_BUCKET",""), type=str)
 
     parser.add_argument("--storage",
-                        help="", dest='storage', default='storage0-devel-us', type=str)
-                        #help="", dest='storage', default='storage0-us', type=str)
+                        #help="", dest='storage', default='storage0-devel-us', type=str)
+                        help="", dest='storage', default='storage0-us', type=str)
 
     # if enabled, we make incremental tar archives on every save operation and
     # upload them to this bucket.  These are made directly using tar on the filesystem,
