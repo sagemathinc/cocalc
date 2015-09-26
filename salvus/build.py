db3599dd53e84bae14890b27bb83e71f7ac57702
William Stein <wstein@gmail.com>
2015-09-26 23:17:08 +0000

    add ipywidgets package, as requested by Ivica Nakić

--- salvus/build.py
+++ salvus/build.py
@@ -201,7 +201,7 @@ Then
 
 # IPYTHON3 in Python3 systemwide
 
-    sudo pip3 install --upgrade ipython
+    sudo pip3 install --upgrade ipython  ipywidgets
     sudo ipython3 kernelspec install-self
 
 Then edit /usr/local/share/jupyter/kernels/python3 and add a "-E" option before "-m" so that python3 can start with the sage -sh environment set.
