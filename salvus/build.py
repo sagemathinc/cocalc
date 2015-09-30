b7b50c486fc0c96f208e55349eb3acc8310440a5
William Stein <wstein@gmail.com>
2015-09-30 22:11:52 +0000

    add beautifulsoup4 package

--- salvus/build.py
+++ salvus/build.py
@@ -614,6 +614,7 @@ SAGE_PIP_PACKAGES_ENV = {'clawpack':{'LDFLAGS':'-shared'}}
 SAGE_PIP_PACKAGES_DEPS = [
     'Nikola[extras]',
     'enum34', 'singledispatch', 'funcsigs', 'llvmlite', # used for numba
+    'beautifulsoup4'
 ]
 
 
