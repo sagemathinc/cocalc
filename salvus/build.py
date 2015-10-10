551bd8ad6df5e6db8f03f3b6dd0039ac68a54c87
William Stein <wstein@gmail.com>
2015-10-10 07:19:12 +0000

    also add filterpy to python3

--- salvus/build.py
+++ salvus/build.py
@@ -208,7 +208,7 @@ Then
 # IPYTHON3 in Python3 systemwide
 
     sudo pip3 install --upgrade ipython  ipywidgets
-    sudo ipython3 kernelspec install-self rethinkdb
+    sudo ipython3 kernelspec install-self rethinkdb filterpy
 
 Then edit /usr/local/share/jupyter/kernels/python3 and add a "-E" option before "-m" so that python3 can start with the sage -sh environment set.
 
