3b3b76df85d3fb65ad551b423be5a27063e28744
William Stein <wstein@sagemath.com>
2015-10-25 00:05:26 +0000

    update_primus client side code generation script

--- salvus/static/primus/update_primus.js
+++ salvus/static/primus/update_primus.js
@@ -1,2 +1,3 @@
-fs = require('fs')
-primus = new require('primus')(require('http').createServer(), {transformer: 'engine.io',  pathname : '/hub'}); fs.writeFileSync('primus-engine.js',primus.library());
+fs = require('fs');
+primus = new require('primus')(require('http').createServer(), {transformer: 'engine.io',  pathname : '/hub'});
+fs.writeFileSync('primus-engine.js', primus.library());
