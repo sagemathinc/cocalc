ad759fb70f718d200952b70d40f897a132572e95
Harald Schilly <hsy@sagemath.com>
2015-11-24 21:42:57 +0000

    sagews2pdf: <p> should cause newlines, fixes #292

--- src/smc_pyutil/smc_pyutil/sagews2pdf.py
+++ src/smc_pyutil/smc_pyutil/sagews2pdf.py
@@ -24,7 +24,8 @@
 
 
 """
-Copyright (c) 2014, William Stein
+Copyright (c) 2014 -- 2015   The SageMathCloud Authors.
+
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
@@ -256,7 +257,7 @@ class Parser(HTMLParser.HTMLParser):
             self.result += '\\subsubsection{'
         elif tag == 'i':
             self.result += '\\textemph{'
-        elif tag == 'div':
+        elif tag == 'div' or tag == 'p':
             self.result += '\n\n{'
         elif tag == 'ul':
             self.result += '\\begin{itemize}'
