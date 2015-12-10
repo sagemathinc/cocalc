aa7d7f41dc84f0b91eee11e02207cd5b82352d42
William Stein <wstein@gmail.com>
2015-12-10 21:03:00 +0000

    updating links to snapshots: first step

---
+++ src/smc_pyutil/smc_pyutil/udpate_snapshots.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import json, os, sys
+join = os.path.join
+
+SNAPSHOTS = join(os.environ['HOME'], '.snapshots')
+MNT = "/mnt/snapshots/"
+
+project_id = json.loads(open(join(os.environ['SMC'], 'info.json')).read())['project_id']
+
+def find_snapshots():
+    for x in open(join(MNT,'listing')).read().split('\n\n'):
+        v = x.split()
+        if len(v) > 1:
+            path = join(MNT, v[0][:-1])
+            recent = v[-1]
+            if os.path.exists(join(path, recent, project_id)):
+                return path, v[1:]
+
+def update_snapshots():
+    z = find_snapshots()
+    if not z:  # nothing
+        return
+    [path, snapshots] = z
+    if not os.path.exists(SNAPSHOTS):
+        os.makedirs(SNAPSHOTS)
+    valid = set(snapshots)
+    current = set(os.listdir(SNAPSHOTS))
+    for s in current:
+        if s not in valid:
+            try:
+                os.unlink(s)
+            except:
+                pass
+    for s in snapshots:
+        if s not in current:
+            os.symlink(join(MNT, path, s, project_id), join(SNAPSHOTS, s))
