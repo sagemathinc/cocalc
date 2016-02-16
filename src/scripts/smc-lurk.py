1dfb1c2dc186deb7520f7926a3b0ba69a8b93bea
Harald Schilly <hsy@sagemath.com>
2016-02-16 20:17:21 +0000

    scripts: error handling

--- src/scripts/smc-lurk.py
+++ src/scripts/smc-lurk.py
@@ -35,39 +35,45 @@ q = Queue()
 def print_data():
     # doing this async because of Nominatim
     while True:
-        left, right = q.get()
-        c = left["new_val"]
-        event = c["event"]
-        value = c["value"]
-        now = datetime.utcnow().replace(tzinfo = utc)
-        last_active = right.get("last_active", None)
-        if last_active:
-            ago = (now - last_active).total_seconds() / (24 * 60 * 60)
-            last_active = str(last_active)[:16]
-            right.pop("last_active")
-        else:
-            last_active = "NaN"
+        try:
+            left, right = q.get()
+            c = left["new_val"]
+            event = c["event"]
+            value = c["value"]
+            now = datetime.utcnow().replace(tzinfo = utc)
+            last_active = right.get("last_active", None)
+            if last_active:
+                ago = (now - last_active).total_seconds() / (24 * 60 * 60)
+                last_active = str(last_active)[:16]
+                right.pop("last_active")
+            else:
+                last_active = "NaN"
 
-        ip = value["ip_address"]
-        email_address = right.get("email_address", value.get("email_address", None))
-        name = "{first_name} {last_name} <{email}>".format(email = email_address, **right)
-        print("{name:<60s}\n    last seen: {last_active} ({ago:.2f} days ago)"\
-              .format(ago=ago, name = name, ip = ip, last_active = last_active, **right))
+            ip = value["ip_address"]
+            email_address = right.get("email_address", value.get("email_address", None))
+            name = "{first_name} {last_name} <{email}>".format(email = email_address, **right)
+            print("{name:<60s}\n    last seen: {last_active} ({ago:.2f} days ago)"\
+                  .format(ago=ago, name = name, ip = ip, last_active = last_active, **right))
 
-        geo = geolite2.lookup(ip)
-        loc = geolocator.reverse("{0}, {1}".format(*geo.location))
-        addr = loc.address.split(",")
-        addr = ", ".join(addr[2:])
-        print(u"    location: {}".format(addr))
-        print("")
-        q.task_done()
+            geo = geolite2.lookup(ip)
+            if geo is not None:
+                loc = geolocator.reverse("{0}, {1}".format(*geo.location))
+                addr = loc.address.split(",")
+                addr = ", ".join(addr[2:])
+            else:
+                addr = "NaN"
+            print(u"    location: {}".format(addr))
+            print("")
+        except Exception as ex:
+            print("Error: %s" % ex)
+        finally:
+            q.task_done()
 
 t = Thread(target = print_data)
 t.daemon = True
 t.start()
 
 for c in central_log.changes().eq_join(get_acc_id, accounts).run():
-    left, right = c["left"], c["right"]
-    q.put((left, right))
+    q.put((c["left"], c["right"]))
 
 q.join()
