e3312cc438f6f163943b7db0a47d3ff29ab6a010
William Stein <wstein@sagemath.com>
2016-06-24 22:15:41 +0000

    k8s: make first minimal driver actually work!

--- src/k8s/storage/driver/smc-storage.py
+++ src/k8s/storage/driver/smc-storage.py
@@ -13,36 +13,58 @@ LOG('argsv', sys.argv)
 def log(obj):
     print(json.dumps(obj))
 
+def cmd(s):
+    z = os.popen(s)
+    t = z.read()
+    if z.close():
+        raise RuntimeError(t)
+
 def init(args):
     LOG('init', args)
     # TODO: would ensure zfs kernel module is available (?)
     return
 
+def image_filename(project_id):
+    return os.path.join("/tmp/%s.img"%project_id)
+
+# Attach device to minion
 def attach(args):
     LOG('attach', args)
     params = json.loads(args.json_params)
     project_id = params.get("project_id", None)
     if not project_id:
         raise RuntimeError("must specify project_id")
-    return {'device':project_id}
+    img = image_filename(project_id)
+    if not os.path.exists(img):
+        cmd('truncate -s 3G %s'%img)
+        format = True
+    else:
+        format = False
+    device = os.popen("losetup -v -f %s"%img).read().split()[-1]
+    if format:
+        cmd('mkfs.ext4 -q %s'%device)
+    return {'device':device}
 
-def detach(sarg):
-    LOG('detach', args)
-    pass
 
 def mount(args):
     LOG('mount', args)
     mount_dir  = args.mount_dir
-    project_id = args.project_id
+    device     = args.device
     params     = json.loads(args.json_params)
     if not os.path.exists(mount_dir):
         os.makedirs(mount_dir)
+    cmd("mount %s %s"%(device, mount_dir))
 
 def unmount(args):
     LOG('unmount', args)
     mount_dir  = args.mount_dir
     if os.path.exists(mount_dir):
-        shutil.rmtree(mount_dir)
+        cmd("umount %s"%mount_dir)
+
+def detach(args):
+    LOG('detach', args)
+    device = args.device
+    cmd("losetup -d %s"%device)
 
 if __name__ == '__main__':
     import argparse
@@ -56,19 +78,19 @@ if __name__ == '__main__':
     sub.add_argument('json_params', type=str, help="""json of object '{"project_id": "f8cf98ed-299e-4423-a167-870e8658e081"}""")
     sub.set_defaults(func=attach)
 
-    sub = subparsers.add_parser('detach', help='export ZFS pool and remove snapshot bind mount')
-    sub.add_argument('project_id', type=str, help='project_id')
-    sub.set_defaults(func=detach)
-
     sub = subparsers.add_parser('mount', help='mount the ZFS pool, which is assumed to exist, at a given mountpoint; also create bind mounts for snapshots')
     sub.add_argument('mount_dir', type=str, help='mount dir')
-    sub.add_argument('project_id', type=str, help='project_id')
+    sub.add_argument('device', type=str, help='device name')
     sub.add_argument('json_params', type=str, help='json params {...}')
     sub.set_defaults(func=mount)
 
     sub = subparsers.add_parser('unmount', help='')
     sub.add_argument('mount_dir', type=str, help='mount dir')
     sub.set_defaults(func=unmount)
+
+    sub = subparsers.add_parser('detach', help='export ZFS pool and remove snapshot bind mount')
+    sub.add_argument('device', type=str, help='mount device')
+    sub.set_defaults(func=detach)
 
     args = parser.parse_args()
     if hasattr(args, 'func'):
