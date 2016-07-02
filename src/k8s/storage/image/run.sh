9fee5a5a10b52820e585af457a2ff7f33ab5c1e3
William Stein <wstein@sagemath.com>
2016-07-02 14:59:16 +0000

    k8s storage -- generate an ssh public/private key pair in the kubernetes secrets thing for the namespace, put in right place, and get everything working again with automatic install of deps, etc.

--- src/k8s/storage/image/run.sh
+++ src/k8s/storage/image/run.sh
@@ -3,9 +3,16 @@
 # Enable very fast (but less secure) cipher; all we need since already on a LAN.
 echo "Ciphers arcfour128">>/etc/ssh/sshd_config
 
+# Copy over ssh keys from the k8s secret
+mkdir -p /root/.ssh
+cp /ssh/id-rsa /root/.ssh/id_rsa
+cp /ssh/id-rsa.pub /root/.ssh/id_rsa.pub
+cp /ssh/id-rsa.pub /root/.ssh/authorized_keys
+chmod og-rwx -R /root/.ssh
+
 service ssh start
 
-# Ugly hack to do nothing and wait for SIGTERM
+# Wait for SIGTERM
 while true; do
     sleep 5
 done
