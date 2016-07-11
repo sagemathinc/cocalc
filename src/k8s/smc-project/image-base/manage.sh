ae8581e331189e5ce897bf2f7be14fa36e7db54d
Harald Schilly <hsy@sagemath.com>
2016-07-11 12:41:51 +0000

    k8s/project images: entrypoint for management script, language env, update task

---
+++ src/k8s/smc-project/image-base/manage.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+# thin management wrapper lib, used for Docker's ENTRYPOINT
+
+# init: clean existing code and grab newest version
+init () {
+    cd /
+    rm -rf /smc/
+    git clone --depth 1 https://github.com/sagemathinc/smc.git
+    cd /smc/src/smc-build/smc-ansible
+}
+
+case "$1" in
+    install)
+        # this runs the meta-playbook "compute-setup" with the modified machines file on localhost
+        init
+        ansible-playbook -i container.ini compute-setup.yaml
+        ;;
+
+    update)
+        # this runs the meta-playbook "compute-setup" with the tags "update"
+        # they're supposed to check for upgraded packages and do not compile so much -- hence finishes faster
+        init
+        ansible-playbook -i container.ini compute-setup.yaml --tags=update
+        ;;
+
+    bash)
+        # fork to be a command-line
+        exec bash
+        ;;
+
+    *)
+        echo $"Usage: $0 {install|update|bash}"
+        exit 1
+esac
--- src/k8s/smc-project/image-base/update.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-cd /
-rm -rf /smc/
-git clone --depth 1 https://github.com/sagemathinc/smc.git
-cd /smc/src/smc-build/smc-ansible
-
