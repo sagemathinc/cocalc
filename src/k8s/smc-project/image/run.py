d1da46b90dc508ffd3f341c22d246a16760f3931
Harald Schilly <hsy@sagemath.com>
2016-07-23 18:27:21 +0000

    k8s: work on base image

--- src/k8s/smc-project/image/run.py
+++ src/k8s/smc-project/image/run.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
-import os, shutil
-from subprocess import call
+import os, sys, shutil
+from subprocess import call, Popen
 
 project_id = os.environ['SMC_PROJECT_ID']
 
@@ -24,19 +24,39 @@ n //= 2  # up to 2^31   (floor div so will work with python3 too)
 uid = n if n>65537 else n+65537   # 65534 used by linux for user sync, etc.
 
 os.chown(project_path, uid, uid)
-
 os.setgid(uid)
 os.setuid(uid)
+os.umask(0077) # octal, in py3 that will be 0o077
 os.environ['HOME'] = project_path
 os.environ['SMC']  = os.path.join(project_path, '.smc')
+
+data = os.path.join(os.environ['SMC'], 'local_hub')
+shutil.rmtree(data, ignore_errors=True)
+os.makedirs(data)
 
 username = project_id.replace('-','')
 os.environ['USER'] = os.environ['USERNAME'] =  os.environ['LOGNAME'] = username
 os.environ['MAIL'] = '/var/mail/%s'%username
 
 os.chdir(project_path)
-# optional args: tcp port and raw port
-call("smc-start --tcp_port 6000 --raw_port 6001".split())
-call('tail -f .smc/local_hub/local_hub.log', shell=True)
+
+if len(sys.argv) >= 2:
+    # this is usually ["bash"]
+    call(sys.argv[1:])
+else:
+    # start smc project with specific tcp port and raw port
+    # before, this was: call("smc-start --tcp_port 6000 --raw_port 6001".split())
+    # now, bypass smc-start & smc-local-hub by searching local_hub.coffee relative to smc-local-hub
+    import distutils.spawn
+    from os.path import join, dirname, abspath
+    slh = distutils.spawn.find_executable('smc-local-hub')
+    slh = abspath(join(dirname(slh), os.readlink(slh)))
+    lhc = abspath(os.path.join(slh, '..', '..', 'local_hub.coffee'))
+    # lhc is most likely /usr/lib/node_modules/smc-project/local_hub.coffee
+    cmd = 'coffee %s --tcp_port 6000 --raw_port 6001'%lhc
+    print(cmd)
+    pid = Popen(cmd.split()).pid
+    print('spawned local_hub with PID %s'%pid)
+    call('tail -F .smc/local_hub/local_hub.log', shell=True)
 
 
