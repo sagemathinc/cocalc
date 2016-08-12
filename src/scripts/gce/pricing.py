dca7564f36fbd095e27b32f81889a061d3f3f9e0
William Stein <wstein@sagemath.com>
2016-08-12 18:47:36 +0000

    gce pre-empt price reduction

--- src/scripts/gce/pricing.py
+++ src/scripts/gce/pricing.py
@@ -11,27 +11,27 @@ PRICING = {
     'pd-standard'      : 0.04,
 
     'n1-standard-hour' : 0.05,          # for equivalent of -1, so multiply by number of cpu's (the suffix)
-    'n1-standard-hour-pre'  : 0.015,
+    'n1-standard-hour-pre'  : 0.01,
     'n1-standard-month': 0.035*30.5*24,  # price for sustained use for a month
     'n1-standard-ram'  : 3.75,           # amount in GB of base machine
 
     'n1-highmem-hour'  : 0.096/2,
-    'n1-highmem-hour-pre'   : 0.035/2,
+    'n1-highmem-hour-pre'   : 0.025/2,
     'n1-highmem-month' : 0.088*30.5*24/2,
     'n1-highmem-ram'   : 6.5,
 
     'n1-highcpu-hour'  : 0.076/2,
-    'n1-highcpu-hour-pre'   : 0.02/2,
+    'n1-highcpu-hour-pre'   : 0.015/2,
     'n1-highcpu-month' : 0.053*30.5*24/2,
     'n1-highcpu-ram'   : 0.9,
 
     'g1-small-hour'    : 0.021,
-    'g1-small-hour-pre': 0.01,
+    'g1-small-hour-pre': 0.007,
     'g1-small-month'   : 0.019*30.5*24,
     'g1-small-ram'     : 1.7,
 
     'f1-micro-hour'    : 0.008,
-    'f1-micro-hour-pre': 0.005,
+    'f1-micro-hour-pre': 0.0035,
     'f1-micro-month'   : 0.0056*30.5*24,
     'f1-micro-ram'     : 0.60,
 
