e980fcf0e8892b5d9d53df0285c9a71ca91a485f
Harald Schilly <hsy@sagemath.com>
2016-09-10 09:53:20 +0000

    sagews/test: complaints about one newline too many

--- src/smc_sagews/smc_sagews/tests/test_sagews.py
+++ src/smc_sagews/smc_sagews/tests/test_sagews.py
@@ -27,9 +27,8 @@ def test_issue70(exec2):
         pass
     'x'
     """)
-    output = dedent(r"""
-    'x'
-    """).lstrip()
+    output = dedent(r"""'x'
+    """)
     exec2(code, output)
 
 def test_issue819(exec2):
