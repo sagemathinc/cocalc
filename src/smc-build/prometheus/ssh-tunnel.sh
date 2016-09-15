22a73cd6794c27890a6fcdf3264f3bc7025e5bd8
Harald Schilly <hsy@sagemath.com>
2016-09-15 16:08:04 +0200

    prometheus: ssh proxy with variable port number

--- src/smc-build/prometheus/ssh-tunnel.sh
+++ src/smc-build/prometheus/ssh-tunnel.sh
@@ -2,8 +2,10 @@
 # uses gcloud computes ssh key to connect to admin0
 
 admin0ip=`gcloud --format="value(networkInterfaces[0].accessConfigs[0].natIP)" compute instances describe admin0`
+port=${1:-3000}
+echo "forwarding from $admin0ip with portnumber $port"
 
-ssh -CNL 3000:localhost:3000 \
+ssh -CNL $port:localhost:$port \
     -i ~/.ssh/google_compute_engine \
     -o UserKnownHostsFile=~/.ssh/google_compute_known_hosts \
     -o IdentitiesOnly=yes -o CheckHostIP=no \
