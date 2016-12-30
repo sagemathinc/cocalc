ff83dd0bb92d3c77374f561501d7ddd7a2ee66a2
William Stein <wstein@sagemath.com>
2016-12-30 23:56:44 +0000

    comment about why strip null bytes

--- src/scripts/postgresql/migrate/json_to_csv.py
+++ src/scripts/postgresql/migrate/json_to_csv.py
@@ -4,7 +4,8 @@ import os, sys
 
 for x in sys.argv[1:]:
     base, ext = os.path.splitext(x)
-    #if os.path.exists('%s.csv'%base): continue
+    # The grep -v '\\\\u0000' skips any json record with null bytes.  These are not valid/meaningful
+    # for postgres, and happen in a very small handful of non-important records.
     s = "time sed 's/,$//' %s | head -n -1 | tail -n +2 | grep -v '\\\\u0000' > %s.csv"%(x, base)
     print(s)
     os.system(s)
