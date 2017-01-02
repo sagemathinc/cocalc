7585256be448e6b41c9bff76694de25b908ebc7c
Hal Snyder <hsnyder@sagemath.com>
2017-01-02 09:45:54 +0000

    rmd2html: knitr::knit writes output file to current directory

--- src/smc_pyutil/smc_pyutil/rmd2html.py
+++ src/smc_pyutil/smc_pyutil/rmd2html.py
@@ -12,10 +12,15 @@ def rmd2html(path):
     if ext.lower() != ".rmd":
         raise ValueError('Rmd input file required, got {}'.format(path))
 
-    cmd = '''Rscript -e "library(knitr); knit('{}')" >/dev/null 2>/dev/null'''.format(path)
+    (head,tail) = os.path.split(path)
+    if head == '':
+        head = "./"
+    cmd = '''Rscript -e "library(knitr); setwd('{}'); knit('{}')" >/dev/null 2>/dev/null'''.format(head, tail)
     if subprocess.call(cmd, shell=True) == 0:
-        cmd2 = "pandoc -s {}.md -t html 2>/dev/null".format(root)
-        subprocess.call(cmd2, shell=True)
+        cmd2 = "pandoc -s {}.md -o {}.html 2>/dev/null".format(root, root)
+        if subprocess.call(cmd2, shell=True) == 0:
+            with open("{}.html".format(root), 'r') as htmlfile:
+                print(htmlfile.read())
 
 def main():
     if len(sys.argv) != 2:
