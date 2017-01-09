c48d5ef1c5fb4fbf454cf2d98b24e99f47cbc998
William Stein <wstein@sagemath.com>
2017-01-09 07:17:51 +0000

    import blobs

---
+++ src/scripts/postgresql/migrate/import-blobs_json.sql
@@ -0,0 +1,22 @@
+/*
+time psql -d migrate -a -f blobs.sql
+
+Copies data from RethinkDB JSON to the blobs table.
+
+NOTE: a tiny fraction of ids were 40 characters
+*/
+
+INSERT INTO blobs
+  (SELECT
+    (a#>>'{id}')::UUID,
+    NULL,
+    to_timestamp((a#>>'{expire,epoch_time}')::FLOAT),
+    to_timestamp((a#>>'{created,epoch_time}')::FLOAT),
+    (a#>>'{project_id}')::UUID,
+    to_timestamp((a#>>'{last_active,epoch_time}')::FLOAT),
+    (a#>>'{count}')::INTEGER,
+    (a#>>'{size}')::INTEGER,
+    (a#>>'{gcloud}'),
+    (a#>>'{backup}')::BOOL
+  FROM blobs_json WHERE CHAR_LENGTH(a#>>'{id}') = 36
+);
