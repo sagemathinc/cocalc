b108f994869da48a9017badd511d9d1ade58393c
william stein <wstein@sagemath.com>
2017-02-11 17:23:15 +0000

    delete most rethinkdb-related code (pretty obvious stuff) -- more work to make this stable and work again

--- src/dev/laptop/start_rethinkdb.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-import os, util
-
-util.chdir()
-
-if not os.path.exists('rethinkdb_data'):
-    util.cmd('rethinkdb create -d "rethinkdb_data"')
-
-util.cmd('rethinkdb serve')
