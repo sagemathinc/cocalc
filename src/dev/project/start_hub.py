683cd7bf3da35033b0ec64100d29178b83b5da44
William A. Stein <wstein@sagemath.com>
2017-09-26 21:50:11 +0000

    make project dev hub listen on 0.0.0.0, due to k8s switch; much simpler fix, and means can curl directly from project to project for easier testing.

--- src/dev/project/start_hub.py
+++ src/dev/project/start_hub.py
@@ -15,17 +15,7 @@ util.chdir()
 ports = util.get_ports()
 base_url = util.base_url()
 
-# disabling hostname, because nslookup of project-UUID does not return anything
-#hostname = socket.gethostname()
-# instead, take the ip of the local hostname
-from subprocess import check_output
-host_ips = check_output(['hostname', '--all-ip-addresses']).splitlines()[0]
-
-# will be --host=... for the hub's http server, no lookup needed any more
-hostname = host_ips
-
-cmd = "cd ../../ && . smc-env &&  service_hub.py --dev --foreground --hostname={hostname} --port={hub_port} --proxy_port=0 --gap=0 --base_url={base_url} start".format(
-    hostname      = hostname,
+cmd = "cd ../../ && . smc-env &&  service_hub.py --dev --foreground --hostname=0.0.0.0 --port={hub_port} --proxy_port=0 --gap=0 --base_url={base_url} start".format(
     base_url      = base_url,
     hub_port      = ports['hub'])
 
