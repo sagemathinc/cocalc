sagemathinc/cocalc

commit 5ef485ab16e6aa7ada7b2eac8dbce896a5d2d0dd

share: fixing allout from 21cfd116f8406b218d62ed6f4631e01b77764e27 (only encode the filename, not the entire path)

src/smc-webapp/share/public-paths-browser.cjsx
@@ -69,7 +69,8 @@ exports.PublicPathsBrowser = rclass
 
     render_public_path_link: (info, bgcolor) ->
         id         = info.get('id')
-        info_path  = encodeURIComponent(info.get('path'))
+        path_split = info.get('path').split('/')
+        info_path  = (encodeURIComponent(p) for p in path_split).join('/')
 
         <div key={id} style={padding: '5px 10px', background:bgcolor}>
             <a href={"#{id}/#{info_path}?viewer=share"} style={display:'inline-block', width:'100%'}>

commit 368e93bf088fbd737de689979fa2af13d1b6ccb6

share: 5ef485ab16e6aa7ada7b2eac8dbce896a5d2d0dd introduced another issue, now using misc.encode_path and also using it for the directory listing. for testing, this handles paths like "sub/wfwe?asf/ifä ä wFđø/ÖFa äWÜ ?fnwføpÞ.md"

src/smc-hub/share/public_paths.coffee
@@ -7,6 +7,7 @@ the DB before returning results).  And, since we have everything in memory, we c
 things involving iterating over everything before writing proper queries.
 ###
 
+
 EventEmitter = require('events')
 
 immutable = require('immutable')

src/smc-hub/share/render-public-path.cjsx
@@ -42,9 +42,9 @@ exports.render_public_path = (opts) ->
             path_to_file: os_path.join(opts.dir, opts.path)
         fs.lstat locals.path_to_file, (err, stats) ->
             if err
-
                 opts.res.sendStatus(404)
                 return
+
             if stats.isDirectory()
                 if opts.path.slice(-1) != '/'
                     util.redirect_to_directory(opts.req, opts.res)
@@ -92,10 +92,12 @@ exports.render_public_path = (opts) ->
                         else
                             locals.content = data.toString()
                             cb()
+
             get_content (err) ->
                 if err
                     opts.res.sendStatus(404)
                     return
+
                 component = <PublicPath
                     info     = {opts.info}
                     content  = {locals.content}

src/smc-hub/share/router.cjsx
@@ -127,7 +127,9 @@ exports.share_router = (opts) ->
                 project_id = info.get('project_id')
 
             path = req.params[0]
+            #dbg("router.get '/:id/*?': #{project_id} and #{path}: #{public_paths.is_public(project_id, path)}, info: #{misc.to_json(info)}, path: #{path}")
             if not path?
+                #dbg("no path → 404")
                 res.sendStatus(404)
                 return
 
@@ -140,6 +142,7 @@ exports.share_router = (opts) ->
             #   by what happens to be in the path to files.  So share server not having
             #   updated yet is a problem, but ALSO, in some cases (dev server, docker personal)
             #   that path is just to the live files in the project, so very dangerous.
+
 
             if not public_paths.is_public(project_id, path)
                 res.sendStatus(404)

src/smc-util/misc.coffee
@@ -1187,6 +1187,7 @@ exports.containing_public_path = (path, paths) ->
     return undefined
 
 # encode a UNIX path, which might have # and % in it.
+# Maybe alternatively, (encodeURIComponent(p) for p in path.split('/')).join('/') ?
 exports.encode_path = (path) ->
     path = encodeURI(path)  # doesn't escape # and ?, since they are special for urls (but not unix paths)
     return path.replace(/#/g,'%23').replace(/\?/g,'%3F')

src/smc-webapp/share/directory-listing.cjsx
@@ -65,6 +65,7 @@ DirectoryListingEntry = rclass
 
     get_href: ->
         href = @props.name
+        href = misc.encode_path(href)
         if @props.isdir
             href += '/'
         if @props.viewer

src/smc-webapp/share/public-paths-browser.cjsx
@@ -3,7 +3,7 @@ Share server top-level landing page.
 ###
 
 {rclass, React, ReactDOM, rtypes} = require('../smc-react')
-
+misc = require('smc-util/misc')
 {Space, TimeAgoElement} = require('../r_misc')
 
 INDEX_STYLE =
@@ -69,8 +69,7 @@ exports.PublicPathsBrowser = rclass
 
     render_public_path_link: (info, bgcolor) ->
         id         = info.get('id')
-        path_split = info.get('path').split('/')
-        info_path  = (encodeURIComponent(p) for p in path_split).join('/')
+        info_path  = misc.encode_path(info.get('path'))
 
         <div key={id} style={padding: '5px 10px', background:bgcolor}>
             <a href={"#{id}/#{info_path}?viewer=share"} style={display:'inline-block', width:'100%'}>