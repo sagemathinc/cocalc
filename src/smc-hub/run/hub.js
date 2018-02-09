8f186630f3815fa684657b7a050029c18603ce8d
John Jeng <john.jeng5@gmail.com>
2018-02-09 01:24:30 +0000

    Remove a breaking instance of coffee-cache

--- src/smc-hub/run/hub.js
+++ src/smc-hub/run/hub.js
@@ -1,3 +1,3 @@
 require('coffeescript/register') /* so we can require coffeescript */
-require('coffee-cache').setCacheDir('.coffee/cache')  /* two level is NECESSARY; so coffeescript doesn't get recompiled every time we require it */
+// require('coffee-cache').setCacheDir('.coffee/cache')  /* two level is NECESSARY; so coffeescript doesn't get recompiled every time we require it */
 require('../hub.coffee')
