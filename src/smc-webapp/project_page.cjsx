sagemathinc/cocalc

commit e40452b9f9ecfab9efe1cb3c7f4ac72303b5dd20

address some style issues with markdown editor

src/smc-webapp/project_page.cjsx
@@ -256,7 +256,10 @@ ProjectContentViewer = rclass
         if not Editor?
             <Loading />
         else
-            <div ref='editor_inner_container' style={height:'100%', display:'flex', flexDirection:'column', overflowX:'hidden', willChange: 'transform'}>
+            <div
+                ref       = 'editor_inner_container'
+                className = 'smc-vfill'
+                style     = {height:'100%', willChange: 'transform'}>
                 <Editor
                     name         = {redux_name}
                     path         = {path}

src/smc-webapp/code-editor/frame-tree.cjsx
@@ -149,8 +149,8 @@ exports.FrameTree = FrameTree = rclass
                     return <div>Invalid frame tree {misc.to_json(desc)}</div>
         <div
             className    = {'smc-vfill'}
-            onClick      = {=>@props.actions.set_active_id(desc.get('id'))}
-            onTouchStart = {=>@props.actions.set_active_id(desc.get('id'))}
+            onClick      = {=>@props.actions.set_active_id(desc.get('id'), 10)}
+            onTouchStart = {=>@props.actions.set_active_id(desc.get('id'), 10)}
         >
             {@render_titlebar(desc)}
             {child}

src/smc-webapp/file-associations.coffee
@@ -44,7 +44,7 @@ codemirror_associations =
     json   : 'javascript'
     lua    : 'lua'
     m      : 'text/x-octave'
-    md     : 'gfm2'
+    md     : 'gfm'
     ml     : 'text/x-ocaml'
     mysql  : 'text/x-sql'
     patch  : 'text/x-diff'
@@ -56,7 +56,7 @@ codemirror_associations =
     py     : 'python'
     pyx    : 'python'
     r      : 'r'
-    rmd    : 'gfm2'
+    rmd    : 'gfm'
     rnw    : 'stex2'
     rst    : 'rst'
     rb     : 'text/x-ruby'
@@ -135,13 +135,13 @@ file_associations['html'] =
 file_associations['md'] =
     editor : 'html-md'
     icon   : 'cc-icon-markdown'
-    opts   : {indent_unit:4, tab_size:4, mode:'gfm2'}
+    opts   : {indent_unit:4, tab_size:4, mode:'gfm'}
     name   : "markdown"
 
 file_associations['rmd'] =
     editor : 'html-md'
     icon   : 'cc-icon-r'
-    opts   : {indent_unit:4, tab_size:4, mode:'gfm2'}
+    opts   : {indent_unit:4, tab_size:4, mode:'gfm'}
     name   : "Rmd"
 
 file_associations['java'] =

src/smc-webapp/markdown-editor/actions.coffee
@@ -21,3 +21,4 @@ class exports.Actions extends Actions
             return
         cm.edit_selection({cmd:cmd, args:args})
         cm.focus()
+        @set_syncstring_to_codemirror()

src/smc-webapp/markdown-editor/rendered-markdown.cjsx
@@ -13,6 +13,11 @@ It also:
 {Loading, Markdown} = require('../r_misc')
 {React, rclass, rtypes}     = require('../smc-react')
 
+# If the user makes the viewport really wide, it is very hard to read
+# the markdown, so we max the width out at 900px.  I have no idea if 900px
+# is a good choice...
+MAX_WIDTH = '900px'
+
 exports.RenderedMarkdown = rclass
     displayName: 'MarkdownEditor-RenderedMarkdown'
 
@@ -26,11 +31,13 @@ exports.RenderedMarkdown = rclass
         value      : rtypes.string
 
     render: ->
-        <div style={overflow:'auto', 'padding': '0 10px'}>
-            <Markdown
-                value      = {@props.value}
-                project_id = {@props.project_id}
-                file_path  = {@props.path}
-            />
+        <div style={overflow:'auto', width:'100%'}>
+            <div style={maxWidth: MAX_WIDTH, margin: '0 auto', padding:'10px'}>
+                <Markdown
+                    value      = {@props.value}
+                    project_id = {@props.project_id}
+                    file_path  = {@props.path}
+                />
+            </div>
         </div>
 

commit 114fc06957effcaed36ec01b054a62c991521fd1

webapp: fixing a vertical scrolling issue caused by e40452b9f9ecfab9efe1cb3c7f4ac72303b5dd20

src/smc-webapp/project_page.cjsx
@@ -257,9 +257,9 @@ ProjectContentViewer = rclass
             <Loading />
         else
             <div
-                ref       = 'editor_inner_container'
-                className = 'smc-vfill'
-                style     = {height:'100%', willChange: 'transform'}>
+                ref       = {'editor_inner_container'}
+                className = {'smc-vfill'}
+                style     = {height:'100%', willChange: 'transform', overflowY:'auto'}>
                 <Editor
                     name         = {redux_name}
                     path         = {path}