c281aedfaa08038d11922bfa51f8d55b1d5a7cc4
William A. Stein <wstein@sagemath.com>
2018-04-19 22:20:12 +0000

    move the import of the latex mode to the right place (for now!)

--- src/smc-webapp/latex-editor/register.js
+++ src/smc-webapp/latex-editor/register.js
@@ -8,6 +8,9 @@ import { Actions } from "./actions";
 
 import { register_file_editor } from "../code-editor/register-generic";
 
+// Load plugin so that codemirror can automatically close latex environments.
+import './codemirror-autoclose-latex'
+
 register_file_editor({
     ext: "tex",
     component: Editor,
