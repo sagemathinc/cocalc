3c05738431be0d7f8ee9ee9d3ca116c4327feca3
William A. Stein <wstein@sagemath.com>
2018-04-21 18:20:33 +0000

    working on docs...

--- src/smc-webapp/latex-editor/mouse-draggable.ts
+++ src/smc-webapp/latex-editor/mouse-draggable.ts
@@ -25,43 +25,53 @@ interface Position {
 }
 
 function mouse_draggable(): void {
+    // the element that is being dragged around.
     const elt = $(this);
+
+    // dragpos = the position that the user just dragged the document to
     let dragpos: Position = { left: undefined, top: undefined };
 
+    // when the mouse button goes down, we change the cursor, initialize the dragpos,
+    // and activate the mousemove handler.
     elt.on("mousedown", e => {
         e.preventDefault();
         // Still need to remove the focus from the codemirror textarea
         // otherwise, space-key and others have no effect on scrolling.
         $(document.activeElement).blur();
-        elt.on("mousemove", mousemove_handler);
+        elt.css("cursor", "move");
         dragpos = {
             left: e.clientX,
             top: e.clientY
         };
+        elt.on("mousemove", mousemove_handler);
     });
 
+    // done with dragging document around -- reset cursor to default, and stop listening for mouse movement.
     function reset(): void {
         elt.css("cursor", "");
         elt.off("mousemove", mousemove_handler);
     }
 
+    // finished dragging -- reset everything.
     elt.on("mouseup", e => {
         e.preventDefault();
         reset();
         return false;
     });
 
+    // handle mouse moving with button down.
     const mousemove_handler = e => {
         e.preventDefault();
 
         // this checks, if we come back into the viewport after leaving it
-        // but the mouse is no longer pressed
+        // but the mouse button is no longer pressed
         if (e.which !== 1) {
             reset();
             return;
         }
-        elt.css("cursor", "move");
 
+        // if any positions are undefined, which maybe technically could happen, do not do anything -- just
+        // wait for the user to lift their mouse button.  (Basically, this satisfies the typescript.)
         if (
             dragpos.left === undefined ||
             dragpos.top === undefined ||
@@ -69,6 +79,7 @@ function mouse_draggable(): void {
             e.clientY === undefined
         )
             return;
+
         const delta = {
             left: e.clientX - dragpos.left,
             top: e.clientY - dragpos.top
@@ -76,8 +87,10 @@ function mouse_draggable(): void {
         const left: coord = elt.scrollLeft(),
             top: coord = elt.scrollTop();
         if (left === undefined || top === undefined) return;
+
         elt.scrollLeft(left - delta.left);
         elt.scrollTop(top - delta.top);
+
         dragpos = {
             left: e.clientX,
             top: e.clientY
