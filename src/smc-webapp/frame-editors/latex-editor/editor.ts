d72ab1e8d1641e40f49a7186eb06d2e2ae20ad40
William A. Stein <wstein@sagemath.com>
2018-05-07 22:24:27 +0000

    moving some more experimental code out of the way

--- src/smc-webapp/frame-editors/latex-editor/editor.ts
+++ src/smc-webapp/frame-editors/latex-editor/editor.ts
@@ -8,13 +8,9 @@ import { createEditor } from "../frame-tree/editor";
 
 import { PDFJS } from "./pdfjs.tsx";
 import { PDFEmbed } from "./pdf-embed.tsx";
-//import { CodemirrorEditor } from "../code-editor/codemirror-editor";
-const { CodemirrorEditor } = require("../code-editor/codemirror-editor");
+import { CodemirrorEditor } from "../code-editor/codemirror-editor";
 import { Build } from "./build.tsx";
 import { ErrorsAndWarnings } from "./errors-and-warnings.tsx";
-
-// import { LaTeXJS } from "./latexjs";
-// import { PEG } from "./peg";
 
 import { pdf_path } from "./util";
 
