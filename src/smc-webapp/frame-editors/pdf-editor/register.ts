0652fd0103c937bb263e8cb88113137e6cf05a7f
William A. Stein <wstein@sagemath.com>
2018-06-06 01:20:49 +0000

    disbale old pdf renderer

---
+++ src/smc-webapp/frame-editors/pdf-editor/register.ts
@@ -0,0 +1,14 @@
+/*
+Register the PDF editor
+*/
+
+import { Editor } from "./editor";
+import { PDFActions } from "./actions";
+
+import {register_file_editor} from '../frame-tree/register';
+
+register_file_editor({
+  ext: "pdf",
+  component: Editor,
+  Actions : PDFActions
+});
