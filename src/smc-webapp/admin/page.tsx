6547dfe08fb6e40dffb6328576f2722c5941eefa
William Stein <wstein@sagemath.com>
2018-06-07 18:16:57 +0000

    add a new admin top-level page

---
+++ src/smc-webapp/admin/page.tsx
@@ -0,0 +1,11 @@
+import { React, Component } from "../frame-editors/generic/react";
+
+export class AdminPage extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Administrative server settings</h3>
+      </div>
+    );
+  }
+}
