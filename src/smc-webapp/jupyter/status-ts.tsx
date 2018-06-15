7b2c1256cc76b27f228b48d3144fa75f55fbbba9
Travis Scholl <tscholl@sagemath.com>
2018-06-15 23:21:11 +0000

    some top bar ts files

--- src/smc-webapp/jupyter/status-ts.tsx
+++ src/smc-webapp/jupyter/status-ts.tsx
@@ -1,10 +1,4 @@
 /*
- * decaffeinate suggestions:
- * DS102: Remove unnecessary code created because of implicit returns
- * DS207: Consider shorter variations of null checks
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
- */
-/*
 Kernel display
 */
 
