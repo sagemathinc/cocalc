26a9e6be82fd9fa6eb105133c884af38a43fc613
William A. Stein <wstein@sagemath.com>
2018-06-17 21:40:38 +0000

    starting to work on editable cells (just a trivial one to get exec, etc., in place)

--- src/smc-webapp/frame-editors/sagews-editor/input-cell.tsx
+++ src/smc-webapp/frame-editors/sagews-editor/input-cell.tsx
@@ -1,12 +1,27 @@
 import { React, Component, Rendered } from "../generic/react";
 
 interface Props {
-  id : string;
+  id: string;
   input: string;
 }
 
-export class InputCell extends Component<Props, {}> {
+interface State {
+  input: string;
+}
+
+export class InputCell extends Component<Props, State> {
+  constructor(props, state) {
+    super(props, state);
+    this.state = { input: this.props.input };
+  }
   render(): Rendered {
-    return <textarea value={this.props.input} onChange={function() {}} />;
+    return (
+      <textarea
+        value={this.state.input}
+        onChange={evt => {
+          this.setState({ input: evt.target.value });
+        }}
+      />
+    );
   }
 }
