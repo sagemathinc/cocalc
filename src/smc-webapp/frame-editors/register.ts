0ed9a3293dd99d1d89f02762bc974ec3c4ca3f05
William A. Stein <wstein@sagemath.com>
2018-06-20 03:37:17 +0000

    disable sagews

--- src/smc-webapp/frame-editors/register.ts
+++ src/smc-webapp/frame-editors/register.ts
@@ -14,6 +14,7 @@ import "./latex-editor/register.ts";
 
 import "./pdf-editor/register.ts";
 
-import "./sagews-editor/register.ts";
+// Work in progress -- uncomment to use new sagews support.
+// import "./sagews-editor/register.ts";
 
 import "./generic/test/init";
