756104f4db20a148ee76d12e5803fa3e845569b4
William A. Stein <wstein@sagemath.com>
2018-06-21 23:49:56 +0000

    increase some test timeouts

--- src/smc-webapp/frame-editors/code-editor/test/format.ts
+++ src/smc-webapp/frame-editors/code-editor/test/format.ts
@@ -6,7 +6,7 @@ import {
 } from "../../generic/test/util";
 
 describe("CodeEditor - tests related to the format bar", function() {
-  this.timeout(5000);
+  this.timeout(10000);
 
   describe("checks that there is no format bar when editing a txt file", function() {
     let editor;
