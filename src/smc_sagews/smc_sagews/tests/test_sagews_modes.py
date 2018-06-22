c0be05f0cb5715fd649de78e20d36b8b8f559733
Hal Snyder <hsnyder@sagemath.com>
2018-06-22 02:12:13 +0000

    make tests work with python 3.5

--- src/smc_sagews/smc_sagews/tests/test_sagews_modes.py
+++ src/smc_sagews/smc_sagews/tests/test_sagews_modes.py
@@ -250,8 +250,8 @@ class TestAnaconda3Mode:
 
 class TestJuliaMode:
     def test_julia_quadratic(self, exec2):
-        exec2('%julia\nquadratic(a, sqr_term, b) = (-b + sqr_term) / 2a\nquadratic(2.0, -2.0, -12.0)', '2.5')
+        exec2('%julia\nquadratic(a, sqr_term, b) = (-b + sqr_term) / 2a\nquadratic(2.0, -2.0, -12.0)', '2.5', timeout=40)
 
     def test_julia_version(self, exec2):
-        exec2("%julia\nVERSION", pattern='"0.6.3"')
+        exec2("%julia\nVERSION", pattern='"0.6.3"', timeout=40)
 
