856d1d80c75f4a95637a90a588fad820a03da29e
William Stein <wstein@sagemath.com>
2018-06-26 17:59:40 +0000

    Improve user display

--- src/smc-webapp/admin/users/user.tsx
+++ src/smc-webapp/admin/users/user.tsx
@@ -2,6 +2,7 @@
 Display of basic information about a user, with link to get more information about that user.
 */
 
+const { TimeAgo } = require("smc-webapp/r_misc");
 
 import {
   React,
@@ -9,10 +10,34 @@ import {
   Rendered
 } from "smc-webapp/frame-editors/generic/react";
 
+import { Row, Col } from "react-bootstrap";
+
 import { User } from "smc-webapp/frame-editors/generic/client";
 
-export class UserResult extends Component<User,{}> {
-  render() : Rendered {
-    return <div>{this.props.first_name} {this.props.last_name} {this.props.email_address}</div>
+export class UserResult extends Component<User, {}> {
+  render_created(): Rendered {
+    if (!this.props.created) {
+      return <span>unknown</span>;
+    }
+    return <TimeAgo date={this.props.created} />;
+  }
+
+  render_last_active(): Rendered {
+    if (!this.props.last_active) {
+      return <span>unknown</span>;
+    }
+    return <TimeAgo date={this.props.last_active} />;
+  }
+
+  render(): Rendered {
+    return (
+      <Row style={{borderTop: '1px solid #ccc'}}>
+        <Col md={2}>{this.props.first_name}</Col>
+        <Col md={2}>{this.props.last_name}</Col>
+        <Col md={3}>{this.props.email_address}</Col>
+        <Col md={2}>Created: {this.render_created()}</Col>
+        <Col md={3}>Last active: {this.render_last_active()})</Col>
+      </Row>
+    );
   }
 }
