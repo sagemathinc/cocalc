452617134ae2465c54e7562fe837d5125d417d7e
Harald Schilly <hsy@sagemath.com>
2018-07-02 14:13:39 +0000

    webapp/latex/rnw: parsing knitr errors

--- src/smc-webapp/frame-editors/latex-editor/knitr.ts
+++ src/smc-webapp/frame-editors/latex-editor/knitr.ts
@@ -31,6 +31,29 @@ export async function knitr(
   });
 }
 
+/**
+Knitr error example:
+
+Testing a few cases, it looks like we should report everything after the line "Error in..."
+In case of multiple errors, only the first error is reported.
+There is not much sense with parsing the lines, because usually they aren't reported.
+Warnings are always below and start with a line ending in "Warning message:".
+
+Loading required package: knitr
+processing file: test2.rnw
+Error in parse(text = code, keep.source = FALSE) :
+  <text>:1:29: unexpected symbol
+1: x <- c(2,3,4,5,1,2,3,3,3,3,4a
+                                ^
+Quitting from lines 26-101 (test2.rnw)
+Error in paste(x, collapse = "+") : object 'x' not found
+Calls: knit ... inline_exec -> hook_eval -> withVisible -> eval -> eval -> paste
+In addition: Warning message:
+In highr::hilight(x, format, prompt = options$prompt, markup = opts$markup) :
+  the syntax of the source code is invalid; the fallback mode is used
+Execution halted
+**/
+
 export function knitr_errors(output: BuildLog): ProcessedLatexLog {
   let i = 0;
 
@@ -40,25 +63,43 @@ export function knitr_errors(output: BuildLog): ProcessedLatexLog {
   const typesetting: Error[] = [];
 
   let file: string = "";
+  let err: Error | undefined = undefined;
+
+  const warnmsg = "Warning message:";
 
   for (let line of output.stderr.split("\n")) {
     console.log(`line ${i}: ${line}`);
     if (line.search("Error") == 0) {
-      let e: Error = {
+      err = {
         line: null,
-        file: `./${file}`,
+        file: `${file}`,
         level: "error",
-        message: "ERROR PROC",
-        content: "test error",
+        message: line,
+        content: "",
         raw: ""
       };
-      file = "";
-      errors.push(e);
-      all.push(e);
+      errors.push(err);
+      all.push(err);
+      continue;
+    }
+    if (line.substring(line.length - warnmsg.length) == warnmsg) {
+      err = {
+        line: null,
+        file: `${file}`,
+        level: "warning",
+        message: line,
+        content: "",
+        raw: ""
+      };
+      warnings.push(err);
+      all.push(err);
       continue;
     }
     if (line.search("processing file:") == 0) {
       file = line.substring("processing file:".length).trim();
+    }
+    if (err != undefined) {
+      err.content += `${line}\n`;
     }
     i += 1;
   }
