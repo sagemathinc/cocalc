9da781cdad85ad23e69529e3e794076b767bcbd9
John Jeng <j3@sagemath.com>
2018-07-05 14:57:23 -0700

    Replace styles.coffee

---
+++ src/smc-webapp/course/styles.ts
@@ -0,0 +1,32 @@
+const misc = require("smc-util/misc");
+const { types } = misc;
+
+export const entry_style = {
+  paddingTop: "5px",
+  paddingBottom: "5px"
+};
+
+export const selected_entry = misc.merge(
+  {
+    border: "1px solid #aaa",
+    boxShadow: "5px 5px 5px #999",
+    borderRadius: "3px",
+    marginBottom: "10px"
+  },
+  exports.entry_style
+);
+
+export const note = {
+  borderTop: "3px solid #aaa",
+  marginTop: "10px",
+  paddingTop: "5px"
+};
+
+export function show_hide_deleted(opts) {
+  types(opts, { needs_margin: types.bool.isRequired });
+
+  return {
+    marginTop: opts.needs_margin ? "15px" : "0px",
+    float: "right"
+  };
+}
