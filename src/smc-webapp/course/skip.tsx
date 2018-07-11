acaeb73cc706630363234b217edfbcec420ea8e7
John Jeng <j3@sagemath.com>
2018-07-10 18:06:44 -0700

    Remove typescript errors

--- src/smc-webapp/course/skip.tsx
+++ src/smc-webapp/course/skip.tsx
@@ -8,50 +8,76 @@
 Skip assigning or collecting an assignment, so next step can be attempted.
 */
 
-import { React, rclass, rtypes } from '../app-framework';
+import { React, Component } from "../app-framework";
+import { CourseActions } from "./actions";
+import { AssignmentRecord } from "./store";
 
-import { Icon, Space, Tip } from '../r_misc';
-import { Button } from 'react-bootstrap';
+const { Icon, Space, Tip } = require("../r_misc");
+const { Button } = require("react-bootstrap");
 
-export let SkipCopy = rclass({
-    propTypes : {
-        assignment : rtypes.object.isRequired,
-        step       : rtypes.string,
-        actions    : rtypes.object.isRequired
-    },
+interface SkipCopyProps {
+  assignment: AssignmentRecord;
+  step?: string;
+  actions: CourseActions;
+  not_done?: number;
+}
 
-    render_checkbox() {
-        if (this.props.not_done === 0) {
-            return <span style={{fontSize:'12pt'}}>
-                <Icon name='check-circle' />
-                <Space/>
-            </span>;
-        }
-    },
-
-    click() {
-        return this.props.actions.set_skip(this.props.assignment, this.props.step, !this.props.assignment.get(`skip_${this.props.step}`));
-    },
-
-    render() {
-        let icon;
-        let extra = undefined;
-        if (this.props.assignment.get(`skip_${this.props.step}`)) {
-            icon = 'check-square-o';
-            if (__guard__(this.props.assignment.get('peer_grade'), x => x.get('enabled'))) {
-                // don't bother even trying to implement skip and peer grading at once.
-                extra = <span><Space /> (Please disable this or peer grading.)</span>;
-            }
-        } else {
-            icon = 'square-o';
-        }
-        return <Tip placement='left' title="Skip step in workflow" tip="Click this checkbox to enable doing the next step after this step, e.g., you can try to collect assignments that you never explicitly assigned (maybe the students put them in place some other way).">
-            <Button onClick={this.click}>
-                <Icon name={icon} /> Skip {this.props.step} {extra}
-            </Button>
-        </Tip>;
+export class SkipCopy extends Component<SkipCopyProps> {
+  render_checkbox() {
+    if (this.props.not_done === 0) {
+      return (
+        <span style={{ fontSize: "12pt" }}>
+          <Icon name="check-circle" />
+          <Space />
+        </span>
+      );
     }
-});
+  }
+
+  click() {
+    return this.props.actions.set_skip(
+      this.props.assignment,
+      this.props.step,
+      !this.props.assignment.get(`skip_${this.props.step}` as any)
+    );
+  }
+
+  render() {
+    let icon;
+    let extra: any = undefined;
+    if (this.props.assignment.get(`skip_${this.props.step}` as any)) {
+      icon = "check-square-o";
+      if (
+        __guard__(this.props.assignment.get("peer_grade"), x =>
+          x.get("enabled")
+        )
+      ) {
+        // don't bother even trying to implement skip and peer grading at once.
+        extra = (
+          <span>
+            <Space /> (Please disable this or peer grading.)
+          </span>
+        );
+      }
+    } else {
+      icon = "square-o";
+    }
+    return (
+      <Tip
+        placement="left"
+        title="Skip step in workflow"
+        tip="Click this checkbox to enable doing the next step after this step, e.g., you can try to collect assignments that you never explicitly assigned (maybe the students put them in place some other way)."
+      >
+        <Button onClick={this.click}>
+          <Icon name={icon} /> Skip {this.props.step} {extra}
+        </Button>
+      </Tip>
+    );
+  }
+}
+
 function __guard__(value, transform) {
-  return (typeof value !== 'undefined' && value !== null) ? transform(value) : undefined;
+  return typeof value !== "undefined" && value !== null
+    ? transform(value)
+    : undefined;
 }
