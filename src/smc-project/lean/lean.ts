af454ec4de48aef99c3f73c68d8d58e24f28f209
William A. Stein <wstein@sagemath.com>
2018-07-26 09:16:52 +0000

    starting to add lean to projects

---
+++ src/smc-project/lean/lean.ts
@@ -0,0 +1,11 @@
+import * as lean from "lean-client-js-node";
+
+const transport = new lean.ProcessTransport("lean", ".", []);
+const server = new lean.Server(transport);
+server.error.on(err => console.log("error:", err));
+
+server.allMessages.on(allMessages => console.log("messages: ", allMessages.msgs));
+
+server.tasks.on(currentTasks => console.log("tasks: ", currentTasks.tasks));
+
+server.connect();
