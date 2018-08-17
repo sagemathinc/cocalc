06fa608ed782f88463ffe490731594ed35150ef9
Harald Schilly <hsy@sagemath.com>
2018-08-17 16:18:06 +0000

    webapp/project: globally import a typed "$" for jquery

--- src/smc-project/globals.d.ts
+++ src/smc-project/globals.d.ts
@@ -1,3 +1,6 @@
-declare const $: any;
+import * as $ from "jquery"; // picks jQuery from @types
+declare const $ = $;
+// declare const $: any; // old more general approach
 declare const window: any;
 declare const localStorage: any;
+// export { $, window, localStorage }; // didn't work, but maybe it should?
