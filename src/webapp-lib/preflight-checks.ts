b611d3541c5a78d7d4bdf9f3a69b54657ae1efd9
Harald Schilly <hsy@sagemath.com>
2018-08-28 12:49:44 +0000

    static: preflight check can be skipped and some details ...

--- src/webapp-lib/preflight-checks.ts
+++ src/webapp-lib/preflight-checks.ts
@@ -7,15 +7,13 @@ Opera: 55
 Chrome is wrong on that page, I assume we should check for 61 or 62.
 */
 
-/* credits: https://stackoverflow.com/a/38080051/54236 */
-// type TBrowser = "Firefox" | "IE" | "Edge" | "Safari" | "Opera";
-
 interface ISpecs {
   name: string;
   version: number;
 }
 
-const spec = (function(): ISpecs {
+/* credits: https://stackoverflow.com/a/38080051/54236 */
+const get_spec = function(): ISpecs {
   const mstr = /(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i;
   const ua = navigator.userAgent;
   let tem: RegExpMatchArray | null;
@@ -37,26 +35,32 @@ const spec = (function(): ISpecs {
     M.splice(1, 1, tem[1]);
   }
   return { name: M[0], version: parseInt(M[1]) };
-})();
+};
 
-navigator["browserSpecs"] = spec;
+const SKIP_TOKEN = "skip_preflight";
 
-console.log("browser spec:", spec); //Object { name: "Firefox", version: 42 }
+function preflight_check(): void {
+  const spec = get_spec();
+  navigator["browserSpecs"] = spec;
 
-const oldFF = spec.name === "Firefox" && spec.version < 54;
-const oldIE = spec.name === "MSIE" || spec.name === "IE"; // all of them are a problem
-const oldEdge = spec.name === "Edge" && spec.version < 14;
-const oldSafari = spec.name === "Safari" && spec.version < 10;
-const oldOpera = spec.name === "Opera" && spec.version < 55;
-const oldChrome = spec.name === "Chrome" && spec.version < 62;
+  console.log("browser spec:", spec); //Object { name: "Firefox", version: 42 }
 
-if (oldFF || oldIE || oldEdge || oldSafari || oldOpera || oldChrome) {
-  const banner = document.getElementById("smc-startup-banner");
-  if (banner != null && banner.parentNode != null) {
-    banner.parentNode.removeChild(banner);
-  }
+  const oldFF = spec.name === "Firefox" && spec.version < 54;
+  const oldIE = spec.name === "MSIE" || spec.name === "IE"; // all of them are a problem
+  const oldEdge = spec.name === "Edge" && spec.version < 14;
+  const oldSafari = spec.name === "Safari" && spec.version < 10;
+  const oldOpera = spec.name === "Opera" && spec.version < 55;
+  const oldChrome = spec.name === "Chrome" && spec.version < 62;
 
-  const msg = `<div style='text-align:center'>
+  if (oldFF || oldIE || oldEdge || oldSafari || oldOpera || oldChrome) {
+    for (let eid of ["smc-startup-banner-status", "smc-startup-banner"]) {
+      const banner = document.getElementById(eid);
+      if (banner != null && banner.parentNode != null) {
+        banner.parentNode.removeChild(banner);
+      }
+    }
+
+    const msg = `<div style='text-align:center'>
       <h1 style="color:red;font-size:300%">&#9888;</h1>
       <h2>Your browser ${spec.name} of version ${spec.version} is too old.</h2>
       <div>
@@ -64,10 +68,18 @@ if (oldFF || oldIE || oldEdge || oldSafari || oldOpera || oldChrome) {
           <p>Learn more about our
             <a href="https://github.com/sagemathinc/cocalc/wiki/BrowserRequirements">browser requirements</a>.
           </p>
+          <p style="font-weight:bold; font-size: 115%">
+            <a href="./app?${SKIP_TOKEN}">Continue anyways ...</a>
+          </p>
       </div>
     </div>`;
-  document.open();
-  document.write(msg);
-  document.close();
-  window.stop();
+    document.open();
+    document.write(msg);
+    document.close();
+    window.stop();
+  }
+}
+
+if (window.location.search.indexOf(SKIP_TOKEN) < 0) {
+  preflight_check();
 }
