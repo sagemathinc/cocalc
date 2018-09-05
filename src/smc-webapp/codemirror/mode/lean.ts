318e17d3c9b58ba9d91b45637c78d64fdcbb1e7a
William A. Stein <wstein@sagemath.com>
2018-09-05 17:57:47 +0000

    some lean syntax improvements (hsy wrote this)

--- src/smc-webapp/codemirror/mode/lean.ts
+++ src/smc-webapp/codemirror/mode/lean.ts
@@ -6,6 +6,12 @@ This is just a first tiny quick step.  To finish this:
 - Look at https://codemirror.net/demo/simplemode.html for how this works.
 - Put everything from https://github.com/leanprover/vscode-lean/blob/master/syntaxes/lean.json in here.
 
+NOTE(hsy): I think the alternative that works with safari is this negative look ahead
+
+/((?!\.).{1}|^)\b(import|prelude|theory|definition|def|abbreviation|instance|renaming|hiding|exposing|parameter|parameters|begin|constant|constants|lemma|variable|variables|theorem|example|open|axiom|inductive|coinductive|with|structure|universe|universes|alias|precedence|reserve|postfix|prefix|infix|infixl|infixr|notation|end|using|namespace|section|local|set_option|extends|include|omit|class|classes|instances|raw|run_cmd)\b/gm
+
+playgroud: https://regex101.com/r/lop9Se/1
+
 */
 
 (CodeMirror as any).defineSimpleMode("lean", {
@@ -36,5 +42,17 @@ This is just a first tiny quick step.  To finish this:
     { regex: /begin/, indent: true },
     { regex: /end/, dedent: true },
     { regex: /[a-z$][\w$]*/, token: "variable" }
-  ]
+  ],
+  string: [
+    {regex: /"/, token: "string", next: "start"},
+    {regex: /(?:[^\\"]|\\(?:.|$))*/, token: "string"}
+  ],
+  comment: [{ regex: /\(-.*-\)/, token: "comment", next: "start" }],
+  meta: {
+    dontIndentStates: ["comment"],
+    /* electricInput: /^\s*\}$/, */
+    blockCommentStart: "(-",
+    blockCommentEnd: "-)",
+    lineComment: "--"
+  }
 });
