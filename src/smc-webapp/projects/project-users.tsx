43e9c08875fdc377fe4ce0c34ee9387243018738
John Jeng <john.jeng5@gmail.com>
2018-10-29 15:04:18 +0000

    Fix errors

--- src/smc-webapp/projects/project-users.tsx
+++ src/smc-webapp/projects/project-users.tsx
@@ -13,83 +13,72 @@
 /*
 Showing list of users of a project
 */
-
-const { Component, React, redux, rtypes, rclass } = require("../app-framework");
+import { Component, React, redux, rclass } from "../app-framework";
+import { UserMap } from "../todo-types";
 
 const { User } = require("../users");
-
 const { Loading, r_join } = require("../r_misc");
 
-class ProjectUsers extends Component {
-  static initClass() {
-    this.prototype.displayName = "ProjectUsers";
+interface ReactProps {
+  project: any;
+  none: React.ComponentType<any>;
+}
 
-    this.prototype.reduxProps = {
-      users: {
-        user_map: rtypes.immutable
-      },
-      account: {
-        account_id: rtypes.string
+interface ReduxProps {
+  user_map: UserMap;
+  account_id: string;
+}
+
+export const ProjectUsers = rclass<ReactProps>(
+  class ProjectUsers extends Component<ReactProps & ReduxProps> {
+    render() {
+      let left;
+      let account_id;
+      if (this.props.user_map == null) {
+        return <Loading />;
       }
-    };
-  }
+      const users =
+        (left = __guard__(this.props.project.get("users"), x =>
+          x.keySeq().toArray()
+        )) != null
+          ? left
+          : [];
 
-  propTypes() {
-    return {
-      project: rtypes.immutable.Map.isRequired,
-      none: rtypes.object // optional component to display if there are no other users
-    };
-  }
-
-  render() {
-    let left;
-    let account_id;
-    if (this.props.user_map == null) {
-      return <Loading />;
-    }
-    const users =
-      (left = __guard__(this.props.project.get("users"), x =>
-        x.keySeq().toArray()
-      )) != null
-        ? left
-        : [];
-    const other = (() => {
-      const result = [];
+      const other: any[] = [];
       for (account_id of Array.from(users)) {
         if (account_id !== this.props.account_id) {
-          result.push({ account_id });
+          other.push({ account_id });
         }
       }
-      return result;
-    })();
-    redux
-      .getStore("projects")
-      .sort_by_activity(other, this.props.project.get("project_id"));
-    const v = [];
-    for (
-      let i = 0, end = other.length, asc = 0 <= end;
-      asc ? i < end : i > end;
-      asc ? i++ : i--
-    ) {
-      v.push(
-        <User
-          key={other[i].account_id}
-          last_active={other[i].last_active}
-          account_id={other[i].account_id}
-          user_map={this.props.user_map}
-        />
-      );
-    }
-    if (v.length > 0) {
-      return r_join(v);
-    } else if (this.props.none) {
-      return this.props.none;
-    } else {
-      return <span />;
+
+      redux
+        .getStore("projects")
+        .sort_by_activity(other, this.props.project.get("project_id"));
+      const v: any = [];
+      for (
+        let i = 0, end = other.length, asc = 0 <= end;
+        asc ? i < end : i > end;
+        asc ? i++ : i--
+      ) {
+        v.push(
+          <User
+            key={other[i].account_id}
+            last_active={other[i].last_active}
+            account_id={other[i].account_id}
+            user_map={this.props.user_map}
+          />
+        );
+      }
+      if (v.length > 0) {
+        return r_join(v);
+      } else if (this.props.none) {
+        return this.props.none;
+      } else {
+        return <span />;
+      }
     }
   }
-}
-ProjectUsers.initClass();
+);
 
 function __guard__(value, transform) {
   return typeof value !== "undefined" && value !== null
