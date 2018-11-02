93941d7df5a9557166b92b144b8ad762ea49218e
John Jeng <john.jeng5@gmail.com>
2018-11-02 15:07:03 +0000

    Decaffeinate SignUp and Passports

---
+++ src/smc-webapp/passports.tsx
@@ -0,0 +1,115 @@
+import * as React from "react";
+
+const { Icon, Tip } = require("./r_misc");
+/*
+ * decaffeinate suggestions:
+ * DS101: Remove unnecessary use of Array.from
+ * DS102: Remove unnecessary code created because of implicit returns
+ * DS104: Avoid inline assignments
+ * DS206: Consider reworking classes to avoid initClass
+ * DS207: Consider shorter variations of null checks
+ * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
+ */
+
+export class Passports extends React.Component {
+  static initClass() {
+    this.prototype.displayName = "Passports";
+
+    this.prototype.propTypes = {
+      strategies: rtypes.immutable.List,
+      get_api_key: rtypes.string,
+      small_size: rtypes.bool,
+      no_header: rtypes.bool,
+      style: rtypes.object
+    };
+
+    this.prototype.styles = {
+      facebook: {
+        backgroundColor: "#395996",
+        color: "white"
+      },
+      google: {
+        backgroundColor: "#DC4839",
+        color: "white"
+      },
+      twitter: {
+        backgroundColor: "#55ACEE",
+        color: "white"
+      },
+      github: {
+        backgroundColor: "white",
+        color: "black"
+      }
+    };
+  }
+
+  render_strategy(name) {
+    let size;
+    if (name === "email") {
+      return;
+    }
+    let url = `${window.app_base_url}/auth/${name}`;
+    if (this.props.get_api_key) {
+      url += `?get_api_key=${this.props.get_api_key}`;
+    }
+    if (this.props.small_size) {
+      size = undefined;
+    } else {
+      size = "2x";
+    }
+    const style = misc.copy(this.styles[name]);
+    style.display = "inline-block";
+    style.padding = "6px";
+    style.borderRadius = "50%";
+    style.width = "50px";
+    style.height = "50px";
+    style.marginRight = "10px";
+    style.textAlign = "center";
+    const cname = misc.capitalize(name);
+    const title = (
+      <span>
+        <Icon name={name} /> {cname}
+      </span>
+    );
+    return (
+      <a href={url} key={name} style={{ fontSize: "28px" }}>
+        <Tip
+          placement="bottom"
+          title={title}
+          tip={`Use ${cname} to sign into your CoCalc account instead of an email address and password.`}
+        >
+          <Icon name={name} style={style} />
+        </Tip>
+      </a>
+    );
+  }
+
+  render_heading() {
+    if (this.props.no_heading) {
+      return;
+    }
+    return <h3 style={{ marginTop: 0 }}>Connect with</h3>;
+  }
+
+  render() {
+    let left;
+    const strategies =
+      (left =
+        this.props.strategies != null
+          ? this.props.strategies.toJS()
+          : undefined) != null
+        ? left
+        : [];
+    //# strategies = ['facebook', 'google', 'twitter', 'github']   # for testing.
+    return (
+      <div style={this.props.style}>
+        {this.render_heading()}
+        <div>
+          {Array.from(strategies).map(name => this.render_strategy(name))}
+        </div>
+        <hr style={{ marginTop: 10, marginBottom: 10 }} />
+      </div>
+    );
+  }
+}
+Passports.initClass();
---
+++ src/smc-webapp/sign-up.tsx
@@ -0,0 +1,215 @@
+import * as React from "react";
+import { Passports } from "./passports";
+
+const { Icon, Loading } = require("./r_misc");
+const {
+  HelpEmailLink,
+  TermsOfService,
+  AccountCreationEmailInstructions
+} = require("./customize");
+const {
+  Button,
+  CheckBox,
+  FormControl,
+  FormGroup,
+  Well
+} = require("react-bootstrap");
+
+const ERROR_STYLE: React.CSSProperties = {
+  color: "white",
+  fontSize: "125%",
+  backgroundColor: "red",
+  border: "1px solid lightgray",
+  padding: "15px",
+  marginTop: "5px",
+  marginBottom: "5px"
+};
+
+/*
+ * decaffeinate suggestions:
+ * DS102: Remove unnecessary code created because of implicit returns
+ * DS206: Consider reworking classes to avoid initClass
+ * DS207: Consider shorter variations of null checks
+ * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
+ */
+export class SignUp extends React.Component {
+  static initClass() {
+    this.prototype.displayName = "SignUp";
+
+    this.prototype.propTypes = {
+      strategies: rtypes.immutable.List,
+      get_api_key: rtypes.string,
+      sign_up_error: rtypes.immutable.Map,
+      token: rtypes.bool,
+      has_account: rtypes.bool,
+      signing_up: rtypes.bool,
+      style: rtypes.object,
+      has_remember_me: rtypes.bool
+    };
+  }
+
+  getInitialState() {
+    return {
+      terms_checkbox: false,
+      first_name: "",
+      last_name: "",
+      email: "",
+      password: "",
+      user_token: ""
+    };
+  }
+
+  make_account(e) {
+    e.preventDefault();
+    return this.actions("account").create_account(
+      this.state.first_name,
+      this.state.last_name,
+      this.state.email,
+      this.state.password,
+      this.state.user_token
+    );
+  }
+
+  render_error(field) {
+    const err =
+      this.props.sign_up_error != null
+        ? this.props.sign_up_error.get(field)
+        : undefined;
+    if (err != null) {
+      return <div style={ERROR_STYLE}>{err}</div>;
+    }
+  }
+
+  render_passports() {
+    if (this.props.strategies == null) {
+      return <Loading />;
+    }
+    if (this.props.strategies.size > 1) {
+      return (
+        <div>
+          <Passports
+            strategies={this.props.strategies}
+            get_api_key={this.props.get_api_key}
+            style={{ textAlign: "center" }}
+          />
+          Or sign up via email
+          <br />
+        </div>
+      );
+    }
+  }
+
+  render_token_input() {
+    if (this.props.token) {
+      return (
+        <FormGroup>
+          <FormControl
+            type={"text"}
+            placeholder={"Enter the secret token"}
+            onChange={e => this.setState({ user_token: e.target.value })}
+          />
+        </FormGroup>
+      );
+    }
+  }
+
+  render_terms() {
+    return (
+      <FormGroup style={{ fontSize: "12pt", margin: "20px" }}>
+        <Checkbox
+          onChange={e => this.setState({ terms_checkbox: e.target.checked })}
+        >
+          <TermsOfService />
+        </Checkbox>
+      </FormGroup>
+    );
+  }
+
+  render_creation_form() {
+    return (
+      <div>
+        {this.render_token_input()}
+        {this.render_error("token")}
+        {this.render_error("generic")}
+        {this.render_error("account_creation_failed")}
+        {this.state.terms_checkbox ? this.render_passports() : undefined}
+        <form
+          style={{ marginTop: 20, marginBottom: 20 }}
+          onSubmit={this.make_account}
+        >
+          <FormGroup>
+            {this.render_error("first_name")}
+            <FormControl
+              ref="first_name"
+              type="text"
+              autoFocus={false}
+              placeholder="First name"
+              onChange={e => this.setState({ first_name: e.target.value })}
+              maxLength={120}
+            />
+          </FormGroup>
+          <FormGroup>
+            {this.render_error("last_name")}
+            <FormControl
+              ref="last_name"
+              type="text"
+              autoFocus={false}
+              placeholder="Last name"
+              onChange={e => this.setState({ last_name: e.target.value })}
+              maxLength={120}
+            />
+          </FormGroup>
+          <FormGroup>
+            {this.render_error("email_address")}
+            <FormControl
+              ref="email_address"
+              type="email"
+              placeholder="Email address"
+              maxLength={254}
+              onChange={e => this.setState({ email: e.target.value })}
+            />
+          </FormGroup>
+          <FormGroup>
+            {this.render_error("password")}
+            <FormControl
+              ref="password"
+              type="password"
+              placeholder="Choose a password"
+              maxLength={64}
+              onChange={e => this.setState({ password: e.target.value })}
+            />
+          </FormGroup>
+          <Button
+            style={{ marginBottom: UNIT, marginTop: UNIT }}
+            disabled={this.props.signing_up}
+            bsStyle={"success"}
+            bsSize={"large"}
+            type={"submit"}
+            block
+          >
+            {this.props.signing_up ? <Icon name="spinner" spin /> : undefined}{" "}
+            Sign Up!
+          </Button>
+        </form>
+      </div>
+    );
+  }
+
+  render() {
+    const well_style = {
+      marginTop: "10px",
+      borderColor: COLORS.LANDING.LOGIN_BAR_BG
+    };
+    return (
+      <Well style={well_style}>
+        <AccountCreationEmailInstructions />
+        {this.render_terms()}
+        {this.state.terms_checkbox ? this.render_creation_form() : undefined}
+        <div style={{ textAlign: "center" }}>
+          Email <HelpEmailLink /> if you need help.
+        </div>
+      </Well>
+    );
+  }
+}
+SignUp.initClass();
