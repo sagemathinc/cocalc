bd4da7b9e9d6d7021ea60886ad72ad5e5916963b
John Jeng <john.jeng5@gmail.com>
2018-11-13 17:10:26 +0000

    Add basic click simulation test

--- src/smc-webapp/r_misc/__tests__/selector-input.test.tsx
+++ src/smc-webapp/r_misc/__tests__/selector-input.test.tsx
@@ -9,7 +9,7 @@ describe("Smoke test:", () => {
     expect(tree).toMatchSnapshot();
   });
 
-  test("it renders a list of {value, display} objects as children of <option></option>", () => {
+  test("it renders a list of {value, display} objects", () => {
     const display_objects = [
       { value: "value_1", display: <div>Susan</div> },
       { value: "value_2", display: <div>Harry</div> }
@@ -33,14 +33,16 @@ describe("Smoke test:", () => {
 });
 
 describe("Interactions:", () => {
-  test("it calls on_change with the clicked option's value (this test uses Simulate)", () => {
+  // Not the most robust testing, but propagation isn't well supported.
+  // Might as well be explicit
+  test("it calls on_change using e.target.value", () => {
     const names = ["Susan", "Harry", "Steve"];
     const selection_mock = jest.fn();
 
     let tree = shallow(
       <SelectorInput options={names} on_change={selection_mock} />
     );
-    tree.children().simulate("change", { target: { value: "Susan" }});
+    tree.children().simulate("change", { target: { value: "Susan" } });
     expect(selection_mock.mock.calls[0][0]).toBe("Susan");
   });
 });
