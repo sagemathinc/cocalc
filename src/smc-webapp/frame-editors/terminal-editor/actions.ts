587c07d1024c377de74590b00f28c2eb6648c57a
Harald Schilly <hsy@sagemath.com>
2018-12-06 14:54:20 +0000

    webapp: fix terminal help url and use popup-blocker aware tab opener

--- src/smc-webapp/frame-editors/terminal-editor/actions.ts
+++ src/smc-webapp/frame-editors/terminal-editor/actions.ts
@@ -3,6 +3,9 @@ Terminal Editor Actions
 */
 import { Actions as CodeEditorActions } from "../code-editor/actions";
 import { FrameTree } from "../frame-tree/types";
+const { open_new_tab } = require("smc-webapp/misc_page");
+
+const HELP_URL = "https://doc.cocalc.com/terminal.html";
 
 export class Actions extends CodeEditorActions {
   // no need to open any syncstring for terminals -- they don't use database sync.
@@ -12,5 +15,9 @@ export class Actions extends CodeEditorActions {
 
   _raw_default_frame_tree(): FrameTree {
     return { type: "terminal" };
+  }
+
+  help(): void {
+    open_new_tab(HELP_URL);
   }
 }
