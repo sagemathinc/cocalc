be25965ef54daa02824caf6a24419c0c1753ef4e
William Stein <wstein@sagemath.com>
2018-12-29 19:08:49 +0000

    syncts: test changefeed

--- src/smc-util/sync/table/changefeed.ts
+++ src/smc-util/sync/table/changefeed.ts
@@ -13,21 +13,32 @@ export class Changefeed extends EventEmitter {
   private id: string;
   private options: any;
 
-  constructor({ do_query, query_cancel, options, query, table }) {
+  constructor({
+    do_query,
+    query_cancel,
+    options,
+    query,
+    table
+  }: {
+    do_query: Function;
+    query_cancel: Function;
+    options: any;
+    table: string;
+    query: any;
+  }) {
     super();
     this.do_query = do_query;
     this.query_cancel = query_cancel;
     this.query = query;
     this.options = options;
     this.table = table;
-    this.handle_update = this.handle_update.bind(this);
   }
 
   // Query for state of the table, connects to the
   // changefeed, and return the initial state
   // of the table.  Throws an exception if anything
   // goes wrong.
-  async connect(): Promise<any> {
+  public async connect(): Promise<any> {
     if (this.state != "disconnected") {
       throw Error(
         `can only connect if state is 'disconnected' but it is ${this.state}`
@@ -35,7 +46,7 @@ export class Changefeed extends EventEmitter {
     }
     this.state = "connecting";
     const resp = await callback(this.run_the_query.bind(this));
-    if (this.state === "closed" as State) {
+    if (this.state === ("closed" as State)) {
       throw Error("closed");
     }
     if (resp.event === "query_cancel") {
@@ -50,7 +61,6 @@ export class Changefeed extends EventEmitter {
     return resp.query[this.table];
   }
 
-  // This is overloaded in the ChangefeedStandby derived class.
   private run_the_query(cb: Function): void {
     // This query_function gets called first on the
     // initial query, then repeatedly with each changefeed
@@ -68,7 +78,7 @@ export class Changefeed extends EventEmitter {
         // This calls cb the first time, and  calls
         // handle_changefeed_update ever after.
         f(err, resp);
-        f = this.handle_update;
+        f = this.handle_update.bind(this);
       }
     });
   }
@@ -78,11 +88,22 @@ export class Changefeed extends EventEmitter {
       this.close();
       return;
     }
-    if (err || (resp != null && resp.event === "query_cancel")) {
+    if (resp == null && err == null) {
+      err = "resp must not be null for non-error";
+    }
+    if (err || resp.event === "query_cancel") {
       this.close();
       return;
     }
-    this.emit("update", { new_val: resp.new_val, old_val: resp.old_val });
+    // Return just the new_val/old_val part of resp.
+    const x: { new_val?: any; old_val?: any } = {};
+    if (resp.new_val) {
+      x.new_val = resp.new_val;
+    }
+    if (resp.old_val) {
+      x.new_val = resp.old_val;
+    }
+    this.emit("update", x);
   }
 
   public close(): void {
@@ -94,6 +115,10 @@ export class Changefeed extends EventEmitter {
     }
     this.emit("close");
     this.removeAllListeners();
+  }
+
+  public get_state(): string {
+    return this.state;
   }
 }
 
---
+++ src/smc-util/sync/table/test/changefeed.test.ts
@@ -0,0 +1,100 @@
+import { Changefeed } from "../changefeed";
+
+describe("first test of the public API of the Changefeed class", () => {
+  const options = [];
+  const query = {
+    system_notifications: { id: null, time: null, text: null, priority: null }
+  };
+  const table = "system_notifications";
+
+  let got: any = {};
+  const init_val = [
+    { id: "0", time: new Date(0), text: "foo", priority: "low" }
+  ];
+  function do_query(opts) {
+    got.do_query = opts;
+    opts.cb(undefined, { query: { [table]: init_val } });
+  }
+  function query_cancel(opts) {
+    got.query_cancel = opts;
+    opts.cb();
+  }
+
+  let changefeed: Changefeed;
+  const opts = {
+    do_query,
+    query_cancel,
+    options,
+    query,
+    table
+  };
+  it("creates the changefeed", () => {
+    changefeed = new Changefeed(opts);
+    expect(changefeed.get_state()).toBe("disconnected");
+  });
+
+  it("initializes the changefeed", async () => {
+    const init = await changefeed.connect();
+    expect(changefeed.get_state()).toBe("connected");
+    expect(init).toBe(init_val);
+  });
+
+  it("causes an update", done => {
+    changefeed.on("update", x => {
+      expect(x).toEqual({ new_val: { text: "bar" } });
+      done();
+    });
+    got.do_query.cb(undefined, { new_val: { text: "bar" } });
+  });
+
+  it("ends the changefeed via query_cancel event", done => {
+    changefeed.on("close", () => {
+      expect(changefeed.get_state()).toBe("closed");
+      done();
+    });
+    got.do_query.cb(undefined, { event: "query_cancel" });
+  });
+
+  it("creates changefeed again", async () => {
+    changefeed = new Changefeed(opts);
+    await changefeed.connect();
+    expect(changefeed.get_state()).toBe("connected");
+  });
+
+  it("ends the changefeed via an error", done => {
+    changefeed.on("close", () => {
+      expect(changefeed.get_state()).toBe("closed");
+      done();
+    });
+    got.do_query.cb("fatal");
+  });
+
+  it("creates changefeed again", async () => {
+    changefeed = new Changefeed(opts);
+    await changefeed.connect();
+    expect(changefeed.get_state()).toBe("connected");
+  });
+
+  it("ends the changefeed via an error", done => {
+    changefeed.on("close", () => {
+      expect(changefeed.get_state()).toBe("closed");
+      done();
+    });
+    got.do_query.cb("fatal");
+  });
+
+  it("creates changefeed again", async () => {
+    changefeed = new Changefeed(opts);
+    await changefeed.connect();
+    expect(changefeed.get_state()).toBe("connected");
+  });
+
+  it("ends the changefeed by explicitly calling close", done => {
+    changefeed.on("close", () => {
+      expect(changefeed.get_state()).toBe("closed");
+      done();
+    });
+    changefeed.close();
+  });
+
+});
