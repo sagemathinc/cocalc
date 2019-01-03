1921c55ba1a831c932bc76fbfb551886583e17d0
William Stein <wstein@sagemath.com>
2019-01-03 22:03:09 +0000

    add more tests to sorted patch list.

--- src/smc-util/sync/editor/generic/test/sorted-patch-list.test.ts
+++ src/smc-util/sync/editor/generic/test/sorted-patch-list.test.ts
@@ -6,7 +6,7 @@ import { SortedPatchList } from "../sorted-patch-list";
 import { make_patch } from "../util";
 import { StringDocument } from "../../string/doc";
 
-function from_str(s : string) : StringDocument {
+function from_str(s: string): StringDocument {
   return new StringDocument(s);
 }
 
@@ -81,7 +81,7 @@ describe("Test empty sorted patch list -- call all public methods", () => {
   });
 });
 
-describe("Test non-empty sorted patch list -- call all public methods", () => {
+describe("Test sorted patch list with one patch", () => {
   let patches: SortedPatchList;
 
   it("creates a sorted patch list", () => {
@@ -110,7 +110,7 @@ describe("Test non-empty sorted patch list -- call all public methods", () => {
     expect(patches.value_no_cache().to_str()).toBe("CoCalc");
   });
 
-  it("gets id of user who made edit at time (error since no edits)", () => {
+  it("gets id of user who made edit at time", () => {
     expect(patches.user_id(patch.time)).toBe(patch.user_id);
   });
 
@@ -133,4 +133,160 @@ describe("Test non-empty sorted patch list -- call all public methods", () => {
   it("number of patch", () => {
     expect(patches.count()).toBe(1);
   });
+});
+
+describe("Test sorted patch list with several patches", () => {
+  let patches: SortedPatchList;
+
+  it("creates a sorted patch list", () => {
+    patches = new SortedPatchList(from_str);
+    expect(patches.value().to_str()).toBe("");
+  });
+
+  const v = [
+    {
+      time: new Date("2019-01-03T20:33:47.360Z"),
+      patch: make_patch("", "CoCalc"),
+      user_id: 0,
+      sent: new Date("2019-01-03T20:33:47.40Z")
+    },
+    {
+      time: new Date("2019-01-03T20:33:50Z"),
+      patch: make_patch("CoCalc", "CoCalc -- Collaborative Calculation"),
+      user_id: 1,
+      sent: new Date("2019-01-03T20:34")
+    },
+    {
+      time: new Date("2019-01-03T20:34:50Z"),
+      patch: make_patch(
+        "CoCalc -- Collaborative Calculation",
+        "CoCalc -- Collaborative Calculation in the Cloud"
+      ),
+      user_id: 0
+    }
+  ];
+
+  it("adds the patches", () => {
+    patches.add(v);
+  });
+
+  it("gets the current value", () => {
+    expect(patches.value().to_str()).toBe(
+      "CoCalc -- Collaborative Calculation in the Cloud"
+    );
+  });
+
+  it("gets the current value again (will use a cache)", () => {
+    expect(patches.value().to_str()).toBe(
+      "CoCalc -- Collaborative Calculation in the Cloud"
+    );
+  });
+
+  it("gets the current value without cache", () => {
+    expect(patches.value_no_cache().to_str()).toBe(
+      "CoCalc -- Collaborative Calculation in the Cloud"
+    );
+  });
+
+  it("gets id of user who made edit at time", () => {
+    for (let patch of v) {
+      expect(patches.user_id(patch.time)).toBe(patch.user_id);
+    }
+  });
+
+  it("gets time sent of patches", () => {
+    for (let patch of v) {
+      expect(patches.user_id(patch.time)).toEqual(patch.user_id);
+    }
+  });
+
+  it("gets patch at time", () => {
+    for (let patch of v) {
+      expect(patches.patch(patch.time)).toEqual(patch);
+    }
+  });
+
+  it("list of versions", () => {
+    expect(patches.versions()).toEqual(v.map(x => x.time));
+  });
+
+  it("most recent patch", () => {
+    expect(patches.newest_patch_time()).toEqual(v[2].time);
+  });
+
+  it("number of patch", () => {
+    expect(patches.count()).toBe(v.length);
+  });
+});
+
+describe("Test inserting missing patches (thus changing history)", () => {
+  let patches: SortedPatchList;
+
+  it("creates a sorted patch list", () => {
+    patches = new SortedPatchList(from_str);
+    expect(patches.value().to_str()).toBe("");
+  });
+
+  const v = [
+    {
+      time: new Date("2019-01-03T20:33:47.360Z"),
+      patch: make_patch("", "SageMathCloud -- "),
+      user_id: 0,
+      sent: new Date("2019-01-03T20:33:47.40Z")
+    },
+    {
+      time: new Date("2019-01-03T20:33:50Z"),
+      patch: make_patch("SageMathCloud -- ", "CoCalc -- "),
+      user_id: 1,
+      sent: new Date("2019-01-03T20:34")
+    },
+    {
+      time: new Date("2019-01-03T20:34:50Z"),
+      patch: make_patch(
+        "SageMathCloud -- ",
+        "SageMathCloud -- Collaborative Calculation in the Cloud"
+      ),
+      user_id: 0,
+      sent: new Date("2019-01-03T20:35")
+    }
+  ];
+
+  it("adds some patches", () => {
+    patches.add([v[0], v[2]]);
+  });
+
+  it("gets the current value", () => {
+    expect(patches.value().to_str()).toBe(
+      "SageMathCloud -- Collaborative Calculation in the Cloud"
+    );
+  });
+
+  it("number of patch", () => {
+    expect(patches.count()).toBe(2);
+  });
+
+  it("adds other user patch (back in time)", () => {
+    patches.add([v[1]]);
+  });
+
+  it("number of patch", () => {
+    expect(patches.count()).toBe(3);
+  });
+
+  it("gets the current value again, which changes", () => {
+    expect(patches.value().to_str()).toBe(
+      "CoCalc -- Collaborative Calculation in the Cloud"
+    );
+  });
+
+  it("gets the current value without cache as double check", () => {
+    expect(patches.value_no_cache().to_str()).toBe(
+      "CoCalc -- Collaborative Calculation in the Cloud"
+    );
+  });
+
+  it("list of versions", () => {
+    expect(patches.versions()).toEqual(v.map(x => x.time));
+  });
+
 });
