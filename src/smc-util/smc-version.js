sagemathinc/cocalc

commit 576382b1729f4b3f34d9401026d000ec907bc152

Fix default allow_collabs value

src/smc-webapp/course/store.ts
@@ -238,15 +238,11 @@ export class CourseStore extends Store<CourseState> {
   }
 
   get_pay() {
-    let left;
-    return (left = this.get("settings").get("pay")) != null ? left : "";
+    return !!this.get("settings").get("pay");
   }
 
   get_allow_collabs() {
-    let left;
-    return (left = this.get("settings").get("allow_collabs")) != null
-      ? left
-      : true;
+    return !!this.get("settings").get("allow_collabs");
   }
 
   get_email_invite() {

src/smc-webapp/course/main.tsx
@@ -115,7 +115,7 @@ const init_redux = function(course_filename, redux: AppRedux, course_project_id)
     expanded_skip_gradings: Set(),
     active_student_sort: { column_name: "last_name", is_descending: false },
     active_assignment_sort: { column_name: "due_date", is_descending: false },
-    settings: { allow_collabs: true },
+    settings: { allow_collabs: false },
     action_all_projects_state: "any"
   };
 

commit 07331527de27b0f1c1ce9f18118919d1b0ac612c

revert bug introduced in https://github.com/sagemathinc/cocalc/commit/576382b1729f4b3f34d9401026d000ec907bc152 and also add ugly workaround to deal with live data.

src/smc-util/smc-version.js
@@ -1,2 +1,2 @@
 /* autogenerated by the update_version script */
-exports.version=1547242469;
+exports.version=1547249943;

src/smc-webapp/course/store.ts
@@ -100,7 +100,7 @@ export type CourseSettingsRecord = TypedMap<{
   description: string;
   email_invite: string;
   institute_pay: boolean;
-  pay: string | number | Date;
+  pay: string | Date;
   shared_project_id: string;
   student_pay: boolean;
   title: string;
@@ -239,7 +239,9 @@ export class CourseStore extends Store<CourseState> {
   }
 
   get_pay() {
-    return !!this.get("settings").get("pay");
+    const pay = this.get("settings").get("pay");
+    if(!pay) return "";
+    return pay;
   }
 
   get_allow_collabs() {

src/smc-webapp/projects.cjsx
@@ -582,6 +582,8 @@ class ProjectsStore extends Store
         if is_student and not @is_deleted(project_id)
             # signed in user is the student
             pay = info.get('pay')
+            if pay == true  # bug -- can delete this workaround in March 2019.
+                pay = new Date('2019-02-15')
             if pay
                 if webapp_client.server_time() >= misc.months_before(-3, pay)
                     # It's 3 months after date when sign up required, so course likely over,