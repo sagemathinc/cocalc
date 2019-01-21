84a4000079de8bc08262f1e3b7b197474151d6e3
Harald Schilly <hsy@sagemath.com>
2019-01-21 11:42:41 +0000

    open cocalc: small fix for scripts/cocalc_setup_theme.py

--- src/scripts/cocalc_setup_theme.py
+++ src/scripts/cocalc_setup_theme.py
@@ -7,15 +7,18 @@ import os
 
 DEFAULT = 'cocalc'
 THEME = os.environ.get('COCALC_THEME', DEFAULT)
+# where symlink is set
+target = 'smc-util/theme.coffee'
 
 print(f"cocalc_setup_theme: THEME = {THEME}")
 
-os.unlink('smc-util/theme.coffee')
+if os.path.exists(target):
+    os.unlink(target)
 
 if THEME == DEFAULT:
-    os.system("cp -l smc-util/theme-cocalc.coffee smc-util/theme.coffee")
+    os.system(f"cp -l smc-util/theme-cocalc.coffee {target}")
 elif THEME == 'kucalc':
-    os.system("cp -l smc-util/theme-kucalc.coffee smc-util/theme.coffee")
+    os.system(f"cp -l smc-util/theme-kucalc.coffee {target}")
 else:
-    print("attempting to hardlink file '{THEME}' to smc-util/theme.coffee")
-    os.system("cp -l '{THEME}' smc-util/theme.coffee")
+    print(f"attempting to hardlink file '{THEME}' to {target}")
+    os.system(f"cp -l '{THEME}' {target}")
