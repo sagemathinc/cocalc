32b875564add40ab0f898efdf1498db76495e2ed
Harald Schilly <hsy@sagemath.com>
2019-01-24 07:50:38 +0000

    project/r styler: process warnings & errors

--- src/smc-project/formatters/r-format.ts
+++ src/smc-project/formatters/r-format.ts
@@ -1,12 +1,14 @@
-const { writeFile, readFile } = require("fs");
+const { writeFile, readFile, unlink } = require("fs");
 const tmp = require("tmp");
 const { callback } = require("awaiting");
 const { spawn } = require("child_process");
 // const { replace_all } = require("smc-util/misc");
 
+type Variant = "styler" | "formatR";
+
 interface ParserOptions {
   parser?: string;
-  variant?: "styler" | "formatR";
+  variant?: Variant;
   tabWidth?: number;
   lineWidth?: number;
 }
@@ -15,10 +17,9 @@ function close(proc, cb): void {
   proc.on("close", code => cb(undefined, code));
 }
 
-function formatR(input_path: string, variant) {
+function formatR(input_path: string, variant: Variant) {
   // in-place is fine, according to my tests
   let expr: string | undefined;
-  variant = variant ? variant : "styler";
   if (variant == "formatR") {
     expr = `suppressMessages(require(formatR)); tidy_source(source="${input_path}", file="${input_path}", indent=2, width.cutoff=80)`;
   } else if (variant == "styler") {
@@ -31,6 +32,26 @@ function formatR(input_path: string, variant) {
   }
 }
 
+/** styler error example
+ *
+ * > styler::style_file("some-code.r")
+ * Styling  1  files:
+ *  some-code.r ⚠
+ * ────────────────────────────────────────
+ * Status  Count   Legend
+ * ✔       0       File unchanged.
+ * ℹ       0       File changed.
+ * ✖       1       Styling threw an error.
+ * ────────────────────────────────────────
+ * Warning message:
+ * When processing some-code.r: <text>:7:17: unexpected '=='
+ * 6:   for (i in xx) {
+ * 7:     if (z %% 2) ==
+ *                    ^
+ * >
+ * >
+ */
+
 export async function r_format(
   input: string,
   options: ParserOptions,
@@ -39,28 +60,36 @@ export async function r_format(
 ): Promise<string> {
   // create input temp file
   const input_path: string = await callback(tmp.file, { postfix: `.${ext}` });
-  await callback(writeFile, input_path, input);
+  try {
+    await callback(writeFile, input_path, input);
+    const variant: Variant = options.variant ? options.variant : "styler";
 
-  // spawn the R formatter
-  const r_formatter = formatR(input_path, options.variant);
+    // spawn the R formatter
+    const r_formatter = formatR(input_path, variant);
+    // stdout/err capture
+    const stdout: string[] = [];
+    const stderr: string[] = [];
+    // read data as it is produced.
+    r_formatter.stdout.on("data", data => stdout.push(data.toString()));
+    r_formatter.stderr.on("data", data => stderr.push(data.toString()));
+    // wait for subprocess to close.
+    let code = await callback(close, r_formatter);
+    // special case: return code 0 but warnings are errors, which we want to report back
+    if (variant == "styler" && stderr.length > 0) {
+      throw new Error(stderr.slice(1).join("\n"));
+    }
+    if (code) {
+      const err_msg = stderr.join("\n");
+      logger.debug(`R_FORMAT ${err_msg}`);
+      throw new Error(err_msg);
+    }
 
-  // stdout/err capture
-  let stdout: string = "";
-  let stderr: string = "";
-  // read data as it is produced.
-  r_formatter.stdout.on("data", data => (stdout += data.toString()));
-  r_formatter.stderr.on("data", data => (stderr += data.toString()));
-  // wait for subprocess to close.
-  let code = await callback(close, r_formatter);
-  if (code) {
-    const err_msg = `${stderr}`;
-    logger.debug(`R_FORMAT ${err_msg}`);
-    throw new Error(err_msg);
+    // all fine, we read from the temp file
+    let output: Buffer = await callback(readFile, input_path);
+    let s: string = output.toString("utf-8");
+
+    return s;
+  } finally {
+    unlink(input_path);
   }
-
-  // all fine, we read from the temp file
-  let output: Buffer = await callback(readFile, input_path);
-  let s: string = output.toString("utf-8");
-
-  return s;
 }
