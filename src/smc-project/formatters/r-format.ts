a53320d38ca3a8eb1ab35f4c4e0c58fb041281f0
Harald Schilly <hsy@sagemath.com>
2019-01-31 17:17:43 +0000

    project/formatter/r: fix stderr processing

--- src/smc-project/formatters/r-format.ts
+++ src/smc-project/formatters/r-format.ts
@@ -44,7 +44,7 @@ function formatR(input_path: string, variant: Variant) {
  * ✖       1       Styling threw an error.
  * ────────────────────────────────────────
  * Warning message:
- * When processing some-code.r: <text>:7:17: unexpected '=='
+ * When processing some-code.r: <text>:7:17: unexpected '=='<strong></strong>p
  * 6:   for (i in xx) {
  * 7:     if (z %% 2) ==
  *                    ^
@@ -67,20 +67,25 @@ export async function r_format(
     // spawn the R formatter
     const r_formatter = formatR(input_path, variant);
     // stdout/err capture
-    const stdout: string[] = [];
-    const stderr: string[] = [];
+    let stdout: string = "";
+    let stderr: string = "";
     // read data as it is produced.
-    r_formatter.stdout.on("data", data => stdout.push(data.toString()));
-    r_formatter.stderr.on("data", data => stderr.push(data.toString()));
+    r_formatter.stdout.on("data", data => (stdout += data.toString()));
+    r_formatter.stderr.on("data", data => (stderr += data.toString()));
     // wait for subprocess to close.
     let code = await callback(close, r_formatter);
     // special case: return code 0 but warnings are errors, which we want to report back
     if (variant == "styler" && stderr.length > 0) {
-      throw new Error(stderr.slice(1).join("\n"));
+      const err_msg = stderr
+        .split("\n")
+        .slice(1)
+        .join("\n");
+      logger.debug(`R_FORMAT: "${err_msg}"`);
+      throw new Error(err_msg);
     }
     if (code) {
-      const err_msg = stderr.join("\n");
-      logger.debug(`R_FORMAT ${err_msg}`);
+      const err_msg = stderr;
+      logger.debug(`R_FORMAT: ${err_msg}`);
       throw new Error(err_msg);
     }
 
