889ded3c74b97eb62946715ef79a0127dffac94c
John Jeng <john.jeng5@gmail.com>
2019-02-26 18:59:37 +0000

    Add input saving test

--- src/smc-webapp/r_misc/__tests__/number-input.test.tsx
+++ src/smc-webapp/r_misc/__tests__/number-input.test.tsx
@@ -18,3 +18,26 @@ test("smoke test", () => {
   );
   expect(rendered).toMatchSnapshot();
 });
+
+test("input save on blur", () => {
+  const starting_num = 12;
+  const mock_change = jest.fn();
+  const min = 10;
+  const max = 20;
+
+  const final_value = 15;
+
+  const rendered = shallow(
+    <NumberInput
+      number={starting_num}
+      min={min}
+      max={max}
+      on_change={mock_change}
+    />
+  );
+  const form = rendered.find("FormControl").first();
+  form.simulate("change", { target: { value: final_value } });
+  form.simulate("blur");
+  expect(mock_change.mock.calls.length).toBe(1);
+  expect(mock_change.mock.calls[0][0]).toBe(final_value);
+});
