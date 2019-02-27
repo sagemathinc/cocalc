778015b10af19c4185940dd13d0a3df4017c3b28
Harald Schilly <hsy@sagemath.com>
2019-02-27 14:17:49 +0000

    webapp/miniterm: restrict max width to avoid being off screen

--- src/smc-webapp/project_miniterm.tsx
+++ src/smc-webapp/project_miniterm.tsx
@@ -61,7 +61,8 @@ export const output_style_miniterm: React.CSSProperties = {
   boxShadow: "0px 0px 7px #aaa",
   maxHeight: "450px",
   overflow: "auto",
-  right: 0
+  right: 0,
+  maxWidth: "80%"
 };
 
 const BAD_COMMANDS = {
