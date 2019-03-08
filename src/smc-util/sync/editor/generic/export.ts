065c15970a3f63831e8a2906b718dacd80ef181f
William Stein <wstein@sagemath.com>
2019-03-08 19:08:58 +0000

    add "export" button to TimeTravel, so you can see how much and when people did stuff...

--- src/smc-util/sync/editor/generic/export.ts
+++ src/smc-util/sync/editor/generic/export.ts
@@ -2,13 +2,15 @@ import { SortedPatchList } from "./sorted-patch-list";
 import { Patch } from "./types";
 
 export interface HistoryEntry {
-  time: Date;
+  time_utc: Date;
   account_id: string;
   patch?: any[];
+  patch_length?: number;
 }
 
 export interface HistoryExportOptions {
   patches?: boolean;
+  patch_lengths?: boolean; // length of each patch (some measure of amount changed)
 }
 
 export function export_history(
@@ -19,17 +21,19 @@ export function export_history(
   const patches: Patch[] = patch_list.export();
   const entries: HistoryEntry[] = [];
   for (let x of patches) {
-    const time = x.time;
+    const time_utc = x.time;
     let account_id = account_ids[x.user_id];
     if (account_id == null) {
       account_id = "unknown"; // should never happen...
     }
+    const entry: HistoryEntry = { time_utc, account_id };
     if (options.patches) {
-      const patch = x.patch;
-      entries.push({ time, account_id, patch });
-    } else {
-      entries.push({ time, account_id });
+      entry.patch = x.patch;
     }
+    if (options.patch_lengths) {
+      entry.patch_length = JSON.stringify(x.patch).length;
+    }
+    entries.push(entry);
   }
   return entries;
 }
