577381755d52f82493e9b0fb117d18cf6a5e15e6
William Stein <wstein@sagemath.com>
2019-03-08 18:16:47 +0000

    finish generic sync-doc history export functionality

--- src/smc-util/sync/editor/generic/export.ts
+++ src/smc-util/sync/editor/generic/export.ts
@@ -3,24 +3,33 @@ import { Patch } from "./types";
 
 export interface HistoryEntry {
   time: Date;
-  user: string;
-  patch: any[];
+  account_id: string;
+  patch?: any[];
+}
+
+export interface HistoryExportOptions {
+  patches?: boolean;
 }
 
 export function export_history(
-  users: string[],
-  patch_list: SortedPatchList
+  account_ids: string[],
+  patch_list: SortedPatchList,
+  options: HistoryExportOptions
 ): HistoryEntry[] {
   const patches: Patch[] = patch_list.export();
   const entries: HistoryEntry[] = [];
   for (let x of patches) {
     const time = x.time;
-    let user = users[x.user_id];
-    if (user == null) {
-      user = "Unknown User";
+    let account_id = account_ids[x.user_id];
+    if (account_id == null) {
+      account_id = "unknown"; // should never happen...
     }
-    const patch = x.patch;
-    entries.push({ time, user, patch });
+    if (options.patches) {
+      const patch = x.patch;
+      entries.push({ time, account_id, patch });
+    } else {
+      entries.push({ time, account_id });
+    }
   }
   return entries;
 }
