25dd34cb565e98563f3e10ae1ce8f4afb9d89b50
William Stein <wstein@sagemath.com>
2019-04-11 19:16:49 +0000

    ipywidgets: implementing output widget. (work in progress)

--- src/smc-webapp/jupyter/output-messages/widget.tsx
+++ src/smc-webapp/jupyter/output-messages/widget.tsx
@@ -10,12 +10,15 @@ import {
   rclass,
   rtypes
 } from "smc-webapp/app-framework";
-import { Map, Set } from "immutable";
+import { Map, Set, fromJS } from "immutable";
 import { JupyterActions } from "../browser-actions";
 
 import * as pWidget from "@phosphor/widgets";
 
 require("@jupyter-widgets/controls/css/widgets.css");
+
+import { Stdout } from "./stdout";
+import { Stderr } from "./stderr";
 
 interface WidgetProps {
   value: Map<string, any>;
@@ -26,8 +29,13 @@ interface WidgetProps {
   widget_model_ids?: Set<string>;
 }
 
-export class Widget0 extends Component<WidgetProps> {
+interface WidgetState {
+  output?: Map<string, any>;
+}
+
+export class Widget0 extends Component<WidgetProps, WidgetState> {
   private view?: any;
+  private model?: any;
   private init_view_is_running: boolean = false;
   private mounted: boolean = false;
 
@@ -39,7 +47,19 @@ export class Widget0 extends Component<WidgetProps> {
     };
   }
 
-  shouldComponentUpdate(nextProps: WidgetProps): boolean {
+  public constructor(props, context) {
+    super(props, context);
+
+    this.update_output = this.update_output.bind(this);
+
+    // state is used to store output state, for output widgets, which we render.
+    this.state = {};
+  }
+
+  shouldComponentUpdate(
+    nextProps: WidgetProps,
+    nextState: WidgetState
+  ): boolean {
     const model_id = this.props.value.get("model_id");
     const next_model_id = nextProps.value.get("model_id");
     if (model_id != next_model_id) {
@@ -55,7 +75,8 @@ export class Widget0 extends Component<WidgetProps> {
       // view not yet initialized and model is now known, so initialize it.
       this.init_view(next_model_id);
     }
-    return false; // no actual react update ever
+    if (nextState.output == null) return false;
+    return !nextState.output.equals(this.state);
   }
 
   componentDidMount(): void {
@@ -74,6 +95,19 @@ export class Widget0 extends Component<WidgetProps> {
     this.mounted = false;
     // always clean up
     this.remove_view();
+  }
+
+  update_output(): void {
+    console.log("update_output -- called");
+    if (!this.mounted) return;
+    const state = this.model.get_state(true);
+    if (state == null) {
+      this.setState({ output: undefined });
+      return;
+    }
+    const value = state.value;
+    console.log("update_output -- got value=", value);
+    this.setState({ output: fromJS(value) });
   }
 
   async init_view(model_id: string | undefined): Promise<void> {
@@ -95,38 +129,31 @@ export class Widget0 extends Component<WidgetProps> {
         //console.log("no widget_manager");
         return;
       }
-      const model = await widget_manager.get_model(model_id);
-      if (model == null || !this.mounted) {
+      this.model = await widget_manager.get_model(model_id);
+      if (this.model == null || !this.mounted) {
         // no way to render at present; wait for widget_counter to increase and try again.
         return;
       }
-      console.log("model = ", model);
 
-      switch (model.module) {
+      switch (this.model.module) {
         case "@jupyter-widgets/controls":
         case "@jupyter-widgets/base":
-          // We use phosphor views for base and controls
-          const view = await widget_manager.create_view(model);
+          // Right now we use phosphor views for all base and controls.
+          // TODO: we can iteratively rewrite some of these using react for a more
+          // consistent look and feel...
+          const view = await widget_manager.create_view(this.model);
           if (!this.mounted) return;
           this.view = view as any;
-
-          const elt = ReactDOM.findDOMNode(this);
+          const elt = ReactDOM.findDOMNode(this.refs.phosphor);
+          if (elt == null) return;
           pWidget.Widget.attach(this.view.pWidget, elt);
           break;
         case "@jupyter-widgets/output":
-          // We use our own react code for output.
-          const f = () => {
-            const elt = ReactDOM.findDOMNode(this);
-            const state = model.get_state(true);
-            const value = state.value;
-
-            $(elt).text(JSON.stringify(value));
-          };
-          model.on("change", f);
-          f();
+          this.model.on("change", this.update_output);
+          this.update_output();
           break;
         default:
-          throw Error(`Not implemented widget module ${model.module}`);
+          throw Error(`Not implemented widget module ${this.model.module}`);
       }
     } catch (err) {
       // TODO -- show an error component somehow...
@@ -142,10 +169,49 @@ export class Widget0 extends Component<WidgetProps> {
       this.view.remove(); // no clue what this does...
       delete this.view;
     }
+    if (this.model != null) {
+      if (this.model.module == "@jupyter-widgets/output") {
+        console.log("removing listener");
+        this.model.off("change", this.update_output);
+      }
+      delete this.model;
+    }
+  }
+
+  render_phosphor(): Rendered {
+    // This div is managed by phosphor, so don't put any react in it!
+    return <div ref="phosphor" />;
+  }
+
+  render_output(): Rendered[] | undefined {
+    console.log("render_output", this.state.output);
+    if (this.state.output == null) return;
+    const v: Rendered[] = [];
+    this.state.output.forEach(message => {
+      const output_type: string = message.get("output_type");
+      const name: string = message.get("name");
+      if (output_type === "stream") {
+        if (name === "stdout") {
+          v.push(<Stdout message={message} />);
+          return;
+        } else if (name === "stderr") {
+          v.push(<Stderr message={message} />);
+          return;
+        }
+      }
+      v.push(<pre>NOT IMPLEMENTED: {JSON.stringify(message)}</pre>);
+    });
+
+    return v;
   }
 
   render(): Rendered {
-    return <div />;
+    return (
+      <div>
+        {this.render_phosphor()}
+        {this.render_output()}
+      </div>
+    );
   }
 }
 
