aa44459ac06b24b5f4f47879ba2b3d474ff4ad51
William Stein <wstein@sagemath.com>
2019-05-05 02:13:53 +0000

    jupyter-frame: icons

--- src/smc-webapp/frame-editors/jupyter-editor/editor.ts
+++ src/smc-webapp/frame-editors/jupyter-editor/editor.ts
@@ -23,107 +23,107 @@ import { ClassicalNotebook } from "./classical-notebook";
 
 export const EDITOR_SPEC = {
   jupyter_cell_notebook: {
-    short: "Cells",
-    name: "Cells (default)",
-    icon: "file-pdf-o",
+    short: "Notebook",
+    name: "Notebook (default)",
+    icon: "cc-icon-ipynb",
     component: CellNotebook,
     buttons: set([])
   },
   jupyter_singledoc_notebook: {
     short: "SingleDoc",
     name: "Single Doc",
-    icon: "file-pdf-o",
+    icon: "code",
     component: SingleDocNotebook,
     buttons: set([])
   },
   jupyter_markdown: {
     short: "Markdown",
     name: "Markdown Doc",
-    icon: "file-pdf-o",
+    icon: "cc-icon-markdown",
     component: MarkdownNotebook,
     buttons: set([])
   },
   jupyter_log: {
     short: "Log",
     name: "Kernel Log",
-    icon: "file-pdf-o",
+    icon: "clipboard-list",
     component: Log,
     buttons: set([])
   },
   jupyter_raw: {
     short: "Raw",
     name: "Raw JSON editor",
-    icon: "file-pdf-o",
+    icon: "cc-icon-markdown",
     component: RawIPynb,
     buttons: set([])
   },
   jupyter_object_browser: {
     short: "Object",
     name: "Object Browser",
-    icon: "file-pdf-o",
+    icon: "sitemap",
     component: ObjectBrowser,
     buttons: set([])
   },
   jupyter_kernel: {
     short: "Kernel",
     name: "Kernel Config",
-    icon: "file-pdf-o",
+    icon: "server",
     component: KernelConfiguration,
     buttons: set([])
   },
   jupyter_assistant: {
     short: "Assistant",
     name: "Assistant Examples",
-    icon: "file-pdf-o",
+    icon: "magic",
     component: Assistant,
     buttons: set([])
   },
   jupyter_ipython_terminal: {
     short: "IPython",
     name: "IPython Terminal",
-    icon: "file-pdf-o",
+    icon: "terminal",
     component: IPythonTerminal,
     buttons: set([])
   },
   jupyter_plain_terminal: {
     short: "Terminal",
     name: "Plain Terminal",
-    icon: "file-pdf-o",
+    icon: "terminal",
     component: PlainTerminal,
     buttons: set([])
   },
   jupyter_file_editor: {
     short: "File",
     name: "File Editor",
-    icon: "file-pdf-o",
+    icon: "cc-icon-python",
     component: FileEditor,
     buttons: set([])
   },
   jupyter_documentation_viewer: {
     short: "Docs",
     name: "Documentation",
-    icon: "file-pdf-o",
+    icon: "question-circle",
     component: DocumentationViewer,
     buttons: set([])
   },
   jupyter_export: {
     short: "Export",
     name: "Export (nbconvert)",
-    icon: "file-pdf-o",
+    icon: "file-export",
     component: Export,
     buttons: set([])
   },
   jupyter_slideshow: {
     short: "Slideshow",
     name: "Slideshow View",
-    icon: "file-pdf-o",
+    icon: "slideshare",
     component: Slideshow,
     buttons: set([])
   },
   jupyter_classical: {
-    short: "Classical",
-    name: "Classical Notebook",
-    icon: "file-pdf-o",
+    short: "Classic",
+    name: "Classic Notebook",
+    icon: "cc-icon-ipynb",
     component: ClassicalNotebook,
     buttons: set([])
   }
