c8fcda410de96bc54e051059bde74990f28a5bb1
Hal Snyder <hsnyder@sagemath.com>
2019-05-24 16:30:07 +0000

    search proj using placeholder

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const HEADLESS = true;
 
 const puppeteer = require('puppeteer');
 const CREDS = require('./creds');
+const sprintf = require('sprintf-js').sprintf;
 
 async function run() {
   const browser = await puppeteer.launch({
@@ -17,6 +18,7 @@ async function run() {
   await page.goto(CREDS.url);
   console.log('01 got sign-in page')
 
+  // get selectors manually by doing Inspect while viewing page in chrome
   const USERNAME_SELECTOR = '#smc-react-container > div > div:nth-child(4) > div > div > div.hidden-xs.row > div:nth-child(1) > form > div > div:nth-child(1) > div.col-xs-5 > div > input'
   const PASSWORD_SELECTOR = '#smc-react-container > div > div:nth-child(4) > div > div > div.hidden-xs.row > div:nth-child(1) > form > div > div:nth-child(1) > div.col-xs-4 > div > input'
   const BUTTON_SELECTOR = '#smc-react-container > div > div:nth-child(4) > div > div > div.hidden-xs.row > div:nth-child(1) > form > div > div:nth-child(1) > div.col-xs-3 > button'
@@ -55,11 +57,30 @@ async function run() {
 
 //  console.log(elt);
 
-  const n = 3;
-  console.log(`wait ${n} seconds`);
-  page.waitFor(n * 1000);
+  //const n = 3;
+  //console.log(`wait ${n} seconds`);
+  //page.waitFor(n * 1000);
   
-  await page.screenshot({ path: 'screenshots/cocalc.png'});
+  // unique attribute for many CoCalc inputs is the placeholder string
+  // here is the selector for project search
+  // input[placeholder="Search for projects..."]
+  sfpPh="Search for projects...";
+  sfpSel = sprintf('input[placeholder=\"%s\"]', sfpPh);
+
+  // pass function definition as string to page.waitForFunction
+  // x will be this:
+  // document.querySelector('input[placeholder="Search for projects..."]').placeholder == "Search for projects..."
+  x = sprintf("document.querySelector(\'%s\').placeholder == \"%s\"", sfpSel, sfpPh);
+  await page.waitForFunction(x);
+  console.log('03 got search for projects input element')
+  
+  // type into the project search blank
+  await page.type(sfpSel, CREDS.project);
+  await page.waitFor(3 * 1000);
+  
+  const spath = 'screenshots/cocalc.png';
+  await page.screenshot({ path: spath});
+  console.log(`04 screenshot saved to ${spath}`);
   browser.close();
 }
 
