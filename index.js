d8ad651085f8b38e2b0d3706c3025d55463289e3
Hal Snyder <hsnyder@sagemath.com>
2019-05-25 19:06:56 +0000

    get word count for a latex document

--- index.js
+++ index.js
@@ -46,8 +46,8 @@ async function run() {
   // pass function definition as string to page.waitForFunction
   // x will be this:
   // document.querySelector('input[placeholder="Search for projects..."]').placeholder == "Search for projects..."
-  x = sprintf("document.querySelector(\'%s\').placeholder == \"%s\"", sfpSel, sfpPh);
-  await page.waitForFunction(x);
+  const sfpx = sprintf("document.querySelector(\'%s\').placeholder == \"%s\"", sfpSel, sfpPh);
+  await page.waitForFunction(sfpx);
   console.log('03 got search for projects input element')
   
   // type into the project search blank
@@ -63,14 +63,68 @@ try {
   } else {
     throw new Error("Link not found");
   }
+  await page.waitForNavigation({'waitUntil':'networkidle0'});
 
-  //await page.waitFor(3 * 1000);
+  // find texfile link and click it
+  sfilePh="Search or create file";
+  sfileSel = sprintf('input[placeholder=\"%s\"]', sfilePh);
+
+  const sfilex = sprintf("document.querySelector(\'%s\').placeholder == \"%s\"", sfileSel, sfilePh);
+  await page.waitForFunction(sfilex);
+  console.log('05 got file search input element')
+
+  // type into the file search blank
+  await page.type(sfileSel, CREDS.texfile);
+  await page.waitForFunction(sfilex);
+
+  // find and click the texfile link
+  //const linkHandlers2 = await page.$x(`//a/span[text()='latex-sample']`);
+  const linkHandlers2 = await page.$x(`//a/span[text()='${CREDS.texfile.slice(0,-4)}']`);
+  console.log('06 number of links matching test texfile name',linkHandlers2.length)
+  if (linkHandlers2.length > 0) {
+    await Promise.all([
+      linkHandlers2[0].click(),
+      page.waitForNavigation({'waitUntil':'networkidle0'})
+    ]);
+    await page.waitFor(3 * 1000);
+  } else {
+    throw new Error("Link not found");
+  }
+  //await page.waitForNavigation({'waitUntil':'networkidle0'});
+
+  // click the frame types pulldown
+  // it displays "Source" in the uppler left frame initially
+  // $x('//button[@id="types"]')[0].click();
+//  const lh3 = await page.$x('//button[@id="types"]');
+//  console.log('07 number of links matching types button',lh3.length)
+//  if (lh3.length > 0) {
+//    await lh3[0].click();
+//  } else {
+//    throw new Error("Link not found");
+//  }
+  await page.click("#types");
+  console.log('07 clicked types menu');
+  //await page.waitForNavigation({'waitUntil':'networkidle0'});
+  
+    const lh4 = await page.$x('//div[1]/div/ul/li[6]/a');
+    console.log('08 number of links matching word count',lh4.length);
+    if (lh4.length > 0) {
+      await lh4[0].click();
+      console.log('09 word count clicked');
+    } else {
+      throw new Error("Link not found");
+    }
+
+  
+  
+} catch (e) {
+  console.log('08 ERROR',e.message);
+}
+  await page.waitFor(3 * 1000);
   const spath = 'screenshots/cocalc.png';
   await page.screenshot({ path: spath});
-  console.log(`05 screenshot saved to ${spath}`);
-} catch (e) {
-  console.log('05 ERROR',e.message);
-}
+  console.log(`08 screenshot saved to ${spath}`);
+  
   browser.close();
 }
 
