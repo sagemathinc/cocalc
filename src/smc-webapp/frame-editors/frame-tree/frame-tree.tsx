c4f509ca1ecbe7be1979c911a484d6b4d5938b3a
Harald Schilly <hsy@sagemath.com>
2019-05-26 08:57:25 +0000

    webapp/frame editor: missing available_features prop for Leafs -- #3848

--- src/smc-webapp/frame-editors/frame-tree/frame-tree.tsx
+++ src/smc-webapp/frame-editors/frame-tree/frame-tree.tsx
@@ -269,6 +269,7 @@ export class FrameTree extends Component<FrameTreeProps, FrameTreeState> {
           complete={this.props.complete.get(desc.get("id"))}
           derived_file_types={this.props.derived_file_types}
           desc={desc}
+          available_features={this.props.available_features}
         />
       </div>
     );
