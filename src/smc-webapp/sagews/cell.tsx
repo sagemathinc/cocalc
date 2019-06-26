63d4b341c85e69229a28c94d67da38036fa6ba84
William Stein <wstein@sagemath.com>
2019-06-26 19:50:07 +0000

    sagews typescript -- cell

---
+++ src/smc-webapp/sagews/cell.tsx
@@ -0,0 +1,35 @@
+/*
+Rendering a Sage worksheet cell
+*/
+
+import { React, Component, Rendered } from "../app-framework";
+
+import { CellInput } from "./input";
+import { CellOutput } from "./output";
+
+interface Props {
+  input: string;
+  output: object;
+  flags: string;
+}
+
+export class Cell extends Component<Props> {
+  private render_input() : Rendered {
+    return <CellInput input={this.props.input} flags={this.props.flags} />;
+  }
+
+  private render_output() : Rendered {
+    if (this.props.output != null) {
+      return <CellOutput output={this.props.output} flags={this.props.flags} />;
+    }
+  }
+
+  public render() : Rendered {
+    return (
+      <div>
+        {this.render_input()}
+        {this.render_output()}
+      </div>
+    );
+  }
+}
