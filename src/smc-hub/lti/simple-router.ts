cf94953a37534a5ac0b39f2cae440d849967ba32
John Jeng <john.jeng5@gmail.com>
2019-07-01 21:23:31 +0000

    Add login functionality

--- src/smc-hub/lti/simple-router.ts
+++ src/smc-hub/lti/simple-router.ts
@@ -1,5 +1,25 @@
 import * as express from "express";
 
+// TODO #V0 Remove when you write a way to save it to the database
+const known_iss = {
+  "https://moodletest.cocalc.com": {
+    client_id: "Ho3mDRdDHybcG5U",
+    token_url: "https://moodletest.cocalc.com/mod/lti/token.php",
+    auth_url: "https://moodletest.cocalc.com/mod/lti/auth.php",
+    jwk_url: "https://moodletest.cocalc.com/mod/lti/certs.php"
+  },
+  "https://canvas.instructure.com": {
+    client_id: "10000000000008",
+    token_url: "http://34.83.75.255/api/lti/login/oauth2/auth",
+    auth_url: "http://34.83.75.255/api/lti/authorize",
+    jwk_url: "http://34.83.75.255/api/lti/securit/jwks"
+  }
+}
+
+function get_iss_data(iss: string): IssuerData {
+  return known_iss[iss];
+}
+
 export function init_LTI_router(): express.Router {
   const router = express.Router();
   router.use(express.json());
@@ -13,6 +33,30 @@ export function init_LTI_router(): express.Router {
   // 5.1.1
   router.route("/login").all((_, res) => {
     res.send("Login via lti");
+
+    const token = req.body;
+    const iss_data = get_iss_data(token.iss);
+    const nonce = uuid.v4();
+    const state = uuid.v4();
+
+    // https://www.imsglobal.org/spec/security/v1p0/#step-2-authentication-request
+    const auth_params = {
+      scope: "openid", // OIDC Scope.
+      response_type: "id_token", // OIDC response is always an id token.
+      client_id: iss_data.client_id,
+      redirect_uri: token.target_link_uri,
+      login_hint: token.login_hint,
+      state: state,
+      response_mode: "form_post", // OIDC response is always a form post.
+      nonce: nonce,
+      prompt: "none", // Don't prompt user on redirect.
+      lti_message_hint: token.lti_message_hint,
+      id_token_hint: token.lti_message_hint
+    };
+    // begin_auth_flow(token.state, auth_params)
+    const query_string = querystring.stringify(auth_params);
+    res.redirect(iss_data.auth_url + "?" + query_string);
+
   });
 
   // Tool Launch URL
