63fa762a556ca5eb280a6cb5f5a6575c7afbb8e3
William Stein <wstein@sagemath.com>
2019-07-17 18:51:53 +0000

    share: massively refactor the router code; add raw route

--- src/smc-hub/share/react.ts
+++ src/smc-hub/share/react.ts
@@ -5,8 +5,6 @@ Load react support for rendering.
 // Code for rendering react components to html.
 import * as ReactDOMServer from "react-dom/server";
 
-import "./jsdom-support";
-
 // Uncomment for cc-in-cc dev benchmarking purposes.  This variable is already set
 // by the Docker container when running in kubernetes.
 //# process.NODE_ENV="production"
