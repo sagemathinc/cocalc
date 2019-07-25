sagemathinc/cocalc

commit 48a6c96a50cca06bf8768e510af500200f152cd5

fix #3048 -- better transform of +New get urls; also rewrite alerts in typescript to better display error messages if they are exceptions.

src/smc-webapp/project/transform-get-url.ts
@@ -1,0 +1,74 @@
+/*
+Apply various transformations to url's before downloading a file using the "+ New" from web thing:
+This is useful, since people often post a link to a page that *hosts* raw content, but isn't raw
+content, e.g., ipython nbviewer, trac patches, github source files (or repos?), etc.
+*/
+
+import { endswith, startswith } from "smc-util/misc2";
+
+const COCALC_SHARE_SERVER = "https://share.cocalc.com/share/";
+
+// returns something like {command:'wget', args:['http://...']}
+export function transform_get_url(
+  url: string
+): { command: string; args: string[] } {
+  let args: string[], command: string;
+  const URL_TRANSFORMS = {
+    "http://trac.sagemath.org/attachment/ticket/":
+      "http://trac.sagemath.org/raw-attachment/ticket/",
+    "https://trac.sagemath.org/attachment/ticket/":
+      "https://trac.sagemath.org/raw-attachment/ticket/",
+    "http://nbviewer.jupyter.org/url/": "http://", // download from original source
+    "http://nbviewer.jupyter.org/urls/": "https://",
+    "https://nbviewer.jupyter.org/url/": "http://", // download from original source
+    "https://nbviewer.jupyter.org/urls/": "https://",
+    [COCALC_SHARE_SERVER]: COCALC_SHARE_SERVER + "raw/" // always download as raw
+  };
+  if (startswith(url, "https://github.com/")) {
+    if (url.indexOf("/blob/") !== -1) {
+      url = url
+        .replace("https://github.com", "https://raw.githubusercontent.com")
+        .replace("/blob/", "/");
+      // issue #1818: https://github.com/plotly/python-user-guide → https://github.com/plotly/python-user-guide.git
+    } else if (!endswith(url, ".git")) {
+      const u = url.split("://")[1];
+      if (u != null && u.split("/").length === 3) {
+        url += ".git";
+      }
+    }
+  }
+
+  if (startswith(url, COCALC_SHARE_SERVER)) {
+    // remove any query params, since they mess up the filename...
+    const i = url.lastIndexOf("?");
+    if (i != -1) {
+      url = url.slice(0, i);
+    }
+  }
+
+  if (startswith(url, "git@github.com:")) {
+    command = "git"; // kind of useless due to host keys...
+    args = ["clone", url];
+  } else if (url.slice(url.length - 4) === ".git") {
+    command = "git";
+    args = ["clone", url];
+  } else {
+    // fall back
+    for (let a in URL_TRANSFORMS) {
+      const b = URL_TRANSFORMS[a];
+      url = url.replace(a, b);
+    } // only replaces first instance, unlike python.  ok for us.
+    // special case, this is only for nbviewer.../github/ URLs
+    if (startswith(url, "http://nbviewer.jupyter.org/github/")) {
+      url = url.replace(
+        "http://nbviewer.jupyter.org/github/",
+        "https://raw.githubusercontent.com/"
+      );
+      url = url.replace("/blob/", "/");
+    }
+    command = "wget";
+    args = [url];
+  }
+
+  return { command, args };
+}

src/smc-util/misc.js
@@ -2761,56 +2761,6 @@ exports.ticket_id_to_ticket_url = tid =>
 exports.is_only_downloadable = string =>
   string.indexOf("://") !== -1 || exports.startswith(string, "git@github.com");
 
-// Apply various transformations to url's before downloading a file using the "+ New" from web thing:
-// This is useful, since people often post a link to a page that *hosts* raw content, but isn't raw
-// content, e.g., ipython nbviewer, trac patches, github source files (or repos?), etc.
-exports.transform_get_url = function(url) {
-  // returns something like {command:'wget', args:['http://...']}
-  let args, command;
-  const URL_TRANSFORMS = {
-    "http://trac.sagemath.org/attachment/ticket/":
-      "http://trac.sagemath.org/raw-attachment/ticket/",
-    "http://nbviewer.jupyter.org/url/": "http://",
-    "http://nbviewer.jupyter.org/urls/": "https://"
-  };
-  if (exports.startswith(url, "https://github.com/")) {
-    if (url.indexOf("/blob/") !== -1) {
-      url = url
-        .replace("https://github.com", "https://raw.githubusercontent.com")
-        .replace("/blob/", "/");
-      // issue #1818: https://github.com/plotly/python-user-guide → https://github.com/plotly/python-user-guide.git
-    } else if (__guard__(url.split("://")[1], x => x.split("/").length) === 3) {
-      url += ".git";
-    }
-  }
-
-  if (exports.startswith(url, "git@github.com:")) {
-    command = "git"; // kind of useless due to host keys...
-    args = ["clone", url];
-  } else if (url.slice(url.length - 4) === ".git") {
-    command = "git";
-    args = ["clone", url];
-  } else {
-    // fall back
-    for (let a in URL_TRANSFORMS) {
-      const b = URL_TRANSFORMS[a];
-      url = url.replace(a, b);
-    } // only replaces first instance, unlike python.  ok for us.
-    // special case, this is only for nbviewer.../github/ URLs
-    if (exports.startswith(url, "http://nbviewer.jupyter.org/github/")) {
-      url = url.replace(
-        "http://nbviewer.jupyter.org/github/",
-        "https://raw.githubusercontent.com/"
-      );
-      url = url.replace("/blob/", "/");
-    }
-    command = "wget";
-    args = [url];
-  }
-
-  return { command, args };
-};
-
 exports.ensure_bound = function(x, min, max) {
   if (x < min) {
     return min;

src/smc-webapp/alerts.ts
@@ -29,14 +29,14 @@ import {
   server_time
 } from "smc-util/misc";
 
-const webapp_client = require("./webapp_client");
+const { webapp_client } = require("./webapp_client");
 
 const types = ["error", "default", "success", "info"];
 const default_timeout = {
-  error: 5,
-  default: 2,
-  success: 2,
-  info: 3
+  error: 8,
+  default: 4,
+  success: 4,
+  info: 6
 };
 
 if (typeof $ === "function") {

src/smc-webapp/project_actions.ts
@@ -27,6 +27,8 @@ import { exec } from "./frame-editors/generic/client";
 import { editor_id, NewFilenames } from "smc-webapp/project/utils";
 import { NEW_FILENAMES } from "smc-util/db-schema";
 
+import { transform_get_url } from "./project/transform-get-url";
+
 let project_file, prom_get_dir_listing_h, wrapped_editors;
 if (typeof window !== "undefined" && window !== null) {
   // don't import in case not in browser (for testing)
@@ -1912,9 +1914,8 @@ export class ProjectActions extends Actions<ProjectStoreState> {
       cb: undefined
     }); // cb(true or false, depending on error)
 
-    const { command, args } = misc.transform_get_url(opts.url);
+    const { command, args } = transform_get_url(opts.url);
 
-    console.log(command, args);
     require("./webapp_client").webapp_client.exec({
       project_id: this.project_id,
       command,
@@ -1924,9 +1925,13 @@ export class ProjectActions extends Actions<ProjectStoreState> {
       cb: (err, result) => {
         if (opts.alert) {
           if (err) {
-            alert_message({ type: "error", message: err });
+            alert_message({ type: "error", message: err, timeout: 15 });
           } else if (result.event === "error") {
-            alert_message({ type: "error", message: result.error });
+            alert_message({
+              type: "error",
+              message: result.error,
+              timeout: 15
+            });
           }
         }
         typeof opts.cb === "function"

commit 7de697f2057f28d8ec541782f7d541855140769a

util/transforming URLs: fixing test after 48a6c96a50cca06bf8768e510af500200f152cd5 broke it, adding test for share server URLs, and using DNS set it theme

src/smc-webapp/project/transform-get-url.ts
@@ -5,8 +5,8 @@ content, e.g., ipython nbviewer, trac patches, github source files (or repos?), 
 */
 
 import { endswith, startswith } from "smc-util/misc2";
-
-const COCALC_SHARE_SERVER = "https://share.cocalc.com/share/";
+import { DNS } from "smc-util/theme";
+const COCALC_SHARE_SERVER = `https://share.${DNS}/share/`;
 
 // returns something like {command:'wget', args:['http://...']}
 export function transform_get_url(

src/smc-util/test/misc-test.coffee
@@ -1468,7 +1468,7 @@ describe 'test converting to and from JSON for sending over a socket -- ', ->
         expect(misc.from_json_socket(misc.to_json_socket(obj))).toEqual(obj)
 
 describe 'misc.transform_get_url mangles some URLs or "understands" what action to take', ->
-    turl = misc.transform_get_url
+    turl = require('smc-webapp/project/transform-get-url').transform_get_url
     it 'preserves "normal" URLs', ->
         turl('http://example.com/file.tar.gz').should.eql  {command:'wget', args:["http://example.com/file.tar.gz"]}
         turl('https://example.com/file.tar.gz').should.eql {command:'wget', args:["https://example.com/file.tar.gz"]}
@@ -1512,7 +1512,10 @@ describe 'misc.transform_get_url mangles some URLs or "understands" what action 
         #turl('http://nbviewer.jupyter.org/gist/darribas/4121857').should.eql
         #    command: 'wget'
         #    args: ['https://gist.githubusercontent.com/darribas/4121857/raw/505e030811332c78e8e50a54aca5e8034605cb4c/guardian_gaza.ipynb']
-
+    it 'transforms the cocalc share server url', ->
+        turl('https://share.cocalc.com/share/df736005116ebb1998f6dda48c42719bcec2f46b/ASM_demo.sagews?viewer=share').should.eql
+            command: 'wget'
+            args: ['https://share.cocalc.com/share/raw/df736005116ebb1998f6dda48c42719bcec2f46b/ASM_demo.sagews']
 
 
 describe 'test closest kernel matching method', ->