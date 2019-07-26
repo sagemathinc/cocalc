1cc6426cc0364ed797b12e1eea3361362a139268
John Jeng <john.jeng5@gmail.com>
2019-07-26 19:27:15 +0000

    Lock selection status if ancestor is selected

--- src/smc-webapp/lti/view/project/file-listing/file-listing.tsx
+++ src/smc-webapp/lti/view/project/file-listing/file-listing.tsx
@@ -26,22 +26,27 @@ export function FileListing({
     if (selected_entries.has(ancestor + "/")) {
       is_ancestor_selected = true;
     }
-    return ancestor + "/" + folder
-  })
+    return ancestor + "/" + folder;
+  });
   const rows: JSX.Element[] = listing.map(path => {
-    const is_selected = is_ancestor_selected || selected_entries.has(current_directory + path);
+    const is_selected =
+      is_ancestor_selected || selected_entries.has(current_directory + path);
     return (
       <ItemRow role={"button"} highlight={is_selected} key={path}>
-        <CheckBox
-          checked={is_selected}
-          on_click={checked => {
-            if (checked) {
-              on_deselect(path);
-            } else {
-              on_select(path);
-            }
-          }}
-        />{" "}
+        {is_ancestor_selected ? (
+          <>🔒</>
+        ) : (
+          <CheckBox
+            checked={is_selected}
+            on_click={checked => {
+              if (checked) {
+                on_deselect(path);
+              } else {
+                on_select(path);
+              }
+            }}
+          />
+        )}{" "}
         <span
           onClick={() => {
             on_path_click(path);
