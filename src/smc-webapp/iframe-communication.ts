41af5789bd5873d31bd29e349ebf3de595a38457
Harald Schilly <hsy@sagemath.com>
2019-08-02 14:42:17 +0000

    webapp/iframe comms: status message, block almost all origins

--- src/smc-webapp/iframe-communication.ts
+++ src/smc-webapp/iframe-communication.ts
@@ -1,6 +1,18 @@
+// This listens to `postMessage`'s and dispatches actions accordingly.
+// For security reasons, this is very restrictive, involves extra checks, and only a selected set of origins is allowed
+//
+// TODO:
+// * some aspects of webapp are configured via a configuration endpoint. Use it to set the allowed origins
+
 import { is_valid_uuid_string } from "../smc-util/misc2";
 
 var initialized = false;
+
+const ALLOWED: ReadonlyArray<string> = Object.freeze<string>([
+  ".minervaproject.com",
+  ".kgi.com",
+  "dev.harald.schil.ly"
+]);
 
 function process_message(mesg) {
   // TODO whitelist mesg.origin domains
@@ -10,6 +22,20 @@ function process_message(mesg) {
     mesg.data
   );
 
+  // check origin
+  let blocked = true;
+  for (const allowed of ALLOWED) {
+    if (mesg.origin.endsWith(allowed)) {
+      blocked = false;
+      break;
+    }
+  }
+  if (blocked) {
+    console.log(`Origin '${mesg.origin}' is blocked.`);
+    return;
+  }
+
+  // check data
   const data = mesg.data;
   if (typeof data !== "object") return;
 
@@ -24,6 +50,16 @@ function process_message(mesg) {
       ) {
         window.alert(`OPEN PATH: ${project_id}/${path}`);
       }
+      break;
+
+    case "status":
+      // TODO reply with an "elaborate" status message, e.g. list each project ID, did it load (true/false), ...
+      const status = {
+        ready: true,
+        connection: "ok",
+        projects: { "9282d61d-8d27-4b9f-ae0f-2fc9bac64203": { ready: true } }
+      };
+      mesg.source.postMessage(status, mesg.origin);
       break;
 
     default:
