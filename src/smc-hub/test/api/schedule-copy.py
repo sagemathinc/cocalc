37fb1a678f7467e27b3c22f3d262fe4b6497d8e9
Harald Schilly <hsy@sagemath.com>
2019-08-22 08:31:19 +0000

    test/api: generalize manual schedule-copy test script

--- src/smc-hub/test/api/schedule-copy.py
+++ src/smc-hub/test/api/schedule-copy.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python3
 """
 Useful for manually testing scheduled copying of files
+
+To use this, create a file ~/test_api.yaml containing
+
+```
+key: "sk_x...."
+src_project: "...uuid..."
+target_project: "...uuid..."
+path: "foo.md"
+```
 """
 
 import time
@@ -11,29 +20,25 @@ import yaml
 from pprint import pprint
 import arrow
 from os.path import expanduser
+import yaml
 
-KEY = open(expanduser('~/test_api')).read().splitlines()[0]
-
+CONFIG = yaml.safe_load(open(expanduser('~/test_api.yaml')))
 
 def call_api(msg="",
              payload={},
-             sk=KEY,
-             base_url="https://test.cocalc.com",
-             max_retries=3,
+             base_url="https://cocalc.com",
              timeout=4):
     r"""
     generic API call with retries
 
     msg - string message type: "create_account", "create_project", etc.
     payload - dict of parameters for the call
-    sk - string, security key
-    retries - int, number of retries on post
 
     return python dict of API response object
     """
     s = requests.Session()
     url = f"{base_url}/api/v1/{msg}"
-    auth = HTTPBasicAuth(sk, '')
+    auth = HTTPBasicAuth(CONFIG['key'], '')
     headers = {'content-type': 'application/json'}
     r = s.post(url,
                auth=auth,
@@ -41,17 +46,20 @@ def call_api(msg="",
                headers=headers,
                timeout=timeout)
     print(f"STATUS: {r.status_code}")
-    return r.json()
+    try:
+        return r.json()
+    except:
+        print("No data")
 
 
 # jsonserialized timestamp, no timezone, a bit in the future...
-future = arrow.now('UTC').shift(seconds=+14).for_json()
+future = arrow.now('UTC').shift(seconds=+30).for_json()
 print(f"future = {future}")
 
 task = {
-    'src_project_id': 'c37fbd83-c4c3-4f92-b66c-37b8d2c8cdf1',
-    'target_project_id': '9282d61d-8d27-4b9f-ae0f-2fc9bac64203',
-    'src_path': 'x.md',
+    'src_project_id': CONFIG['src_project'],
+    'target_project_id': CONFIG['target_project'],
+    'src_path': CONFIG['path'],
     'scheduled': future
 }
 
