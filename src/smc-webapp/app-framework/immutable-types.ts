8388ed828cbfc574e268422934b2c0a386369b8d
John Jeng <j3@sagemath.com>
2019-09-18 18:41:41 +0800

    Avoid converting some built in class types

--- src/smc-webapp/app-framework/immutable-types.ts
+++ src/smc-webapp/app-framework/immutable-types.ts
@@ -1,22 +1,31 @@
 import { Collection, List } from "immutable";
 import { TypedMap } from "./TypedMap";
 
-
 export type Maybe<T> = T | undefined;
 // Return Maybe<U> iff T is a Maybe<?>
 export type CopyMaybe<T, U> = Maybe<T> extends T ? Maybe<U> : U;
 export type CopyAnyMaybe<T, U, V> = CopyMaybe<T, V> | CopyMaybe<U, V>;
 
+type CoveredJSBuiltInTypes =
+  | Date
+  | Map<any, any>
+  | Set<any>
+  | WeakMap<any, any>
+  | WeakSet<any>
+  | Promise<any>
+  | ArrayBuffer;
+
 // Higher Kinded Types could make this generic.
 // ie. if we could pass in an additional generic type U, then
 // all objects could be U<T>
 // As it is, right now that U<T> = TypeMap<T>
-export type DeepImmutable<T> = T extends Collection<any, any>
-  ? T // Any immutable.js data structure
+export type DeepImmutable<T> = T extends  // TODO: Make any non-plain-object retain it's type
+  | Collection<any, any>
+  | TypedMap<any>
+  | CoveredJSBuiltInTypes
+  ? T // Any of the types above should not be TypedMap-ified
   : T extends (infer U)[]
   ? List<U>
-  : T extends TypedMap<infer U>
-  ? TypedMap<U>
   : T extends object // Filter out desired objects above this line
   ? MapToRecurse<T>
   : T; // Base case primatives
