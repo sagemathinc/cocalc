3019ed81d3b907e6f3320e65357885f794f3387f
William Stein <wstein@sagemath.com>
2019-09-23 02:06:19 +0000

    timetravel: actually show diff

--- src/smc-webapp/frame-editors/time-travel-editor/diff-util.ts
+++ src/smc-webapp/frame-editors/time-travel-editor/diff-util.ts
@@ -3,6 +3,7 @@
 import { dmp } from "smc-util/sync/editor/generic/util";
 import { StringCharMapping } from "smc-util/misc";
 import * as CodeMirror from "codemirror";
+require('./style.sass')
 
 interface LineDiff {
   lines: string[];
@@ -46,15 +47,19 @@ function process_line_diff(
     let n2: number = x.start2;
     n1 += len_diff;
     len_diff += x.length1 - x.length2;
+    let sign: string;
     for (let z of x.diffs) {
       for (let c of z[1]) {
         if (z[0] === -1) {
+          sign = "-";
           n1 += 1;
           line_numbers.push([n1, ""]);
         } else if (z[0] === 1) {
+          sign = "+";
           n2 += 1;
           line_numbers.push(["", n2]);
         } else {
+          sign = " ";
           n1 += 1;
           n2 += 1;
           const key: string = `${n1}-${n2}`;
@@ -65,7 +70,7 @@ function process_line_diff(
           line_numbers.push([n1, n2]);
           seen_context[key] = true;
         }
-        lines.push(to_line[c]);
+        lines.push(sign + ' ' + to_line[c]);
         type.push(z[0]);
       }
     }
@@ -74,9 +79,58 @@ function process_line_diff(
   return { lines, type, line_numbers, chunk_boundaries };
 }
 
-export function set_cm_line_diff(cm: CodeMirror.Editor, v0: string, v1:string):void
-{
-  const ld : LineDiff = line_diff(v0, v1);
-  cm.setValueNoJump(JSON.stringify(ld, null, 2));
+export function set_cm_line_diff(
+  cm: CodeMirror.Editor,
+  v0: string,
+  v1: string
+): void {
+  const { lines, type, line_numbers, chunk_boundaries } = line_diff(v0, v1);
+  const s = lines.join("\n");
+  function line_number(i, k) {
+    return $(
+      `<span class='cocalc-history-diff-number'>${line_numbers[i][k]}</span>`
+    )[0];
+  }
+  cm.setValueNoJump(s);
 
+  // TODO: for now we force "default", since anything else is really confusing
+  // as it conflicts with the red/green diff coloring
+  cm.setOption("theme", "default");
+
+  cm.setOption("lineNumbers", false);
+  cm.setOption("showTrailingSpace", false);
+  cm.setOption("gutters", [
+    "cocalc-history-diff-gutter1",
+    "cocalc-history-diff-gutter2"
+  ]);
+  // highlight the lines based on type
+  for (let i = 0; i < type.length; i++) {
+    switch (type[i]) {
+      case -1: // deletion
+        for (let t of ["wrap", "gutter"]) {
+          cm.addLineClass(i, t, `cocalc-history-diff-${t}-delete`);
+          cm.removeLineClass(i, t, `cocalc-history-diff-${t}-insert`);
+        }
+        cm.setGutterMarker(i, "cocalc-history-diff-gutter1", line_number(i, 0));
+        break;
+      case 1: // addition
+        for (let t of ["wrap", "gutter"]) {
+          cm.addLineClass(i, t, `cocalc-history-diff-${t}-insert`);
+          cm.removeLineClass(i, t, `cocalc-history-diff-${t}-delete`);
+        }
+        cm.setGutterMarker(i, "cocalc-history-diff-gutter2", line_number(i, 1));
+        break;
+      case 0:  // context (stays the same)
+        for (let t of ["wrap", "gutter"]) {
+          cm.removeLineClass(i, t);
+          cm.removeLineClass(i, t);
+        }
+        cm.setGutterMarker(i, "cocalc-history-diff-gutter1", line_number(i, 0));
+        cm.setGutterMarker(i, "cocalc-history-diff-gutter2", line_number(i, 1));
+        break;
+    }
+  }
+  for (let i of chunk_boundaries) {
+    cm.addLineClass(i, "wrap", "cocalc-history-diff-wrap-divide");
+  }
 }
