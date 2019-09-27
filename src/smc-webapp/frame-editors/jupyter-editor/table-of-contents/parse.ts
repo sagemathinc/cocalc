fcb63a43585b653fc4b38b72c6a726653f8f30a3
William Stein <wstein@sagemath.com>
2019-09-27 04:42:52 +0000

    jupyter/nbgrader: make table of contents nbgrader aware (it's not aware of state yet; this is just a tiny start)

--- src/smc-webapp/frame-editors/jupyter-editor/table-of-contents/parse.ts
+++ src/smc-webapp/frame-editors/jupyter-editor/table-of-contents/parse.ts
@@ -8,6 +8,7 @@ export interface TableOfContentsInfo {
   id: string;
   level: number;
   value: string;
+  icon: string;
 }
 
 export function parse_headings(
@@ -15,14 +16,41 @@ export function parse_headings(
   cell_list: List<string>
 ): TableOfContentsInfo[] {
   const v: TableOfContentsInfo[] = [];
+  let last_level: number = 0,
+    answer_number: number = 0;
   cell_list.forEach((id: string) => {
     const cell = cells.get(id);
-    if (cell == null || cell.get("cell_type") != "markdown") return;
+    if (cell == null) return;
+    const nbgrader = cell.getIn(["metadata", "nbgrader"]);
+    if (nbgrader != null) {
+      if (nbgrader.get("solution")) {
+        // It's where a student enters an answer.
+        answer_number += 1;
+        v.push({
+          id,
+          level: last_level + 1,
+          value: `Answer ${answer_number}`,
+          icon: "graduation-cap"
+        });
+      } else if (nbgrader.get("grade")) {
+        // solution is false but grade is true, so it's a test cell
+        v.push({
+          id,
+          level: last_level + 1,
+          value: `Test of answer ${answer_number}`,
+          icon: "equals"
+        });
+      }
+    }
+
+    if (cell.get("cell_type") != "markdown") return;
+
     const { level, value } = parse_cell_heading(cell.get("input"));
     if (level > 0) {
+      last_level = level;
       const id = cell.get("id");
       if (id == null) return;
-      v.push({ id, level, value });
+      v.push({ id, level, value, icon: "minus" });
     }
   });
   return v;
