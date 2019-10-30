6807632d994d3bbd93b0cf51ab3a847ce9533ab8
John Jeng <j3@sagemath.com>
2019-10-30 06:18:50 -0700

    Turn off camelCase rule

--- src/.eslintrc.js
+++ src/.eslintrc.js
@@ -16,6 +16,8 @@ module.exports = {
   rules: {
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
     // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+    "camelcase": "off",
+    "@typescript-eslint/camelcase": "off"
   },
   settings: {
     react: {
