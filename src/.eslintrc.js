d33991e0278da1aee9db1abb98cc75740a999bc2
John Jeng <j3@sagemath.com>
2019-10-31 11:13:01 -0700

    Add rule requiring curly brackets for arrow functions

--- src/.eslintrc.js
+++ src/.eslintrc.js
@@ -21,7 +21,9 @@ module.exports = {
     "@typescript-eslint/no-explicit-any": "off",
 
     "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn"
+    "react-hooks/exhaustive-deps": "warn",
+
+    "arrow-body-style": "always"
   },
   settings: {
     react: {
