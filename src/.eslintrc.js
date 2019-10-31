16f25fdda8ff6c00d9c98782ac64ce03ac3c69d4
John Jeng <j3@sagemath.com>
2019-10-31 09:04:59 -0700

    Add linter for react hooks

--- src/.eslintrc.js
+++ src/.eslintrc.js
@@ -1,29 +1,31 @@
 module.exports = {
-  parser: "@typescript-eslint/parser", // Specifies the ESLint parser
+  parser: "@typescript-eslint/parser",
   extends: [
-    "plugin:react/recommended", // Uses the recommended rules from @eslint-plugin-react
-    "plugin:@typescript-eslint/recommended", // Uses the recommended rules from @typescript-eslint/eslint-plugin
-    "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
-    "plugin:prettier/recommended" // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+    "plugin:react/recommended",
+    "plugin:react-hooks",
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint", // Disables rules from @typescript-eslint that would conflict with Prettier
+    "plugin:prettier/recommended" // Make sure this always the last config to be loaded.
   ],
   parserOptions: {
-    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
-    sourceType: "module", // Allows for the use of imports
+    ecmaVersion: 2018,
+    sourceType: "module",
     ecmaFeatures: {
-      jsx: true // Allows for the parsing of JSX
+      jsx: true
     }
   },
   rules: {
-    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
-    // e.g. "@typescript-eslint/explicit-function-return-type": "off",
-    "camelcase": "off",
+    camelcase: "off",
     "@typescript-eslint/camelcase": "off",
     "@typescript-eslint/no-inferrable-types": "off",
-    "@typescript-eslint/no-explicit-any": "off"
+    "@typescript-eslint/no-explicit-any": "off",
+
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn"
   },
   settings: {
     react: {
-      version: "detect" // Tells eslint-plugin-react to automatically detect the version of React to use
+      version: "detect"
     }
   }
 };
