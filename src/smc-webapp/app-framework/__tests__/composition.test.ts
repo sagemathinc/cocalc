a82a92102b66a859cdd06502aa625ede41c44157
John Jeng <j3@sagemath.com>
2019-11-21 09:26:40 -0800

    Fix tests

--- src/smc-webapp/app-framework/__tests__/composition.test.ts
+++ src/smc-webapp/app-framework/__tests__/composition.test.ts
@@ -8,17 +8,17 @@ test("Composition", () => {
     | {
         type2: "b";
       };
-  
-  type ImmutableAction = TypedMap<Action>
-  let Action: ImmutableAction = "" as any;
-  let m = Action.get("type")
+
+  type ImmutableAction = TypedMap<Action>;
+  const Action: ImmutableAction = "" as any;
+  const m = Action.get("type");
   return m;
 });
 
 test("Composition2", () => {
   type Parent = {
-    action: Action
-  }
+    action: Action;
+  };
   type Action =
     | {
         type: "a";
@@ -26,10 +26,10 @@ test("Composition2", () => {
     | {
         type2: "b";
       };
-  
-  type ImmutableParent = TypedMap<Parent>
-  let A: ImmutableParent = "" as any;
-  let m = A.get("action")
+
+  type ImmutableParent = TypedMap<Parent>;
+  const A: ImmutableParent = "" as any;
+  const m = A.get("action");
   //const q = m.get("type") // If parent holds a type2, this is unsafe!
   return m;
 });
