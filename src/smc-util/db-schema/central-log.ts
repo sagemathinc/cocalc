b970a4b40096dcc972f650a3de3e604314958cb9
John Jeng <john.jeng5@gmail.com>
2020-01-20 21:11:42 +0000

    Refactor central log

---
+++ src/smc-util/db-schema/central-log.ts
@@ -0,0 +1,29 @@
+import { create } from "./types";
+
+export const central_log = create({
+  fields: {
+    id: {
+      type: "uuid",
+      desc: "generic id for this event"
+    },
+    event: {
+      type: "string",
+      desc: "any even name which should not conflict with other names"
+    },
+    value: {
+      type: "map",
+      desc: "Any json type data for this event"
+    },
+    time: {
+      type: "timestamp",
+      desc: "When the event took place"
+    }
+  },
+  rules: {
+    desc:
+      "Table for logging system stuff that happens.  Meant to help in running and understanding the system better.",
+    primary_key: "id",
+    durability: "soft", // loss of some log data not serious, since used only for analytics
+    pg_indexes: ["time", "event"]
+  }
+});
