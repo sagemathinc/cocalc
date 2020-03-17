821e7669ca0f9a195c2f7dab420e30f5c0e429c9
Harald Schilly <hsy@sagemath.com>
2020-03-17 10:17:52 +0000

    webapp: fixing firefox display of uncommitted saving warning -- #4434

--- src/smc-webapp/r_misc/uncommited-changes.tsx
+++ src/smc-webapp/r_misc/uncommited-changes.tsx
@@ -16,15 +16,16 @@ const STYLE: React.CSSProperties = {
   fontWeight: "bold",
   marginLeft: "5px",
   marginRight: "-5px",
-  borderRadius: "3px"
+  borderRadius: "3px",
+  whiteSpace: "nowrap"
 };
 
 /**
-* Shows `NOT saved!` if `has_uncommitted_changes` is true for ~delay_ms time.
-* Shows nothing if `has_uncommitted_changes` is false
-*
-* Does not work with changes to `delay_ms`
-*/
+ * Shows `NOT saved!` if `has_uncommitted_changes` is true for ~delay_ms time.
+ * Shows nothing if `has_uncommitted_changes` is false
+ *
+ * Does not work with changes to `delay_ms`
+ */
 export function UncommittedChanges({
   has_uncommitted_changes,
   delay_ms = 5000
