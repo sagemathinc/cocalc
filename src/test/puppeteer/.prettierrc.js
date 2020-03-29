858652f39c273f55ab9a18752efadb6d9815effa
Harald Schilly <hsy@sagemath.com>
2020-03-29 09:51:24 +0000

    prettier2: couple more .js files  we edit

--- src/test/puppeteer/.prettierrc.js
+++ src/test/puppeteer/.prettierrc.js
@@ -1,6 +1,6 @@
-module.exports =  {
-  semi:  true,
-  trailingComma:  'none',
-  printWidth:  120,
-  tabWidth:  2,
+module.exports = {
+  semi: true,
+  trailingComma: "none",
+  printWidth: 120,
+  tabWidth: 2
 };
