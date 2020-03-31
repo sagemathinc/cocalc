ab7497f0c3ab8c28f1fedbcafffef34ac05ac4e2
William A. Stein <wstein@sagemath.com>
2020-03-31 00:19:09 +0000

    make file rename more robust, especially regarding Jupyter notebooks

--- src/smc-project/browser-websocket/move-files.ts
+++ src/smc-project/browser-websocket/move-files.ts
@@ -1,5 +1,9 @@
 import { move, pathExists } from "fs-extra";
+import { stat } from "fs";
 import { path_split, hidden_meta_file } from "../smc-util/misc2";
+import { get_listings_table } from "../sync/listings";
+import { callback } from "awaiting";
+import { move_file_variations } from "../smc-util/delete-files";
 
 function chat_file(path: string): string {
   return hidden_meta_file(path, "sage-chat");
@@ -44,15 +48,28 @@ export async function rename_file(
   dest: string,
   logger: any
 ): Promise<void> {
+  if (src == dest) return; // no-op
   const HOME = home();
   src = HOME + "/" + src;
   dest = HOME + "/" + dest;
   logger.debug(`rename_file ${src} --> ${dest}`);
-  await move(src, dest);
-  const chat = chat_file(src);
-  if (await pathExists(chat)) {
-    const chat_target = chat_file(dest);
-    logger.debug("rename_file chat exists so ${chat} --> ${chat_target}");
-    await move(chat, chat_target);
+  const listings = get_listings_table();
+  await listings?.set_deleted(src); // todo: later may have a set_moved...
+  const to_move: { src: string; dest: string }[] = [{ src, dest }];
+
+  try {
+    const s = await callback(stat, src);
+    if (!s.isDirectory()) {
+      for (const variation of move_file_variations(src, dest)) {
+        if (await pathExists(variation.src)) {
+          await listings?.set_deleted(variation.src);
+          to_move.push(variation);
+        }
+      }
+    }
+  } catch (_err) {}
+
+  for (const x of to_move) {
+    await move(x.src, x.dest);
   }
 }
