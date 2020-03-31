ed0d03f66560177a53fe54ab6beed21c6c21f45a
William A. Stein <wstein@sagemath.com>
2020-03-31 00:34:09 +0000

    make path move more robust

--- src/smc-project/browser-websocket/move-files.ts
+++ src/smc-project/browser-websocket/move-files.ts
@@ -1,13 +1,9 @@
 import { move, pathExists } from "fs-extra";
 import { stat } from "fs";
-import { path_split, hidden_meta_file } from "../smc-util/misc2";
+import { path_split } from "../smc-util/misc2";
 import { get_listings_table } from "../sync/listings";
 import { callback } from "awaiting";
 import { move_file_variations } from "../smc-util/delete-files";
-
-function chat_file(path: string): string {
-  return hidden_meta_file(path, "sage-chat");
-}
 
 function home(): string {
   const { HOME } = process.env;
@@ -28,18 +24,31 @@ export async function move_files(
     dest = HOME + "/" + dest;
   }
   dest += "/";
+  const to_move: { src: string; dest: string }[] = [];
+  const listings = get_listings_table();
   for (let path of paths) {
     path = HOME + "/" + path;
     const target = dest + path_split(path).tail;
     logger.debug(`move_file ${path} --> ${target}`);
-    await move(path, target);
-    const chat = chat_file(path);
-    logger.debug(`move_file chat=${chat}`);
-    if (await pathExists(chat)) {
-      const chat_target = dest + path_split(chat).tail;
-      logger.debug(`move_file chat exists so ${chat} --> ${chat_target}`);
-      await move(chat, chat_target);
-    }
+    await listings?.set_deleted(path);
+    to_move.push({ src: path, dest: target });
+
+    // and the aux files:
+    try {
+      const s = await callback(stat, path);
+      if (!s.isDirectory()) {
+        for (const variation of move_file_variations(path, target)) {
+          if (await pathExists(variation.src)) {
+            await listings?.set_deleted(variation.src);
+            to_move.push(variation);
+          }
+        }
+      }
+    } catch (_err) {}
+  }
+
+  for (const x of to_move) {
+    await move(x.src, x.dest);
   }
 }
 
