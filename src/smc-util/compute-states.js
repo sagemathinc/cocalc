577740c33468e6345ae6fb5691d73a1656ef19e9
Harald Schilly <hsy@sagemath.com>
2020-04-27 15:24:53 +0200

    license: prepping .js

--- src/smc-util/compute-states.js
+++ src/smc-util/compute-states.js
@@ -1,40 +1,16 @@
-//##############################################################################
+// Compute related schema stuff (see compute.coffee)
 //
-//    CoCalc: Collaborative Calculation in the Cloud
+// Here's a picture of the finite state machine defined below:
 //
-//    Copyright (C) 2016, Sagemath Inc.
+//                     ----------[closing] ------- --------- [stopping] <--------
+//                    \|/                        \|/                           |
+// [archived] <-->  [closed] --> [opening] --> [opened] --> [starting] --> [running]
+//                                              /|\                          /|\
+//        [unarchiving]                          |                            |
+//        [archiving]                           \|/                          \|/
+//                                       [saving]  [pending]               [saving]
 //
-//    This program is free software: you can redistribute it and/or modify
-//    it under the terms of the GNU General Public License as published by
-//    the Free Software Foundation, either version 3 of the License, or
-//    (at your option) any later version.
-//
-//    This program is distributed in the hope that it will be useful,
-//    but WITHOUT ANY WARRANTY; without even the implied warranty of
-//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//    GNU General Public License for more details.
-//
-//    You should have received a copy of the GNU General Public License
-//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-//
-//##############################################################################
-
-/*
-Compute related schema stuff (see compute.coffee)
-
-Here's a picture of the finite state machine defined below:
-
-                    ----------[closing] ------- --------- [stopping] <--------
-                   \|/                        \|/                           |
-[archived] <-->  [closed] --> [opening] --> [opened] --> [starting] --> [running]
-                                             /|\                          /|\
-       [unarchiving]                          |                            |
-       [archiving]                           \|/                          \|/
-                                      [saving]  [pending]               [saving]
-
-The icon names below refer to font-awesome, and are used in the UI.
-
-*/
+// The icon names below refer to font-awesome, and are used in the UI.
 
 exports.COMPUTE_STATES = {
   archived: {
--- src/smc-util/syncstring_schema.js
+++ src/smc-util/syncstring_schema.js
@@ -1,33 +1,4 @@
-/*
- * decaffeinate suggestions:
- * DS102: Remove unnecessary code created because of implicit returns
- * DS207: Consider shorter variations of null checks
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
- */
-//##############################################################################
-//
-//    CoCalc: Collaborative Calculation in the Cloud
-//
-//    Copyright (C) 2016, Sagemath Inc.
-//
-//    This program is free software: you can redistribute it and/or modify
-//    it under the terms of the GNU General Public License as published by
-//    the Free Software Foundation, either version 3 of the License, or
-//    (at your option) any later version.
-//
-//    This program is distributed in the hope that it will be useful,
-//    but WITHOUT ANY WARRANTY; without even the implied warranty of
-//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//    GNU General Public License for more details.
-//
-//    You should have received a copy of the GNU General Public License
-//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-//
-//##############################################################################
-
-/*
-Schema for synchronized editing of strings.
-*/
+// Schema for synchronized editing of strings.
 
 const misc = require("./misc");
 
