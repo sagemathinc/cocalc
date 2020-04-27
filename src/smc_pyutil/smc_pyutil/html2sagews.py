4b75ae54d51cf861958b03f3921902eb8ef0ea26
Harald Schilly <hsy@sagemath.com>
2020-04-27 15:01:30 +0200

    license: add header to smc_pyutil + formatting

--- src/smc_pyutil/smc_pyutil/html2sagews.py
+++ src/smc_pyutil/smc_pyutil/html2sagews.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
+# This file is part of CoCalc: Copyright © 2020 Sagemath, Inc.
+# License: AGPLv3 s.t. "Commons Clause" – read LICENSE.md for details
+
 # this script converts an html-exported sagews file back to sagews
 
 from __future__ import print_function, unicode_literals
