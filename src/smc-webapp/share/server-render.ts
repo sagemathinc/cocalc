227f8b73e0ee26139326a33a30702c21f0f1622c
William A. Stein <wstein@sagemath.com>
2020-04-30 17:26:10 +0000

    rewrite art.coffee in typescript

--- src/smc-webapp/share/server-render.ts
+++ src/smc-webapp/share/server-render.ts
@@ -16,16 +16,39 @@ set_share_server(true);
 // Load katex jQuery plugin.
 require("../jquery-plugins/katex");
 
-export function render(res, component, extra: any = ""): void {
+/* Regarding the stream parameter below:
+   if true, use streaming non-blocking rendering;
+   if false, blocks the server (no reason to use stream=false)
+   There should never be a reason to use stream=false, except
+   maybe for testing, but I implemented it so left it in.
+*/
+export function render(
+  res,
+  component,
+  extra: any = "",
+  stream: boolean = true
+): void {
   res.type("html");
   res.write("<!DOCTYPE html>");
   const t0 = new Date().valueOf();
-  const stream = ReactDOMServer.renderToStaticNodeStream(component);
-  stream.pipe(res);
-  stream.once("end", () =>
+  if (stream) {
+    const s = ReactDOMServer.renderToStaticNodeStream(component);
+    s.pipe(res);
+    s.once("end", () =>
+      console.log(
+        `react: time to render and stream out: ${new Date().valueOf() - t0}ms`,
+        extra
+      )
+    );
+  } else {
+    const s = ReactDOMServer.renderToStaticMarkup(component);
+    res.write(s);
     console.log(
-      `react: time to render and stream out: ${new Date().valueOf() - t0}ms`,
+      `react: time to render to string: ${
+        new Date().valueOf() - t0
+      }ms; length of string: ${s.length}`,
       extra
-    )
-  );
+    );
+    res.end();
+  }
 }
