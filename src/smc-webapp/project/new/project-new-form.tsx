495fc21257f28f8cac021f187273eb91ddc999a7
William A. Stein <wstein@sagemath.com>
2020-05-17 16:52:14 +0000

    use PathNavigator more (for new files and search)

--- src/smc-webapp/project/new/project-new-form.tsx
+++ src/smc-webapp/project/new/project-new-form.tsx
@@ -25,12 +25,12 @@ import { ProjectActions } from "../../project_actions";
 const { special_filenames_with_no_extension } = require("../../project_file");
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { SMC_Dropzone } = require("../../smc-dropzone");
-import { PathLink } from "./path-link";
 import { NewFileButton } from "./new-file-button";
 import { NewFileDropdown } from "./new-file-dropdown";
 import { FileTypeSelector } from "./file-type-selector";
 import { AvailableFeatures } from "./types";
 import { ProjectMap } from "smc-webapp/todo-types";
+import { PathNavigator } from "../explorer/path-navigator";
 
 interface ReactProps {
   project_id: string;
@@ -356,9 +356,9 @@ export const ProjectNewForm = rclass<ReactProps>(
         return (
           <span>
             Create new files in{" "}
-            <PathLink
-              path={this.props.current_path}
-              actions={this.props.actions}
+            <PathNavigator
+              project_id={this.props.project_id}
+              style={{ display: "inline" }}
             />
           </span>
         );
--- src/smc-webapp/project/search/header.tsx
+++ src/smc-webapp/project/search/header.tsx
@@ -1,25 +1,17 @@
-import { PathLink } from "../new/path-link";
-import { React, useRedux, redux } from "../../app-framework";
+import { React } from "../../app-framework";
 import { Icon } from "../../r_misc";
+import { PathNavigator } from "../explorer/path-navigator";
 
 export const ProjectSearchHeader: React.FC<{ project_id: string }> = ({
   project_id,
 }) => {
-  const current_path = useRedux(["current_path"], project_id);
   return (
     <h1 style={{ marginTop: "0px" }}>
       <Icon name="search" /> Search{" "}
       <span className="hidden-xs">
         {" "}
         in{" "}
-        {current_path ? (
-          <PathLink
-            path={current_path}
-            actions={redux.getProjectActions(project_id)}
-          />
-        ) : (
-          "home directory"
-        )}
+        <PathNavigator project_id={project_id} style={{ display: "inline" }} />
       </span>
     </h1>
   );
