553d4c5e7f0476abff4edff252767dc59eca9be7
William A. Stein <wstein@sagemath.com>
2020-05-17 04:41:46 +0000

    rewrite OneSSHKey

--- src/smc-webapp/account/ssh-keys/ssh-key-list.tsx
+++ src/smc-webapp/account/ssh-keys/ssh-key-list.tsx
@@ -1,9 +1,10 @@
 import { Map } from "immutable";
-import { React } from "../../app-framework";
-import { HelpIcon, Icon, Space } from "../../r_misc";
-const { OneSSHKey } = require("../../widget-ssh-keys/main");
+import { Popconfirm } from "antd";
+
+import { React, redux } from "../../app-framework";
+import { HelpIcon, Icon, Space, TimeAgo } from "../../r_misc";
 import { cmp } from "smc-util/misc";
-import { Panel } from "../../antd-bootstrap";
+import { Panel, Row, Col, Button } from "../../antd-bootstrap";
 
 // Children are rendered above the list of SSH Keys
 // Takes an optional Help string or node to render as a help modal
@@ -68,5 +69,80 @@ export const SSHKeyList: React.FC<{
       {children}
       {render_keys()}
     </Panel>
+  );
+};
+
+const OneSSHKey: React.FC<{
+  ssh_key: Map<string, any>;
+  project_id?: string;
+}> = ({ ssh_key, project_id }) => {
+  function render_last_use(): JSX.Element {
+    const d = ssh_key.get("last_use_date");
+    if (d) {
+      return (
+        <div style={{ color: "#1e7e34" }}>
+          Last used <TimeAgo date={new Date(d)} />
+        </div>
+      );
+    } else {
+      return <div style={{ color: "#333" }}>Never used</div>;
+    }
+  }
+
+  function delete_key() : void {
+    const fingerprint = ssh_key.get("fingerprint");
+    if (project_id) {
+      redux.getActions("projects").delete_ssh_key_from_project({
+        fingerprint,
+        project_id: project_id,
+      });
+    } else {
+      redux.getActions("account").delete_ssh_key(fingerprint);
+    }
+  }
+
+  const key_style: React.CSSProperties = {
+    fontSize: "42px",
+    color: ssh_key.get("last_use_date") ? "#1e7e34" : undefined,
+  };
+
+  return (
+    <Row
+      style={{
+        border: "1px solid lightgray",
+        padding: "5px",
+        marginBottom: "5px",
+      }}
+    >
+      <Col md={1}>
+        <Icon style={key_style} name="key" />
+      </Col>
+      <Col md={8}>
+        <div style={{ fontWeight: 600 }}>{ssh_key.get("title")}</div>
+        <span style={{ fontWeight: 600 }}>Fingerprint: </span>
+        <code>{ssh_key.get("fingerprint")}</code>
+        <br />
+        Added on {new Date(ssh_key.get("creation_date")).toLocaleDateString()}
+        {render_last_use()}
+      </Col>
+      <Col md={3}>
+        <Popconfirm
+          title={
+            <div>
+              Are you sure you want to delete this SSH key? <br />
+              This CANNOT be undone. <br /> If you want to reuse this key in the
+              future, you will have to upload it again.
+            </div>
+          }
+          onConfirm={() => delete_key()}
+          okText={"Yes, delete key"}
+          cancelText={"Cancel"}
+        >
+          <Button bsStyle="warning" bsSize="small" style={{ float: "right" }}>
+            Delete...
+          </Button>
+        </Popconfirm>
+      </Col>
+    </Row>
   );
 };
