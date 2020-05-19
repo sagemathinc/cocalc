62b9d10daf667c4c6361b4fadc7e961ab896491b
Harald Schilly <hsy@sagemath.com>
2020-05-19 08:36:50 +0000

    hub/analytics: use DNS setting and expire data after the retention period is over

--- src/smc-hub/utils.ts
+++ src/smc-hub/utils.ts
@@ -40,6 +40,19 @@ export async function get_server_settings(
   });
 }
 
+// this converts what's in the pii_expired setting to a new Date in the future
+export function pii_retention_to_future<T extends object>(
+  pii_retention: number | false,
+  data?: T & { expire?: Date }
+): Date | undefined {
+  if (!pii_retention) return;
+  const future: Date = expire_time(pii_retention);
+  if (data != null) {
+    data.expire = future;
+  }
+  return future;
+}
+
 // use this to get the "expire" value for storing certain entries in the DB,
 // which contain personally identifiable information.
 // if data is set, it's expire field will be set. in any case, it returns the "Date"
@@ -49,11 +62,5 @@ export async function pii_expire<T extends object>(
   data?: T & { expire?: Date }
 ): Promise<Date | undefined> {
   const settings = await get_server_settings(db);
-  const secs: number | false = settings.pii_retention;
-  if (!secs) return;
-  const future: Date = expire_time(secs);
-  if (data != null) {
-    data.expire = future;
-  }
-  return future;
+  return pii_retention_to_future<T>(settings.pii_retention, data);
 }
