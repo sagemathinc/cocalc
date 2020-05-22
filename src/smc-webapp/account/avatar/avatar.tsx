e38bcfc676575d1c94da72ca21e1dd2d04d7f8e8
Harald Schilly <hsy@sagemath.com>
2020-05-22 10:20:36 +0000

    add missing license banner

--- src/smc-webapp/account/avatar/avatar.tsx
+++ src/smc-webapp/account/avatar/avatar.tsx
@@ -1,3 +1,8 @@
+/*
+ *  This file is part of CoCalc: Copyright © 2020 Sagemath, Inc.
+ *  License: AGPLv3 s.t. "Commons Clause" – see LICENSE.md for details
+ */
+
 import * as onecolor from "onecolor";
 import { Map } from "immutable";
 import { OverlayTrigger, Tooltip } from "react-bootstrap";
--- src/smc-webapp/account/avatar/users-viewing.tsx
+++ src/smc-webapp/account/avatar/users-viewing.tsx
@@ -1,3 +1,8 @@
+/*
+ *  This file is part of CoCalc: Copyright © 2020 Sagemath, Inc.
+ *  License: AGPLv3 s.t. "Commons Clause" – see LICENSE.md for details
+ */
+
 import useInterval from "use-interval";
 import { merge, cmp, copy } from "smc-util/misc";
 import { React, redux, useMemo, useRedux, useState } from "../../app-framework";
