71e0d077635386a6330d82132b83c39a357a148a
Harald Schilly <hsy@sagemath.com>
2020-05-29 09:11:38 +0000

    compute image: typo & clarifying future role of "default"

--- src/smc-util/compute-images.js
+++ src/smc-util/compute-images.js
@@ -12,12 +12,18 @@ const { DEFAULT_COMPUTE_IMAGE } = require("smc-util/db-schema");
 exports.DEFAULT_COMPUTE_IMAGE = DEFAULT_COMPUTE_IMAGE;
 
 const COMPUTE_IMAGES = {
-  [DEFAULT_COMPUTE_IMAGE]: {
+  // this is called "default", but treat it as if it is ubuntu1804
+  // later, we'll switch DEFAULT_COMPUTE_IMAGE to be "ubuntu2004"
+  default: {
     order: -10,
     title: "Default",
     descr: "Regularly updated, well tested.",
   },
-  previous: { order: -9, title: "Previous", descr: "One or two weeks behind 'default'" },
+  previous: {
+    order: -9,
+    title: "Previous",
+    descr: "One or two weeks behind 'default'",
+  },
   exp: {
     order: -1,
     title: "Experimental",
@@ -25,27 +31,27 @@ const COMPUTE_IMAGES = {
   },
   "stable-2018-08-27": {
     title: "2018-08-27",
-    descr: "Frozen at 2018-08-27 and no longer updated",
+    descr: "Frozen on 2018-08-27 and no longer updated",
   },
   "stable-2019-01-12": {
     title: "2019-01-12",
-    descr: "Frozen at 2019-01-12 and no longer updated",
+    descr: "Frozen on 2019-01-12 and no longer updated",
   },
   "stable-2019-07-15": {
     title: "2019-07-15",
-    descr: "Frozeon at 2019-07-15 and no longer updated",
+    descr: "Frozen on 2019-07-15 and no longer updated",
   },
   "stable-2019-10-25_ro": {
     title: "2019-10-25",
-    descr: "Frozeon at 2019-10-25 and no longer updated",
+    descr: "Frozen on 2019-10-25 and no longer updated",
   },
   "stable-2019-12-15_ro": {
     title: "2019-12-15",
-    descr: "Frozeon at 2019-12-15 and no longer updated",
+    descr: "Frozen on 2019-12-15 and no longer updated",
   },
   "stable-2020-01-26_ro": {
     title: "2020-01-26",
-    descr: "Frozeon at 2020-01-26 and no longer updated",
+    descr: "Frozen on 2020-01-26 and no longer updated",
   },
   ubuntu2004: {
     order: 1,
