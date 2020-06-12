261f2653b77c5feb4db8e80560635a0b6661fca0
William A. Stein <wstein@sagemath.com>
2020-06-12 18:45:28 +0000

    fix bug in bind_methods for binding all methods

--- src/smc-util/misc2.ts
+++ src/smc-util/misc2.ts
@@ -557,18 +557,36 @@ export function unreachable(x: never) {
   throw new Error(`All types should be exhausted, but I got ${x}`);
 }
 
-// Bind all or specified methods of the object to this.  If method_names
-// is not given binds all methods.  Use this instead of tons of binds.
+// Get *all* methods of an object (including from base classes!).
+// See https://flaviocopes.com/how-to-list-object-methods-javascript/
+// This is used by bind_methods below to bind all methods
+// of an instance of an object, all the way up the
+// prototype chain, just to be 100% sure!
+function get_methods(obj: object): string[] {
+  let properties = new Set<string>();
+  let current_obj = obj;
+  do {
+    Object.getOwnPropertyNames(current_obj).map((item) => properties.add(item));
+  } while ((current_obj = Object.getPrototypeOf(current_obj)));
+  return [...properties.keys()].filter(
+    (item) => typeof obj[item] === "function"
+  );
+}
+
+// Bind all or specified methods of the object.  If method_names
+// is not given, binds **all** methods.
 // For example, in a base class constructor, you can do
 //       bind_methods(this);
 // and every method will always be bound even for derived classes
-// (assuming they call super!).
+// (assuming they call super if they overload the constructor!).
+// Do this for classes that don't get created in a tight inner
+// loop and for which you want 'safer' semantics.
 export function bind_methods(
   obj: object,
   method_names: undefined | string[] = undefined
 ): void {
   if (method_names === undefined) {
-    method_names = Object.getOwnPropertyNames(Object.getPrototypeOf(obj));
+    method_names = get_methods(obj);
     method_names.splice(method_names.indexOf("constructor"), 1);
   }
   for (const method_name of method_names) {
