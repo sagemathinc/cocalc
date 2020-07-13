ada2604a79ad500022c049175c08dc762493b937
marts2007 <admin.marts@gmail.com>
2020-07-13 19:52:47 +0300

    Update upgrade-spec.js

--- src/smc-util/upgrade-spec.js
+++ src/smc-util/upgrade-spec.js
@@ -21,7 +21,7 @@ const upgrades = (exports.upgrades = {});
 // these are the base quotas
 exports.DEFAULT_QUOTAS = {
   disk_quota: 3000,
-  cores: 1,
+  cores: 1.5,
   cpu_shares: 0,
   memory: 3000,
   memory_request: 0,
