23cf50834ec1c8c3c944a7d6cd4624510e8f3e1e
William Stein <wstein@sagemath.com>
2020-08-12 21:03:05 +0000

    Revert "make project listings persistent, which would fix #4790 (though we will also fix the root cause in another commit); having this be persistent is better anyways"
    This reverts commit 29bf47e7394a9bbde1de95ac97e5bebf2cb6b9a8.

--- src/smc-webapp/project/websocket/listings.ts
+++ src/smc-webapp/project/websocket/listings.ts
@@ -252,9 +252,7 @@ export class Listings extends EventEmitter {
       ?.get("missing");
   }
 
-  public async get_listing_directly(
-    path: string
-  ): Promise<DirectoryListingEntry[]> {
+  public async get_listing_directly(path: string): Promise<DirectoryListingEntry[]> {
     const store = redux.getStore("projects");
     // make sure that our relationship to this project is known.
     if (store == null) throw Error("bug");
@@ -313,7 +311,7 @@ export class Listings extends EventEmitter {
           },
         ],
       },
-      [{ persistent: true }]
+      []
     );
 
     if ((this.state as State) == "closed") return;
