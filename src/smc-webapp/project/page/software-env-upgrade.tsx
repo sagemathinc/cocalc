a03ebe7a0eff320fc50eb0ff955a89d656d220e0
Harald Schilly <hsy@sagemath.com>
2020-09-02 18:26:39 +0000

    webapp/software update: reorder some lines to avoid violating hook rules

--- src/smc-webapp/project/page/software-env-upgrade.tsx
+++ src/smc-webapp/project/page/software-env-upgrade.tsx
@@ -52,25 +52,17 @@ function useComputeImage(project_id) {
 export const SoftwareEnvUpgrade: React.FC<{ project_id: string }> = ({
   project_id,
 }) => {
-  // if we're outside cocalc.com, this is not applicable
+  // if we're outside cocalc.com, this is not applicable. we can assume this value never changes.
   const customize_kucalc = useTypedRedux("customize", "kucalc");
   if (customize_kucalc !== KUCALC_COCALC_COM) return null;
 
   const [updating, set_updating] = React.useState(false);
   const [hide, set_hide] = React.useState(false);
   const compute_image = useComputeImage(project_id);
-  if (compute_image == null) return null;
-  if (TO_UPGRADE.indexOf(compute_image) == -1) return null;
+  const projects_store = useStore("projects");
 
   // don't tell students to update. Less surprises and let the teacher controls this…
-  const projects_store = useStore("projects");
   const is_student_project = projects_store.is_student_project(project_id);
-  if (is_student_project) return null;
-
-  // just a safety measure, before accessing .title
-  if (COMPUTE_IMAGES[compute_image] == null) return null;
-  const oldname = COMPUTE_IMAGES[compute_image].title;
-  const newname = COMPUTE_IMAGES[DEFAULT_COMPUTE_IMAGE].title;
 
   async function set_image(image: string) {
     set_updating(true);
@@ -107,37 +99,42 @@ export const SoftwareEnvUpgrade: React.FC<{ project_id: string }> = ({
 
   // we only want to re-render if it is really necessary. the "project_map" changes quite often…
   return React.useMemo(() => {
-    if (hide) {
-      return null;
-    } else {
-      return (
-        <Alert bsStyle={"info"} style={UPGRADE_STYLE}>
-          <div style={{ display: "flex" }}>
-            <div style={{ flex: "1 1 auto" }}>
-              <Icon name="exclamation-triangle" />{" "}
-              <strong>Software Update Available!</strong>{" "}
-              <VisibleMDLG>
-                Update this project's software environment from "{oldname}" to "
-                {newname}". Learn more about{" "}
-                <A href={DOC_UBUNTU_2004}>all changes</A>.
-              </VisibleMDLG>
-              <VisibleXSSM>
-                {" "}
-                <A href={DOC_UBUNTU_2004}>Learn more ...</A>
-              </VisibleXSSM>
-              <VisibleMDLG>
-                <br />
-                <span style={{ color: COLORS.GRAY }}>
-                  Alternatively, you can keep this project's software
-                  environment and upgrade later in Project Settings → Project
-                  Control.
-                </span>
-              </VisibleMDLG>
-            </div>
-            {render_controls()}
+    if (hide) return null;
+    if (compute_image == null) return null;
+    if (TO_UPGRADE.indexOf(compute_image) == -1) return null;
+    if (is_student_project) return null;
+
+    // just a safety measure, before accessing .title
+    if (COMPUTE_IMAGES[compute_image] == null) return null;
+    const oldname = COMPUTE_IMAGES[compute_image].title;
+    const newname = COMPUTE_IMAGES[DEFAULT_COMPUTE_IMAGE].title;
+
+    return (
+      <Alert bsStyle={"info"} style={UPGRADE_STYLE}>
+        <div style={{ display: "flex" }}>
+          <div style={{ flex: "1 1 auto" }}>
+            <Icon name="exclamation-triangle" />{" "}
+            <strong>Software Update Available!</strong>{" "}
+            <VisibleMDLG>
+              Update this project's software environment from "{oldname}" to "
+              {newname}". Learn more about{" "}
+              <A href={DOC_UBUNTU_2004}>all changes</A>.
+            </VisibleMDLG>
+            <VisibleXSSM>
+              {" "}
+              <A href={DOC_UBUNTU_2004}>Learn more ...</A>
+            </VisibleXSSM>
+            <VisibleMDLG>
+              <br />
+              <span style={{ color: COLORS.GRAY }}>
+                Alternatively, you can keep this project's software environment
+                and upgrade later in Project Settings → Project Control.
+              </span>
+            </VisibleMDLG>
           </div>
-        </Alert>
-      );
-    }
+          {render_controls()}
+        </div>
+      </Alert>
+    );
   }, [compute_image, updating, hide]);
 };
