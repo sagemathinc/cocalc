30a253871736a12cf736fd3d94c0779594dd8562
Harald Schilly <hsy@sagemath.com>
2020-09-14 12:15:46 +0000

    prettier: rename postcss → css

--- src/smc-util/code-formatter.ts
+++ src/smc-util/code-formatter.ts
@@ -40,7 +40,7 @@ export type Syntax =
   // the ones below are to be eliminated (they're in "Tool")
   | "prettier"
   | "typescript"
-  | "postcss"
+  | "css"
   | "babel"
   | "gofmt"
   | "clang-format"
@@ -74,7 +74,7 @@ export type Tool =
   | "rustfmt"
   | "bib-biber"
   | "prettier" // always available
-  | "postcss" // via prettier
+  | "css" // via prettier
   | "babel" // via prettier
   | "typescript" // via prettier
   | "json" // via prettier
@@ -162,7 +162,7 @@ export const syntax2tool: Readonly<Config> = Object.freeze({
   tsx: "typescript", // in prettier
   TypeScript: "typescript", // in prettier
   typescript: "typescript", // in prettier
-  CSS: "postcss", // in prettier
+  CSS: "css", // in prettier
   json: "json", // in prettier
   JSON: "json", // in prettier
   yaml: "yaml", // in prettier
@@ -207,7 +207,7 @@ export const syntax2display: Readonly<Langs> = Object.freeze({
   typescript: "TypeScript",
   html: "HTML",
   xml: "XML",
-  postcss: "CSS",
+  css: "CSS",
   javascript: "JavaScript",
 } as Langs);
 
