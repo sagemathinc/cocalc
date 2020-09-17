61111cead8030a6e12397c95c13c2f3494806c99
Harald Schilly <hsy@sagemath.com>
2020-09-17 09:29:27 +0000

    api export: make "$ export-api-doc.ts" work

--- src/scripts/export-api-doc.ts
+++ src/scripts/export-api-doc.ts
@@ -1,9 +1,11 @@
+#!/usr/bin/env -S node -r "ts-node/register"
+
 /*
  *  This file is part of CoCalc: Copyright © 2020 Sagemath, Inc.
  *  License: AGPLv3 s.t. "Commons Clause" – see LICENSE.md for details
  */
 
-// run via node-ts:
+// run this file as usual: $ export-api-doc.ts (exec in your PATH) or
 // $ ts-node scripts/export-api-doc.ts
 // then copy 'api.json' over to the root of cocalc-doc
 
