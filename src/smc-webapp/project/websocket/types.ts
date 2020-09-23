bf4e0167b0bbc4fa1638d85e1ed068e5a8f10991
Harald Schilly <hsy@sagemath.com>
2020-09-23 09:55:00 +0000

    project api: finish typing messages

--- src/smc-webapp/project/websocket/types.ts
+++ src/smc-webapp/project/websocket/types.ts
@@ -2,64 +2,26 @@
  *  This file is part of CoCalc: Copyright © 2020 Sagemath, Inc.
  *  License: AGPLv3 s.t. "Commons Clause" – see LICENSE.md for details
  */
-
+import {
+  NBGraderAPIOptions,
+  RunNotebookOptions,
+} from "../../jupyter/nbgrader/api";
 import { Options } from "../../smc-project/formatters";
-
-type Cmd =
-  | "lean"
-  | "syncdoc_call"
-  | "listing"
-  | "delete_files"
-  | "move_files"
-  | "rename_file"
-  | "canonical_paths"
-  | "configuration"
-  | "jupyter_strip_notebook"
-  | "nbgrader"
-  | "jupyter_run_notebook"
-  | "jupyter"
-  | "eval_code"
-  | "symmetric_channel"
-  | "synctable_channel"
-  | "x11_channel"
-  | "lean_channel"
-  | "realpath"
-  | "terminal"
-  | "jupyter";
-
-// TODO define each Mesg separately and tell TS to decide the type on what `cmd:...` is
-interface MesgGenereal {
-  cmd: Cmd;
-  name?: any;
-  path?: any;
-  paths?: any;
-  no_cache?: any;
-  src?: any;
-  dest?: any;
-  opts?: any;
-  endpoint?: any;
-  syncdoc_call?: any;
-  lean?: any;
-  ipynb_path?: any;
-  syncdoc_call?: any;
-  code?: any;
-  hidden?: any;
-  aspect?: any;
-  display?: any;
-  query?: any;
-  mesg?: any;
-}
 
 interface MesgExec {
   cmd: "exec";
   opts: any;
 }
 
+interface MesgDeleteFiles {
+  cmd: "delete_files";
+  paths: string[];
+}
+
 interface MesgFormatterString {
   cmd:
     | "prettier_string" // deprecated, use "formatter_string"
     | "formatter_string";
-  path: string;
   str: string;
   options: Options;
 }
@@ -72,7 +34,129 @@ interface MesgFormatter {
   options: Options;
 }
 
-export type Mesg = MesgExec | MesgGeneral | MesgFormatterString | MesgFormatter;
+interface MesgListing {
+  cmd: "listing";
+  path: string;
+  hidden: boolean;
+}
+
+interface MesgMoveFiles {
+  cmd: "move_files";
+  paths: string[];
+  dest: string;
+}
+
+interface MesgRenameFile {
+  cmd: "rename_file";
+  src: string;
+  dest: string;
+}
+
+interface MesgCanonicalPaths {
+  cmd: "canonical_paths";
+  paths: string[];
+}
+
+interface MesgConfiguration {
+  cmd: "configuration";
+  aspect: string;
+  no_cache: boolean;
+}
+
+interface MesgJupyter {
+  cmd: "jupyter";
+  path: string;
+  endpoint: string;
+  query: any;
+}
+
+interface MesgJupyterStripNotebook {
+  cmd: "jupyter_strip_notebook";
+  ipynb_path: string;
+}
+
+interface MesgNBGrader {
+  cmd: "nbgrader";
+  opts: NBGraderAPIOptions;
+}
+
+interface MesgJupyterRunNotebook {
+  cmd: "jupyter_run_notebook";
+  opts: RunNotebookOptions;
+}
+
+interface MesgEvalCode {
+  cmd: "eval_code";
+  code: string;
+}
+
+interface MesgTerminal {
+  cmd: "terminal";
+  path: string;
+  options: any;
+}
+
+interface MesgLean {
+  cmd: "lean";
+  opts: any;
+}
+
+interface MesgLeanChannel {
+  cmd: "lean_channel";
+  path: string;
+}
+
+interface MesgX11Channel {
+  cmd: "x11_channel";
+  path: string;
+  display: number;
+}
+
+interface MesgSynctableChannel {
+  cmd: "synctable_channel";
+  query: any;
+  options: any[];
+}
+
+interface MesgSyncdocCall {
+  cmd: "syncdoc_call";
+  path: string;
+  mesg: any;
+}
+
+interface MesgSymmetricChannel {
+  cmd: "symmetric_channel";
+  name: string;
+}
+
+interface MesgRealpath {
+  cmd: "realpath";
+  path: string;
+}
+
+export type Mesg =
+  | MesgExec
+  | MesgDeleteFiles
+  | MesgFormatterString
+  | MesgFormatter
+  | MesgListing
+  | MesgMoveFiles
+  | MesgRenameFile
+  | MesgCanonicalPaths
+  | MesgConfiguration
+  | MesgJupyter
+  | MesgJupyterStripNotebook
+  | MesgEvalCode
+  | MesgTerminal
+  | MesgLean
+  | MesgLeanChannel
+  | MesgX11Channel
+  | MesgSynctableChannel
+  | MesgSyncdocCall
+  | MesgSymmetricChannel
+  | MesgRealpath
+  | MesgNBGrader
+  | MesgJupyterRunNotebook;
 
 export interface Channel {
   write(x: any): boolean;
