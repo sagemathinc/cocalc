c4f90c63175359edcbb0141655a89b1bd60c1f29
Harald Schilly <hsy@sagemath.com>
2020-10-03 13:18:57 +0000

    project-info: comments on server code

--- src/smc-project/project-info/server.ts
+++ src/smc-project/project-info/server.ts
@@ -4,7 +4,8 @@
  */
 
 /*
-Project information server, doing the heavy lifting
+Project information server, doing the heavy lifting of telling the client
+about what's going on in a project.
 */
 
 // for testing, see bottom
@@ -40,6 +41,9 @@ function is_in_dev_project() {
   return process.env.SMC_LOCAL_HUB_HOME != null;
 }
 
+// this is a hard limit on the number of processes we gather, just to
+// be on the safe side to avoid processing too much data.
+const LIMIT = 100;
 const bytes2MiB = (bytes) => bytes / (1024 * 1024);
 
 export class ProjectInfoServer extends EventEmitter {
@@ -62,6 +66,9 @@ export class ProjectInfoServer extends EventEmitter {
     return this.last;
   }
 
+  // this is how long the underlying machine is running
+  // we need this information, because the processes' start time is
+  // measured in "ticks" since the machine started
   private async uptime(): Promise<[number, Date]> {
     // return uptime in secs
     const out = await readFile("/proc/uptime", "utf8");
@@ -70,17 +77,20 @@ export class ProjectInfoServer extends EventEmitter {
     return [uptime, boottime];
   }
 
+  // the "stat" file contains all the information
+  // this page explains what is what
+  // https://man7.org/linux/man-pages/man5/proc.5.html
   private async stat(path: string): Promise<Stat> {
     // all time-values are in seconds
     const raw = await readFile(path, "utf8");
+    // the "comm" field could contain additional spaces or parents
     const [i, j] = [raw.indexOf("("), raw.lastIndexOf(")")];
     const start = raw.slice(0, i - 1).trim();
     const end = raw.slice(j + 1).trim();
     const data = `${start} comm ${end}`.split(" ");
     const get = (idx) => parseInt(data[idx]);
-    // https://man7.org/linux/man-pages/man5/proc.5.html
-    // "comm" is a placeholder to keep indices as they are,
-    // except you have to account for 0 vs. 1 based indexing.
+    // "comm" is now a placeholder to keep indices as they are.
+    // don't forget to account for 0 vs. 1 based indexing.
     const ret = {
       ppid: get(3),
       state: data[2] as State,
@@ -96,10 +106,12 @@ export class ProjectInfoServer extends EventEmitter {
     return ret;
   }
 
+  // delta-time for this and the previous process information
   private dt(timestamp) {
     return (timestamp - (this.last?.timestamp ?? 0)) / 1000;
   }
 
+  // calculate cpu times
   private cpu({ pid, stat, timestamp }): Cpu {
     // we are interested in that processes total usage: user + system
     const total_cpu = stat.utime + stat.stime;
@@ -118,20 +130,21 @@ export class ProjectInfoServer extends EventEmitter {
       .filter((c) => c.length > 0);
   }
 
+  // for a process we know (pid, etc.) we try to map to cocalc specific information
   private cocalc({ pid, cmdline }): CoCalcInfo | undefined {
     //this.dbg("classify", { pid, exe, cmdline });
     if (pid === process.pid) {
       return { type: "project" };
     }
     // TODO use get_sage_path to get a path to a sagews
+    const jupyter_kernel = get_kernel_by_pid(pid);
+    if (jupyter_kernel != null) {
+      return { type: "jupyter", path: jupyter_kernel.get_path() };
+    }
     const termpath = terminal_pid2path(pid);
     if (termpath != null) {
       this.dbg("cocalc terminal", termpath);
       return { type: "terminal", path: termpath };
-    }
-    const jupyter_kernel = get_kernel_by_pid(pid);
-    if (jupyter_kernel != null) {
-      return { type: "jupyter", path: jupyter_kernel.get_path() };
     }
     const x11_path = x11_pid2path(pid);
     if (x11_path != null) {
@@ -147,6 +160,7 @@ export class ProjectInfoServer extends EventEmitter {
     }
   }
 
+  // this gathers all the information for a specific process with the given pid
   private async process({ pid: pid_str, uptime, timestamp }): Promise<Process> {
     const base = join("/proc", pid_str);
     const pid = parseInt(pid_str);
@@ -169,8 +183,10 @@ export class ProjectInfoServer extends EventEmitter {
     return data;
   }
 
+  // this is where we gather information about all running processes
   private async processes({ timestamp, uptime }): Promise<Processes> {
     const procs: Processes = {};
+    let n = 0;
     for (const pid of await readdir("/proc")) {
       if (!pid.match(/^[0-9]+$/)) continue;
       try {
@@ -178,12 +194,23 @@ export class ProjectInfoServer extends EventEmitter {
         procs[proc.pid] = proc;
       } catch (err) {
         if (this.testing)
-          this.dbg(`process ${pid} vanished – can happen – ${err}`);
+          this.dbg(`process ${pid} likely vanished – could happen – ${err}`);
+      }
+      // we avoid processing and sending too much data
+      if (n > LIMIT) {
+        this.dbg(`too many processes – limit of ${LIMIT} reached!`);
+        break;
+      } else {
+        n += 1;
       }
     }
     return procs;
   }
 
+  // this is specific to running a project in a CGroup container
+  // however, even without a container this shouldn't fail … just tells
+  // you what the whole system is doing, all your processes,…
+  // NOTE: most of this replaces kucalc.coffee
   private async cgroup({ timestamp }): Promise<CGroup | undefined> {
     if (!is_in_dev_project() && !running_in_kucalc() && !this.testing) return;
     const [
@@ -214,25 +241,30 @@ export class ProjectInfoServer extends EventEmitter {
       parseInt(cfs_quota_raw),
       parseInt(cfs_period_raw),
     ];
+    const mem_stat = mem_stat_raw
+      .split("\n")
+      .map((line) => line.split(" "))
+      .filter(([k, _]) => mem_stat_keys.includes(k))
+      .reduce((stat, [key, val]) => {
+        stat[key] = bytes2MiB(parseInt(val));
+        return stat;
+      }, {});
+    const oom_kills = oom_raw
+      .split("\n")
+      .filter((val) => val.startsWith("oom_kill "))
+      .map((val) => parseInt(val.slice("oom_kill ".length)))[0];
     return {
-      mem_stat: mem_stat_raw
-        .split("\n")
-        .map((line) => line.split(" "))
-        .filter(([k, _]) => mem_stat_keys.includes(k))
-        .reduce((stat, [key, val]) => {
-          stat[key] = bytes2MiB(parseInt(val));
-          return stat;
-        }, {}),
+      mem_stat,
       cpu_usage,
       cpu_usage_rate,
       cpu_cores_limit: cfs_quota / cfs_period,
-      oom_kills: oom_raw
-        .split("\n")
-        .filter((val) => val.startsWith("oom_kill "))
-        .map((val) => parseInt(val.slice("oom_kill ".length)))[0],
+      oom_kills,
     };
   }
 
+  // for cocalc/kucalc we want to know the disk usage + limits of the
+  // users home dir and /tmp. /tmp is a ram disk, which will count against
+  // the overall memory limit!
   private async disk_usage(): Promise<DiskUsage> {
     const convert = function (val) {
       return {
@@ -249,6 +281,7 @@ export class ProjectInfoServer extends EventEmitter {
     return { tmp: convert(tmp), project: convert(project) };
   }
 
+  // this grabs some kernel configuration values we need. they won't change
   private async init(): Promise<void> {
     if (this.ticks == null) {
       const [p_ticks, p_pagesize] = await Promise.all([
@@ -262,10 +295,7 @@ export class ProjectInfoServer extends EventEmitter {
     }
   }
 
-  public stop() {
-    this.running = false;
-  }
-
+  // orchestrating where all the information is bundled up for an update
   private async get_info(): Promise<ProjectInfo> {
     const [uptime, boottime] = await this.uptime();
     const timestamp = new Date().getTime();
@@ -285,6 +315,10 @@ export class ProjectInfoServer extends EventEmitter {
     return info;
   }
 
+  public stop() {
+    this.running = false;
+  }
+
   public async start() {
     this.dbg("start");
     if (this.running) {
@@ -293,7 +327,6 @@ export class ProjectInfoServer extends EventEmitter {
     this.running = true;
     await this.init();
     while (true) {
-      // TODO disable info collection if there is nobody listening for a few minutes…
       //this.dbg(`listeners on 'info': ${this.listenerCount("info")}`);
       const info = await this.get_info();
       this.last = info;
