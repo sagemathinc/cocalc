526240c6b1b7af7eef81d34ed6ffcd8e1b2a99c0
William Stein <wstein@sagemath.com>
2020-10-23 19:10:18 +0000

    fix #4920 -- admin user search: email address shouldn't be case sensitive

--- src/smc-webapp/admin/users/actions.ts
+++ src/smc-webapp/admin/users/actions.ts
@@ -37,7 +37,7 @@ export class AdminUsersActions extends Actions<StoreState> {
     this.set_status("Searching...");
 
     const result = await user_search({
-      query: store.get("query").trim(),
+      query: store.get("query").trim().toLowerCase(), // backend assumes lower case
       admin: true,
       limit: 100,
     });
