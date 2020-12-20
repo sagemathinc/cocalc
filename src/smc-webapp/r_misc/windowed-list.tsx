af1ba13201a7edb0e29773b94a3ed8e42b383b3a
William Stein <wstein@sagemath.com>
2020-12-20 02:57:47 +0000

    fix #4254 by rewriting how WindowedList works.  This probably happens to fix other issues/bugs not obviously related to chat or Safari.

--- src/smc-webapp/r_misc/windowed-list.tsx
+++ src/smc-webapp/r_misc/windowed-list.tsx
@@ -23,16 +23,10 @@
 
 import { delay } from "awaiting";
 
-// The ResizeObserver polyfill in the resize-observer package is
-// really weird because it *always* gets used, even if the browser
-// has its own native ResizeObserver implementation, which is
-// most browsers these days.  Hence we do the following, which saves
-// wasted time according to the profiler.
-let ResizeObserver: any = (window as any).ResizeObserver;
-if (ResizeObserver == null) {
-  ResizeObserver = require("resize-observer").ResizeObserver;
-}
+import ResizeObserver from "resize-observer-polyfill";
+
 const SHRINK_THRESH: number = 10;
+const BIG: number = 9999999;
 
 import { VariableSizeList as List, ListOnScrollProps } from "react-window";
 import AutoSizer from "react-virtualized-auto-sizer";
@@ -102,6 +96,8 @@ export class WindowedList extends Component<Props, State> {
     scrollUpdateWasRequested: false,
   };
 
+  private min_changed_index: number = 0;
+
   public render_info: RenderInfo = {
     overscanStartIndex: 0,
     overscanStopIndex: 0,
@@ -113,7 +109,7 @@ export class WindowedList extends Component<Props, State> {
   constructor(props) {
     super(props);
     this.list_ref = React.createRef();
-    this.resize_observer = new ResizeObserver(this.cell_resized.bind(this));
+    this.resize_observer = new ResizeObserver(this.rows_resized.bind(this));
     let scroll_top: number | undefined = props.scroll_top;
     if (scroll_top == null && this.props.cache_id != null) {
       const x = scroll_cache[this.props.cache_id];
@@ -232,53 +228,68 @@ export class WindowedList extends Component<Props, State> {
     return x.info;
   }
 
-  private cell_resized(entries: any[]): void {
-    let num_changed: number = 0;
-    let min_index: number = 999999;
-    for (const entry of entries) {
-      const elt = entry.target;
-      const key = elt.getAttribute("data-key");
-      if (key == null) continue;
-      if (isNaN(entry.contentRect.height) || entry.contentRect.height === 0) {
-        if (this.row_heights_cache[key] > 0) {
-          // A row was deleted (or isn't visible), so goes from a
-          // possibly big height to 0.
-          this.row_heights_removed[key] = true;
-        }
-        continue;
-      }
+  /* Call when a row may have changed size.  */
+  private row_resized(entry): void {
+    const elt = entry.target;
+    const key = elt.getAttribute("data-key");
+    if (key == null) return;
+    const height = entry.contentRect.height;
 
-      const index = elt.getAttribute("data-index");
-      if (this.row_heights_removed[key]) {
-        delete this.row_heights_removed[key];
-        if (
-          Math.abs(this.row_heights_cache[key] - entry.contentRect.height) < 3
-        ) {
-          // Last time it changed it was removed, and now it is back but with
-          // a (significantly) different height
-          this.row_heights_cache[key] = entry.contentRect.height;
-          this.row_heights_stale[key] = true;
-          num_changed += 1;
-          if (index != null) {
-            min_index = Math.min(min_index, parseInt(index));
-          }
-          continue;
-        }
+    if (isNaN(height) || height == 0) {
+      if ((this.row_heights_cache[key] ?? 0) > 0) {
+        // A row was deleted (or isn't visible!), so goes from a
+        // possibly big height to 0.  This would really
+        // confuse everything to resize all these to 0.
+        this.row_heights_removed[key] = true;
       }
-
-      const s = entry.contentRect.height - this.row_heights_cache[key];
-      if (s == 0 || (s < 0 && -s <= SHRINK_THRESH)) {
-        // not really changed or just disappeared from DOM or just shrunk a little,
-        // ... so continue using what we have cached (or the default).
-        continue;
-      }
-      if (index != null) {
-        min_index = Math.min(min_index, parseInt(index));
-      }
-      this.row_heights_stale[key] = true;
-      num_changed += 1;
+      return;
     }
-    if (num_changed > 0) this.refresh(min_index);
+
+    if (this.row_heights_removed[key]) {
+      // The row was resized to 0 in the previous call, and now
+      // it is no longer 0. We ignore sizing the first time this happens
+      // unless the height was bigger than before.  The next time sizing
+      // happens we'll take that into account.   Basically ResizeObserver
+      // and react-window seems to produce behavior like this, and we're
+      // working around that:
+      //   1. Scroll a row out of view
+      //   2. It resizes to 0 (maybe a browser optimization?)
+      //   3. A fraction of a second later it resizes to around 100
+      //   4. Then it sizes back to what it was.
+      // This sequence has no impact on us due to row_heights_removed.
+      // In the absolute worse case, our heuristic could temporarily
+      // refuse to shrink a row.
+      delete this.row_heights_removed[key];
+      if (height <= (this.row_heights_cache[key] ?? 0)) {
+        return;
+      }
+    }
+
+    const index = elt.getAttribute("data-index");
+
+    const s = height - this.row_heights_cache[key];
+    if (s == 0 || (s < 0 && -s <= SHRINK_THRESH)) {
+      // not really changed or just disappeared from DOM or just
+      // shrunk a little,
+      // ... so continue using what we have cached (or the default).
+      return;
+    }
+    if (index != null) {
+      this.min_changed_index = Math.min(
+        this.min_changed_index,
+        parseInt(index)
+      );
+    }
+    this.row_heights_stale[key] = true;
+  }
+
+  private rows_resized(entries: any[]): void {
+    for (const entry of entries) {
+      this.row_resized(entry);
+    }
+    if (this.min_changed_index != BIG) {
+      this.refresh();
+    }
   }
 
   public disable_refresh(): void {
@@ -289,10 +300,11 @@ export class WindowedList extends Component<Props, State> {
     this._disable_refresh = false;
   }
 
-  public refresh(min_index: number = 0): void {
+  public refresh(): void {
     if (this._disable_refresh) return;
     if (this.list_ref.current == null) return;
-    this.list_ref.current.resetAfterIndex(min_index, true);
+    this.list_ref.current.resetAfterIndex(this.min_changed_index, true);
+    this.min_changed_index = BIG;
   }
 
   public row_ref(key: string): any {
@@ -307,7 +319,9 @@ export class WindowedList extends Component<Props, State> {
     if (h !== undefined && !this.row_heights_stale[key]) {
       return h;
     }
-    if (h === undefined) h = 0;
+    if (h === undefined) {
+      h = 0;
+    }
 
     const elt = this.cell_refs[key];
     if (elt == null) {
