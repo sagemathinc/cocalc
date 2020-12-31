967abf3e1a2ede696367637cb14bfd96311cfc02
William Stein <wstein@sagemath.com>
2020-12-31 17:07:12 +0000

    implement unpublishing of everything.

--- src/smc-webapp/account/public-paths/public-paths.tsx
+++ src/smc-webapp/account/public-paths/public-paths.tsx
@@ -3,6 +3,7 @@
  *  License: AGPLv3 s.t. "Commons Clause" – see LICENSE.md for details
  */
 
+import { Checkbox, Table } from "antd";
 import {
   redux,
   React,
@@ -13,7 +14,6 @@ import {
   useMemo,
   useTypedRedux,
 } from "../../app-framework";
-import { Table } from "antd";
 import { PublicPath as PublicPath0 } from "smc-util/db-schema/public-paths";
 import { trunc_middle } from "smc-util/misc";
 import { webapp_client } from "../../webapp-client";
@@ -28,37 +28,39 @@ export const PublicPaths: React.FC = () => {
   const [data, set_data] = useState<PublicPath[] | undefined>(undefined);
   const [error, set_error] = useState<string>("");
   const [loading, set_loading] = useState<boolean>(false);
+  const [show_listed, set_show_listed] = useState<boolean>(true);
+  const [show_unlisted, set_show_unlisted] = useState<boolean>(false);
+  const [show_unpublished, set_show_unpublished] = useState<boolean>(false);
   const isMountedRef = useIsMountedRef();
   const project_map = useTypedRedux("projects", "project_map");
   const actions = useActions("projects");
 
-  const paths: {
-    published: PublicPath[];
-    unlisted: PublicPath[];
-    unpublished: PublicPath[];
-  } = useMemo(() => {
-    const published: PublicPath[] = [];
-    const unlisted: PublicPath[] = [];
-    const unpublished: PublicPath[] = [];
-
+  const paths: PublicPath[] = useMemo(() => {
+    const v: PublicPath[] = [];
     if (data != null) {
       for (const path of data) {
         if (path.disabled) {
-          unpublished.push(path);
-          path.status = "Private (disabled)";
+          if (show_unpublished) {
+            path.status = "Unpublished";
+            v.push(path);
+          }
           continue;
         }
         if (path.unlisted) {
-          unlisted.push(path);
-          path.status = "Public (unlisted)";
+          if (show_unlisted) {
+            path.status = "Unlisted";
+            v.push(path);
+          }
           continue;
         }
-        path.status = "Public (listed)";
-        published.push(path);
+        if (show_listed) {
+          path.status = "Listed";
+          v.push(path);
+        }
       }
     }
-    return { published, unlisted, unpublished };
-  }, [data]);
+    return v;
+  }, [data, show_listed, show_unlisted, show_unpublished]);
 
   const COLUMNS = [
     {
@@ -163,13 +165,26 @@ export const PublicPaths: React.FC = () => {
     <div style={{ marginBottom: "64px" }}>
       <h2>Public files</h2>
       {loading && <Loading />}
+      {!loading && (
+        <Checkbox.Group
+          options={["Listed", "Unlisted", "Unpublished"]}
+          defaultValue={["Listed"]}
+          onChange={(v) => {
+            set_show_listed(v.indexOf("Listed") != -1);
+            set_show_unlisted(v.indexOf("Unlisted") != -1);
+            set_show_unpublished(v.indexOf("Unpublished") != -1);
+          }}
+        />
+      )}
       <br />
-      {error != "" && <ErrorDisplay error={error} />}
+      {error != "" && (
+        <ErrorDisplay style={{ marginTop: "32px" }} error={error} />
+      )}
       <br />
       {data != null && (
-        <Table rowKey="id" columns={COLUMNS} dataSource={paths.published} />
+        <Table rowKey="id" columns={COLUMNS} dataSource={paths} />
       )}
-      <UnpublishEverything data={data} />
+      <UnpublishEverything data={data} refresh={fetch} />
     </div>
   );
 };
--- src/smc-webapp/account/public-paths/unpublish-everything.tsx
+++ src/smc-webapp/account/public-paths/unpublish-everything.tsx
@@ -4,84 +4,116 @@
  */
 
 import { Alert, Button, Input } from "antd";
-import { React, useMemo, useState } from "../../app-framework";
+import { React, useIsMountedRef, useMemo, useState } from "../../app-framework";
 import { PublicPath } from "smc-util/db-schema/public-paths";
 import { plural } from "smc-util/misc";
+import { webapp_client } from "../../webapp-client";
 
 interface Props {
   data?: PublicPath[];
+  refresh: Function;
 }
 
-export const UnpublishEverything: React.FC<Props> = React.memo(({ data }) => {
-  const [confirm, set_confirm] = useState<boolean>(false);
-  const [confirm_text, set_confirm_text] = useState<string>("");
+export const UnpublishEverything: React.FC<Props> = React.memo(
+  ({ data, refresh }) => {
+    const [confirm, set_confirm] = useState<boolean>(false);
+    const [confirm_text, set_confirm_text] = useState<string>("");
+    const [counter, set_counter] = useState<number>(-1);
+    const isMountedRef = useIsMountedRef();
 
-  const num_published = useMemo(() => {
-    if (data == null) return -1;
-    let n = 0;
-    for (const x of data) {
-      if (!x.disabled) {
-        n += 1;
+    const num_published = useMemo(() => {
+      if (data == null) return -1;
+      let n = 0;
+      for (const x of data) {
+        if (!x.disabled) {
+          n += 1;
+        }
       }
-    }
-    return n;
-  }, [data]);
+      return n;
+    }, [data]);
 
-  function render_confirm(): JSX.Element {
-    const goal = "YES, UNPUBLISH EVERYTHING!";
-    const body = (
-      <div>
-        {`Are you sure you want to unpublish ALL ${num_published} ${plural(
-          num_published,
-          "path"
-        )} published in all projects on which you collaborate?  You cannot easily undo this operation, though you could tediously republish everything.  To unpublish everything type "${goal}" below, then click the button.`}
-        <br />
-        <br />
-        <Input
-          size="large"
-          placeholder={goal}
-          value={confirm_text}
-          onChange={(e) => set_confirm_text(e.target.value)}
-        />
-        <br />
-        <br />
-        <Button
-          disabled={confirm_text != goal}
-          onClick={() => {
-            console.log("do the deed!");
+    function render_confirm(): JSX.Element {
+      const goal = "YES, UNPUBLISH EVERYTHING!";
+      const body = (
+        <div>
+          {`Are you sure you want to unpublish ALL ${num_published} listed and unlisted ${plural(
+            num_published,
+            "path"
+          )} published in all projects on which you collaborate?  You cannot easily undo this operation, though you could tediously republish everything.  To unpublish everything type "${goal}" below, then click the button.`}
+          <br />
+          <br />
+          <Input
+            size="large"
+            placeholder={goal}
+            value={confirm_text}
+            onChange={(e) => set_confirm_text(e.target.value)}
+          />
+          <br />
+          <br />
+          <Button
+            disabled={confirm_text != goal}
+            onClick={() => {
+              set_confirm(false);
+              set_confirm_text("");
+              disable_all();
+            }}
+          >
+            Unpublish everything
+          </Button>
+        </div>
+      );
+      return (
+        <Alert
+          style={{ marginBottom: "20px" }}
+          message="Unpublish Everything?"
+          description={body}
+          type="warning"
+          showIcon
+          closable
+          afterClose={() => {
             set_confirm(false);
             set_confirm_text("");
           }}
+        />
+      );
+    }
+
+    async function disable_all(): Promise<void> {
+      if (data == null) return;
+      set_counter(0);
+      for (const x of data) {
+        if (x.disabled) continue;
+        if (!isMountedRef.current) return;
+        await webapp_client.async_query({
+          query: {
+            public_paths: {
+              id: x.id,
+              project_id: x.project_id,
+              path: x.path,
+              disabled: true,
+            },
+          },
+        });
+        set_counter(counter + 1);
+      }
+      refresh();
+    }
+
+    return (
+      <div>
+        {confirm && render_confirm()}
+        {counter >= 0 && (
+          <h1>
+            Unpublished: {counter}/{num_published}
+          </h1>
+        )}
+        <Button
+          onClick={() => set_confirm(true)}
+          disabled={num_published == 0 || confirm}
         >
-          Unpublish everything
+          Unpublish everything...
         </Button>
       </div>
     );
-    return (
-      <Alert
-        style={{ marginBottom: "20px" }}
-        message="Unpublish Everything?"
-        description={body}
-        type="warning"
-        showIcon
-        closable
-        afterClose={() => {
-          set_confirm(false);
-          set_confirm_text("");
-        }}
-      />
-    );
   }
-
-  return (
-    <div>
-      {confirm && render_confirm()}
-      <Button
-        onClick={() => set_confirm(true)}
-        disabled={num_published == 0 || confirm}
-      >
-        Unpublish everything...
-      </Button>
-    </div>
-  );
-});
+);
