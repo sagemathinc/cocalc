f7ea19bfb09cb297c05a90d65b6937c4ee13eae9
Harald Schilly <hsy@sagemath.com>
2021-01-04 15:24:07 +0000

    smc-project/autorenice: attempt to add test mode, verbose flag

--- src/smc-project/autorenice.ts
+++ src/smc-project/autorenice.ts
@@ -14,11 +14,23 @@ import { delay } from "awaiting";
 import { ProjectInfoServer, get_ProjectInfoServer } from "./project-info";
 import { ProjectInfo } from "./project-info/types";
 
+// only for testing, see bottom
+if (require.main === module) {
+  require("coffee-register");
+}
+
+interface Opts {
+  verbose?: boolean;
+}
+
 class ProcessRenicer {
+  private readonly verbose: boolean;
   private readonly project_info: ProjectInfoServer;
   private info?: ProjectInfo;
 
-  constructor() {
+  constructor(opts?: Opts) {
+    const { verbose = false } = opts ?? {};
+    this.verbose = verbose;
     this.project_info = get_ProjectInfoServer();
     this.init();
     this.start();
@@ -38,17 +50,27 @@ class ProcessRenicer {
 
   // this is the main "infinite loop"
   private start(): void {
+    if (this.verbose) L("starting main loop");
     delay(30 * 1000);
     L(`ProjectInfo: `, this.info);
   }
 }
 
-let pr: ProcessRenicer | undefined = undefined;
+let singleton: ProcessRenicer | undefined = undefined;
 
 export function activate() {
-  if (pr != null) {
+  if (singleton != null) {
     L("blocking attempt to run ProcessRenicer twice");
     return;
   }
-  pr = new ProcessRenicer();
+  singleton = new ProcessRenicer();
+  return singleton;
+}
+
+// testing: $ ts-node autorenice.ts
+if (require.main === module) {
+  const pr = activate();
+  L("activated ProcessRenicer in test mode", pr);
+  delay(3 * 1000);
+  L("test done");
 }
