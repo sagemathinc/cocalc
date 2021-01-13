a5c63cb6d6948a35c4c803ae2311e7fe1f3cbc11
William Stein <wstein@sagemath.com>
2021-01-13 06:58:23 +0000

    slate: mainly make it so state is probably saved on blur (the onBlur handler was in the wrong component)

--- src/smc-webapp/frame-editors/markdown-editor/slate/editable-markdown.tsx
+++ src/smc-webapp/frame-editors/markdown-editor/slate/editable-markdown.tsx
@@ -51,247 +51,258 @@ interface Props {
   is_current?: boolean;
 }
 
-export const EditableMarkdown: React.FC<Props> = ({
-  actions,
-  id,
-  font_size,
-  read_only,
-  value,
-  project_id,
-  path,
-  is_current,
-}) => {
-  const editor: ReactEditor = useMemo(() => {
-    const cur = actions.getSlateEditor(id);
-    if (cur != null) return cur;
-    const ed = withIsInline(withIsVoid(withReact(createEditor())));
-    actions.registerSlateEditor(id, ed);
-    return ed;
-  }, []);
+export const EditableMarkdown: React.FC<Props> = React.memo(
+  ({
+    actions,
+    id,
+    font_size,
+    read_only,
+    value,
+    project_id,
+    path,
+    is_current,
+  }) => {
+    const editor: ReactEditor = useMemo(() => {
+      const cur = actions.getSlateEditor(id);
+      if (cur != null) return cur;
+      const ed = withIsInline(withIsVoid(withReact(createEditor())));
+      actions.registerSlateEditor(id, ed);
+      return ed;
+    }, []);
 
-  const editorMarkdownValueRef = useRef<string | undefined>(undefined);
-  const hasUnsavedChangesRef = useRef<boolean>(false);
-  const [editor_value, setEditorValue] = useState<Node[]>(() =>
-    markdown_to_slate(value)
-  );
-  const scaling = use_font_size_scaling(font_size);
+    const editorMarkdownValueRef = useRef<string | undefined>(undefined);
+    const hasUnsavedChangesRef = useRef<boolean>(false);
+    const [editorValue, setEditorValue] = useState<Node[]>(() =>
+      markdown_to_slate(value)
+    );
+    const scaling = use_font_size_scaling(font_size);
 
-  const editor_markdown_value = useCallback(() => {
-    if (editorMarkdownValueRef.current != null) {
+    const editor_markdown_value = useCallback(() => {
+      if (editorMarkdownValueRef.current != null) {
+        return editorMarkdownValueRef.current;
+      }
+      editorMarkdownValueRef.current = slate_to_markdown(editor.children);
       return editorMarkdownValueRef.current;
-    }
-    editorMarkdownValueRef.current = slate_to_markdown(editor.children);
-    return editorMarkdownValueRef.current;
-  }, []);
+    }, []);
 
-  const save_value = useCallback(() => {
-    // console.log("save_value", { hasUnsaved: hasUnsavedChangesRef.current });
-    if (!hasUnsavedChangesRef.current) return;
-    hasUnsavedChangesRef.current = false;
-    actions.set_value(editor_markdown_value());
-    actions.ensure_syncstring_is_saved();
-  }, []);
-
-  // We don't want to do save_value too much, since it presumably can be slow,
-  // especially if the document is large. By debouncing, we only do this when
-  // the user pauses typing for a moment. Also, this avoids making too many commits.
-  const save_value_debounce = useMemo(
-    () => debounce(save_value, SAVE_DEBOUNCE_MS),
-    []
-  );
-
-  function onKeyDown(e) {
-    if (read_only) return;
-    //console.log("onKeyDown", { keyCode: e.keyCode, key: e.key });
-    if (!e.shiftKey && !e.ctrlKey && !e.metaKey && e.key == "Tab") {
-      // Markdown doesn't have a notion of tabs in text...
-      // Putting in four spaces for now, but we'll probably change this...
-      editor.insertText("    ");
-      e.preventDefault();
-      return;
-    }
-    if (e.shiftKey && e.key == "Enter") {
-      // insert a hard break instead of a new pagraph like enter creates.
-      Transforms.insertNodes(editor, [hardbreak()]);
-      e.preventDefault();
-      return;
-    }
-    if ((e.ctrlKey || e.metaKey) && e.keyCode == 83) {
-      actions.save(true);
-      e.preventDefault();
-      return;
-    }
-    if ((e.ctrlKey && e.keyCode == 188) || (e.metaKey && e.keyCode == 189)) {
-      actions.change_font_size(-1);
-      e.preventDefault();
-      return;
-    }
-    if ((e.ctrlKey && e.keyCode == 190) || (e.metaKey && e.keyCode == 187)) {
-      actions.change_font_size(+1);
-      e.preventDefault();
-      return;
-    }
-    if ((e.metaKey || e.ctrlKey) && e.keyCode == 90) {
-      if (e.shiftKey) {
-        // redo
-        actions.redo(id);
-      } else {
-        // undo
-        actions.undo(id);
+    const save_value = useCallback(() => {
+      if (!hasUnsavedChangesRef.current) {
+        return;
       }
       hasUnsavedChangesRef.current = false;
-      e.preventDefault();
-      ReactEditor.focus(editor);
-      return;
-    }
-    if (handleFormatCommands(e)) {
-      return;
-    }
-  }
+      actions.set_value(editor_markdown_value());
+      actions.ensure_syncstring_is_saved();
+    }, []);
 
-  function handleFormatCommands(e) {
-    if (!(e.ctrlKey || e.metaKey)) {
-      return;
-    }
+    // We don't want to do save_value too much, since it presumably can be slow,
+    // especially if the document is large. By debouncing, we only do this when
+    // the user pauses typing for a moment. Also, this avoids making too many commits.
+    const save_value_debounce = useMemo(
+      () => debounce(save_value, SAVE_DEBOUNCE_MS),
+      []
+    );
 
-    switch (e.key) {
-      case "b":
-      case "i":
-      case "u":
-      case "x":
-        if (e.key == "x" && !e.shiftKey) return;
+    function onKeyDown(e) {
+      if (read_only) return;
+      //console.log("onKeyDown", { keyCode: e.keyCode, key: e.key });
+      if (!e.shiftKey && !e.ctrlKey && !e.metaKey && e.key == "Tab") {
+        // Markdown doesn't have a notion of tabs in text...
+        // Putting in four spaces for now, but we'll probably change this...
+        editor.insertText("    ");
         e.preventDefault();
-        formatSelectedText(
-          editor,
-          { b: "bold", i: "italic", u: "underline", x: "strikethrough" }[e.key]
-        );
-        return true;
-    }
-  }
-
-  useEffect(() => {
-    if (!is_current) {
-      if (hasUnsavedChangesRef.current) {
-        // just switched from focused to not and there was an unsaved change,
-        // so save state.
+        return;
+      }
+      if (e.shiftKey && e.key == "Enter") {
+        // insert a hard break instead of a new pagraph like enter creates.
+        Transforms.insertNodes(editor, [hardbreak()]);
+        e.preventDefault();
+        return;
+      }
+      if ((e.ctrlKey || e.metaKey) && e.keyCode == 83) {
+        actions.save(true);
+        e.preventDefault();
+        return;
+      }
+      if ((e.ctrlKey && e.keyCode == 188) || (e.metaKey && e.keyCode == 189)) {
+        actions.change_font_size(-1);
+        e.preventDefault();
+        return;
+      }
+      if ((e.ctrlKey && e.keyCode == 190) || (e.metaKey && e.keyCode == 187)) {
+        actions.change_font_size(+1);
+        e.preventDefault();
+        return;
+      }
+      if ((e.metaKey || e.ctrlKey) && e.keyCode == 90) {
+        if (e.shiftKey) {
+          // redo
+          actions.redo(id);
+        } else {
+          // undo
+          actions.undo(id);
+        }
         hasUnsavedChangesRef.current = false;
-        actions.set_value(editor_markdown_value());
-        actions.ensure_syncstring_is_saved();
-      }
-    }
-  }, [is_current]);
-
-  // Make sure to save the state of the slate editor
-  // to the syncstring *before* merging in a change
-  // from upstream.
-  useEffect(() => {
-    function before_change() {
-      if (ReactEditor.isFocused(editor)) {
-        actions.set_value(editor_markdown_value());
-      }
-    }
-    actions.get_syncstring().on("before-change", before_change);
-    return () => actions.get_syncstring().off("before-change", before_change);
-  }, []);
-
-  const setEditorValueNoJump = useCallback(async (new_value) => {
-    ReactEditor.blur(editor);
-    setEditorValue(new_value);
-    // Critical to wait until after next render loop, or
-    // ReactEditor.toDOMPoint below will not detect the problem.
-    await delay(0);
-    if (editor.selection != null) {
-      try {
-        ReactEditor.toDOMPoint(editor, editor.selection.anchor);
-        ReactEditor.toDOMPoint(editor, editor.selection.focus);
+        e.preventDefault();
         ReactEditor.focus(editor);
-      } catch (_err) {
-        // Do not focus -- better than crashing the browser.
-        // When the user clicks again to focus, they will choose
-        // a valid cursor point.
-        // TODO: we need to either figure out where the cursor
-        // would move to, etc., or we need to use a sequences of
-        // Transforms rather than just setting the editor value.
-        // This is of course very hard, but it is the right thing
-        // to do in general.
-        // Another option might be to insert something in the DOM
-        // at the cursor, make changes, find that thing, and put the
-        // cursor back there.  But that might not work if it disrupts
-        // a user while they are typing.
+        return;
+      }
+      if (handleFormatCommands(e)) {
+        return;
       }
     }
-  }, []);
 
-  useEffect(() => {
-    if (value == editorMarkdownValueRef.current) {
-      // Setting to current value, so no-op.
-      return;
+    function handleFormatCommands(e) {
+      if (!(e.ctrlKey || e.metaKey)) {
+        return;
+      }
+
+      switch (e.key) {
+        case "b":
+        case "i":
+        case "u":
+        case "x":
+          if (e.key == "x" && !e.shiftKey) return;
+          e.preventDefault();
+          formatSelectedText(
+            editor,
+            { b: "bold", i: "italic", u: "underline", x: "strikethrough" }[
+              e.key
+            ]
+          );
+          return true;
+      }
     }
-    editorMarkdownValueRef.current = value;
-    const new_value = markdown_to_slate(value);
 
-    if (ReactEditor.isFocused(editor) && editor.selection != null) {
-      setEditorValueNoJump(new_value);
-    } else {
+    useEffect(() => {
+      if (!is_current) {
+        if (hasUnsavedChangesRef.current) {
+          // just switched from focused to not and there was an unsaved change,
+          // so save state.
+          hasUnsavedChangesRef.current = false;
+          actions.set_value(editor_markdown_value());
+          actions.ensure_syncstring_is_saved();
+        }
+      }
+    }, [is_current]);
+
+    // Make sure to save the state of the slate editor
+    // to the syncstring *before* merging in a change
+    // from upstream.
+    useEffect(() => {
+      function before_change() {
+        if (ReactEditor.isFocused(editor)) {
+          actions.set_value(editor_markdown_value());
+        }
+      }
+      actions.get_syncstring().on("before-change", before_change);
+      return () => actions.get_syncstring().off("before-change", before_change);
+    }, []);
+
+    const setEditorValueNoJump = useCallback(async (new_value) => {
+      ReactEditor.blur(editor);
       setEditorValue(new_value);
-    }
-  }, [value]);
+      // Critical to wait until after next render loop, or
+      // ReactEditor.toDOMPoint below will not detect the problem.
+      await delay(0);
+      if (editor.selection != null) {
+        try {
+          ReactEditor.toDOMPoint(editor, editor.selection.anchor);
+          ReactEditor.toDOMPoint(editor, editor.selection.focus);
+          ReactEditor.focus(editor);
+        } catch (_err) {
+          // Do not focus -- better than crashing the browser.
+          // When the user clicks again to focus, they will choose
+          // a valid cursor point.
+          // TODO: we need to either figure out where the cursor
+          // would move to, etc., or we need to use a sequences of
+          // Transforms rather than just setting the editor value.
+          // This is of course very hard, but it is the right thing
+          // to do in general.
+          // Another option might be to insert something in the DOM
+          // at the cursor, make changes, find that thing, and put the
+          // cursor back there.  But that might not work if it disrupts
+          // a user while they are typing.
+        }
+      }
+    }, []);
 
-  (window as any).z = {
-    editor,
-    Transforms,
-    Node,
-    ReactEditor,
-  };
+    useEffect(() => {
+      if (value == editorMarkdownValueRef.current) {
+        // Setting to current value, so no-op.
+        return;
+      }
+      const new_value = markdown_to_slate(value);
+      editorMarkdownValueRef.current = value;
 
-  return (
-    <div
-      className="smc-vfill"
-      style={{ overflowY: "auto", backgroundColor: "#eee" }}
-    >
-      <Path is_current={is_current} path={path} project_id={project_id} />
+      if (ReactEditor.isFocused(editor) && editor.selection != null) {
+        setEditorValueNoJump(new_value);
+      } else {
+        setEditorValue(new_value);
+      }
+    }, [value]);
+
+    (window as any).z = {
+      editor,
+      Transforms,
+      Node,
+      ReactEditor,
+    };
+
+    return (
       <div
-        style={{
-          ...STYLE,
-          fontSize: font_size,
-          maxWidth: `${(1 + (scaling - 1) / 2) * MAX_WIDTH_NUM}px`,
-        }}
+        className="smc-vfill"
+        style={{ overflowY: "auto", backgroundColor: "#eee" }}
       >
-        <Slate
-          editor={editor}
-          value={editor_value}
-          onChange={(new_value) => {
-            if (!is_current) {
-              // Do not save when editor not current since user could be typing
-              // into another editor of the same underlying document.   This will
-              // cause bugs (e.g., type, switch from slate to codemirror, type, and
-              // see what you typed into codemirror disappear). E.g., this
-              // happens due to a spurious change when the editor is defocused.
-              return;
-            }
-            hasUnsavedChangesRef.current = true;
-            editorMarkdownValueRef.current = undefined; // markdown value now not known.
-            if (ReactEditor.isFocused(editor)) {
-              // If editor is focused, scroll cursor into view.
-              scroll_into_view();
-            }
-            setEditorValue(new_value);
-            save_value_debounce();
+        <Path is_current={is_current} path={path} project_id={project_id} />
+        <div
+          style={{
+            ...STYLE,
+            fontSize: font_size,
+            maxWidth: `${(1 + (scaling - 1) / 2) * MAX_WIDTH_NUM}px`,
           }}
-          onBlur={save_value}
         >
-          <Editable
-            style={{ margin: "0 auto", padding: "50px 75px" }}
-            readOnly={read_only}
-            renderElement={Element}
-            renderLeaf={Leaf}
-            onKeyDown={!read_only ? onKeyDown : undefined}
-          />
-        </Slate>
+          <Slate
+            editor={editor}
+            value={editorValue}
+            onChange={(newEditorValue) => {
+              if (editorValue === newEditorValue) {
+                // Editor didn't actually change value so nothing to do.
+                hasUnsavedChangesRef.current = false;
+                return;
+              }
+              hasUnsavedChangesRef.current = true;
+              editorMarkdownValueRef.current = undefined; // markdown value now not known.
+              if (ReactEditor.isFocused(editor)) {
+                // If editor is focused, scroll cursor into view.
+                scroll_into_view();
+              }
+              setEditorValue(newEditorValue);
+
+              if (!is_current) {
+                // Do not save when editor not current since user could be typing
+                // into another editor of the same underlying document.   This will
+                // cause bugs (e.g., type, switch from slate to codemirror, type, and
+                // see what you typed into codemirror disappear). E.g., this
+                // happens due to a spurious change when the editor is defocused.
+                return;
+              }
+              save_value_debounce();
+            }}
+          >
+            <Editable
+              style={{ margin: "0 auto", padding: "50px 75px" }}
+              readOnly={read_only}
+              renderElement={Element}
+              renderLeaf={Leaf}
+              onKeyDown={!read_only ? onKeyDown : undefined}
+              onBlur={save_value}
+            />
+          </Slate>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+);
 
 const withIsVoid = (editor) => {
   const { isVoid } = editor;
