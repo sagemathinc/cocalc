64f8b787015dd2ea87c68f00e45bec2b89ca0457
William Stein <wstein@sagemath.com>
2021-02-01 19:46:16 -0800

    slate: make hr's look nicer

--- src/smc-webapp/frame-editors/markdown-editor/slate/elements/hr.tsx
+++ src/smc-webapp/frame-editors/markdown-editor/slate/elements/hr.tsx
@@ -25,15 +25,27 @@ register({
     // See https://css-tricks.com/examples/hrs/ for the cool style...
     return (
       <div {...attributes}>
-        <hr
+        <div
           contentEditable={false}
           style={{
-            border: focused && selected ? `1px solid ${FOCUSED_COLOR}` : 0,
-            height: "3px",
-            background: "#333",
-            backgroundImage: "linear-gradient(to right, #ccc, #333, #ccc)",
+            border:
+              focused && selected
+                ? `2px solid ${FOCUSED_COLOR}`
+                : "2px solid white",
+            height: "13px",
+            padding: "2px 10px",
+            borderRadius: "5px",
           }}
-        />
+        >
+          <hr
+            style={{
+              margin: 0,
+              height: "3px",
+              background: "#333",
+              backgroundImage: "linear-gradient(to right, #ccc, #333, #ccc)",
+            }}
+          />
+        </div>
         {children}
       </div>
     );
