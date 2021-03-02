7e6c3bbf5ccc0cb8b61b5aa0fdecf2fd0da901d9
William Stein <wstein@sagemath.com>
2021-03-02 06:49:35 +0000

    This is https://github.com/ianstormtaylor/slate/pull/4107 -- fix: cut do not remove void element when only select void element

--- src/smc-webapp/frame-editors/markdown-editor/slate/slate-react/components/editable.tsx
+++ src/smc-webapp/frame-editors/markdown-editor/slate/slate-react/components/editable.tsx
@@ -679,8 +679,15 @@ export const Editable: React.FC<EditableProps> = (props: EditableProps) => {
               ReactEditor.setFragmentData(editor, event.clipboardData);
               const { selection } = editor;
 
-              if (selection && Range.isExpanded(selection)) {
-                Editor.deleteFragment(editor);
+              if (selection) {
+                if (Range.isExpanded(selection)) {
+                  Editor.deleteFragment(editor);
+                } else {
+                  const node = Node.parent(editor, selection.anchor.path);
+                  if (Editor.isVoid(editor, node)) {
+                    Transforms.delete(editor);
+                  }
+                }
               }
             }
           },
