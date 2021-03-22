a86b98fc0b38bbfecfeaf2899301310426ddf345
Harald Schilly <hsy@sagemath.com>
2021-03-22 14:40:11 +0000

    pwa: register empty functions in service worker

--- src/webapp-lib/serviceWorker.js
+++ src/webapp-lib/serviceWorker.js
@@ -1,1 +1,13 @@
-// for v0, we keep it empty
+/*
+ *  This file is part of CoCalc: Copyright © 2020 Sagemath, Inc.
+ *  License: AGPLv3 s.t. "Commons Clause" – see LICENSE.md for details
+ */
+
+// for v0, we keep all of this empty
+
+// Listen for install event, set callback
+self.addEventListener("install", function (event) {});
+
+self.addEventListener("activate", function (event) {});
+
+self.addEventListener("fetch", function (event) {});
