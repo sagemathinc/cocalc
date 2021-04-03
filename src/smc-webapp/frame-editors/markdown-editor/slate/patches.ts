8853adf62198efd8642fd800b8b67cad4a401e14
William Stein <wstein@sagemath.com>
2021-04-03 22:49:55 +0000

    slate: cursor bug -- merge upstream *workaround* for https://github.com/ianstormtaylor/slate/issues/4131

--- src/smc-webapp/frame-editors/markdown-editor/slate/patches.ts
+++ src/smc-webapp/frame-editors/markdown-editor/slate/patches.ts
@@ -3,7 +3,7 @@
  *  License: AGPLv3 s.t. "Commons Clause" – see LICENSE.md for details
  */
 
-import { Node } from "slate";
+import { Editor, Node, Transforms } from "slate";
 
 // The version of isNodeList in slate is **insanely** slow, and this hack
 // is likely to be sufficient for our use.
@@ -11,3 +11,21 @@ import { Node } from "slate";
 Node.isNodeList = (value: any): value is Node[] => {
   return Array.isArray(value) && (value?.length == 0 || Node.isNode(value[0]));
 };
+
+// This is hack that addresses https://github.com/ianstormtaylor/slate/issues/4131
+export function withFix4131(editor: Editor) {
+  var a = editor;
+  var { apply } = editor;
+
+  a.apply = function (op) {
+    apply(op);
+    if (op.type === "remove_node" && op.path[op.path.length - 1] === 0) {
+      Transforms.setSelection(a, {
+        anchor: { path: op.path, offset: 0 },
+        focus: { path: op.path, offset: 0 },
+      });
+    }
+  };
+
+  return a;
+}
