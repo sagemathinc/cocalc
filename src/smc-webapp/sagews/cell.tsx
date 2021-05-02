b29f4b3ff4a96a5a08b7732972d1aa618dfe895d
William Stein <wstein@sagemath.com>
2021-05-02 16:26:49 +0000

    sagews static: rewrite cell rendering as a functional component

--- src/smc-webapp/sagews/cell.tsx
+++ src/smc-webapp/sagews/cell.tsx
@@ -7,7 +7,7 @@
 Rendering a Sage worksheet cell
 */
 
-import { React, Component, Rendered } from "../app-framework";
+import * as React from "react";
 
 import { CellInput } from "./input";
 import { CellOutput } from "./output";
@@ -18,23 +18,11 @@ interface Props {
   flags: string;
 }
 
-export class Cell extends Component<Props> {
-  private render_input(): Rendered {
-    return <CellInput input={this.props.input} flags={this.props.flags} />;
-  }
-
-  private render_output(): Rendered {
-    if (this.props.output != null) {
-      return <CellOutput output={this.props.output} flags={this.props.flags} />;
-    }
-  }
-
-  public render(): Rendered {
-    return (
-      <div>
-        {this.render_input()}
-        {this.render_output()}
-      </div>
-    );
-  }
+export function Cell({ input, output, flags }: Props) {
+  return (
+    <div>
+      <CellInput input={input} flags={flags} />
+      {output != null && <CellOutput output={output} flags={flags} />}
+    </div>
+  );
 }
