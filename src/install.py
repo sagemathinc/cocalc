b5a6fc2508b7af35cce52d9aa9b5fa489473d112
William A. Stein <wstein@sagemath.com>
2021-05-14 22:27:02 +0000

    workspaces: working on getting things to build

--- src/install.py
+++ src/install.py
@@ -62,14 +62,15 @@ def install_sagews():
 
 
 def install_project():
-    # unsafe-perm below is needed so can build C code as root
-    cmd("cd smc-project && npm --loglevel=warn --progress=false ci")
+    # This function assumes npm_run_make() ran successfully.
 
-    # global install, hence no "npm ci" (!)
+    # unsafe-perm below is needed so can build C code as root
+    # global install.
     for pkg in ['coffeescript', 'forever']:
         c = f"npm --loglevel=warn --unsafe-perm=true --progress=false install --upgrade {pkg} -g"
         cmd(SUDO + c)
 
+    #### TODO!
     pkgs = ['./smc-project', './smc-webapp', './smc-util-node', './smc-util']
 
     # TODO switch to use npm ci to install these (which doesn't exist for global installs, AFAIU)
@@ -81,120 +82,47 @@ def install_project():
         total = sum(_ for _ in executor.map(build_op, pkgs))
         print(f"TOTAL PROJECT PKG BUILD TIME: {total:.1f}s")
 
-    # UGLY; hard codes the path -- TODO: fix at some point.
-    try:
-        cmd("cd /usr/local/lib/node_modules/smc-project/jupyter && %s npm --loglevel=warn ci --unsafe-perm=true --progress=false --upgrade"
-            % SUDO)
-    except:
-        cmd("cd /usr/lib/node_modules/smc-project/jupyter && %s npm --loglevel=warn ci --unsafe-perm=true --progress=false --upgrade"
-            % SUDO)
 
-    # At least run typescript...
-    # TODO: currently this errors somewhere in building something in node_modules in smc-webapp, since I can't get
-    # tsconfig to just leave that code alone (since it is used?).  Hmm...
-    cmd("cd /cocalc/src/smc-project; /cocalc/src/node_modules/.bin/tsc -p tsconfig.json",
-        error=False)
-
-    # Pre-compile everything to Javascript, so that loading is much faster and more efficient.
-    # This can easily save more than 2 seconds, given how big things have got.
-    try:
-        cmd("cd /usr/local/lib/node_modules && coffee -c smc-util smc-util-node smc-webapp smc-project smc-project/jupyter"
-            )
-    except:
-        cmd("cd /usr/lib/node_modules && coffee -c smc-util smc-util-node smc-webapp smc-project smc-project/jupyter"
-            )
+def npm_run_make():
+    cmd("npm run make")
 
 
-def install_hub():
-    paths = [
-        '.', 'smc-hub', 'smc-util-node', 'smc-util', 'webapp-lib/resources'
-    ]
-
-    # npm ci for using pkg lock file
-    def build_op(path):
-        return cmd(f"cd {path} && npm --loglevel=warn --progress=false ci")
-
-    with ThreadPoolExecutor(max_workers=WORKERS) as executor:
-        total = sum(_ for _ in executor.map(build_op, paths))
-        print(f"TOTAL HUB BUILD TIME: {total:.1f}s")
-
-
-def install_webapp(*args):
+def install_webapp():
     nice()
-    action = args[0].action if args else 'build'
-    nothing = True
+    # ASSUME npm_run_make() has been called before!
+    cmd("git submodule update --init")
+    cmd("cd examples && env OUTDIR=../webapp-lib/examples make")
+    # E.g., this depends on running npm_run_make first, see above
+    cmd("python3 webapp-lib/resources/setup.py")
 
-    if 'build' in action:
-        cmd("git submodule update --init")
-        cmd("cd examples && env OUTDIR=../webapp-lib/examples make")
+    # react static step must come *before* webpack step
+    cmd("update_react_static")
 
-        paths = ['smc-webapp', '.', 'smc-util', 'webapp-lib/resources']
-
-        # npm ci for using pkg lock file
-        def build_op(path):
-            return cmd(f"cd {path} && npm --loglevel=warn --progress=false ci")
-
-        with ThreadPoolExecutor(max_workers=WORKERS) as executor:
-            total = sum(_ for _ in executor.map(build_op, paths))
-            print(f"TOTAL WEBAPP BUILD TIME: {total:.1f}s")
-
-        # this depends on running npm ci first, see above
-        cmd("python3 webapp-lib/resources/setup.py")
-
-        # react static step must come *before* webpack step
-        cmd("update_react_static")
-
-        # download compute environment information
-        if os.environ.get('CC_COMP_ENV') == 'true':
-            print(
-                "Downloading compute environment information, because 'CC_COMP_ENV' is true"
-            )
-            # this is python3-only
-            from urllib.request import urlretrieve
-            try:
-                host = 'https://storage.googleapis.com/cocalc-compute-environment/'
-                files = ['compute-inventory.json', 'compute-components.json']
-                for fn in files:
-                    out = os.path.join(SRC, 'webapp-lib', fn)
-                    urlretrieve(host + fn, out)
-            except Exception as ex:
-                print(
-                    "WARNING: problem while downloading the compute environment information"
-                )
-                raise ex
-
-        # update primus - so client has it.
-        install_primus()
-        # update term.js
-        cmd("cd webapp-lib/term; ./compile")
-        wtype = 'debug' if (len(args) > 0 and args[0].debug) else 'production'
-        if len(args) > 0 and args[0].debug:
-            wtype = 'debug'
-            est = 3
-        else:
-            wtype = 'production'
-            est = 10
+    # download compute environment information
+    if os.environ.get('CC_COMP_ENV') == 'true':
         print(
-            f"Building {wtype} webpack -- this should take up to {est} minutes"
+            "Downloading compute environment information, because 'CC_COMP_ENV' is true"
         )
-        cmd(f"npm --loglevel=warn --progress=false run webpack-{wtype}")
-        nothing = False
+        # this is python3-only
+        from urllib.request import urlretrieve
+        try:
+            host = 'https://storage.googleapis.com/cocalc-compute-environment/'
+            files = ['compute-inventory.json', 'compute-components.json']
+            for fn in files:
+                out = os.path.join(SRC, 'webapp-lib', fn)
+                urlretrieve(host + fn, out)
+        except Exception as ex:
+            print(
+                "WARNING: problem while downloading the compute environment information"
+            )
+            raise ex
 
-    if 'pull' == action:
-        cmd("webapp-control.sh pull")
-        nothing = False
-
-    if 'push' in action:
-        cmd("webapp-control.sh push")
-        nothing = False
-
-    if 'clean' == action:
-        cmd("webapp-control.sh clean")
-        nothing = False
-
-    # some fallback check, just in case ...
-    if nothing:
-        raise ValueError("action %s unknown" % action)
+    # update primus - so client has it.
+    install_primus()
+    # update term.js
+    cmd("cd webapp-lib/term; ./compile")
+    print(f"Building webpack -- this should take at least 10 minutes")
+    cmd(f"npm --loglevel=warn --progress=false run webpack-production")
 
 
 def install_primus():
@@ -212,7 +140,7 @@ def install_primus():
 def install_all(compute=False, web=False):
     if compute or web:
         # also contains compute server right now (will refactor later)
-        install_hub()
+        npm_run_make()
     if compute:
         install_pyutil()
         install_sagews()
@@ -230,28 +158,6 @@ def main():
         'pull', help='pull latest version of code from github')
     parser_pull.set_defaults(func=lambda *args: pull())
 
-    parser_hub = subparsers.add_parser(
-        'hub',
-        help='install/update any node.js dependencies for smc-[util/util*/hub]'
-    )
-    parser_hub.set_defaults(func=lambda *args: install_hub())
-
-    parser_webapp = subparsers.add_parser(
-        'webapp',
-        help=
-        'install/update any node.js dependencies for smc-[util*/webapp] and use webpack to build production js (takes several minutes!)'
-    )
-    parser_webapp.add_argument(
-        'action',
-        help=
-        'either "build" the webapp or "pull/push" compiled files from a repository -- see scripts/webapp-control.sh how this works',
-        choices=['build', 'pull', 'push', 'build-push', 'clean'])
-    parser_webapp.add_argument(
-        "--debug",
-        action="store_true",
-        help="if set, build debug version of code (rather than production)")
-    parser_webapp.set_defaults(func=install_webapp)
-
     parser_primus = subparsers.add_parser(
         'primus', help='update client-side primus websocket code')
     parser_primus.set_defaults(func=lambda *args: install_primus())
@@ -268,10 +174,6 @@ def main():
                                default='sage',
                                type=str)
     parser_sagews.set_defaults(func=lambda *args: install_sagews())
-
-    parser_project = subparsers.add_parser(
-        'project', help='install project server code system-wide')
-    parser_project.set_defaults(func=lambda *args: install_project())
 
     parser_all = subparsers.add_parser(
         'all',
--- src/scripts/cocalc-dirs.sh
+++ src/scripts/cocalc-dirs.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-CODE_DIRS=($SMC_ROOT  $SMC_ROOT/smc-project $SMC_ROOT/smc-project/jupyter $SMC_ROOT/smc-webapp $SMC_ROOT/smc-hub $SMC_ROOT/smc-util-node $SMC_ROOT/smc-util $SMC_ROOT/webapp-lib/resources)
+# the order here probably matters...
+
+CODE_DIRS=($SMC_ROOT  $SMC_ROOT/smc-project $SMC_ROOT/smc-webapp $SMC_ROOT/smc-hub $SMC_ROOT/smc-util-node $SMC_ROOT/smc-util $SMC_ROOT/webapp-lib/resources)
