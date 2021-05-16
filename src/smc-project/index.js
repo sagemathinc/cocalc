5ae3f0fafc02d0cbf6c28d41d6eb675e42feb9ec
William A. Stein <wstein@sagemath.com>
2021-05-16 17:25:38 +0000

    workspaces: move some deps to dev dependencies

--- src/smc-project/index.js
+++ src/smc-project/index.js
@@ -3,9 +3,6 @@
  *  License: AGPLv3 s.t. "Commons Clause" – see LICENSE.md for details
  */
 
-require("coffeescript/register"); /* so we can require coffeescript */
-require("coffee-cache"); /* so coffeescript doesn't get recompiled every time we require it */
-
 if (!process.env.SMC) {
   process.env.SMC = path.join(process.env.HOME, ".smc");
 }
