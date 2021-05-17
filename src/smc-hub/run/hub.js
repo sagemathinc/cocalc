a45db5d2ff515223d7bfcedaef2704e3b3898be8
William A. Stein <wstein@sagemath.com>
2021-05-17 01:27:00 +0000

    working on smc-hub packaging so that it is easy to run

--- src/smc-hub/run/hub.js
+++ src/smc-hub/run/hub.js
@@ -1,5 +1,7 @@
+#!/usr/bin/env node
+
 /*
- *  This file is part of CoCalc: Copyright © 2020 Sagemath, Inc.
+ *  This file is part of CoCalc: Copyright © 2021 Sagemath, Inc.
  *  License: AGPLv3 s.t. "Commons Clause" – see LICENSE.md for details
  */
 
