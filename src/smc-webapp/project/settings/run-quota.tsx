c6bc840b155cd819ce87f83d4019d06405c5b1ed
Harald Schilly <hsy@sagemath.com>
2021-05-21 10:34:42 +0000

    webapp/quotas: more details, show maximum, ... WIP

--- src/smc-webapp/project/settings/run-quota.tsx
+++ src/smc-webapp/project/settings/run-quota.tsx
@@ -4,9 +4,16 @@
  */
 
 import * as React from "react";
-import { Table } from "antd";
-import { CheckCircleTwoTone, CloseCircleTwoTone } from "@ant-design/icons";
+import { Table, Typography } from "antd";
+const { Text } = Typography;
+import {
+  CheckCircleTwoTone,
+  CloseCircleTwoTone,
+  PoweroffOutlined,
+  QuestionCircleOutlined,
+} from "@ant-design/icons";
 import { isEqual } from "lodash";
+import { Tip } from "../../r_misc";
 import { useTypedRedux, useState } from "../../app-framework";
 import { PROJECT_UPGRADES } from "smc-util/schema";
 import { seconds2hms } from "smc-util/misc";
@@ -37,6 +44,14 @@ function upgrade2quota_key(key: string): keyof RunQuota {
   }
   return key as keyof RunQuota;
 }
+
+const Q: React.FC<{ children; tip }> = ({ children, tip }) => {
+  return (
+    <Tip tip={tip}>
+      {children} <QuestionCircleOutlined />
+    </Tip>
+  );
+};
 
 interface QuotaData {
   key: string;
@@ -77,7 +92,7 @@ export const RunQuota: React.FC<Props> = React.memo((props: Props) => {
   const run_quota = useRunQuota(project_id);
   const max_upgrades = useMaxUpgrades();
 
-  function quota_value(key: keyof RunQuota): string | boolean | number {
+  function quota_limit(key: keyof RunQuota): string | boolean | number {
     const val = run_quota[key];
     if (val == null) return "N/A";
     if (key == "idle_timeout" && typeof val === "number")
@@ -86,57 +101,122 @@ export const RunQuota: React.FC<Props> = React.memo((props: Props) => {
   }
 
   const data = React.useMemo(() => {
+    const ar = !!run_quota.always_running;
     return PROJECT_UPGRADES.field_order.map((name: string) => {
       const key = upgrade2quota_key(name);
       const display = PARAMS[name]?.display ?? name;
-      const value = quota_value(key);
+      const desc = PARAMS[name]?.desc ?? "";
+      const limit = key == "idle_timeout" && ar ? "&infin;" : quota_limit(key);
       const maximum = max_upgrades?.[name] ?? "N/A";
-      return { key, display, value, maximum };
+      const usage = "123";
+      return { key, display, limit, maximum, desc, usage };
     });
   }, [run_quota]);
 
+  function render_limit(_, record) {
+    if (state != "running") {
+      return (
+        <Tip
+          tip={
+            "The project is currently not running. Start the project to see the effective quotas."
+          }
+        >
+          <PoweroffOutlined />
+        </Tip>
+      );
+    }
+    if (record.key === "idle_timeout" && record.limit === "&infin;") {
+      return (
+        <Q tip="If the project stops or the underlying VM goes into maintenance, the project will automatically restart.">
+          &infin;
+        </Q>
+      );
+    }
+
+    const val = record.limit;
+    if (typeof val === "boolean") {
+      if (val) {
+        return <CheckCircleTwoTone twoToneColor={COLORS.ANTD_GREEN} />;
+      } else {
+        return <CloseCircleTwoTone twoToneColor={COLORS.ANTD_RED} />;
+      }
+    }
+    return <Text strong>{val}</Text>;
+  }
+
+  function render_usage(_, record) {
+    return record.usage;
+  }
+
+  function render_maximum(text, record) {
+    if (SHOW_MAX.includes(record.key)) {
+      return <Text type="secondary">{text}</Text>;
+    } else {
+      return "";
+    }
+  }
+
+  function render_extra(record) {
+    return (
+      <div>
+        <p>{record.desc}</p>
+      </div>
+    );
+  }
+
   function render_quotas() {
     return (
-      <Table<QuotaData> dataSource={data} size="small" pagination={false}>
-        <Table.Column<QuotaData> key="key" title="Quota" dataIndex="display" />
+      <Table<QuotaData>
+        dataSource={data}
+        size="small"
+        pagination={false}
+        expandedRowRender={render_extra}
+      >
         <Table.Column<QuotaData>
           key="key"
-          title="Value"
-          dataIndex="value"
-          render={(_, record) => {
-            const val = record.value;
-            if (typeof val === "boolean") {
-              if (val) {
-                return <CheckCircleTwoTone twoToneColor={COLORS.ANTD_GREEN} />;
-              } else {
-                return <CloseCircleTwoTone twoToneColor={COLORS.ANTD_RED} />;
-              }
-            }
-            return val;
-          }}
+          title={
+            <Q tip="Name of the quota. Click on [+] to expand its details">
+              Quota
+            </Q>
+          }
+          dataIndex="display"
+          width={6}
         />
         <Table.Column<QuotaData>
           key="key"
-          title="Maximum"
+          title={<Q tip="Current usage of the project.">Usage</Q>}
+          dataIndex="usage"
+          render={render_usage}
+          width={1}
+          align={"right"}
+        />
+        <Table.Column<QuotaData>
+          key="key"
+          title={
+            <Q tip="Usage limit imposed by the current quota. Adjust Quotas or Licenses to change this limit.">
+              Limit
+            </Q>
+          }
+          dataIndex="value"
+          render={render_limit}
+          width={1}
+          align={"right"}
+        />
+        <Table.Column<QuotaData>
+          key="key"
+          title={
+            <Q tip="Largest possible quota value. Projects can't be upgraded beyond that limit.">
+              Max. Quota
+            </Q>
+          }
           dataIndex="maximum"
-          render={(text, record) => {
-            console.log("render max", text, record.key);
-            if (SHOW_MAX.includes(record.key)) {
-              return text;
-            } else {
-              return "";
-            }
-          }}
+          render={render_maximum}
+          width={1}
+          align={"right"}
         />
       </Table>
     );
   }
 
-  return (
-    <div>
-      <h3>Current Quotas</h3>
-      <p>state: {state}</p>
-      {render_quotas()}
-    </div>
-  );
+  return <div>{render_quotas()}</div>;
 });
