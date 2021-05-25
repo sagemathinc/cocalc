ea8af1d0e82dfa1629b47a58727f1e311609d71d
William A. Stein <wstein@sagemath.com>
2021-05-25 16:09:30 +0000

    defer loading pdfjs until needed (hopefully)

--- src/smc-webapp/frame-editors/pdf-editor/register.ts
+++ src/smc-webapp/frame-editors/pdf-editor/register.ts
@@ -7,13 +7,10 @@
 Register the PDF editor
 */
 
-import { Editor } from "./editor";
-import { PDFActions } from "./actions";
-
 import { register_file_editor } from "../frame-tree/register";
 
 register_file_editor({
   ext: "pdf",
-  component: Editor,
-  Actions: PDFActions,
+  editor: async () => await import("./editor"),
+  actions: async () => await import("./actions"),
 });
