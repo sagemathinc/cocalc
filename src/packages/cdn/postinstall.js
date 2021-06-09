e373e5efbc3fae96af245fde81b93347844a5ac3
William A. Stein <wstein@sagemath.com>
2021-06-09 23:50:53 +0000

    don't run postinstall version symlinking on initial "npm ci".

--- src/packages/cdn/postinstall.js
+++ src/packages/cdn/postinstall.js
@@ -2,7 +2,12 @@
 
 console.log("Ensuring versioned symlinks exist.");
 
-const { versions } = require("./dist/index.js");
+try {
+  const { versions } = require("./dist/index.js");
+} catch (err) {
+  // versions info doesn't exist yet, e.g., when doing an initial "npm ci".
+  process.exit(0);
+}
 
 const { statSync, symlinkSync } = require("fs");
 
