6c48a5da25efe91aa040e9eae3f5c5fc5dcd36d2
William A. Stein <wstein@sagemath.com>
2021-06-09 18:55:19 +0000

    packages/static -- production build has trivial base url for primus

---
+++ src/packages/static/production-build.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import os, shutil
+
+
+def handle_path(s, path=None):
+    desc = s
+    if path is not None:
+        os.chdir(path)
+        desc += " # in '%s'" % path
+    print(desc)
+
+
+def cmd(s, path=None):
+    home = os.path.abspath(os.curdir)
+    try:
+        handle_path(s, path)
+        if os.system(s):
+            raise RuntimeError("Error executing '%s'" % s)
+    finally:
+        os.chdir(home)
+
+
+try:
+    # Build with production BASE_URL:
+    # TODO -- this is dumb and we must get rid of hardcoding of the base url. But that is another problem for later...
+    # This is necessary for now, since webpack gets content -- such as the primus script that hardcodes the base url --
+    # from webapp-lib, so for our bundle files to be correct, we have to build webapp-lib.
+    cmd('BASE_URL="" npm run build', '../../webapp-lib')
+    shutil.rmtree('dist')
+    cmd("NODE_OPTIONS=--max_old_space_size=8000 COCALC_BASE_URL='/' BABEL_ENV=production NODE_ENV=production webpack --progress --color"
+        )
+finally:
+    # Build again with non-production base url.
+    cmd('npm run build', '../../webapp-lib')
--- src/webapp-lib/primus/update-primus.js
+++ src/webapp-lib/primus/update-primus.js
@@ -3,18 +3,22 @@
  *  License: AGPLv3 s.t. "Commons Clause" – see LICENSE.md for details
  */
 
-const fs   = require('fs');
-const path = require('path');
-const program = require('commander');
+const fs = require("fs");
+const path = require("path");
+const program = require("commander");
 
 const path_to_base_url = path.join(
   process.env["SALVUS_ROOT"],
   "data",
   "base_url"
 );
-const BASE_URL = fs.existsSync(path_to_base_url)
-  ? fs.readFileSync(path_to_base_url).toString().trim()
-  : "";
+
+let BASE_URL = process.env.BASE_URL;
+if (BASE_URL == null) {
+  BASE_URL = fs.existsSync(path_to_base_url)
+    ? fs.readFileSync(path_to_base_url).toString().trim()
+    : "";
+}
 
 function update() {
   const opts = { pathname: path.join(BASE_URL, "/hub") };
@@ -22,7 +26,7 @@ function update() {
   const primus = new require("primus")(require("http").createServer(), opts);
   fs.writeFileSync("primus-engine.js", primus.library());
   process.exit();
-};
+}
 
 program.usage("[options]").parse(process.argv);
 
