17f61fd97ee4e65cd27a2cb16e079753f46b08a5
Harald Schilly <hsy@sagemath.com>
2021-06-17 13:59:23 +0000

    webapp/jupyter: FC-ing more output, message and javascript

--- src/smc-webapp/jupyter/output-messages/message.tsx
+++ src/smc-webapp/jupyter/output-messages/message.tsx
@@ -8,7 +8,7 @@ Handling of output messages.
 */
 
 import { NotebookFrameActions } from "../../frame-editors/jupyter-editor/cell-notebook/actions";
-import { React, Component, Rendered } from "smc-webapp/app-framework";
+import { React, Rendered } from "smc-webapp/app-framework";
 import { JupyterActions } from "../actions";
 import { Map } from "immutable";
 import { OUTPUT_STYLE, OUTPUT_STYLE_SCROLLED } from "./style";
@@ -58,23 +58,23 @@ interface CellOutputMessageProps {
   trust?: boolean; // is notebook trusted by the user (if not won't eval javascript)
 }
 
-export class CellOutputMessage extends Component<CellOutputMessageProps> {
-  render() {
-    const C: any = message_component(this.props.message);
+export const CellOutputMessage: React.FC<CellOutputMessageProps> = React.memo(
+  (props: CellOutputMessageProps) => {
+    const C: any = message_component(props.message);
     return (
       <C
-        message={this.props.message}
-        project_id={this.props.project_id}
-        directory={this.props.directory}
-        actions={this.props.actions}
-        frame_actions={this.props.frame_actions}
-        name={this.props.name}
-        trust={this.props.trust}
-        id={this.props.id}
+        message={props.message}
+        project_id={props.project_id}
+        directory={props.directory}
+        actions={props.actions}
+        frame_actions={props.frame_actions}
+        name={props.name}
+        trust={props.trust}
+        id={props.id}
       />
     );
   }
-}
+);
 
 interface CellOutputMessagesProps {
   output: Map<string, any>; // the actual messages
@@ -88,90 +88,101 @@ interface CellOutputMessagesProps {
   id?: string;
 }
 
-export class CellOutputMessages extends Component<CellOutputMessagesProps> {
-  shouldComponentUpdate(nextProps): boolean {
-    return (
-      !nextProps.output.equals(this.props.output) ||
-      nextProps.scrolled !== this.props.scrolled ||
-      nextProps.trust !== this.props.trust
-    );
-  }
+function should_memoize(prev, next) {
+  return (
+    next.output.equals(prev.output) &&
+    next.scrolled === prev.scrolled &&
+    next.trust === prev.trust
+  );
+}
 
-  render_output_message(n: string, mesg: Map<string, any>): Rendered {
-    return (
-      <CellOutputMessage
-        key={n}
-        message={mesg}
-        project_id={this.props.project_id}
-        directory={this.props.directory}
-        actions={this.props.actions}
-        frame_actions={this.props.frame_actions}
-        name={this.props.name}
-        trust={this.props.trust}
-        id={this.props.id}
-      />
-    );
-  }
+export const CellOutputMessages: React.FC<CellOutputMessagesProps> = React.memo(
+  (props: CellOutputMessagesProps) => {
+    const {
+      output,
+      actions,
+      frame_actions,
+      name,
+      project_id,
+      directory,
+      scrolled,
+      trust,
+      id,
+    } = props;
 
-  message_list = (): Map<string, any>[] => {
-    const v: any[] = [];
-    let k = 0;
-    // TODO: use caching to make this more efficient...
-    for (
-      let n = 0, end = this.props.output.size, asc = 0 <= end;
-      asc ? n < end : n > end;
-      asc ? n++ : n--
-    ) {
-      const mesg = this.props.output.get(`${n}`);
-      // Make this renderer robust against any possible weird shape of the actual
-      // output object, e.g., undefined or not immmutable js.
-      // Also, we're checking that get is defined --
-      //   see https://github.com/sagemathinc/cocalc/issues/2404
-      if (mesg == null || typeof mesg.get != "function") {
-        console.warn(`Jupyter -- ignoring invalid mesg ${mesg}`);
-        continue;
-      }
-      const name = mesg.get("name");
-      if (
-        k > 0 &&
-        (name === "stdout" || name === "stderr") &&
-        v[k - 1].get("name") === name
-      ) {
-        // combine adjacent stdout / stderr messages...
-        let text = mesg.get("text");
-        if (typeof text != "string") {
-          text = `${text}`;
-        }
-        v[k - 1] = v[k - 1].set(
-          "text",
-          v[k - 1].get("text") + mesg.get("text")
-        );
-      } else {
-        v[k] = mesg;
-        k += 1;
-      }
+    function render_output_message(
+      n: string,
+      mesg: Map<string, any>
+    ): Rendered {
+      return (
+        <CellOutputMessage
+          key={n}
+          message={mesg}
+          project_id={project_id}
+          directory={directory}
+          actions={actions}
+          frame_actions={frame_actions}
+          name={name}
+          trust={trust}
+          id={id}
+        />
+      );
     }
-    return v;
-  };
 
-  render(): Rendered {
-    // (yes, I know n is a string in the next line, but that's fine since it is used only as a key)
+    function message_list(): Map<string, any>[] {
+      const v: any[] = [];
+      let k = 0;
+      for (let n = 0, end = output.size; n < end; n++) {
+        const mesg = output.get(`${n}`);
+        // Make this renderer robust against any possible weird shape of the actual
+        // output object, e.g., undefined or not immmutable js.
+        // Also, we're checking that get is defined --
+        //   see https://github.com/sagemathinc/cocalc/issues/2404
+        if (mesg == null || typeof mesg.get !== "function") {
+          console.warn(`Jupyter -- ignoring invalid mesg ${mesg}`);
+          continue;
+        }
+        const name = mesg.get("name");
+        if (
+          k > 0 &&
+          (name === "stdout" || name === "stderr") &&
+          v[k - 1].get("name") === name
+        ) {
+          // combine adjacent stdout / stderr messages...
+          let text = mesg.get("text");
+          if (typeof text !== "string") {
+            text = `${text}`;
+          }
+          const merged = v[k - 1].get("text") + mesg.get("text");
+          v[k - 1] = v[k - 1].set("text", merged);
+        } else {
+          v[k] = mesg;
+          k += 1;
+        }
+      }
+      return v;
+    }
+
+    // for the same output map, we derive the same list of objects
+    const object: Map<string, any>[] = React.useMemo(message_list, [output]);
+
     const v: Rendered[] = [];
-    const object: Map<string, any>[] = this.message_list();
+    // (yes, I know n is a string in the next line, but that's fine since it is used only as a key)
     let n: string;
     for (n in object) {
       const mesg = object[n];
       if (mesg != null) {
-        v.push(this.render_output_message(n, mesg));
+        v.push(render_output_message(n, mesg));
       }
     }
     return (
       <div
-        style={this.props.scrolled ? OUTPUT_STYLE_SCROLLED : OUTPUT_STYLE}
+        style={scrolled ? OUTPUT_STYLE_SCROLLED : OUTPUT_STYLE}
         className="cocalc-jupyter-rendered"
       >
         {v}
       </div>
     );
-  }
-}
+  },
+  should_memoize
+);
--- src/smc-webapp/jupyter/output-messages/more-output.tsx
+++ src/smc-webapp/jupyter/output-messages/more-output.tsx
@@ -5,9 +5,10 @@
 
 import { Button } from "react-bootstrap";
 import { Icon } from "../../r_misc/icon";
-import { React, Component, Rendered } from "smc-webapp/app-framework";
+import { React } from "smc-webapp/app-framework";
 import { Map } from "immutable";
 import { JupyterActions } from "../actions";
+import { all_fields_equal } from "smc-util/misc";
 
 interface MoreOutputProps {
   message: Map<string, any>;
@@ -15,21 +16,19 @@ interface MoreOutputProps {
   actions?: JupyterActions; // if not set, then can't get more output (button disabled)
 }
 
-export class MoreOutput extends Component<MoreOutputProps> {
-  shouldComponentUpdate(nextProps: MoreOutputProps): boolean {
-    return (
-      nextProps.message !== this.props.message || nextProps.id != this.props.id
-    );
-  }
+function should_memoize(prev, next) {
+  return all_fields_equal(prev, next, ["message", "id"]);
+}
 
-  show_more_output = (): void => {
-    this.props.actions != null
-      ? this.props.actions.fetch_more_output(this.props.id)
-      : undefined;
-  };
+export const MoreOutput: React.FC<MoreOutputProps> = React.memo(
+  (props: MoreOutputProps) => {
+    const { id, message, actions } = props;
 
-  render(): Rendered {
-    if (this.props.actions == null || this.props.message.get("expired")) {
+    function show_more_output(): void {
+      actions?.fetch_more_output(id);
+    }
+
+    if (actions == null || message.get("expired")) {
       return (
         <Button bsStyle="info" disabled>
           <Icon name="eye-slash" /> Additional output not available
@@ -37,10 +36,11 @@ export class MoreOutput extends Component<MoreOutputProps> {
       );
     } else {
       return (
-        <Button onClick={this.show_more_output} bsStyle="info">
+        <Button onClick={show_more_output} bsStyle="info">
           <Icon name="eye" /> Fetch additional output...
         </Button>
       );
     }
-  }
-}
+  },
+  should_memoize
+);
