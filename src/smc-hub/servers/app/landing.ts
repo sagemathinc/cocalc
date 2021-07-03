749c146109ca56385fc940325928302698649250
William Stein <wstein@sagemath.com>
2021-07-03 23:54:35 +0000

    nextjs landing: support dev and production modes

--- src/smc-hub/servers/app/landing.ts
+++ src/smc-hub/servers/app/landing.ts
@@ -9,7 +9,8 @@ import basePath from "smc-util-node/base-path";
 export default async function init(app: Application) {
   const winston = getLogger("landing");
   winston.info("initializing the landing server");
-  const handler = await initLandingServer({ basePath, dev: true, winston });
+  const dev = process.env.NODE_ENV !== "production";
+  const handler = await initLandingServer({ basePath, dev, winston });
   winston.info("Now using next.js handler to handle select endpoints");
   app.all(basePath, handler);
   app.all(basePath + "/_next/*", handler);
