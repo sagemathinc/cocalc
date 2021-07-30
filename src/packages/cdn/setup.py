a84bc18d041154608ca05376f687ab74e49ef29d
William Stein <wstein@sagemath.com>
2021-07-30 19:49:32 +0000

    use typescript for autogenerated index for cdn package

--- src/packages/cdn/setup.py
+++ src/packages/cdn/setup.py
@@ -38,6 +38,10 @@ for path, data in deps.items():
 copytree("../pix", "pix")
 
 # finally, write the version info such that it can be loaded
-with open('index.js', 'w') as out:
-    out.write(f'exports.versions = {json.dumps(versions)};\n')
-    out.write('exports.path = __dirname;\n')
+with open('index.ts', 'w') as out:
+    out.write(f'export const versions = {json.dumps(versions)};\n')
+    out.write('declare const __dirname : string;')
+    out.write('export const path : string = __dirname;\n')
+
+if os.system("npx tsc --declaration index.ts"):
+    raise RuntimeError("error building index.js")
