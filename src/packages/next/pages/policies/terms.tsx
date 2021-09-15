b6db706c93d23a747baf87a7275ab67680755e59
William Stein <wstein@sagemath.com>
2021-09-15 20:12:32 +0000

    next: rewrote all the policies pages

--- src/packages/next/pages/policies/terms.tsx
+++ src/packages/next/pages/policies/terms.tsx
@@ -25,7 +25,7 @@ export default function TermsOfService({ customize }) {
           }}
         >
           <div style={{ textAlign: "center", color: "#444" }}>
-            <h1 style={{ fontSize: "28pt" }}>CoCalc Terms of Service</h1>
+            <h1 style={{ fontSize: "28pt" }}>CoCalc  - Terms of Service</h1>
             <h2>Last Updated: September 15, 2021</h2>
           </div>
           <div style={{ fontSize: "12pt" }}>
