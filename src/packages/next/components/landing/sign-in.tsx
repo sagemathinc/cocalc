ccf4440f112eab321d0e21b19304d36a3fb67a27
William Stein <wstein@sagemath.com>
2021-10-03 17:49:46 +0000

    make the sign in buttons in features part of landing pages be aware if user is signed in

--- src/packages/next/components/landing/sign-in.tsx
+++ src/packages/next/components/landing/sign-in.tsx
@@ -2,16 +2,34 @@ import { join } from "path";
 import { useCustomize } from "lib/customize";
 import basePath from "lib/base-path";
 import { ReactNode } from "react";
+import A from "components/misc/A";
 
 interface Props {
   startup?: ReactNode; // customize the button, e.g. "Start Jupyter Now".
   hideFree?: boolean;
 }
 
+const STYLE = { textAlign: "center", padding: "30px 15px 0 15px" };
+
 export default function SignIn({ startup, hideFree }: Props) {
-  const { anonymousSignup, siteName } = useCustomize();
+  const { anonymousSignup, siteName, account } = useCustomize();
+  if (account != null) {
+    return (
+      <div style={STYLE}>
+        <A
+          className="ant-btn"
+          href={join(basePath, "projects")}
+          external={true}
+          style={{ margin: "15px" }}
+          title={`Open the ${siteName} app and view your projects`}
+        >
+          View Your {siteName} Projects...
+        </A>
+      </div>
+    );
+  }
   return (
-    <div style={{ textAlign: "center", padding: "30px 15px" }}>
+    <div style={STYLE}>
       {/* We use className="ant-btn" instead of an actual Button, because otherwise
             we get a ton of useLayoutEffects due to server-side rendering.*/}
       {anonymousSignup && (
