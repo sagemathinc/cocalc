add60168cddb4f00903e517a9d3015f67cffd3fb
William Stein <wstein@sagemath.com>
2021-10-14 20:23:54 +0000

    add signed in message on top level landing page

--- src/packages/next/components/account/avatar.tsx
+++ src/packages/next/components/account/avatar.tsx
@@ -3,19 +3,9 @@ Show an avatar for a given user account.
 */
 
 import { Avatar as AntdAvatar } from "antd";
-import { CSSProperties, useEffect, useState } from "react";
-import apiPost from "lib/api/post";
-import { Profile } from "@cocalc/backend/accounts/profile/types";
+import { CSSProperties } from "react";
 import { avatar_fontcolor } from "@cocalc/frontend/account/avatar/font-color";
-import LRU from "lru-cache";
-
-// This cache is to avoid flicker when navigating around, since
-// we want to show the last known avatar for a given user before
-// checking if there is a new one.
-const cache = new LRU<string, object>({ max: 300 });
-
-// How often to check for new avatars.
-const DEFAULT_CACHE_S = 30;
+import useProfile from "lib/hooks/profile";
 
 interface Props {
   account_id: string;
@@ -28,27 +18,7 @@ export default function Avatar({ account_id, size, style }: Props) {
     // Default size=40 to match the cocalc logo.
     size = 40;
   }
-  const [profile, setProfile] = useState<Profile | undefined>(
-    cache.get(account_id)
-  );
-
-  async function getProfile(): Promise<void> {
-    try {
-      const { profile } = await apiPost(
-        "/accounts/profile",
-        { account_id },
-        DEFAULT_CACHE_S
-      );
-      setProfile(profile);
-      cache.set(account_id, profile);
-    } catch (err) {
-      console.warn("Unable to fetch a profile -- ", err);
-    }
-  }
-
-  useEffect(() => {
-    getProfile();
-  }, []);
+  const profile = useProfile(account_id);
 
   if (!profile) {
     // not loaded yet
---
+++ src/packages/next/components/account/name.tsx
@@ -0,0 +1,16 @@
+import useProfile from "lib/hooks/profile";
+import { trunc } from "lib/share/util";
+
+interface Props {
+  account_id: string;
+  style?: CSSProperties;
+}
+
+export default function Name({ account_id, style }: Props) {
+  const profile = useProfile(account_id);
+  if (profile == null) {
+    return <></>;
+  }
+  const { first_name, last_name } = profile;
+  return <>{trunc(`${first_name} ${last_name}`, 50)}</>;
+}
