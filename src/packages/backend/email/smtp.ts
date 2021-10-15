707df343b1bb0326989bd0a816aa45223af66d4c
William Stein <wstein@sagemath.com>
2021-10-15 20:22:49 +0000

    need to wait to ensure message gets sent

--- src/packages/backend/email/smtp.ts
+++ src/packages/backend/email/smtp.ts
@@ -25,7 +25,7 @@ export default async function sendEmail(
     }
   }
   const server = await getServer(settings);
-  server.sendMail(await appendFooter(message));
+  await server.sendMail(await appendFooter(message));
 }
 
 let server: undefined | Transporter = undefined;
