7fc588e76b904bb3210ab42d1ebb496182bb24af
Harald Schilly <hsy@sagemath.com>
2021-10-29 14:41:40 +0000

    Publish new version of package packages/frontend to npmjs package repo.

--- src/packages/frontend/project/warnings/disk-space.tsx
+++ src/packages/frontend/project/warnings/disk-space.tsx
@@ -40,6 +40,7 @@ export const DiskSpaceWarning: React.FC<{ project_id: string }> = ({
     return null;
   }
 
+  // the disk_usage comes from the project.status datatbase entry – not the "project-status" synctable
   const project_status = project.get("status");
   const disk_usage = project_status?.get("disk_MB");
   if (disk_usage == null) return null;
@@ -49,10 +50,12 @@ export const DiskSpaceWarning: React.FC<{ project_id: string }> = ({
     return null;
   }
 
+  const disk_free = Math.max(0, quotas.disk_quota - disk_usage);
+
   return (
     <Alert bsStyle="danger" style={ALERT_STYLE}>
       <Icon name="exclamation-triangle" /> WARNING: This project is running out
-      of disk space: only {disk_usage} MB out of {quotas.disk_quota} MB
+      of disk space: only {disk_free} MB out of {quotas.disk_quota} MB
       available.
       <a onClick={() => actions?.set_active_tab("settings")}>
         Increase the "Disk Space" quota
