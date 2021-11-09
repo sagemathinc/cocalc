324f584605841db572e6e3b85da1b7b9719b5a0c
William Stein <wstein@sagemath.com>
2021-11-09 06:59:35 +0000

    Publish new version of package packages/server to npmjs package repo.

--- src/packages/server/projects/control/util.ts
+++ src/packages/server/projects/control/util.ts
@@ -373,9 +373,9 @@ export async function copyPath(
   if (opts.wait_until_done ?? true) {
     try {
       const stdout = await spawnAsync("rsync", args, {
-        timeout:
-          1000 *
-          opts.timeout /* spawnAsync has ms units, but rsync has second units */,
+        timeout: opts.timeout
+          ? 1000 * opts.timeout
+          : undefined /* spawnAsync has ms units, but rsync has second units */,
       });
       winston.info(`finished rsync ${stdout}`);
     } catch (err) {
