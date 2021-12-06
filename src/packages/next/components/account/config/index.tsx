9f976c69efd9c67bc627441b35814780b3ed0c4f
William Stein <wstein@sagemath.com>
2021-12-05 18:11:54 -0800

    delete some unused commented out code

--- src/packages/next/components/account/config/index.tsx
+++ src/packages/next/components/account/config/index.tsx
@@ -11,37 +11,6 @@ import "./account/api";
 import "./account/delete-account";
 import "./account/sign-out";
 
-/*{
-  search: {
-    input: Search,
-  },
-  account: {
-    name: AccountName,
-    avatar: Avatar,
-    link: Link,
-    ssh: SSHKeys,
-    api: APIKey,
-    delete: DeleteAccount,
-    "sign-out": SignOut,
-  },
-};
-*/
-
-/*
-// Import all the components, which fills in the Components structure above, and also
-// the search data.
-import AccountName from "./account/name";
-import "./account/email";
-import Avatar from "./account/avatar";
-import Link from "./account/link";
-import SSHKeys from "./account/ssh";
-import APIKey from "./account/api";
-import DeleteAccount from "./account/delete-account";
-import SignOut from "./account/sign-out";
-
-import Search from "./search/component";
-*/
-
 interface Props {
   main: string;
   sub: string;
