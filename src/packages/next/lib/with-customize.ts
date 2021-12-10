103fbded6ee1af9bc24f136c88ac824c1a226707
Harald Schilly <hsy@sagemath.com>
2021-12-10 11:45:25 +0000

    next/share: tell search engines to "noindex" if unlisted

--- src/packages/next/lib/with-customize.ts
+++ src/packages/next/lib/with-customize.ts
@@ -1,3 +1,8 @@
+/*
+ *  This file is part of CoCalc: Copyright © 2020 Sagemath, Inc.
+ *  License: AGPLv3 s.t. "Commons Clause" – see LICENSE.md for details
+ */
+
 import getCustomize from "@cocalc/server/settings/customize";
 import getAccountId from "lib/account/get-account";
 import { getName } from "lib/share/get-account-info";
@@ -48,6 +53,8 @@ export default async function withCustomize(
       }
     }
   }
+
+  customize.noindex = obj.props.unlisted ?? false;
 
   if (obj == null) {
     return { props: { customize } };
