68fcea2dbacb5e59f0f74f242c8abf7a877416fc
William Stein <wstein@sagemath.com>
2022-01-19 17:13:43 -0800

    fix typo in pr #5648

--- src/packages/frontend/course/configuration/envvars-config.tsx
+++ src/packages/frontend/course/configuration/envvars-config.tsx
@@ -98,7 +98,7 @@ export const EnvironmentVariablesConfig: React.FC<Props> = (props: Props) => {
         reflected after the next start of a student project.
       </p>
       <p>
-        Nodte: inherited variables will take precedence over the ones defined in
+        Node: inherited variables will take precedence over the ones defined in
         the student project with the same name.
       </p>
       {toggle()}
