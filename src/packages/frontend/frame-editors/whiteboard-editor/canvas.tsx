43e79b2b9818ca37168a1cbe956eb7cebb967ce2
William Stein <wstein@sagemath.com>
2022-01-21 22:21:37 -0800

    whiteboard: just playing with grids and scale

--- src/packages/frontend/frame-editors/whiteboard-editor/canvas.tsx
+++ src/packages/frontend/frame-editors/whiteboard-editor/canvas.tsx
@@ -6,7 +6,7 @@ This is NOT an HTML5 canvas.  It has nothing do with that.   We define
 "the whiteboard" as everything -- the controls, settings, etc. -- and
 the canvas as the area where the actual drawing appears.
 */
-import { ReactNode, useEffect, useRef } from "react";
+import { ReactNode, useRef } from "react";
 import { Element } from "./types";
 import RenderElement from "./elements/render";
 
@@ -23,19 +23,25 @@ export default function Canvas({
   focusedId,
   margin,
 }: Props) {
+  margin = margin ?? 300;
   const canvasRef = useRef<any>(null);
   const scale = font_size ? font_size / 14 : 1;
-  const scaledMargin = (margin ?? 1000) / scale;
+  /*const scaledMargin = margin / scale;
+  console.log({ scaledMargin });
   useEffect(() => {
     const { current } = canvasRef;
     if (current != null) {
       current.scrollTop = scaledMargin;
       current.scrollLeft = scaledMargin;
+      setTimeout(() => {
+        current.scrollTop = scaledMargin;
+        current.scrollLeft = scaledMargin;
+      }, 0);
     }
-  }, []);
+  }, []);*/
 
   const v: ReactNode[] = [];
-  const transforms = getTransforms(elements, scaledMargin);
+  const transforms = getTransforms(elements, margin);
   for (const element of elements) {
     const { id, style } = element;
     const { x, y } = element;
@@ -75,6 +81,7 @@ export default function Canvas({
     });
   }
 
+  console.log({ transforms });
   return (
     <div
       className={"smc-vfill"}
@@ -84,23 +91,19 @@ export default function Canvas({
     >
       <div
         style={{
+          backgroundSize: "40px 40px",
+          backgroundImage:
+            "linear-gradient(to right, #e0e0e0 1px, transparent 1px),linear-gradient(to bottom, #e0e0e0 1px, transparent 1px)",
           transform: `scale(${scale})`,
           transformOrigin: "top left",
           position: "relative",
+          paddingBottom: `${
+            (1 / scale) * transforms.height
+          }px` /* have to use padding due to position:absolute children */,
+          paddingRight: `${(1 / scale) * transforms.width}px`,
         }}
       >
         {v}
-
-        {/*This hidden div ensures the containing div is big enough to contain everything and a margin*/}
-        <div
-          style={{
-            position: "absolute",
-            left: transforms.width,
-            top: transforms.height,
-          }}
-        >
-          XXX
-        </div>
       </div>
     </div>
   );
@@ -120,10 +123,13 @@ function getTransforms(
   // Returns functions to transform back and forth.
   // Just be really dumb for the first version.
 
-  let xmin = 0,
-    ymin = 0,
-    xmax = 0,
-    ymax = 0;
+  let xmin, ymin, xmax, ymax;
+  if (elements.length == 0) {
+    xmin = ymin = xmax = ymax = 0;
+  } else {
+    xmin = xmax = elements[0].x ?? 0;
+    ymin = ymax = elements[0].y ?? 0;
+  }
   for (let { x, y } of elements) {
     if (x != null) {
       if (x < xmin) {
