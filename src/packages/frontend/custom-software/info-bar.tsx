d82ef9f1653dfa364b4faae382663e1e88789e7c
Harald Schilly <hsy@sagemath.com>
2022-01-28 17:17:01 +0000

    frontend/custom-software: make button bar consistent, more React.FC and fix #5683

--- src/packages/frontend/custom-software/info-bar.tsx
+++ src/packages/frontend/custom-software/info-bar.tsx
@@ -5,7 +5,6 @@
 
 // in "Files", this shows some information and action buttons related to the custom software environment
 
-import { Component } from "../app-framework";
 import { Map as iMap } from "immutable";
 import {
   CUSTOM_SOFTWARE_HELP_URL as help_url,
@@ -17,7 +16,7 @@ import { ComputeImages } from "./init";
 import { path_split, trunc, trunc_middle } from "@cocalc/util/misc";
 import { open_new_tab } from "../misc";
 import { Icon, Tip, HiddenXSSM, VisibleMDLG, VisibleXSSM } from "../components";
-import { ButtonGroup, Button } from "react-bootstrap";
+import { Button } from "antd";
 import { Available as AvailableFeatures } from "../project_configuration";
 import { serverURL } from "../project/named-server-panel";
 import LinkRetry from "../components/link-retry";
@@ -32,18 +31,22 @@ interface Props {
   project_is_running: boolean;
 }
 
-export class CustomSoftwareInfo extends Component<Props, {}> {
-  render_path = (path) => {
-    if (!this.props.project_is_running) return null;
+export const CustomSoftwareInfo: React.FC<Props> = (props: Props) => {
+  const {
+    project_id,
+    actions,
+    available_features,
+    show_custom_software_reset,
+    project_is_running,
+  } = props;
+
+  function render_path(path) {
+    if (!project_is_running) return null;
     if (path.length === 0) return null;
 
-    const onClick = (() => {
-      if (path.endsWith("/")) {
-        return () => this.props.actions.open_directory(path);
-      } else {
-        return () => this.props.actions.open_file({ path: path });
-      }
-    })();
+    const onClick = path.endsWith("/")
+      ? () => actions.open_directory(path)
+      : () => actions.open_file({ path: path });
 
     // boil down what user sees as the launch button description
     const display_path = path.endsWith("/")
@@ -51,29 +54,27 @@ export class CustomSoftwareInfo extends Component<Props, {}> {
       : path_split(path).tail;
 
     return (
-      <Button onClick={onClick}>
+      <Button onClick={onClick} >
         <Tip title={`Open '${path}'`} placement={"bottom"}>
           <Icon name={"rocket"} />{" "}
           <VisibleMDLG>{trunc_middle(display_path, 40)}</VisibleMDLG>
         </Tip>
       </Button>
     );
-  };
+  }
 
-  reset = () =>
-    this.props.actions.toggle_custom_software_reset(
-      !this.props.show_custom_software_reset
-    );
+  function reset() {
+    actions.toggle_custom_software_reset(!show_custom_software_reset);
+  }
 
-  render_jupyter = () => {
-    if (this.props.available_features == null) return null;
+  function render_jupyter(): JSX.Element | null {
+    if (available_features == null) return null;
 
-    const href_jupyterlab = serverURL(this.props.project_id, "jupyterlab");
-    const href_jupyterclassic = serverURL(this.props.project_id, "jupyter");
+    const href_jupyterlab = serverURL(project_id, "jupyterlab");
+    const href_jupyterclassic = serverURL(project_id, "jupyter");
 
-    const have_jupyterlab = this.props.available_features.jupyter_lab || false;
-    const have_jupyterclassic =
-      this.props.available_features.jupyter_notebook || false;
+    const have_jupyterlab = available_features.jupyter_lab || false;
+    const have_jupyterclassic = available_features.jupyter_notebook || false;
 
     return (
       <>
@@ -96,40 +97,38 @@ export class CustomSoftwareInfo extends Component<Props, {}> {
           </LinkRetry>
         ) : undefined}
 
-        <Button onClick={this.reset}>
+        <Button  onClick={reset}>
           <Icon name={RESET_ICON} /> <VisibleMDLG>Reset...</VisibleMDLG>
         </Button>
 
-        <Button onClick={() => open_new_tab(help_url)}>
+        <Button  onClick={() => open_new_tab(help_url)}>
           <Icon name={"question-circle"} />
         </Button>
       </>
     );
-  };
+  }
 
-  img_info = (img) => {
+  function img_info(img) {
     const disp = img.get("display", "");
     const id = img.get("id", "");
     return `${disp} (${id})`;
-  };
+  }
 
-  render = () => {
-    const img = props2img(this.props);
-    if (img == null) return null;
-    const path = img.get("path", "");
+  const img = props2img(props);
+  if (img == null) return null;
+  const path = img.get("path", "");
 
-    return (
-      <>
-        <ButtonGroup bsSize={"small"} style={{ whiteSpace: "nowrap" }}>
-          {this.render_path(path)}
-          {this.render_jupyter()}
-        </ButtonGroup>
-        <div style={title_style}>
-          <Tip title={this.img_info(img)} placement={"bottom"}>
-            {trunc(img.get("display", ""), 100)}
-          </Tip>
-        </div>
-      </>
-    );
-  };
-}
+  return (
+    <>
+      <div style={{ whiteSpace: "nowrap" }}>
+        {render_path(path)}
+        {render_jupyter()}
+      </div>
+      <div style={title_style}>
+        <Tip title={img_info(img)} placement={"bottom"}>
+          {trunc(img.get("display", ""), 100)}
+        </Tip>
+      </div>
+    </>
+  );
+};
