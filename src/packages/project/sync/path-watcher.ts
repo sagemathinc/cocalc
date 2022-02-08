7e110b2cc49631371775342a6b8f8cd2650bbd44
Harald Schilly <harald.schilly@gmail.com>
2022-02-08 19:08:50 +0100

    project/fs watching: support NFS by switching to chokidar

--- src/packages/project/sync/path-watcher.ts
+++ src/packages/project/sync/path-watcher.ts
@@ -18,87 +18,123 @@ The code below deals with two very different cases:
  - when that path doesn't exist: use fs.watch on the parent directory.
         NOTE: this case can't happen when path='', which exists, so we can assume to have read perms on parent.
  - when the path does exist: use fs.watch (hence inotify) on the path itself to report when it changes
+
+NOTE: if you are running on a filesystem like NFS, inotify won't work well or not at all.
+In that case, set the env variable COCALC_FS_WATCHER=poll to use fs.watchFile instead.
 */
 
-import { watch, FSWatcher } from "fs";
+import { watch, WatchOptions } from "chokidar";
+import { FSWatcher } from "fs";
 import { join } from "path";
 import { EventEmitter } from "events";
 import { debounce } from "lodash";
 import { exists } from "../jupyter/async-utils-node";
 import { close, path_split } from "@cocalc/util/misc";
+import { getLogger } from "@cocalc/project/logger";
+const L = getLogger("fs-watcher");
+
+const COCALC_FS_WATCHER = process.env.COCALC_FS_WATCHER ?? "inotify";
+if (!["inotify", "poll"].includes(COCALC_FS_WATCHER)) {
+  throw new Error(
+    `$COCALC_FS_WATCHER=${COCALC_FS_WATCHER} -- must be "inotify" or "poll"`
+  );
+}
+const POLLING = COCALC_FS_WATCHER === "poll";
+
+const DEFAULT_POLL_MS = parseInt(
+  process.env.COCALC_FS_WATCHER_POLL_INTERVAL_MS ?? "2000"
+);
+
+const ChokidarOpts: WatchOptions = {
+  persistent: true, // otherwise won't work
+  followSymlinks: false, // don't wander about
+  disableGlobbing: true, // watch the path as it is, that's it
+  usePolling: POLLING,
+  interval: DEFAULT_POLL_MS,
+  binaryInterval: DEFAULT_POLL_MS,
+  depth: 0, // we only care about the explicitly mentioned path – there could be a lot of files and sub-dirs!
+  // maybe some day we want this:
+  // awaitWriteFinish: {
+  //   stabilityThreshold: 100,
+  //   pollInterval: 50,
+  // },
+  ignorePermissionErrors: true,
+} as const;
 
 export class Watcher extends EventEmitter {
   private path: string;
   private exists: boolean;
-  private watch_contents?: FSWatcher;
-  private watch_existence?: FSWatcher;
+  private watchContents?: FSWatcher;
+  private watchExistence?: FSWatcher;
   private debounce_ms: number;
+  private debouncedChange: any;
   private log: Function;
 
-  constructor(path: string, debounce_ms: number, log: Function) {
+  constructor(path: string, debounce_ms: number) {
     super();
-    this.debounce_ms = debounce_ms;
-    this.log = log;
-    this.log(`Watcher("${path}")`);
+    this.log = L.extend(path).debug;
+    this.log(`initalizing: poll=${POLLING}`);
     if (process.env.HOME == null) throw Error("bug -- HOME must be defined");
     this.path = join(process.env.HOME, path);
+    this.debounce_ms = debounce_ms;
+    this.debouncedChange = debounce(this.change.bind(this), this.debounce_ms, {
+      leading: true,
+      trailing: true,
+    }).bind(this);
     this.init();
   }
 
   private async init(): Promise<void> {
     this.exists = await exists(this.path);
     if (this.path != "") {
-      this.init_watch_existence();
+      this.initWatchExistence();
     }
     if (this.exists) {
-      this.init_watch_contents();
+      this.initWatchContents();
     }
   }
 
-  private init_watch_contents(): void {
-    this.watch_contents = watch(
-      this.path,
-      debounce(this.change.bind(this), this.debounce_ms, {
-        leading: true,
-        trailing: true,
-      })
-    );
+  private initWatchContents(): void {
+    this.watchContents = watch(this.path, ChokidarOpts);
+    this.watchContents.on("all", this.debouncedChange);
   }
 
-  private async init_watch_existence(): Promise<void> {
+  private async initWatchExistence(): Promise<void> {
     const containing_path = path_split(this.path).head;
-    this.watch_existence = watch(containing_path, async (_, filename) => {
-      const path = join(containing_path, filename);
-      if (path != this.path) return;
-      const e = await exists(this.path);
-      if (!this.exists && e) {
-        // it sprung into existence
-        this.exists = e;
-        this.init_watch_contents();
-        this.change();
-      } else if (this.exists && !e) {
-        // it got deleted
-        this.exists = e;
-        if (this.watch_contents != null) {
-          this.watch_contents.close();
-          delete this.watch_contents;
-        }
-        this.change();
-      }
-    });
+    this.watchExistence = watch(containing_path, ChokidarOpts);
+    this.watchExistence.on("all", this.watchExistenceChange(containing_path));
   }
+
+  private watchExistenceChange = (containing_path) => async (_, filename) => {
+    const path = join(containing_path, filename);
+    if (path != this.path) return;
+    const e = await exists(this.path);
+    if (!this.exists && e) {
+      // it sprung into existence
+      this.exists = e;
+      this.initWatchContents();
+      this.change();
+    } else if (this.exists && !e) {
+      // it got deleted
+      this.exists = e;
+      for (const w in ["watchContentsInotify", "watchContentsPoll"]) {
+        if (this[w] != null) {
+          this[w].close();
+          delete this[w];
+        }
+      }
+
+      this.change();
+    }
+  };
 
   private change(): void {
     this.emit("change");
   }
 
   public close(): void {
-    if (this.watch_contents != null) {
-      this.watch_contents.close();
-    }
-    if (this.watch_existence != null) {
-      this.watch_existence.close();
-    }
+    this.watchExistence?.close();
+    this.watchContents?.close();
     close(this);
   }
 }
