3b7f6c617d43584a03257ba0271cbd951d41bd69
Harald Schilly <harald.schilly@gmail.com>
2022-02-24 12:45:24 +0100

    frontend: React.FC-ing account/Sign Out

--- src/packages/frontend/account/sign-out.tsx
+++ src/packages/frontend/account/sign-out.tsx
@@ -3,7 +3,7 @@
  *  License: AGPLv3 s.t. "Commons Clause" – see LICENSE.md for details
  */
 
-import { Component, React, Rendered, redux } from "../app-framework";
+import { React, Rendered, redux } from "../app-framework";
 import { Button, Popconfirm } from "antd";
 import { LogoutOutlined } from "@ant-design/icons";
 
@@ -14,57 +14,57 @@ interface Props {
   style?: React.CSSProperties;
 }
 
-export class SignOut extends Component<Props> {
-  private sign_out(): void {
+export const SignOut: React.FC<Props> = (props: Props) => {
+  const { everywhere, sign_in, highlight, style } = props;
+
+  function sign_out(): void {
     const account = redux.getActions("account");
     if (account != null) {
-      account.sign_out(!!this.props.everywhere, !!this.props.sign_in);
+      account.sign_out(!!everywhere, !!sign_in);
     }
   }
 
-  private render_body(): Rendered {
-    if (this.props.sign_in) {
+  function render_body(): Rendered {
+    if (sign_in) {
       return <span>Sign in to your account...</span>;
     } else {
-      return (
-        <span>Sign out{this.props.everywhere ? " everywhere" : ""}...</span>
-      );
+      return <span>Sign out{everywhere ? " everywhere" : ""}...</span>;
     }
   }
-  public render(): Rendered {
-    // I think not using reduxProps is fine for this, since it's only rendered once
-    // you are signed in, and falling back to "your account" isn't bad.
-    const store = redux.getStore("account");
-    let account: string | undefined = store.get("email_address");
-    if (!account) {
-      account = "your account";
-    }
-    let title: string = `Are you sure you want to sign ${account} out `;
-    if (this.props.everywhere) {
-      title +=
-        "on all web browsers? Every web browser will have to reauthenticate before using this account again.";
-    } else {
-      title += "on this web browser?";
-    }
-    if (store.get("is_anonymous")) {
-      title +=
-        "\n Everything you have done using this TEMPORARY ACCOUNT will be immediately deleted!  If you would like to save your work to a new account, click cancel and sign up below.";
-    }
-    return (
-      <Popconfirm
-        title={<div style={{ maxWidth: "60ex" }}>{title}</div>}
-        onConfirm={this.sign_out.bind(this)}
-        okText={`Yes, sign out${this.props.everywhere ? " everywhere" : ""}`}
-        cancelText={"Cancel"}
+
+  // I think not using reduxProps is fine for this, since it's only rendered once
+  // you are signed in, and falling back to "your account" isn't bad.
+  const store = redux.getStore("account");
+  const account: string = store.get("email_address") ?? "your account";
+
+  let title: string = `Are you sure you want to sign ${account} out `;
+
+  if (everywhere) {
+    title +=
+      "on all web browsers? Every web browser will have to reauthenticate before using this account again.";
+  } else {
+    title += "on this web browser?";
+  }
+
+  if (store.get("is_anonymous")) {
+    title +=
+      "\n Everything you have done using this TEMPORARY ACCOUNT will be immediately deleted!  If you would like to save your work to a new account, click cancel and sign up below.";
+  }
+
+  return (
+    <Popconfirm
+      title={<div style={{ maxWidth: "60ex" }}>{title}</div>}
+      onConfirm={sign_out}
+      okText={`Yes, sign out${everywhere ? " everywhere" : ""}`}
+      cancelText={"Cancel"}
+    >
+      <Button
+        icon={<LogoutOutlined />}
+        type={highlight ? "primary" : undefined}
+        style={style}
       >
-        <Button
-          icon={<LogoutOutlined />}
-          type={this.props.highlight ? "primary" : undefined}
-          style={this.props.style}
-        >
-          {this.render_body()}
-        </Button>
-      </Popconfirm>
-    );
-  }
-}
+        {render_body()}
+      </Button>
+    </Popconfirm>
+  );
+};
