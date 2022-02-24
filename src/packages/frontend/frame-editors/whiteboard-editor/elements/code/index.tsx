2a8b1427fd60a36ce1f796451c4cfea48c28afb5
William Stein <wstein@sagemath.com>
2022-02-24 00:08:59 +0000

    whiteboard: render jupyter output (except png images)

--- src/packages/frontend/frame-editors/whiteboard-editor/elements/code/index.tsx
+++ src/packages/frontend/frame-editors/whiteboard-editor/elements/code/index.tsx
@@ -1,8 +1,8 @@
-import { CSSProperties } from "react";
 import { Element } from "../../types";
 import ControlBar from "./control";
 import Input from "./input";
 import Output from "./output";
+import getStyle from "./style";
 
 interface Props {
   element: Element;
@@ -11,24 +11,10 @@ interface Props {
 }
 
 export default function Code({ element, focused, canvasScale }: Props) {
-  const style = {
-    height: "100%",
-    overflowY: "auto",
-    fontSize: element.data?.fontSize,
-    border: element.data?.radius
-      ? `${2 * (element.data?.radius ?? 1)}px solid ${
-          element.data?.color ?? "#ccc"
-        }`
-      : undefined,
-    borderRadius: "5px",
-    padding: "5px",
-    background: "white",
-  } as CSSProperties;
-
   const { hideInput, hideOutput } = element.data ?? {};
 
   return (
-    <div className={focused ? "nodrag" : undefined} style={style}>
+    <div className={focused ? "nodrag" : undefined} style={getStyle(element)}>
       {!hideInput && (
         <Input element={element} focused={focused} canvasScale={canvasScale} />
       )}
--- src/packages/frontend/frame-editors/whiteboard-editor/elements/code/static.tsx
+++ src/packages/frontend/frame-editors/whiteboard-editor/elements/code/static.tsx
@@ -1,14 +1,26 @@
 import { CodeMirrorStatic } from "@cocalc/frontend/jupyter/codemirror-static";
+import CellOutput from "@cocalc/frontend/jupyter/nbviewer/cell-output";
 import { codemirrorMode } from "@cocalc/frontend/file-extensions";
 import { Element } from "../../types";
+import getStyle from "./style";
 
 export default function Code({ element }: { element: Element }) {
   const mode = codemirrorMode("py"); // TODO!
+
+  const { hideInput, hideOutput } = element.data ?? {};
+
   return (
-    <CodeMirrorStatic
-      value={element.str ??""}
-      font_size={element.data?.fontSize}
-      options={{ lineNumbers: false, mode }}
-    />
+    <div style={getStyle(element)}>
+      {!hideInput && (
+        <CodeMirrorStatic
+          value={element.str ?? ""}
+          font_size={element.data?.fontSize}
+          options={{ lineNumbers: false, mode }}
+        />
+      )}
+      {!hideOutput && element.data?.output && (
+        <CellOutput cell={{ id: element.id, output: element.data?.output }} />
+      )}
+    </div>
   );
 }
