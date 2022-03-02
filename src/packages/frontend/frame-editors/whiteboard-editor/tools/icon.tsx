2875b7c16b957b16f59e786193ef337ac43a8809
William Stein <wstein@sagemath.com>
2022-03-02 07:22:31 -0800

    whiteboard: improve icons tool panel a little.

--- src/packages/frontend/frame-editors/whiteboard-editor/tools/icon.tsx
+++ src/packages/frontend/frame-editors/whiteboard-editor/tools/icon.tsx
@@ -10,31 +10,38 @@ Icon configuration:
 
 import ToolPanel, { getPresetManager, Tool } from "./tool-panel";
 import { Icon, IconName } from "@cocalc/frontend/components/icon";
+import { capitalize } from "@cocalc/util/misc";
 
 const ICONS: IconName[] = [
-  "circle",
+  "thumbs-up",
+  "thumbs-down",
+  "question-circle",
+  "heart",
+  "star",
+  "plus-one",
   "jupyter",
+  "smile",
+  "frown",
+  "fire",
   "sagemath",
   "tex",
   "bolt",
   "graduation-cap",
   "python",
   "r",
-  "bullhorn",
   "calculator",
   "cocalc-ring",
   "hand",
   "exchange",
   "exclamation-triangle",
   "user",
-  "square",
   "cube",
   "dot-circle",
 ];
 
 const DEFAULTS: Params[] = [];
 for (let id = 0; id < ICONS.length; id++) {
-  DEFAULTS.push({ icon: ICONS[id] });
+  DEFAULTS.push({ icon: ICONS[id], fontSize: 24 });
 }
 
 const tool = "icon" as Tool;
@@ -53,7 +60,7 @@ export default function IconToolPanel() {
       Preview={Preview}
       ButtonPreview={ButtonPreview}
       buttonTitle={({ icon, fontSize }: Params) =>
-        `Name: ${icon}` + (fontSize ? `, Size: ${fontSize}px` : "")
+        `${capitalize(icon)}` + (fontSize ? `, Size: ${fontSize}px` : "")
       }
       editableParams={new Set(["icon", "fontSize", "color"])}
       style={{ width: "145px" }}
@@ -78,10 +85,11 @@ function ButtonPreview({ icon, color }: Params) {
       style={{
         color,
         width: "30px",
-        height: "25px",
+        height: "30px",
+        textAlign: "center",
       }}
     >
-      <Icon name={icon} />
+      <Icon name={icon} style={{ fontSize: "24px" }} />
     </div>
   );
 }
