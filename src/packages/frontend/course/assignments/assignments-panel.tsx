e953b43f3650e26d7013af6454d2ee30aa1098c8
Harald Schilly <harald.schilly@gmail.com>
2022-03-04 18:42:57 +0100

    frontend/course: react.fc assignments panel

--- src/packages/frontend/course/assignments/assignments-panel.tsx
+++ src/packages/frontend/course/assignments/assignments-panel.tsx
@@ -3,15 +3,15 @@
  *  License: AGPLv3 s.t. "Commons Clause" – see LICENSE.md for details
  */
 
-// CoCalc libraries
 import { Button } from "@cocalc/frontend/antd-bootstrap";
 import {
   AppRedux,
-  Component,
-  rclass,
-  redux,
+  React,
   Rendered,
-  rtypes,
+  useActions,
+  useMemo,
+  useRedux,
+  useState,
 } from "@cocalc/frontend/app-framework";
 import { Icon, Space, Tip } from "@cocalc/frontend/components";
 import ScrollableList from "@cocalc/frontend/components/scrollable-list";
@@ -31,7 +31,7 @@ import * as styles from "../styles";
 import * as util from "../util";
 import { Assignment } from "./assignment";
 
-interface AssignmentsPanelReactProps {
+interface Props {
   frame_id?: string;
   name: string;
   project_id: string;
@@ -42,86 +42,72 @@ interface AssignmentsPanelReactProps {
   user_map: object;
 }
 
-interface AssignmentsPanelReduxProps {
-  expanded_assignments: Set<string>;
-  active_assignment_sort: SortDescription;
-  active_student_sort: SortDescription;
-  expanded_peer_configs: Set<string>;
-  active_feedback_edits: IsGradingMap;
-  nbgrader_run_info?: NBgraderRunInfo;
-}
+export const AssignmentsPanel: React.FC<Props> = React.memo((props: Props) => {
+  const { frame_id, name, project_id, redux, assignments, students, user_map } =
+    props;
 
-interface AssignmentsPanelState {
-  err?: string;
-  search: string;
-  show_deleted: boolean;
-}
+  const course_actions = useActions<CourseActions>({ name });
 
-export const AssignmentsPanel = rclass<AssignmentsPanelReactProps>(
-  class AssignmentsPanel extends Component<
-    AssignmentsPanelReactProps & AssignmentsPanelReduxProps,
-    AssignmentsPanelState
-  > {
-    constructor(props) {
-      super(props);
-      this.state = {
-        err: undefined, // error message to display at top.
-        search: "", // search query to restrict which assignments are shown.
-        show_deleted: false, // whether or not to show deleted assignments on the bottom
-      };
+  const expanded_assignments: Set<string> = useRedux(
+    name,
+    "expanded_assignments"
+  );
+  const active_assignment_sort: SortDescription = useRedux(
+    name,
+    "active_assignment_sort"
+  );
+  const active_student_sort: SortDescription = useRedux(
+    name,
+    "active_student_sort"
+  );
+  const expanded_peer_configs: Set<string> = useRedux(
+    name,
+    "expanded_peer_configs"
+  );
+  const active_feedback_edits: IsGradingMap = useRedux(
+    name,
+    "active_feedback_edits"
+  );
+  const nbgrader_run_info: NBgraderRunInfo | undefined = useRedux(
+    name,
+    "nbgrader_run_info"
+  );
+
+  // search query to restrict which assignments are shown.
+  const [search, set_search] = useState<string>("");
+  // whether or not to show deleted assignments on the bottom
+  const [show_deleted, set_show_deleted] = useState<boolean>(false);
+
+  function get_assignment(id: string): AssignmentRecord {
+    const assignment = assignments.get(id);
+    if (assignment == undefined) {
+      console.warn(`Tried to access undefined assignment ${id}`);
     }
+    return assignment as any;
+  }
 
-    static reduxProps = ({ name }) => {
-      return {
-        [name]: {
-          expanded_assignments: rtypes.immutable.Set,
-          active_assignment_sort: rtypes.immutable.Map,
-          active_student_sort: rtypes.immutable.Map,
-          expanded_peer_configs: rtypes.immutable.Set,
-          active_feedback_edits: rtypes.immutable.Map,
-          nbgrader_run_info: rtypes.immutable.Map,
-        },
-      };
-    };
-
-    private get_actions(): CourseActions {
-      return redux.getActions(this.props.name);
-    }
-
-    private get_assignment(id: string): AssignmentRecord {
-      const assignment = this.props.assignments.get(id);
-      if (assignment == undefined) {
-        console.warn(`Tried to access undefined assignment ${id}`);
-      }
-      return assignment as any;
-    }
-
-    private compute_assignment_list(): {
+  const { shown_assignments, deleted_assignments, num_omitted, num_deleted } =
+    useMemo((): {
       shown_assignments: any[];
       deleted_assignments: any[];
       num_omitted: number;
       num_deleted: number;
-    } {
+    } => {
       let deleted, f, num_deleted, num_omitted;
-      let list = util.immutable_to_list(
-        this.props.assignments,
-        "assignment_id"
-      );
+      let list = util.immutable_to_list(assignments, "assignment_id");
 
       ({ list, num_omitted } = util.compute_match_list({
         list,
         search_key: "path",
-        search: this.state.search.trim(),
+        search: search.trim(),
       }));
 
-      if (this.props.active_assignment_sort.get("column_name") === "due_date") {
+      if (active_assignment_sort.get("column_name") === "due_date") {
         f = (a) => [
           a.due_date != null ? a.due_date : 0,
           a.path != null ? a.path.toLowerCase() : undefined,
         ];
-      } else if (
-        this.props.active_assignment_sort.get("column_name") === "dir_name"
-      ) {
+      } else if (active_assignment_sort.get("column_name") === "dir_name") {
         f = (a) => [
           a.path != null ? a.path.toLowerCase() : undefined,
           a.due_date != null ? a.due_date : 0,
@@ -131,8 +117,8 @@ export const AssignmentsPanel = rclass<AssignmentsPanelReactProps>(
       ({ list, deleted, num_deleted } = util.order_list({
         list,
         compare_function: (a, b) => cmp_array(f(a), f(b)),
-        reverse: this.props.active_assignment_sort.get("is_descending"),
-        include_deleted: this.state.show_deleted,
+        reverse: active_assignment_sort.get("is_descending"),
+        include_deleted: show_deleted,
       }));
 
       return {
@@ -141,215 +127,202 @@ export const AssignmentsPanel = rclass<AssignmentsPanelReactProps>(
         num_omitted,
         num_deleted,
       };
-    }
+    }, [assignments, active_assignment_sort, show_deleted, search]);
 
-    private render_sort_link(
-      column_name: string,
-      display_name: string
-    ): Rendered {
+  function render_sort_link(
+    column_name: string,
+    display_name: string
+  ): Rendered {
+    return (
+      <a
+        href=""
+        onClick={(e) => {
+          e.preventDefault();
+          return course_actions.assignments.set_active_assignment_sort(
+            column_name
+          );
+        }}
+      >
+        {display_name}
+        <Space />
+        {active_assignment_sort.get("column_name") === column_name ? (
+          <Icon
+            style={{ marginRight: "10px" }}
+            name={
+              active_assignment_sort.get("is_descending")
+                ? "caret-up"
+                : "caret-down"
+            }
+          />
+        ) : undefined}
+      </a>
+    );
+  }
+
+  function render_assignment_table_header(): Rendered {
+    return (
+      <div style={{ borderBottom: "1px solid #e5e5e5" }}>
+        <Row style={{ marginRight: "0px" }}>
+          <Col md={12}>{render_sort_link("dir_name", "Assignment Name")}</Col>
+          <Col md={12}>{render_sort_link("due_date", "Due Date")}</Col>
+        </Row>
+      </div>
+    );
+  }
+
+  function render_assignment(assignment_id: string, index: number): Rendered {
+    return (
+      <Assignment
+        key={assignment_id}
+        project_id={project_id}
+        frame_id={frame_id}
+        name={name}
+        redux={redux}
+        assignment={get_assignment(assignment_id)}
+        background={index % 2 === 0 ? "#eee" : undefined}
+        students={students}
+        user_map={user_map}
+        is_expanded={expanded_assignments.has(assignment_id)}
+        active_student_sort={active_student_sort}
+        expand_peer_config={expanded_peer_configs.has(assignment_id)}
+        active_feedback_edits={active_feedback_edits}
+        nbgrader_run_info={nbgrader_run_info}
+      />
+    );
+  }
+
+  function render_assignments(
+    assignments: { assignment_id: string }[]
+  ): Rendered {
+    if (assignments.length == 0) {
+      return render_no_assignments();
+    }
+    return (
+      <ScrollableList
+        windowing={util.windowing(50)}
+        rowCount={assignments.length}
+        rowRenderer={({ key, index }) => render_assignment(key, index)}
+        rowKey={(index) => assignments[index]?.assignment_id ?? ""}
+        cacheId={`course-assignments-${name}-${frame_id}`}
+      />
+    );
+  }
+
+  function render_no_assignments(): Rendered {
+    const message = (
+      <div>
+        <h3>Add an Assignment to your Course</h3>
+        <p>
+          An assignment is a <i>directory</i> of files somewhere in your CoCalc
+          project. You copy the assignment to your students and they work on it;
+          later, you collect it, grade it, and return the graded version to
+          them.
+        </p>
+
+        <p>
+          Add an assignment to your course by creating a directory using the
+          Files tab, then type the name of the directory in the box in the upper
+          right and click to search.
+        </p>
+      </div>
+    );
+
+    return (
+      <Alert
+        type="info"
+        style={{ margin: "auto", fontSize: "12pt", maxWidth: "800px" }}
+        message={message}
+      />
+    );
+  }
+
+  function render_show_deleted(
+    num_deleted: number,
+    num_shown: number
+  ): Rendered {
+    if (show_deleted) {
       return (
-        <a
-          href=""
-          onClick={(e) => {
-            e.preventDefault();
-            return this.get_actions().assignments.set_active_assignment_sort(
-              column_name
-            );
+        <Button
+          style={styles.show_hide_deleted({ needs_margin: num_shown > 0 })}
+          onClick={() => set_show_deleted(false)}
+        >
+          <Tip
+            placement="left"
+            title="Hide deleted"
+            tip="Assignments are never really deleted.  Click this button so that deleted assignments aren't included at the bottom of the list.  Deleted assignments are always hidden from the list of grades for a student."
+          >
+            Hide {num_deleted} deleted assignments
+          </Tip>
+        </Button>
+      );
+    } else {
+      return (
+        <Button
+          style={styles.show_hide_deleted({ needs_margin: num_shown > 0 })}
+          onClick={() => {
+            set_show_deleted(true);
+            set_search("");
           }}
         >
-          {display_name}
-          <Space />
-          {this.props.active_assignment_sort.get("column_name") ===
-          column_name ? (
-            <Icon
-              style={{ marginRight: "10px" }}
-              name={
-                this.props.active_assignment_sort.get("is_descending")
-                  ? "caret-up"
-                  : "caret-down"
-              }
-            />
-          ) : undefined}
-        </a>
-      );
-    }
-
-    private render_assignment_table_header(): Rendered {
-      return (
-        <div style={{ borderBottom: "1px solid #e5e5e5" }}>
-          <Row style={{ marginRight: "0px" }}>
-            <Col md={12}>
-              {this.render_sort_link("dir_name", "Assignment Name")}
-            </Col>
-            <Col md={12}>{this.render_sort_link("due_date", "Due Date")}</Col>
-          </Row>
-        </div>
-      );
-    }
-
-    private render_assignment(assignment_id: string, index: number): Rendered {
-      return (
-        <Assignment
-          key={assignment_id}
-          project_id={this.props.project_id}
-          frame_id={this.props.frame_id}
-          name={this.props.name}
-          redux={this.props.redux}
-          assignment={this.get_assignment(assignment_id)}
-          background={index % 2 === 0 ? "#eee" : undefined}
-          students={this.props.students}
-          user_map={this.props.user_map}
-          is_expanded={this.props.expanded_assignments.has(assignment_id)}
-          active_student_sort={this.props.active_student_sort}
-          expand_peer_config={this.props.expanded_peer_configs.has(
-            assignment_id
-          )}
-          active_feedback_edits={this.props.active_feedback_edits}
-          nbgrader_run_info={this.props.nbgrader_run_info}
-        />
-      );
-    }
-
-    private render_assignments(
-      assignments: { assignment_id: string }[]
-    ): Rendered {
-      if (assignments.length == 0) {
-        return this.render_no_assignments();
-      }
-      return (
-        <ScrollableList
-          windowing={util.windowing(50)}
-          rowCount={assignments.length}
-          rowRenderer={({ key, index }) => this.render_assignment(key, index)}
-          rowKey={(index) => assignments[index]?.assignment_id ?? ""}
-          cacheId={`course-assignments-${this.props.name}-${this.props.frame_id}`}
-        />
-      );
-    }
-
-    private render_no_assignments(): Rendered {
-      const message = (
-        <div>
-          <h3>Add an Assignment to your Course</h3>
-          <p>
-            An assignment is a <i>directory</i> of files somewhere in your
-            CoCalc project. You copy the assignment to your students and they
-            work on it; later, you collect it, grade it, and return the graded
-            version to them.
-          </p>
-
-          <p>
-            Add an assignment to your course by creating a directory using the
-            Files tab, then type the name of the directory in the box in the
-            upper right and click to search.
-          </p>
-        </div>
-      );
-
-      return (
-        <Alert
-          type="info"
-          style={{ margin: "auto", fontSize: "12pt", maxWidth: "800px" }}
-          message={message}
-        />
-      );
-    }
-
-    private render_show_deleted(
-      num_deleted: number,
-      num_shown: number
-    ): Rendered {
-      if (this.state.show_deleted) {
-        return (
-          <Button
-            style={styles.show_hide_deleted({ needs_margin: num_shown > 0 })}
-            onClick={() => this.setState({ show_deleted: false })}
+          <Tip
+            placement="left"
+            title="Show deleted"
+            tip="Assignments are not deleted forever even after you delete them.  Click this button to show any deleted assignments at the bottom of the list of assignments.  You can then click on the assignment and click undelete to bring the assignment back."
           >
-            <Tip
-              placement="left"
-              title="Hide deleted"
-              tip="Assignments are never really deleted.  Click this button so that deleted assignments aren't included at the bottom of the list.  Deleted assignments are always hidden from the list of grades for a student."
-            >
-              Hide {num_deleted} deleted assignments
-            </Tip>
-          </Button>
-        );
-      } else {
-        return (
-          <Button
-            style={styles.show_hide_deleted({ needs_margin: num_shown > 0 })}
-            onClick={() => this.setState({ show_deleted: true, search: "" })}
-          >
-            <Tip
-              placement="left"
-              title="Show deleted"
-              tip="Assignments are not deleted forever even after you delete them.  Click this button to show any deleted assignments at the bottom of the list of assignments.  You can then click on the assignment and click undelete to bring the assignment back."
-            >
-              Show {num_deleted} deleted assignments
-            </Tip>
-          </Button>
-        );
-      }
-    }
-
-    private yield_adder(deleted_assignments): (string) => void {
-      const deleted_paths = {};
-      deleted_assignments.map((obj) => {
-        if (obj.path) {
-          deleted_paths[obj.path] = obj.assignment_id;
-        }
-      });
-
-      return (path) => {
-        if (deleted_paths[path] != null) {
-          this.get_actions().assignments.undelete_assignment(
-            deleted_paths[path]
-          );
-        } else {
-          this.get_actions().assignments.add_assignment(path);
-        }
-      };
-    }
-
-    public render(): Rendered {
-      const {
-        shown_assignments,
-        deleted_assignments,
-        num_omitted,
-        num_deleted,
-      } = this.compute_assignment_list();
-
-      const add_assignment = this.yield_adder(deleted_assignments);
-
-      const header = (
-        <div style={{ marginBottom: "15px" }}>
-          <FoldersToolbar
-            search={this.state.search}
-            search_change={(value) => this.setState({ search: value })}
-            num_omitted={num_omitted}
-            project_id={this.props.project_id}
-            items={this.props.assignments}
-            add_folders={(paths) => paths.map(add_assignment)}
-            item_name={"assignment"}
-            plural_item_name={"assignments"}
-          />
-        </div>
-      );
-
-      return (
-        <div className={"smc-vfill"} style={{ margin: "0" }}>
-          {header}
-          {shown_assignments.length > 0
-            ? this.render_assignment_table_header()
-            : undefined}
-          <div className="smc-vfill">
-            {this.render_assignments(shown_assignments)}{" "}
-            {num_deleted
-              ? this.render_show_deleted(num_deleted, shown_assignments.length)
-              : undefined}
-          </div>
-        </div>
+            Show {num_deleted} deleted assignments
+          </Tip>
+        </Button>
       );
     }
   }
-);
+
+  function yield_adder(deleted_assignments): (string) => void {
+    const deleted_paths = {};
+    deleted_assignments.map((obj) => {
+      if (obj.path) {
+        deleted_paths[obj.path] = obj.assignment_id;
+      }
+    });
+
+    return (path) => {
+      if (deleted_paths[path] != null) {
+        course_actions.assignments.undelete_assignment(deleted_paths[path]);
+      } else {
+        course_actions.assignments.add_assignment(path);
+      }
+    };
+  }
+
+  function header() {
+    const add_assignment = yield_adder(deleted_assignments);
+    return (
+      <div style={{ marginBottom: "15px" }}>
+        <FoldersToolbar
+          search={search}
+          search_change={(value) => set_search(value)}
+          num_omitted={num_omitted}
+          project_id={project_id}
+          items={assignments}
+          add_folders={(paths) => paths.map(add_assignment)}
+          item_name={"assignment"}
+          plural_item_name={"assignments"}
+        />
+      </div>
+    );
+  }
+
+  return (
+    <div className={"smc-vfill"} style={{ margin: "0" }}>
+      {header()}
+      {shown_assignments.length > 0
+        ? render_assignment_table_header()
+        : undefined}
+      <div className="smc-vfill">
+        {render_assignments(shown_assignments)}{" "}
+        {num_deleted
+          ? render_show_deleted(num_deleted, shown_assignments.length)
+          : undefined}
+      </div>
+    </div>
+  );
+});
