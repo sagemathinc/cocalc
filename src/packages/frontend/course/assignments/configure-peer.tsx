b64aaad290d538f7190fe61682db743eed4195cf
Harald Schilly <harald.schilly@gmail.com>
2022-03-05 12:53:09 +0100

    frontend/course: dusting off the peer grading dialog

--- src/packages/frontend/course/assignments/configure-peer.tsx
+++ src/packages/frontend/course/assignments/configure-peer.tsx
@@ -7,17 +7,15 @@
 Panel for configuring peer grading.
 */
 
-import { Button, Checkbox } from "@cocalc/frontend/antd-bootstrap";
 import { React, Rendered } from "@cocalc/frontend/app-framework";
 import {
   DateTimePicker,
   Icon,
   MarkdownInput,
-  NumberInput,
   Tip,
 } from "@cocalc/frontend/components";
 // import { server_days_ago } from "@cocalc/util/misc";
-import { Card, Col, Row } from "antd";
+import { Button, Card, Col, InputNumber, Row, Switch, Typography } from "antd";
 import { CourseActions } from "../actions";
 import { AssignmentRecord } from "../store";
 
@@ -30,18 +28,25 @@ export const ConfigurePeerGrading: React.FC<Props> = React.memo(
   (props: Props) => {
     const { assignment, actions } = props;
 
+    const peer_info = assignment.get("peer_grade");
+    const config: { enabled: boolean; number: number } = {
+      number: 1,
+      enabled: false,
+      ...peer_info?.toJS(),
+    };
+
     function render_configure_peer_checkbox(config): Rendered {
       return (
-        <Checkbox
-          checked={config.enabled != null ? config.enabled : false}
-          key="peer_grade_checkbox"
-          onChange={(e) =>
-            set_peer_grade({ enabled: (e.target as any).checked })
-          }
-          style={{ display: "inline-block", verticalAlign: "middle" }}
-        >
-          Enable Peer Grading
-        </Checkbox>
+        <Row style={{ marginTop: "10px" }}>
+          <Col span={12}>
+            <Switch
+              checked={config.enabled}
+              onChange={(checked) => set_peer_grade({ enabled: checked })}
+              style={{ display: "inline-block", verticalAlign: "middle" }}
+            />
+          </Col>
+          <Col span={12}>Enable Peer Grading</Col>
+        </Row>
       );
     }
 
@@ -77,10 +82,11 @@ export const ConfigurePeerGrading: React.FC<Props> = React.memo(
         </Tip>
       );
       return (
-        <Row>
+        <Row style={{ marginTop: "10px" }}>
           <Col span={12}>{label}</Col>
           <Col span={12}>
             <DateTimePicker
+              style={{ width: "100%" }}
               placeholder={"Set Peer Grading Due Date"}
               value={peer_due(config.due_date)}
               onChange={peer_due_change}
@@ -94,14 +100,14 @@ export const ConfigurePeerGrading: React.FC<Props> = React.memo(
       const store = actions.get_store();
       const maxVal = (store?.num_students() ?? 2) - 1;
       return (
-        <Row>
+        <Row style={{ marginTop: "10px" }}>
           <Col span={12}>Number of students who will grade each assignment</Col>
           <Col span={12}>
-            <NumberInput
-              on_change={(n) => set_peer_grade({ number: n })}
+            <InputNumber
+              onChange={(n) => set_peer_grade({ number: n })}
               min={1}
               max={maxVal}
-              number={config.number != null ? config.number : 1}
+              value={config.number}
             />
           </Col>
         </Row>
@@ -110,58 +116,51 @@ export const ConfigurePeerGrading: React.FC<Props> = React.memo(
 
     function render_configure_grading_guidelines(config): Rendered {
       return (
-        <div style={{ marginTop: "10px" }}>
-          <Row>
-            <Col span={12}>
-              Grading guidelines, which will be made available to students in
-              their grading folder in a file GRADING_GUIDE.md. Tell your
-              students how to grade each problem. Since this is a markdown file,
-              you might also provide a link to a publicly shared file or
-              directory with guidelines.
-            </Col>
-            <Col span={12}>
-              <div
-                style={{
-                  background: "white",
-                  padding: "10px",
-                  border: "1px solid #ccc",
-                  borderRadius: "3px",
-                }}
-              >
-                <MarkdownInput
-                  persist_id={
-                    assignment.get("path") +
-                    assignment.get("assignment_id") +
-                    "grading-guidelines"
-                  }
-                  attach_to={actions.name}
-                  rows={16}
-                  placeholder="Enter your grading guidelines for this assignment..."
-                  default_value={config.guidelines}
-                  on_save={(x) => set_peer_grade({ guidelines: x })}
-                />
-              </div>
-            </Col>
-          </Row>
-        </div>
+        <Row style={{ marginTop: "10px" }}>
+          <Col span={12}>
+            Grading guidelines, which will be made available to students in
+            their grading folder in a file GRADING_GUIDE.md. Tell your students
+            how to grade each problem. Since this is a markdown file, you might
+            also provide a link to a publicly shared file or directory with
+            guidelines.
+          </Col>
+          <Col span={12}>
+            <div
+              style={{
+                background: "white",
+                padding: "10px",
+                border: "1px solid #ccc",
+                borderRadius: "3px",
+              }}
+            >
+              <MarkdownInput
+                persist_id={
+                  assignment.get("path") +
+                  assignment.get("assignment_id") +
+                  "grading-guidelines"
+                }
+                attach_to={actions.name}
+                rows={16}
+                placeholder="Enter your grading guidelines for this assignment..."
+                default_value={config.guidelines}
+                on_save={(x) => set_peer_grade({ guidelines: x })}
+              />
+            </div>
+          </Col>
+        </Row>
       );
     }
 
     function render_configure_grid(config): Rendered {
       return (
-        <div>
+        <>
           {render_configure_peer_number(config)}
           {render_configure_peer_due(config)}
           {render_configure_grading_guidelines(config)}
-        </div>
+        </>
       );
     }
 
-    const peer_info = assignment.get("peer_grade");
-    let config: { enabled?: boolean } = {};
-    if (peer_info) {
-      config = peer_info.toJS();
-    }
     return (
       <Card
         style={{ background: "#fcf8e3", whiteSpace: "normal" }}
@@ -171,23 +170,25 @@ export const ConfigurePeerGrading: React.FC<Props> = React.memo(
           </h3>
         }
       >
-        <div style={{ color: "#666" }}>
+        <Typography.Text type="secondary">
           Use peer grading to randomly (and anonymously) redistribute collected
           homework to your students, so that they can grade it for you.
-        </div>
+        </Typography.Text>
 
         {render_configure_peer_checkbox(config)}
         {config.enabled ? render_configure_grid(config) : undefined}
-        <Button
-          onClick={() =>
-            actions.toggle_item_expansion(
-              "peer_config",
-              assignment.get("assignment_id")
-            )
-          }
-        >
-          Close
-        </Button>
+        <div style={{ marginTop: "10px" }}>
+          <Button
+            onClick={() =>
+              actions.toggle_item_expansion(
+                "peer_config",
+                assignment.get("assignment_id")
+              )
+            }
+          >
+            Close
+          </Button>
+        </div>
       </Card>
     );
   }
