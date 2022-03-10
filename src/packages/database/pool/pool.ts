dfe98248abcc04413e3d2ab1e2c662f35c6a4bba
William Stein <wstein@sagemath.com>
2022-03-10 14:34:11 +0000

    temporarily disable certain database caching used by next app

--- src/packages/database/pool/pool.ts
+++ src/packages/database/pool/pool.ts
@@ -10,15 +10,17 @@ import {
 } from "@cocalc/backend/data";
 import dbPassword from "./password";
 export * from "./util";
-import getCachedPool, { Length } from "./cached";
+import /*getCachedPool, */{ Length } from "./cached";
 
 import { Pool } from "pg";
 let pool: Pool | undefined = undefined;
 
 export default function getPool(cacheLength?: Length): Pool {
-  if (cacheLength != null) {
-    return getCachedPool(cacheLength);
-  }
+  // temporarily disabling all caching, since there is an issue.
+  //   if (cacheLength != null) {
+  //     return getCachedPool(cacheLength);
+  //   }
+  cacheLength = cacheLength;
   if (pool == null) {
     pool = new Pool({ password: dbPassword(), user, host, database });
   }
