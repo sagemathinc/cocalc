b1c8b0474aa6724bf483c49f1f6c5b3b292e82a3
William Stein <wstein@sagemath.com>
2022-03-19 12:12:39 -0700

    whitebaord: disable SELECTED_PADDING, since it's not quite working

--- src/packages/frontend/frame-editors/whiteboard-editor/elements/style.ts
+++ src/packages/frontend/frame-editors/whiteboard-editor/elements/style.ts
@@ -1,6 +1,10 @@
 export const SELECTED_BORDER_COLOR = "#40a9ff";
-export const SELECTED_BORDER_WIDTH = 2;
+export const SELECTED_BORDER_WIDTH = 2.5;
 export const SELECTED_BORDER_TYPE = "solid";
-export const SELECTED_PADDING = 5;
+
+// This doesn't fully work when bigger than 1, e.g., the drag handles
+// are off. Also it is questionable, since it might be confusing, as
+// the resizing isn't technically at the actual boundary anymore.
+export const SELECTED_PADDING = 0;
 
 export const EDIT_BORDER_COLOR = "#66bb6a";
