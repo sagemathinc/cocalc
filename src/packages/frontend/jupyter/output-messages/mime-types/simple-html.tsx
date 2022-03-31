66d9031614c42ea70dc8f2360cdbff19a406a49c
William Stein <wstein@sagemath.com>
2022-03-31 14:59:45 +0000

    prefer latex to html mime types for rendering jupyter notebooks on share server -- otherwise %display latex in a sage notebook is very broken due to how our XSS sanitization works

--- src/packages/frontend/jupyter/output-messages/mime-types/simple-html.tsx
+++ src/packages/frontend/jupyter/output-messages/mime-types/simple-html.tsx
@@ -9,12 +9,12 @@ const Html = ({ value }) => {
   );
 };
 
+register("text/html", 3, Html);
+
+// put latex as HTML, since jupyter requires $'s anyways:
 const Math = ({ value }) => {
   value = value.replace("\\newcommand{\\Bold}[1]{\\mathbf{#1}}", ""); // hack for sage kernel for now.
   return <HTML value={value} />;
 };
 
-register("text/html", 3, Html);
-
-// put latex as HTML, since jupyter requires $'s anyways:
-register("text/latex", 2.1, Math);
+register("text/latex", 3.5, Math);
