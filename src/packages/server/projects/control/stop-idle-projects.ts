c7b916f10f31d52617f8ac24265b8813cfede753
Harald Schilly <harald.schilly@gmail.com>
2022-04-01 19:32:56 +0200

    postgres 14: EXTRACT returns a numeric, which is converted to a string. We force this to stay a double precision number (postgres 13) -- issue #5824

--- src/packages/server/projects/control/stop-idle-projects.ts
+++ src/packages/server/projects/control/stop-idle-projects.ts
@@ -20,9 +20,10 @@ async function stopIdleProjects(stopProject: (string) => Promise<void>) {
 
   logger.debug("query database for all running projects");
   const runningProjects = (
+    // ::float necessary for Postgres 14, see @cocalc/database/pool/util.ts timeInSeconds for more info
     await callback2(db()._query, {
       query:
-        "SELECT project_id, EXTRACT(EPOCH FROM NOW() - last_edited) as idle_time, settings, run_quota FROM projects WHERE state ->> 'state' = 'running'",
+        "SELECT project_id, (EXTRACT(EPOCH FROM NOW() - last_edited))::FLOAT as idle_time, settings, run_quota FROM projects WHERE state ->> 'state' = 'running'",
     })
   ).rows;
   logger.debug("got ", runningProjects);
