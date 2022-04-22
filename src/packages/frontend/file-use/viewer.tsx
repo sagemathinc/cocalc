80cc852768915420e9374a66ecb9b38c6456c9c8
William A. Stein <wstein@sagemath.com>
2022-04-22 04:28:18 +0000

    rewrite file use viewer (the notification thing in upper right you get when clicking the bell) to (1) be a modern functional component, and (2) use Virtuoso

--- src/packages/frontend/file-use/viewer.tsx
+++ src/packages/frontend/file-use/viewer.tsx
@@ -3,17 +3,18 @@
  *  License: AGPLv3 s.t. "Commons Clause" – see LICENSE.md for details
  */
 
+import { useEffect, useRef, useState } from "react";
 import { Map as iMap, List as iList } from "immutable";
 import { FileUseInfo } from "./info";
-import { Alert, Button, Col, Row } from "react-bootstrap";
-import { Component, React, Rendered } from "../app-framework";
-import { SearchInput, WindowedList, Icon } from "../components";
+import { Alert, Button, Col, Row } from "@cocalc/frontend/antd-bootstrap";
+import { redux } from "@cocalc/frontend/app-framework";
+import { SearchInput, Icon } from "@cocalc/frontend/components";
 import { FileUseActions } from "./actions";
 import { open_file_use_entry } from "./util";
 import { search_match, search_split } from "@cocalc/util/misc";
+import { Virtuoso, VirtuosoHandle } from "react-virtuoso";
 
 interface Props {
-  redux: any;
   file_use_list: iList<FileUseInfoMap>;
   user_map: iMap<string, any>;
   project_map: iMap<string, any>;
@@ -21,191 +22,162 @@ interface Props {
   unseen_mentions_size: number;
 }
 
-interface State {
-  search: string;
-  cursor: number; // cursor position
-}
-
 type FileUseInfoMap = iMap<string, any>;
 
-export class FileUseViewer extends Component<Props, State> {
-  private num_missing: number = 0;
-  private windowed_list_ref = React.createRef<WindowedList>();
-  private visible_list: iList<FileUseInfoMap> | undefined = undefined;
+export default function FileUseViewer({
+  file_use_list,
+  user_map,
+  project_map,
+  account_id,
+  unseen_mentions_size,
+}: Props) {
+  const [search, setSearch] = useState<string>("");
+  const [cursor, setCursor] = useState<number>(0); // cursor position
+  const numMissingRef = useRef<number>(0);
+  const virtuosoRef = useRef<VirtuosoHandle>(null);
+  const visibleListRef = useRef<iList<FileUseInfoMap> | null>(null);
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      search: "",
-      cursor: 0,
-    };
-  }
+  useEffect(() => {
+    visibleListRef.current = null;
+  }, [file_use_list, search]);
 
-  public shouldComponentUpdate(nextProps: Props, nextState: State): boolean {
-    if (
-      this.props.file_use_list != nextProps.file_use_list ||
-      this.state.search != nextState.search
-    ) {
-      delete this.visible_list;
-      return true;
-    }
+  function render_how_many_hidden_by_search() {
+    get_visible_list(); // make sure num_missing is updated.
+    if (numMissingRef.current == 0) return;
     return (
-      this.props.unseen_mentions_size != nextProps.unseen_mentions_size ||
-      this.state.cursor != nextState.cursor ||
-      this.props.user_map != nextProps.user_map ||
-      this.props.project_map != nextProps.project_map
-    );
-  }
-
-  render_how_many_hidden_by_search(): Rendered {
-    this.get_visible_list(); // make sure num_missing is updated.
-    if (this.num_missing == 0) return;
-    return (
-      <Alert bsStyle="warning" key="not_showing">
-        Hiding {this.num_missing} file use notifications that do not match
-        search for '{this.state.search}'.
+      <Alert
+        bsStyle="warning"
+        key="not_showing"
+        style={{ marginBottom: "5px" }}
+      >
+        Hiding {numMissingRef.current} file use notifications that do not match
+        search for '{search}'.
       </Alert>
     );
   }
 
-  private set_cursor(cursor: number): void {
-    if (cursor >= this.get_visible_list().size) {
-      cursor = this.get_visible_list().size - 1;
+  function set_cursor(cursor: number): void {
+    if (cursor >= get_visible_list().size) {
+      cursor = get_visible_list().size - 1;
     }
     if (cursor < 0) {
       cursor = 0;
     }
-    this.setState({ cursor });
-    if (this.windowed_list_ref.current != null) {
-      this.windowed_list_ref.current.scrollToRow(cursor);
-    }
+    setCursor(cursor);
+    virtuosoRef.current?.scrollIntoView({ index: cursor });
   }
 
-  render_search_box(): Rendered {
+  function render_search_box() {
     return (
       <span key="search_box" className="smc-file-use-notifications-search">
         <SearchInput
           autoFocus={true}
           placeholder="Search..."
-          default_value={this.state.search}
-          on_change={(value) => this.setState({ search: value, cursor: 0 })}
+          default_value={search}
+          on_change={(value) => {
+            setSearch(value);
+            setCursor(0);
+          }}
           on_submit={() => {
-            this.open_selected();
+            open_selected();
           }}
           on_escape={(before) => {
             if (!before) {
-              const a = this.props.redux.getActions("page");
+              const a = redux.getActions("page");
               if (a != null) {
                 (a as any).toggle_show_file_use();
               }
-              this.setState({ cursor: 0 });
+              setCursor(0);
             }
           }}
-          on_up={() => this.set_cursor(this.state.cursor - 1)}
-          on_down={() => this.set_cursor(this.state.cursor + 1)}
+          on_up={() => set_cursor(cursor - 1)}
+          on_down={() => set_cursor(cursor + 1)}
         />
       </span>
     );
   }
 
-  click_mark_all_read(): void {
-    const a: FileUseActions = this.props.redux.getActions("file_use");
+  function click_mark_all_read(): void {
+    const a: FileUseActions = redux.getActions("file_use");
     if (a != null) {
       a.mark_all("read");
     }
-    const p = this.props.redux.getActions("page");
+    const p = redux.getActions("page");
     if (p != null) {
       (p as any).toggle_show_file_use();
     }
   }
 
-  render_mark_all_read_button(): Rendered {
+  function render_mark_all_read_button() {
     return (
-      <Button
-        key="mark_all_read_button"
-        onClick={() => this.click_mark_all_read()}
-      >
+      <Button key="mark_all_read_button" onClick={() => click_mark_all_read()}>
         <Icon name="check-square" /> Mark All Read
       </Button>
     );
   }
 
-  open_selected(): void {
-    if (this.visible_list == null) return;
-    const x = this.visible_list.get(this.state.cursor);
+  function open_selected(): void {
+    if (visibleListRef.current == null) return;
+    const x = visibleListRef.current.get(cursor);
     if (x == null) return;
     open_file_use_entry(
       x.get("project_id"),
       x.get("path"),
       x.get("show_chat", false),
-      this.props.redux
+      redux
     );
   }
 
-  private get_visible_list(): iList<FileUseInfoMap> {
-    if (this.visible_list == null) {
-      this.visible_list = this.props.file_use_list;
-      if (this.state.search) {
-        const s = search_split(this.state.search.toLowerCase());
-        this.visible_list = this.visible_list.filter((info) =>
+  function get_visible_list(): iList<FileUseInfoMap> {
+    if (visibleListRef.current == null) {
+      visibleListRef.current = file_use_list;
+      if (search) {
+        const s = search_split(search.toLowerCase());
+        visibleListRef.current = visibleListRef.current.filter((info) =>
           search_match(info.get("search"), s)
         );
-        this.num_missing =
-          this.props.file_use_list.size - this.visible_list.size;
+        numMissingRef.current =
+          file_use_list.size - visibleListRef.current.size;
       } else {
-        this.num_missing = 0;
+        numMissingRef.current = 0;
       }
-      if (this.visible_list == null) throw new Error("bug");
+      if (visibleListRef.current == null) throw new Error("bug");
     }
-    return this.visible_list;
+    return visibleListRef.current;
   }
 
-  private row_key(index: number): string {
-    return `${index}`;
-  }
-
-  private row_renderer({ index }): Rendered {
-    const info = this.get_visible_list().get(index);
-    if (info == null) return;
+  function row_renderer(index) {
+    const info = get_visible_list().get(index);
+    if (info == null) {
+      // shouldn't happen
+      return <div style={{ height: "1px" }}></div>;
+    }
     return (
       <FileUseInfo
-        cursor={index === this.state.cursor}
-        redux={this.props.redux}
+        key={`${index}`}
+        cursor={index === cursor}
+        redux={redux}
         info={info}
-        account_id={this.props.account_id}
-        user_map={this.props.user_map}
-        project_map={this.props.project_map}
+        account_id={account_id}
+        user_map={user_map}
+        project_map={project_map}
       />
     );
   }
 
-  private render_list(): Rendered {
-    return (
-      <WindowedList
-        ref={this.windowed_list_ref}
-        overscan_row_count={20}
-        estimated_row_size={56}
-        row_count={this.get_visible_list().size}
-        row_renderer={this.row_renderer.bind(this)}
-        row_key={this.row_key.bind(this)}
-        cache_id={"file-use"}
-      />
-    );
-  }
-
-  render_see_mentions_link(): Rendered {
+  function render_see_mentions_link() {
     let notifications_page_text:
       | string
-      | JSX.Element = `Mentions (${this.props.unseen_mentions_size})`;
-    if (this.props.unseen_mentions_size > 0) {
+      | JSX.Element = `Mentions (${unseen_mentions_size})`;
+    if (unseen_mentions_size > 0) {
       notifications_page_text = <b>{notifications_page_text}</b>;
     }
     return (
       <Link
         style={{ fontSize: "16px", whiteSpace: "nowrap" }}
         on_click={() => {
-          this.props.redux.getActions("page").set_active_tab("notifications");
-          this.props.redux.getActions("page").toggle_show_file_use();
+          redux.getActions("page").set_active_tab("notifications");
+          redux.getActions("page").toggle_show_file_use();
         }}
       >
         {notifications_page_text}
@@ -213,26 +185,26 @@ export class FileUseViewer extends Component<Props, State> {
     );
   }
 
-  render(): Rendered {
-    const link = this.render_see_mentions_link();
-    return (
-      <div className={"smc-vfill smc-file-use-viewer"}>
-        <Row key="top">
-          <Col sm={7}>{this.render_search_box()}</Col>
-          <Col sm={2} style={{ paddingTop: "5px" }}>
-            {link}
-          </Col>
-          <Col sm={3}>
-            <div style={{ float: "right" }}>
-              {this.render_mark_all_read_button()}
-            </div>
-          </Col>
-        </Row>
-        {this.render_how_many_hidden_by_search()}
-        {this.render_list()}
-      </div>
-    );
-  }
+  const link = render_see_mentions_link();
+  return (
+    <div className={"smc-vfill smc-file-use-viewer"}>
+      <Row key="top" style={{ marginBottom: "5px" }}>
+        <Col sm={7}>{render_search_box()}</Col>
+        <Col sm={2} style={{ paddingTop: "5px" }}>
+          {link}
+        </Col>
+        <Col sm={3}>
+          <div style={{ float: "right" }}>{render_mark_all_read_button()}</div>
+        </Col>
+      </Row>
+      {render_how_many_hidden_by_search()}
+      <Virtuoso
+        ref={virtuosoRef}
+        totalCount={get_visible_list().size}
+        itemContent={row_renderer}
+      />
+    </div>
+  );
 }
 
 function Link({ on_click, children, style }) {
