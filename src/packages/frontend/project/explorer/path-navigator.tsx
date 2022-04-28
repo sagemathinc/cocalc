9acb0cbded85b2280c4008fb715da3b0edd433e3
Harald Schilly <harald.schilly@gmail.com>
2022-04-28 11:29:08 +0200

    frontend: fix slightly changed structure of the nav Breadcrumb, make the segments a component, etc.

--- src/packages/frontend/project/explorer/path-navigator.tsx
+++ src/packages/frontend/project/explorer/path-navigator.tsx
@@ -23,44 +23,46 @@ export const PathNavigator: React.FC<Props> = React.memo((props: Props) => {
 
   function make_path(): JSX.Element[] {
     const v: JSX.Element[] = [];
-    v.push(
-      // yes, must be called as a normal function
-      PathSegmentLink({
-        path: "",
-        display: <HomeOutlined />,
-        full_name: "",
-        key: 0,
-        on_click: () => actions?.open_directory("", true, false),
-      })
-    );
-
-    const is_root = current_path[0] === "/";
 
     const current_path_depth =
       (current_path == "" ? 0 : current_path.split("/").length) - 1;
     const history_segments = history_path.split("/");
+    const is_root = current_path[0] === "/";
+
+    v.push(
+      // yes, must be called as a normal function
+      <PathSegmentLink
+        path={""}
+        display={<HomeOutlined />}
+        full_name={""}
+        key={0}
+        on_click={() => actions?.open_directory("", true, false)}
+        active={current_path_depth === -1}
+      />
+    );
+
     history_segments.forEach((segment, i) => {
       if (is_root && i === 0) return;
       const is_current = i === current_path_depth;
       const is_history = i > current_path_depth;
       v.push(
         // yes, must be called as a normal function
-        PathSegmentLink({
-          path: history_segments.slice(0, i + 1 || undefined).join("/"),
-          display: trunc_middle(segment, 15),
-          full_name: segment,
-          key: i + 1,
-          on_click: (path) => actions?.open_directory(path, true, false),
-          active: is_current,
-          history: is_history,
-        })
+        <PathSegmentLink
+          path={history_segments.slice(0, i + 1 || undefined).join("/")}
+          display={trunc_middle(segment, 15)}
+          full_name={segment}
+          key={i + 1}
+          on_click={(path) => actions?.open_directory(path, true, false)}
+          active={is_current}
+          history={is_history}
+        />
       );
     });
     return v;
   }
 
-  // Background color below is so that things look good even for
-  // multiline long paths.
+  // Background color is set via .cc-project-files-path-nav > nav
+  // so that things look good even for multiline long paths.
   return (
     <Breadcrumb style={style} className="cc-path-navigator">
       {make_path()}
