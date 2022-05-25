886513241125fbdb8bb6b83d918820189c344273
Harald Schilly <harald.schilly@gmail.com>
2022-05-25 15:02:56 +0200

    next/server: fix email setup to be before that rewrite – #5949

--- src/packages/server/email/smtp.ts
+++ src/packages/server/email/smtp.ts
@@ -3,25 +3,28 @@
  *  License: AGPLv3 s.t. "Commons Clause" – see LICENSE.md for details
  */
 
-import getPool from "@cocalc/database/pool";
 import { createTransport } from "nodemailer";
 import type { Transporter } from "nodemailer";
 import type { Message } from "./message";
 import getHelpEmail from "./help";
 import appendFooter from "./footer";
+import { getServerSettings } from "../settings/server-settings";
+
+type BackendType = "email" | "password_reset";
 
 export default async function sendEmail(
   message: Message,
-  type: "email" | "password_reset" = "email"
+  type: BackendType = "email"
 ): Promise<void> {
-  let settings: Settings;
+  let settings: SMTPSettings;
   try {
-    settings = await getSettings(type);
+    settings = await getSMTPSettings(type);
   } catch (err) {
     throw Error(
       `SMTP ${type} is not properly configured for this server. Contact the site administrator. -- ${err}`
     );
   }
+
   if (!message.from) {
     if (settings.from) {
       message.from = settings.from;
@@ -52,7 +55,7 @@ async function getServer(settings): Promise<Transporter> {
   return server;
 }
 
-interface Settings {
+interface SMTPSettings {
   server: string;
   login: string;
   password: string;
@@ -60,21 +63,54 @@ interface Settings {
   port?: string;
 }
 
-async function getSettings(
-  type: "email" | "password_reset"
-): Promise<Settings> {
+/**
+ * Depending on if this is for regular emails or password resets,
+ * we return the SMTP settings depending on the server settings.
+ * Usually, password reset and regular email settings are the same for SMTP.
+ * Only exception is when that override entry is set to smtp, then use the
+ * secondary SMTP configuartion.
+ */
+async function getEmailServerSettings(
+  type: BackendType
+): Promise<SMTPSettings> {
+  const settings = await getServerSettings();
+
+  const defaultSMTP = {
+    server: settings.email_smtp_server,
+    login: settings.email_smtp_login,
+    password: settings.email_smtp_password,
+    from: settings.email_smtp_from,
+    port: settings.email_smtp_port,
+  };
+
+  if (type == "email") {
+    return defaultSMTP;
+  }
+
+  switch (settings.password_reset_override) {
+    case "default":
+      return defaultSMTP;
+
+    case "smtp":
+      return {
+        server: settings.password_reset_smtp_server,
+        login: settings.password_reset_smtp_login,
+        password: settings.password_reset_smtp_password,
+        from: settings.password_reset_smtp_from,
+        port: settings.password_reset_smtp_port,
+      };
+  }
+  throw new Error(
+    `unexpected password_reset_override: ${settings.password_reset_override}`
+  );
+}
+
+async function getSMTPSettings(type: BackendType): Promise<SMTPSettings> {
   if (type != "email" && type != "password_reset") {
     throw Error("type must be 'email' or 'password_reset'");
   }
-  const pool = getPool("long"); // rarely changes
-  const { rows } = await pool.query(
-    `SELECT name, value FROM server_settings WHERE name LIKE '${type}_smtp_%'`
-  );
-  const settings: Partial<Settings> = {};
-  const n = `${type}_smtp_`.length;
-  for (const row of rows) {
-    settings[row.name.slice(n)] = row.value;
-  }
+  const settings: SMTPSettings = await getEmailServerSettings(type);
+
   if (!settings.server) {
     throw Error(`SMTP ${type} server must be configured`);
   }
@@ -85,5 +121,5 @@ async function getSettings(
     throw Error(`SMTP ${type} password must be configured`);
   }
 
-  return settings as Settings;
+  return settings;
 }
