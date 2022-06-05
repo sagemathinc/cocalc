e2c8408fae63fe368050e7f6e7eba8ffd56cc3d9
William A. Stein <wstein@sagemath.com>
2022-06-05 02:41:09 +0000

    latex: start experimenting with transforming the output of unified-latex for our purposes

--- src/packages/frontend/components/latex/latex.tsx
+++ src/packages/frontend/components/latex/latex.tsx
@@ -1,5 +1,10 @@
-import latexToSlate from "./to-slate";
-import RenderStatic from "@cocalc/frontend/editors/slate/render-static";
+import { CSSProperties } from "react";
+import { convertToHtml } from "@unified-latex/unified-latex-to-hast";
+import { parse } from "@unified-latex/unified-latex-util-parse";
+import htmlReactParser, { domToReact } from "html-react-parser";
+import { Element, Text } from "domhandler";
+import { useFileContext } from "@cocalc/frontend/lib/file-context";
+import DefaultMath from "@cocalc/frontend/components/math/ssr";
 
 type State = any;
 
@@ -7,20 +12,87 @@ interface Props {
   value: string;
   state?: State;
   isInline?: boolean;
+  style?: CSSProperties;
 }
 
-export function LaTeX({ value }: Props) {
-  const slate = latexToSlate(value);
-  return <RenderStatic slate={slate} />;
-}
+export default function LaTeX({ value, isInline, style, state = {} }: Props) {
+  const { MathComponent } = useFileContext();
 
-import { convertToHtml } from "@unified-latex/unified-latex-to-hast";
-import { parse } from "@unified-latex/unified-latex-util-parse";
-import HTML from "@cocalc/frontend/components/html-ssr";
-
-export default function LaTeXViaHTML({ value, isInline }: Props) {
   // console.log("LaTeX: ", { isInline, value });
-  const latexAst = parse(value);
-  const htmlString = convertToHtml(latexAst);
-  return <HTML value={htmlString} isInline={isInline} />;
+  const ast = parse(value);
+  const html = convertToHtml(ast);
+
+  const options = {
+    replace: (node) => {
+      // console.log("node = ", node);
+      if (!(node instanceof Element)) return;
+      const { attribs, children, name, type } = node;
+      if (type != "tag") return;
+      if (
+        (attribs["class"] == "display-math" ||
+          attribs["class"] == "inline-math") &&
+        children[0] instanceof Text &&
+        children[0].data
+      ) {
+        let data = "$" + children[0].data + "$";
+        if (attribs["class"] == "display-math") {
+          data = "$" + data + "$";
+        }
+        if (MathComponent != null) {
+          return <MathComponent data={data} />;
+        }
+        return <DefaultMath data={data} />;
+      }
+      if (
+        attribs["class"] != "starred" &&
+        (name == "h3" || name == "h4" || name == "h5")
+      ) {
+        const env =
+          name == "h3"
+            ? "section"
+            : name == "h4"
+            ? "subsection"
+            : name == "h5"
+            ? "subsubsection"
+            : "";
+        // these come from chapter/section/subsection/subsection in unified-latex
+        if (state.section == null) {
+          state.section = 0;
+          state.subsection = state.subsubsection = 1;
+        }
+        if (env == "section") {
+          state.section += 1;
+          state.subsection = 0;
+          state.subsubsection = 0;
+          return (
+            <h3>
+              {state.section} {domToReact(children, options)}
+            </h3>
+          );
+        } else if (env == "subsection") {
+          state.subsection += 1;
+          state.subsubsection = 0;
+          return (
+            <h4>
+              {state.section}.{state.subsection} {domToReact(children, options)}
+            </h4>
+          );
+        } else if (env == "subsubsection") {
+          state.subsubsection += 1;
+          return (
+            <h4>
+              {state.section}.{state.subsection}.{state.subsubsection}{" "}
+              {domToReact(children, options)}
+            </h4>
+          );
+        }
+      }
+    },
+  };
+
+  if (isInline) {
+    return <span style={style}>{htmlReactParser(html, options)}</span>;
+  } else {
+    return <div style={style}>{htmlReactParser(html, options)}</div>;
+  }
 }
