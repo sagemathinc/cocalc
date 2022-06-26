af2067d82531c75fdb151bfdcdcc1243fb2e83c9
William Stein <wstein@sagemath.com>
2022-06-26 00:43:32 +0000

    cache the metrics by name, so if module that defines one is loaded twice for some reason, that's OK.

--- src/packages/database/metrics.ts
+++ src/packages/database/metrics.ts
@@ -2,7 +2,13 @@ import { Counter, Gauge, Histogram } from "prom-client";
 
 const PREFIX = "cocalc_database_";
 
+const cache: any = {};
+
 export function newCounter(name: string, help, labelNames: string[] = []) {
+  const key = `counter-${name}`;
+  if (cache[key] != null) {
+    return cache[key];
+  }
   // a prometheus counter -- https://github.com/siimon/prom-client#counter
   // use it like counter.labels(labelA, labelB).inc([positive number or default is 1])
   if (!name.endsWith("_total")) {
@@ -10,19 +16,27 @@ export function newCounter(name: string, help, labelNames: string[] = []) {
       `Counter metric names have to end in [_unit]_total but got '${name}' -- https://prometheus.io/docs/practices/naming/`
     );
   }
-  return new Counter({
+  const C = new Counter({
     name: PREFIX + name,
     help,
     labelNames,
   });
+  cache[key] = C;
+  return C;
 }
 
 export function newGauge(name: string, help, labelNames: string[] = []) {
-  return new Gauge({
+  const key = `gauge-${name}`;
+  if (cache[key] != null) {
+    return cache[key];
+  }
+  const G = new Gauge({
     name: PREFIX + name,
     help,
     labelNames,
   });
+  cache[key] = G;
+  return G;
 }
 
 export function newHistogram(
@@ -30,6 +44,10 @@ export function newHistogram(
   help,
   config: { buckets?: number[]; labels?: string[] } = {}
 ) {
+  const key = `hist-${name}`;
+  if (cache[key] != null) {
+    return cache[key];
+  }
   // invoked as histogram.observe(value)
   if (!config.buckets) {
     config.buckets = [0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1, 2.5, 5, 10];
@@ -37,10 +55,12 @@ export function newHistogram(
   if (!config.labels) {
     config.labels = [];
   }
-  return new Histogram({
+  const H = new Histogram({
     name: PREFIX + name,
     help,
     labelNames: config.labels,
     buckets: config.buckets,
   });
+  cache[key] = H;
+  return H;
 }
