4e0f09ae5ae5ae93bda0bd4e4cd2516ca31d7531
William Stein <wstein@sagemath.com>
2022-08-10 18:02:31 +0000

    fix issue with importing art and typescript

--- src/packages/frontend/art.ts
+++ src/packages/frontend/art.ts
@@ -3,12 +3,27 @@
  *  License: AGPLv3 s.t. "Commons Clause" – see LICENSE.md for details
  */
 
+// @ts-ignore
 import APP_ICON from "@cocalc/assets/cocalc-icon.svg";
+// @ts-ignore
 import APP_ICON_WHITE from "@cocalc/assets/cocalc-icon-white.svg";
+// @ts-ignore
 import APP_LOGO from "@cocalc/assets/cocalc-logo.svg";
+// @ts-ignore
 import APP_LOGO_WHITE from "@cocalc/assets/cocalc-icon-white-transparent.svg";
+// @ts-ignore
 import APP_LOGO_NAME from "@cocalc/assets/cocalc-font-black.svg";
+// @ts-ignore
 import APP_LOGO_NAME_WHITE from "@cocalc/assets/cocalc-font-white.svg";
+
+console.log({
+  APP_ICON,
+  APP_ICON_WHITE,
+  APP_LOGO,
+  APP_LOGO_WHITE,
+  APP_LOGO_NAME,
+  APP_LOGO_NAME_WHITE,
+});
 
 export {
   APP_ICON,
