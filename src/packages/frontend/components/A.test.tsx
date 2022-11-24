4fb043e07d96ed616930644e2fc4b9d9ed676d4b
William Stein <wstein@sagemath.com>
2022-11-23 20:25:35 -0800

    add an actual frickin test of a react component.
    - there are hundreds more unit tests like this that would be
      relatively easy to write using the same pattern...!
    
    - added testing to pre-publish

---
+++ src/packages/frontend/components/A.test.tsx
@@ -0,0 +1,11 @@
+import { A } from "./A";
+import renderer from "react-test-renderer";
+
+test("some basic properites of A are correct so will open a new tab at right url", () => {
+  const component = renderer.create(<A href="https://cocalc.com">CoCalc</A>);
+  let tree = component.toJSON();
+  expect(tree.type).toBe("a");
+  expect(tree.props.target).toBe("_blank");
+  expect(tree.props.href).toBe("https://cocalc.com");
+  expect(tree.props.rel).toBe("noopener");
+});
