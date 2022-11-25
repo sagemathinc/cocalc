0f3ca289b07cc4f32572ee8d17fa7f3edaaf303d
William Stein <wstein@sagemath.com>
2022-11-24 20:52:15 -0800

    add a test to cdn package

---
+++ src/packages/cdn/test.js
@@ -0,0 +1,19 @@
+describe("test that the built module description object exists and has some properties", () => {
+  const obj = require(".");
+
+  it("has a path that contains cdn", () => {
+    expect(obj.path).toContain("/cdn/");
+  });
+
+  it("has versions for the five modules it should have", () => {
+    for (const name of [
+      "bootstrap",
+      "codemirror",
+      "katex",
+      "mathjax",
+      "pdfjs-dist",
+    ]) {
+      expect(obj.versions).toHaveProperty(name);
+    }
+  });
+});
