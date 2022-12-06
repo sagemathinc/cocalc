41a88ebbd1758df22065d6a527e18426c28a62ba
William Stein <wstein@sagemath.com>
2022-12-06 13:03:32 -0800

    nocodb: redirect top-level /nocodb requests to dashboard
    - this is just because nocodb itself messes this up when
    there is a base url.  Maybe this could instead be fixed upstream.

--- src/packages/hub/servers/app/nocodb.ts
+++ src/packages/hub/servers/app/nocodb.ts
@@ -29,22 +29,38 @@ export default async function initNocoDB(app: Application, httpServer: Server) {
   winston.info(`Initializing the NocoDB server at ${base}`);
   const nocoHandler = await Noco.init({}, httpServer, app);
   const handler = async (req, res) => {
-    if (!(await isAdmin(req))) {
-      // only admins can connected to nocodb
-      res.send(
-        `You must be <a href="${join(
-          basePath,
-          "auth/sign-in"
-        )}">signed in</a> as an Administrator of this site to access ${join(
-          base,
-          req.url
-        )}`
-      );
-    } else {
-      nocoHandler(req, res);
+    // We block all requests that aren't from a signed in admin.
+    if (!(await ensureAdmin(req, res, base))) {
+      // TODO: this should really be a middleware right?  I'm basically doing middleware by hand.
+      return;
     }
+    if (req.url == "" || req.url == "/") {
+      // nocodb tries to redirect to the dashboard in a way that is not compatible with our baseUrl, so we just do it here instead
+      res.redirect(join(base, "dashboard"));
+      return;
+    }
+
+    nocoHandler(req, res);
   };
   app.use(base, handler);
+}
+
+async function ensureAdmin(req, res, base) {
+  if (!(await isAdmin(req))) {
+    // only admins can connected to nocodb
+    res.send(
+      `You must be <a href="${join(
+        basePath,
+        "auth/sign-in"
+      )}">signed in</a> as an Administrator of this site to access ${join(
+        base,
+        req.url
+      )}`
+    );
+    return false;
+  }
+
+  return true;
 }
 
 async function isAdmin(req: Request): Promise<boolean> {
