000a645bee75776f1ccaeb68cbf02eb8d75e5aaf
William Stein <wstein@sagemath.com>
2022-12-17 18:28:22 -0800

    crm: added ability to add tags

--- src/packages/frontend/frame-editors/crm-editor/fields/tags.tsx
+++ src/packages/frontend/frame-editors/crm-editor/fields/tags.tsx
@@ -1,5 +1,5 @@
 import { render } from "./register";
-import { Popconfirm, Tag } from "antd";
+import { Popconfirm, Select, Space, Tag } from "antd";
 import sha1 from "sha1";
 import { useEditableContext } from "./context";
 import { Icon } from "@cocalc/frontend/components";
@@ -38,50 +38,96 @@ render({ type: "tags", editable: true }, ({ field, obj }) => {
   }, [counter]);
 
   return (
-    <div style={{ lineHeight: "2em", display: "inline-block" }}>
-      {tags?.map((name) => {
-        const color = nameToColor(name);
-        const style = { color: avatar_fontcolor(color) };
-        return (
-          <Tag
-            color={color}
-            style={style}
-            closable
-            onClose={(e) => e.preventDefault()}
-            closeIcon={
-              <Popconfirm
-                title={
-                  <>
-                    Remove the{" "}
-                    <Tag color={color} style={style}>
-                      {name}
-                    </Tag>
-                    tag?
-                  </>
+    <Space direction="vertical">
+      {tags != null && tags.length > 0 && (
+        <div style={{ lineHeight: "2em", display: "inline-block" }}>
+          {tags?.map((name) => {
+            const color = nameToColor(name);
+            const style = { color: avatar_fontcolor(color) };
+            return (
+              <Tag
+                color={color}
+                style={style}
+                closable
+                onClose={(e) => e.preventDefault()}
+                closeIcon={
+                  <Popconfirm
+                    title={
+                      <>
+                        Remove the{" "}
+                        <Tag color={color} style={style}>
+                          {name}
+                        </Tag>
+                        tag?
+                      </>
+                    }
+                    onConfirm={async () => {
+                      const newTags = tags.filter((tag) => tag != name);
+                      setTags(newTags);
+                      try {
+                        await save(obj, newTags);
+                      } catch (_) {
+                        // failed -- revert the change at the UI level.
+                        setTags(obj[field]);
+                      }
+                    }}
+                    okText="Yes"
+                    cancelText="No"
+                  >
+                    <Icon style={style} name="times" />
+                  </Popconfirm>
                 }
-                onConfirm={async () => {
-                  console.log("confirmed!");
-                  const newTags = tags.filter((tag) => tag != name);
-                  setTags(newTags);
-                  try {
-                    await save(obj, newTags);
-                  } catch (_) {
-                    // failed -- revert the change at the UI level.
-                    setTags(obj[field]);
-                  }
-                }}
-                okText="Yes"
-                cancelText="No"
               >
-                <Icon style={style} name="times" />
-              </Popconfirm>
-            }
-          >
+                {name}
+              </Tag>
+            );
+          })}
+        </div>
+      )}
+      {error}
+      <AddTags
+        onBlur={async (addedTags: string[]) => {
+          if (addedTags.length == 0) return;
+          const newTags = Array.from(
+            new Set((tags ?? []).concat(addedTags))
+          ).sort();
+          setTags(newTags);
+          try {
+            await save(obj, newTags);
+          } catch (_) {
+            // failed -- revert the change at the UI level.
+            setTags(obj[field]);
+          }
+        }}
+      />
+    </Space>
+  );
+});
+
+function AddTags({ onBlur }) {
+  const [value, setValue] = useState<any>([]);
+
+  return (
+    <Select
+      size="small"
+      value={value}
+      maxTagTextLength={30 /* doesn't by-hand input size though */}
+      mode="tags"
+      style={{ width: "100%", minWidth: "6em" }}
+      placeholder="Add Tags"
+      onBlur={() => {
+        onBlur(value);
+        setValue([]);
+      }}
+      onChange={setValue}
+      tagRender={({ value: name }) => {
+        const color = nameToColor(name);
+        return (
+          <Tag color={color} style={{ color: avatar_fontcolor(color) }}>
             {name}
           </Tag>
         );
-      })}
-      {error}
-    </div>
+      }}
+    />
   );
-});
+}
