c8666cbc6376062358ef72e30a5e6254ad14f436
William Stein <wstein@sagemath.com>
2022-12-22 12:07:02 -0800

    crm: further syncdb + hook refactoring

--- src/packages/frontend/frame-editors/crm-editor/syncdb/use-view-filter.ts
+++ src/packages/frontend/frame-editors/crm-editor/syncdb/use-view-filter.ts
@@ -1,27 +1,22 @@
 /* Filter for a specific view, syncd using syncdb. */
 
-import { useSyncdbRecord } from "@cocalc/frontend/app-framework/syncdb";
+import useRecord from "./use-record";
 import { useCallback } from "react";
-
-const TABLE = "view-filters";
 
 export default function useViewFilter({
   id,
 }: {
   id: string; // id of a view if the given dbtable.
 }): [filter: string, setFilter: (string) => void] {
-  const [record, setRecord] = useSyncdbRecord<{
-    table: "view-filters";
-    id: string;
+  const [record, setRecord] = useRecord<{
     filter?: string;
   }>({
-    key: { id, table: TABLE },
-    defaultValue: { id, table: TABLE, filter: "" },
+    id,
+    table: "view-filters",
+    defaultValue: { filter: "" },
   });
   const setFilter = useCallback(
-    (filter: string) => {
-      setRecord({ table: TABLE, id, filter });
-    },
+    (filter: string) => setRecord({ filter }),
     [setRecord]
   );
 
