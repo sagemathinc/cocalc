d4b690f4e98d64e7dc0ec84831d34d6951429bac
William Stein <wstein@sagemath.com>
2022-12-24 10:56:36 -0800

    crm: also use new debounce hook with filter

--- src/packages/frontend/frame-editors/crm-editor/views/filter-input.tsx
+++ src/packages/frontend/frame-editors/crm-editor/views/filter-input.tsx
@@ -1,9 +1,9 @@
-import { ReactNode, useEffect, useMemo } from "react";
+import { ReactNode, useMemo } from "react";
 import { Input } from "antd";
-import { debounce } from "lodash";
 import useFilter from "./filter-hook";
 import useViewFilter from "../syncdb/use-view-filter";
 import { plural } from "@cocalc/util/misc";
+import useDebounceEffect from "@cocalc/frontend/app-framework/use-debounce-effect";
 
 export default function useFilterInput({ data, id }): {
   filteredData: any[];
@@ -18,11 +18,13 @@ export default function useFilterInput({ data, id }): {
     numHidden,
   } = useFilter({ data, defaultFilter: filter });
 
-  useEffect(
-    debounce(() => {
-      setFilter0(filter);
-    }, 500),
-    [filter]
+  useDebounceEffect<[filter: string, setFilter0: Function]>(
+    {
+      func: ([filter]) => setFilter0(filter),
+      wait: 500,
+      options: { leading: true, trailing: true },
+    },
+    [filter, setFilter0]
   );
 
   const Filter = useMemo(
