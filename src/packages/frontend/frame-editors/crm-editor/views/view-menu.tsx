b85188699ab004e5705c2b388fc06afde0a0c037
William Stein <wstein@sagemath.com>
2022-12-24 09:33:23 -0800

    crm: refactor out sort by menu

--- src/packages/frontend/frame-editors/crm-editor/views/view-menu.tsx
+++ src/packages/frontend/frame-editors/crm-editor/views/view-menu.tsx
@@ -16,8 +16,7 @@ import {
 } from "antd";
 import { TYPE_TO_ICON } from "./index";
 import { Icon } from "@cocalc/frontend/components";
-import { SortDirection, parseSort } from "../syncdb/use-sort-fields";
-import type { ColumnsType } from "../fields";
+import SortBy from "./sort-by";
 
 export default function ViewMenu({
   name,
@@ -332,69 +331,6 @@ function Filter({ field, title }) {
           console.log("change filter for ", field);
         }}
       />
-    </Space>
-  );
-}
-
-interface SortByProps {
-  columns: ColumnsType[];
-  field?: string; // if not set, then adding
-  setSortField: (
-    field: string,
-    newField: string,
-    direction: SortDirection | null
-  ) => void;
-}
-
-function SortBy({ columns, field, setSortField }: SortByProps) {
-  const { sortField, direction } = useMemo(() => parseSort(field), [field]);
-  return (
-    <Space style={{ width: "100%" }}>
-      <Select
-        value={sortField}
-        size="small"
-        style={{ width: "200px" }}
-        showSearch
-        placeholder="Find a field..."
-        filterOption={(input, option) =>
-          ((option?.label ?? "") as string)
-            .toLowerCase()
-            .includes(input.toLowerCase())
-        }
-        onChange={(newSortField: string) => {
-          setSortField(sortField, newSortField, direction);
-        }}
-        optionFilterProp="children"
-        options={columns.map(({ dataIndex, title }) => {
-          return {
-            value: dataIndex,
-            label: title,
-          };
-        })}
-      />
-      {sortField && (
-        <Select
-          size="small"
-          style={{ width: "150px" }}
-          value={direction}
-          onChange={(direction: SortDirection) => {
-            setSortField(sortField, sortField, direction);
-          }}
-          options={[
-            { value: "ascending" as SortDirection, label: "Ascending" },
-            { value: "descending" as SortDirection, label: "Descending" },
-          ]}
-        />
-      )}
-      {sortField && (
-        <Button
-          style={{ float: "right" }}
-          type="link"
-          onClick={() => setSortField(sortField, sortField, null)}
-        >
-          <Icon name="times" />
-        </Button>
-      )}
     </Space>
   );
 }
