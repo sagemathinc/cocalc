2e453a5b60ea1404ff6b2e181a2abb0d2a11341a
Harald Schilly <harald.schilly@gmail.com>
2023-01-03 20:19:51 +0100

    frontend/jupyter: adding small hover buttons + tooltip to the blue bar to make inserting cells much easier and discoverable

--- src/packages/frontend/jupyter/insert-cell.tsx
+++ src/packages/frontend/jupyter/insert-cell.tsx
@@ -4,13 +4,35 @@
  */
 
 /*
-Insert a cell
+Divider between cells, which allows inserting a new cell.
+
+NOTE: the hover logic is in CSS (_jupyter.sass).
+Event based onMouseOver/Out leaves too often
+buttons in the hover state (even when tacking mouse moves!),
+which is confusing.
 */
 
-import { React } from "../app-framework";
-const { IS_TOUCH } = require("../feature"); // TODO: use import with types
-import { JupyterActions } from "./browser-actions";
+import { Button, Space, Tooltip } from "antd";
+
+import { CSS, React } from "@cocalc/frontend/app-framework";
 import useNotebookFrameActions from "@cocalc/frontend/frame-editors/jupyter-editor/cell-notebook/hook";
+import { COLORS } from "@cocalc/util/theme";
+import { IS_TOUCH } from "../feature";
+import { JupyterActions } from "./browser-actions";
+
+const BTN_HEIGHT = 12;
+
+const TINY_BTN_STYLE: CSS = {
+  margin: "0px",
+  padding: "0px 10px",
+  fontSize: `${BTN_HEIGHT - 2}px`,
+  lineHeight: `${BTN_HEIGHT}px`,
+  height: `${BTN_HEIGHT}px`,
+  borderRadius: "2px",
+  border: "none",
+  backgroundColor: COLORS.FG_BLUE,
+  color: "white",
+};
 
 export interface InsertCellProps {
   actions: JupyterActions;
@@ -28,6 +50,7 @@ function should_memoize(prev, next) {
 
 export const InsertCell: React.FC<InsertCellProps> = React.memo(
   (props: InsertCellProps) => {
+    const { position } = props;
     const frameActions = useNotebookFrameActions();
 
     if (IS_TOUCH) {
@@ -37,21 +60,76 @@ export const InsertCell: React.FC<InsertCellProps> = React.memo(
       return <div style={{ height: "6px" }}></div>;
     }
 
-    function click(e) {
-      e.preventDefault();
-      e.stopPropagation();
-      const { actions, id, position } = props;
+    function insertCell(type: "code" | "markdown"): void {
+      const { actions, id } = props;
       if (frameActions.current == null) return;
       frameActions.current.set_cur_id(id);
       const new_id = frameActions.current.insert_cell(
         position === "below" ? 1 : -1
       );
-      if (e.shiftKey || e.ctrlKey || e.altKey || e.metaKey) {
+      if (type === "markdown") {
         actions.set_cell_type(new_id, "markdown");
       }
     }
 
-    return <div className="cocalc-jupyter-insert-cell" onClick={click} />;
+    function barClick(e) {
+      e.preventDefault();
+      e.stopPropagation();
+      const type =
+        e.shiftKey || e.ctrlKey || e.altKey || e.metaKey ? "markdown" : "code";
+      insertCell(type);
+    }
+
+    function btnClick(e, type: "code" | "markdown") {
+      e.preventDefault();
+      e.stopPropagation();
+      insertCell(type);
+    }
+
+    function TinyButton(props: {
+      cellType: "code" | "markdown";
+      children?: React.ReactNode;
+    }) {
+      const { cellType, children } = props;
+      return (
+        <Button
+          style={TINY_BTN_STYLE}
+          size={"small"}
+          onClick={(e) => btnClick(e, cellType)}
+        >
+          {children}
+        </Button>
+      );
+    }
+
+    function renderControls() {
+      return (
+        <div className="cocalc-jupyter-insert-cell-controls">
+          <Space size="large">
+            <TinyButton cellType="code">+ Code</TinyButton>
+            <TinyButton cellType="markdown">+ Text</TinyButton>
+          </Space>
+        </div>
+      );
+    }
+
+    const style: CSS =
+      position === "below" ? { marginBottom: `${BTN_HEIGHT}px` } : {};
+
+    return (
+      <div
+        className="cocalc-jupyter-insert-cell"
+        style={style}
+        onClick={barClick}
+      >
+        <Tooltip
+          title="Insert a new (text) cell – you can also [shift]-click on the blue bar to insert a [text] cell"
+          placement="bottom"
+        >
+          {renderControls()}
+        </Tooltip>
+      </div>
+    );
   },
   should_memoize
 );
