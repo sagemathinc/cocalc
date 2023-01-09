5c55634f3a80ef5c4b58d5c074bd255aa5e9c25c
William Stein <wstein@sagemath.com>
2023-01-09 14:35:09 -0800

    only show Ubuntu version menu when it should be there; also include 22.04

--- src/packages/next/lib/landing/consts.ts
+++ src/packages/next/lib/landing/consts.ts
@@ -17,8 +17,8 @@ export const LANGUAGE_NAMES = [
 
 // sort this starting from the newest to the oldest – appears in the UI, e.g. on that /software/index page
 // TODO: after https://github.com/sagemathinc/cocalc/pull/6284 has been merged, make 22.04 the first entry in that list
-export const SOFTWARE_ENV_NAMES = [/*"22.04",*/ "20.04", "18.04"] as const;
-export const SOFTWARE_ENV_DEFAULT = SOFTWARE_ENV_NAMES[0];
+export const SOFTWARE_ENV_NAMES = ["22.04", "20.04", "18.04"] as const;
+export const SOFTWARE_ENV_DEFAULT = "20.04";
 export type SoftwareEnvNames = typeof SOFTWARE_ENV_NAMES[number];
 
 export const SOFTWARE_URLS: { [key in SoftwareEnvNames]: string } = fromPairs(
@@ -30,11 +30,11 @@ export const SOFTWARE_URLS: { [key in SoftwareEnvNames]: string } = fromPairs(
 
 import SOFTWARE_1804 from "dist/software-inventory/18.04.json";
 import SOFTWARE_2004 from "dist/software-inventory/20.04.json";
-//import SOFTWARE_2204 from "dist/software-inventory/22.04.json";
+import SOFTWARE_2204 from "dist/software-inventory/22.04.json";
 
 // Note: we need to be explicit with these rougher types, because TS can't infer them from the JSON files since they're too large.
 export const SOFTWARE_FALLBACK: { [key in SoftwareEnvNames]: EnvData } = {
   "18.04": SOFTWARE_1804 as EnvData,
   "20.04": SOFTWARE_2004 as EnvData,
-  //"22.04": SOFTWARE_2204 as EnvData,
+  "22.04": SOFTWARE_2204 as EnvData,
 } as const;
