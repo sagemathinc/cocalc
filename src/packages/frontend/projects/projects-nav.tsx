fec22f0a98b4ce2bf4f27d434b2652c8cfd73190
William Stein <wstein@sagemath.com>
2023-01-11 13:59:30 -0800

    project tabs: rewrite using antd; dragging not implemented yet

--- src/packages/frontend/projects/projects-nav.tsx
+++ src/packages/frontend/projects/projects-nav.tsx
@@ -3,9 +3,9 @@
  *  License: AGPLv3 s.t. "Commons Clause" – see LICENSE.md for details
  */
 
-import { Avatar } from "antd";
-import { Nav, NavItem } from "react-bootstrap";
-import CloseX from "@cocalc/frontend/project/page/close-x";
+import { Avatar, Tabs } from "antd";
+import type { TabsProps } from "antd";
+
 import { trunc } from "@cocalc/util/misc";
 import { COLORS } from "@cocalc/util/theme";
 import { COMPUTE_STATES } from "@cocalc/util/schema";
@@ -14,54 +14,21 @@ import { set_window_title } from "@cocalc/frontend/browser";
 import {
   redux,
   useActions,
-  useMemo,
   useRedux,
   useTypedRedux,
 } from "@cocalc/frontend/app-framework";
-import { createElement, useState, CSSProperties } from "react";
+import { useMemo, useState, CSSProperties } from "react";
 import { Loading, Icon, Tip } from "@cocalc/frontend//components";
-import { NavTab } from "@cocalc/frontend//app/nav-tab";
 import { WebsocketIndicator } from "@cocalc/frontend/project/websocket/websocket-indicator";
-
-const NavWrapper = ({
-  style,
-  children,
-  id,
-  className,
-}: {
-  style: CSSProperties;
-  children;
-  id?: string;
-  className?: string;
-}) => createElement(Nav, { style, id, className }, children);
-
-const SortableNavTab = NavTab;
-const SortableNav = NavWrapper;
-
-const GHOST_STYLE: CSSProperties = {
-  flexShrink: 1,
-  width: "200px",
-  height: "36px",
-  overflow: "hidden",
-} as const;
 
 const PROJECT_NAME_STYLE: CSSProperties = {
   whiteSpace: "nowrap",
   overflow: "hidden",
   textOverflow: "ellipsis",
-} as const;
-
-const PROJECT_TAB_STYLE: CSSProperties = {
-  flexShrink: 1,
-  width: "200px",
   maxWidth: "200px",
-  height: "36px",
-  overflow: "hidden",
-  lineHeight: "1.75em",
 } as const;
 
 interface ProjectTabProps {
-  index: number;
   project_id: string;
 }
 
@@ -76,7 +43,6 @@ function useProjectStatusAlerts(project_id: string) {
 }
 
 function ProjectTab({ project_id }: ProjectTabProps) {
-  const actions = useActions("page");
   const active_top_tab = useTypedRedux("page", "active_top_tab");
   const project = useRedux(["projects", "project_map", project_id]);
   const public_project_titles = useTypedRedux(
@@ -84,29 +50,15 @@ function ProjectTab({ project_id }: ProjectTabProps) {
     "public_project_titles"
   );
   const project_websockets = useTypedRedux("projects", "project_websockets");
-  const is_anonymous = useTypedRedux("account", "is_anonymous");
   const any_alerts = useProjectStatusAlerts(project_id);
 
-  function render_websocket_indicator() {
+  function renderWebsocketIndicator() {
     return (
       // Hiding this on very skinny devices isn't necessarily bad, since the exact same information is
       // now visible via a big "Connecting..." banner after a few seconds.
       <span style={{ paddingRight: "5px" }} className="hidden-xs">
         <WebsocketIndicator state={project_websockets?.get(project_id)} />
       </span>
-    );
-  }
-
-  function render_close_x() {
-    if (is_anonymous) {
-      // you have one project and you can't close it.
-      return;
-    }
-    return (
-      <CloseX
-        closeFile={() => actions.close_project_tab(project_id)}
-        clearGhostFileTabs={() => actions.clear_ghost_tabs()}
-      />
     );
   }
 
@@ -121,12 +73,6 @@ function ProjectTab({ project_id }: ProjectTabProps) {
   if (active_top_tab == project_id) {
     set_window_title(title);
   }
-
-  const nav_style: CSSProperties = {
-    ...PROJECT_TAB_STYLE,
-    color:
-      project_id === active_top_tab ? COLORS.TOP_BAR.TEXT_ACTIVE : undefined,
-  };
 
   const nav_style_inner: CSSProperties = {
     float: "right",
@@ -172,16 +118,8 @@ function ProjectTab({ project_id }: ProjectTabProps) {
   }
 
   return (
-    <SortableNavTab
-      name={project_id}
-      active_top_tab={active_top_tab}
-      style={nav_style}
-      is_project={true}
-    >
-      <div style={nav_style_inner}>
-        {render_websocket_indicator()}
-        {render_close_x()}
-      </div>
+    <div>
+      <div style={nav_style_inner}>{renderWebsocketIndicator()}</div>
       <div style={PROJECT_NAME_STYLE} onClick={click_title}>
         <Tip title={title} tip={render_tip()} placement="bottom" size="small">
           {icon}
@@ -196,62 +134,68 @@ function ProjectTab({ project_id }: ProjectTabProps) {
           <span style={{ marginLeft: 5, position: "relative" }}>{title}</span>
         </Tip>
       </div>
-    </SortableNavTab>
+    </div>
   );
 }
 
+function DraggableTabNode({ children }) {
+  return <div>{children}</div>;
+}
+
+const renderTabBar = (tabBarProps, DefaultTabBar) => (
+  <DefaultTabBar {...tabBarProps}>
+    {(node) => <DraggableTabNode key={node.key}>{node}</DraggableTabNode>}
+  </DefaultTabBar>
+);
+
 export function ProjectsNav({ style }: { style?: CSSProperties }) {
-  const num_ghost_tabs = useTypedRedux("page", "num_ghost_tabs");
-  const open_projects = useTypedRedux("projects", "open_projects");
+  const actions = useActions("page");
+  const activeTopTab = useTypedRedux("page", "active_top_tab");
+  const openProjects = useTypedRedux("projects", "open_projects");
+  const isAnonymous = useTypedRedux("account", "is_anonymous");
 
-  //const actions = useActions("projects");
-  //   function on_sort_end({ oldIndex, newIndex }) {
-  //     actions.move_project_tab({
-  //       old_index: oldIndex,
-  //       new_index: newIndex,
-  //     });
-  //   }
-
-  function render_project_tabs(): undefined | JSX.Element[] {
-    if (open_projects == null) {
-      return;
-    }
-    const v: JSX.Element[] = [];
-    open_projects.map((project_id, index) => {
-      v.push(
-        <ProjectTab index={index} project_id={project_id} key={project_id} />
-      );
+  const items: TabsProps["items"] = useMemo(() => {
+    if (openProjects == null) return [];
+    return openProjects.toJS().map((project_id) => {
+      return {
+        label: <ProjectTab project_id={project_id} />,
+        key: project_id,
+      };
     });
+  }, [openProjects]);
 
-    if (num_ghost_tabs === 0) {
-      return v;
+  const onEdit = (project_id: string, action: "add" | "remove") => {
+    if (action == "add") {
+      actions.set_active_tab("projects");
+    } else {
+      // close given project
+      actions.close_project_tab(project_id);
     }
+  };
 
-    const num_real_tabs = open_projects.size;
-    const num_tabs = num_real_tabs + num_ghost_tabs;
-    for (let index = num_real_tabs; index < num_tabs; index++) {
-      v.push(<NavItem key={index} style={GHOST_STYLE} />);
-    }
-    return v;
-  }
-
-  // NOTE!!! The margin:'0' in the style in SortableNav below is
-  // critical; without it, when you make the screen skinny, the tabs
-  // get mangled looking.  DO NOT delete without being aware of this!
   return (
     <div
       style={{
-        display: "flex",
-        flex: "1",
+        flex: 1,
         overflow: "hidden",
         height: "36px",
-        margin: "0",
+        //display: "flex",
+        //justifyContent: "center",
         ...style,
       }}
     >
-      <SortableNav style={{ display: "flex", overflow: "hidden", margin: "0" }}>
-        {render_project_tabs()}
-      </SortableNav>
+      {items.length > 0 && (
+        <Tabs
+          activeKey={activeTopTab}
+          onEdit={onEdit}
+          onChange={(project_id) => {
+            actions.set_active_tab(project_id);
+          }}
+          type={isAnonymous ? "card" : "editable-card"}
+          renderTabBar={renderTabBar}
+          items={items}
+        />
+      )}
     </div>
   );
 }
