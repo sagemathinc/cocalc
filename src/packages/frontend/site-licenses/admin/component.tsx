094729c001a0afaf4fa4002793e303ee73812892
Harald Schilly <harald.schilly@gmail.com>
2023-01-11 19:12:21 +0100

    frontend/admin/licenses: respect the max_upgrades setting in the UI when creating a license and some refactoring

--- src/packages/frontend/site-licenses/admin/component.tsx
+++ src/packages/frontend/site-licenses/admin/component.tsx
@@ -7,41 +7,29 @@
 Viewing and configuring site licenses
 */
 
-function download_data(filename, data, type = "text/json") {
-  const blob = new Blob([data], { type });
-  if (typeof window.navigator["msSaveBlob"] == "function") {
-    window.navigator["msSaveBlob"](blob, filename);
-  } else {
-    const elem = window.document.createElement("a");
-    elem.href = window.URL.createObjectURL(blob);
-    elem.download = filename;
-    document.body.appendChild(elem);
-    elem.click();
-    document.body.removeChild(elem);
-  }
-}
-
+import { Alert, Button, Popconfirm } from "antd";
 import { DebounceInput } from "react-debounce-input";
+
 import {
   React,
   Rendered,
+  useIsMountedRef,
   useState,
   useTypedRedux,
-  useIsMountedRef,
 } from "@cocalc/frontend/app-framework";
-import { plural } from "@cocalc/util/misc";
 import {
   ErrorDisplay,
   Icon,
   Loading,
-  Space,
   r_join,
+  Space,
 } from "@cocalc/frontend/components";
-import { actions } from "./actions";
-import { Alert, Button, Popconfirm } from "antd";
-import { License } from "./license";
-import { COLORS } from "@cocalc/util/theme";
 import { webapp_client } from "@cocalc/frontend/webapp-client";
+import { plural } from "@cocalc/util/misc";
+import { COLORS } from "@cocalc/util/theme";
+import { actions } from "./actions";
+import { License } from "./license";
+import { download_data } from "./download-data";
 
 const LICENSE_QUERY = {
   site_licenses: [
