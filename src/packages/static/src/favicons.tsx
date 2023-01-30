a99da3ece74eda19731eeabe3842da6f52d09f95
William Stein <wstein@sagemath.com>
2023-01-30 11:52:30 -0800

    fix favicon fallback

--- src/packages/static/src/favicons.tsx
+++ src/packages/static/src/favicons.tsx
@@ -1,15 +1,19 @@
 // Specify the favicon.
 
 import { Helmet } from "react-helmet";
-
+import { join } from "path";
+import { appBasePath } from "@cocalc/frontend/customize/app-base-path";
 import useCustomize from "./customize";
 
 export default function LoadFavicons() {
-  const customize = useCustomize()
+  const customize = useCustomize();
 
   return (
     <Helmet>
-      <link rel="icon" href={customize.logo_square} />
+      <link
+        rel="icon"
+        href={customize.logo_square ?? join(appBasePath, "webapp/favicon.ico")}
+      />
     </Helmet>
   );
 }
