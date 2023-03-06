b0ab3dff0427fe1c9d74d54824c6d452cbc9db9e
William Stein <wstein@sagemath.com>
2023-03-06 15:54:22 -0800

    make /share/public_paths not error

---
+++ src/packages/next/pages/share/public_paths/index.tsx
@@ -0,0 +1,41 @@
+/*
+ *  This file is part of CoCalc: Copyright © 2020 Sagemath, Inc.
+ *  License: AGPLv3 s.t. "Commons Clause" – see LICENSE.md for details
+ */
+
+import { Layout } from "antd";
+import Link from "next/link";
+
+import Footer from "components/landing/footer";
+import Head from "components/landing/head";
+import Header from "components/landing/header";
+import Logo from "components/logo";
+import SiteName from "components/share/site-name";
+import { Customize } from "lib/share/customize";
+import withCustomize from "lib/with-customize";
+
+export default function Home({ customize }) {
+  return (
+    <Customize value={customize}>
+      <Head title={"Shared Public Files"} />
+      <Layout>
+        <Header />
+        <div style={{ fontSize: "16pt", textAlign: "center", margin: "60px" }}>
+          <Logo type="icon" style={{ width: "120px", height: "120px" }} />
+          <br />
+          <br />
+          <br />
+          Browse recent{" "}
+          <Link href="/share/public_paths/page/1">
+            <SiteName /> Shared Public Files...
+          </Link>
+        </div>
+        <Footer />
+      </Layout>
+    </Customize>
+  );
+}
+
+export async function getServerSideProps(context) {
+  return await withCustomize({ context });
+}
