sagemathinc/cocalc

commit 5088bddeb4584dbceed5c6c19c3cd4d4799c403c

fix #6504 -- next app: flicker on switching between certain pages

src/packages/next/components/logo.tsx
@@ -62,8 +62,8 @@ export default function Logo(props: Props) {
     style.maxWidth = `${props.width}px`;
   }
 
-  // we "fake" a full logo it by stacking the suare logo on top of the rectangular one in a div
-  function customFull(): JSX.Element {
+  if (type === "full" && logoSquareURL && logoRectangularURL) {
+    // we "fake" a full logo it by stacking the square logo on top of the rectangular one in a div
     return (
       <div
         style={{
@@ -80,6 +80,7 @@ export default function Logo(props: Props) {
             width: "50%",
           }}
         />
+        )
         <div>
           <Image
             src={logoRectangularURL}
@@ -89,10 +90,6 @@ export default function Logo(props: Props) {
         </div>
       </div>
     );
-  }
-
-  if (type === "full" && logoSquareURL && logoRectangularURL) {
-    return customFull();
   } else if (custom) {
     return <Image alt={alt} src={src} style={style} />;
   } else {

src/packages/next/components/account/navtab.tsx
@@ -18,7 +18,6 @@ import {
   MenuItem,
   MenuItems,
 } from "components/antd-menu-items";
-import { LinkStyle } from "components/landing/header";
 import A from "components/misc/A";
 import apiPost from "lib/api/post";
 import basePath from "lib/base-path";
@@ -34,11 +33,29 @@ interface Props {
   style: CSSProperties;
 }
 
+// We make this menu fixed width in all cases, since otherwise the entire top navbar
+// would flicker when profile isn't initially defined. See
+// https://github.com/sagemathinc/cocalc/issues/6504
+
+const WIDTH = "125px";
+
 export default function AccountNavTab({ style }: Props) {
   const router = useRouter();
   const { isCommercial, shareServer, siteName, sshGateway } = useCustomize();
   const profile = useProfile();
-  if (!profile) return null;
+  if (!profile) {
+    return (
+      <div
+        style={{
+          cursor: "pointer",
+          ...style,
+          width: WIDTH,
+        }}
+      >
+        Account
+      </div>
+    );
+  }
 
   const { first_name, last_name, name, account_id, is_admin, is_anonymous } =
     profile;
@@ -218,17 +235,23 @@ export default function AccountNavTab({ style }: Props) {
   // NOTE: we had a dark theme before for the menu, but that's deprecated from antd
   // https://github.com/ant-design/ant-design/issues/4903
   return (
-    <Dropdown menu={{ items }}>
-      <div style={{ ...LinkStyle, cursor: "pointer", ...style }}>
-        {/* The negative margin fixes some weird behavior that stretches header. */}
-        {account_id && (
-          <>
-            <Avatar account_id={account_id} style={{ margin: "-10px 0" }} />
-            &nbsp;&nbsp;
-          </>
-        )}
-        Account <Icon name="angle-down" />
-      </div>
-    </Dropdown>
+    <div
+      style={{
+        display: "inline-block",
+        cursor: "pointer",
+        width: WIDTH,
+      }}
+    >
+      {/* The negative margin fixes some weird behavior that stretches header. */}
+      {account_id && (
+        <>
+          <Avatar account_id={account_id} style={{ margin: "-10px 0" }} />
+          &nbsp;&nbsp;
+        </>
+      )}
+      <Dropdown menu={{ items }}>
+        <span style={style}>Account</span>
+      </Dropdown>
+    </div>
   );
 }

src/packages/next/components/landing/header.tsx
@@ -27,10 +27,12 @@ export const LinkStyle: React.CSSProperties = {
   display: "inline-block",
 } as const;
 
+// The style shouldn't change the size of the label, e.g., don't
+// use bold.  Otherwise, everything moves a little when you select
+// an option, which looks weird.
 const SelectedStyle: React.CSSProperties = {
   ...LinkStyle,
   color: COLORS.LANDING.TOP_BG,
-  fontWeight: "bold",
   borderBottom: "5px solid #c7d9f5",
 } as const;
 

src/packages/next/pages/config/[...page].tsx
@@ -40,7 +40,7 @@ export default function Preferences({ customize, page }) {
     <Customize value={customize}>
       <Head title="Configuration" />
       <Layout>
-        <Header />
+        <Header page={"account"} />
         <ConfigLayout page={page} />
         {noteAboutConfig()}
         <Footer />

commit 4b0a086f5752abc7aced749307d9d15de990ce01

next/index: "fake" logo for on-prem has a closing bracket -- bug introduced in 5088bddeb4584dbceed5c6c19c3cd4d4799c403c

src/packages/next/components/logo.tsx
@@ -80,7 +80,6 @@ export default function Logo(props: Props) {
             width: "50%",
           }}
         />
-        )
         <div>
           <Image
             src={logoRectangularURL}