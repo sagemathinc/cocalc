d17acdb2082e1f4a4f20d9e330455dd0993cc216
William Stein <wstein@sagemath.com>
2023-03-14 21:17:08 -0700

    add google search button to all share pages (not just top)

--- src/packages/next/components/share/google-search.tsx
+++ src/packages/next/components/share/google-search.tsx
@@ -3,16 +3,28 @@
  *  License: AGPLv3 s.t. "Commons Clause" – see LICENSE.md for details
  */
 
+import { CSSProperties, useState } from "react";
 import { Input } from "antd";
+import { Icon } from "@cocalc/frontend/components/icon";
 
-export default function GoogleSearch() {
+export default function GoogleSearch({ style }: { style?: CSSProperties }) {
+  const [focus, setFocus] = useState<boolean>(false);
   return (
     <Input.Search
-      size="small"
+      onFocus={() => setFocus(true)}
+      onBlur={() => setFocus(false)}
+      style={style}
       placeholder="Search..."
       allowClear
-      enterButton="Google"
+      enterButton={
+        <>
+          <Icon name="google" />
+          {!focus && <> Google</>}
+        </>
+      }
       onSearch={(value) => {
+        value = value.trim();
+        if (!value) return;
         const host = window.location.host;
         const url = `https://www.google.com/search?q=site%3A${host}/share+${value}`;
         // Open url in a new tab.
