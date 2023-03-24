2b16513f559ebd532c49f10035ea846ab7bce90e
William Stein <wstein@sagemath.com>
2023-03-23 20:12:06 -0700

    jupyter chatgpt: working on get help button

--- src/packages/frontend/jupyter/chatgpt/error.tsx
+++ src/packages/frontend/jupyter/chatgpt/error.tsx
@@ -2,9 +2,13 @@
 Use ChatGPT to explain an error message and help the user fix it.
 */
 
-import { CSSProperties } from "react";
+import { CSSProperties, useState } from "react";
 import { Button } from "antd";
+//import { Icon } from "@cocalc/frontend/components/icon";
+//      <Icon name="robot" style={{ color: "rgb(16, 163, 127)" }} />
+
 import OpenAIAvatar from "@cocalc/frontend/components/openai-avatar";
+import type { JupyterActions } from "../browser-actions";
 
 interface Props {
   actions?;
@@ -13,15 +17,42 @@ interface Props {
 }
 
 export default function ChatGPTError({ actions, id, style }: Props) {
-  if (!actions?.redux.getStore("customize").get("openai_enabled")) return null;
+  const [gettingHelp, setGettingHelp] = useState<boolean>(false);
+  if (
+    actions == null ||
+    !actions.redux?.getStore("customize").get("openai_enabled")
+  ) {
+    return null;
+  }
   return (
-    <Button style={style}>
+    <Button
+      style={style}
+      disabled={gettingHelp}
+      onClick={async () => {
+        setGettingHelp(true);
+        try {
+          await getHelp({ id, actions });
+        } finally {
+          setGettingHelp(false);
+        }
+      }}
+    >
       <OpenAIAvatar
         size={16}
         style={{ marginRight: "5px" }}
         innerStyle={{ top: "2.5px" }}
-      />{" "}
-      Help me fix this error...
+      />
+      @ChatGPT, help me fix this error...
     </Button>
   );
+}
+
+async function getHelp({
+  id,
+  actions,
+}: {
+  id: string;
+  actions: JupyterActions;
+}) {
+  console.log("getting help...", id, actions);
 }
