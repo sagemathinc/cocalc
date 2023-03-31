54772f97a1a0612f6b9cb00ac4ba3b756e6e8bd1
Harald Schilly <harald.schilly@gmail.com>
2023-03-31 12:18:31 +0200

    frontend/project/home: first iteration of a "recent files" list

--- src/packages/frontend/project/page/home-page/index.tsx
+++ src/packages/frontend/project/page/home-page/index.tsx
@@ -4,29 +4,40 @@
  */
 
 import { Col, Row } from "antd";
-// import { useState } from "react";
 
-import { useActions, useRedux } from "@cocalc/frontend/app-framework";
+import { redux, useActions, useRedux } from "@cocalc/frontend/app-framework";
 import { Title } from "@cocalc/frontend/components";
 import StaticMarkdown from "@cocalc/frontend/editors/slate/static-markdown";
 import { ProjectLog } from "@cocalc/frontend/project/history";
 import { ProjectTitle } from "@cocalc/frontend/projects/project-title";
-import ChatGPTGenerateJupyterNotebook from "./chatgpt-generate-jupyter";
 import { Block } from "./block";
+import ChatGPTGenerateJupyterNotebook from "./chatgpt-generate-jupyter";
 import { HomeRecentFiles } from "./recent-files";
 
 export default function HomePage({ project_id }) {
   const desc = useRedux(["projects", "project_map", project_id, "description"]);
   const actions = useActions({ project_id });
 
+  function renderGPTGenerator() {
+    // if not available, the entire block should be gone
+    // making room for the toher blocks to move into its place
+    if (!redux.getStore("projects").hasOpenAI(project_id)) return null;
+
+    return (
+      <Col span={12}>
+        <ChatGPTGenerateJupyterNotebook project_id={project_id} />
+      </Col>
+    );
+  }
+
   return (
     <div style={{ margin: "15px" }}>
       <Row gutter={[30, 30]}>
-        <Col span={12} style={{ }}>
+        <Col span={12} style={{}}>
           <Title
             level={2}
             onClick={() => actions?.set_active_tab("settings")}
-            style={{ cursor: "pointer" , textAlign: "center" }}
+            style={{ cursor: "pointer", textAlign: "center" }}
           >
             <ProjectTitle project_id={project_id} noClick />
           </Title>
@@ -43,9 +54,8 @@ export default function HomePage({ project_id }) {
           </div>
           <HomeRecentFiles project_id={project_id} />
         </Col>
-        <Col span={12}>
-          <ChatGPTGenerateJupyterNotebook project_id={project_id} />
-        </Col>
+
+        {renderGPTGenerator()}
 
         <Col span={12}>
           <Block style={{ margin: "auto" }}>
--- src/packages/frontend/project/page/home-page/recent-files.tsx
+++ src/packages/frontend/project/page/home-page/recent-files.tsx
@@ -7,44 +7,92 @@ import {
   Loading,
   Paragraph,
   PathLink,
+  Text,
   TimeAgo,
   Title,
 } from "@cocalc/frontend/components";
-import { useTypedRedux } from "@cocalc/frontend/app-framework";
-import { User } from "../../../users";
+import { useMemo, useTypedRedux } from "@cocalc/frontend/app-framework";
+import { EventRecordMap } from "../../history/types";
+import { User } from "@cocalc/frontend/users";
 
+interface OpenedFile {
+  filename: string;
+  time: Date;
+  account_id: string;
+}
 interface Props {
   project_id: string;
 }
 
+/**
+ * This is a distillation of the project log, showing only the most recently opened files.
+ */
 export function HomeRecentFiles(props: Props) {
   const { project_id } = props;
 
   const project_log = useTypedRedux({ project_id }, "project_log");
   const user_map = useTypedRedux("users", "user_map");
 
+  const log: OpenedFile[] = useMemo(() => {
+    if (project_log == null) return [];
+
+    const dedupe: string[] = [];
+
+    return project_log
+      .valueSeq()
+      .filter(
+        (entry: EventRecordMap) =>
+          entry.getIn(["event", "filename"]) &&
+          entry.getIn(["event", "event"]) === "open"
+      )
+      .sort((a, b) => b.get("time").getTime() - a.get("time").getTime())
+      .filter((entry: EventRecordMap) => {
+        const fn = entry.getIn(["event", "filename"]);
+        if (dedupe.includes(fn)) return false;
+        dedupe.push(fn);
+        return true;
+      })
+      .slice(0, 10)
+      .map((entry: EventRecordMap) => {
+        return {
+          filename: entry.getIn(["event", "filename"]),
+          time: entry.get("time"),
+          account_id: entry.get("account_id"),
+        };
+      })
+      .toJS();
+  }, [project_log]);
+
   function recent() {
     if (project_log == null) {
       return <Loading />;
     }
-    const log = project_log.slice(0, 5);
+
     return (
       <Paragraph>
-        {log
-          .filter((entry) => entry.event === "open")
-          .map((entry, i) => {
-            const time = entry.get("time");
-            const account_id = entry.get("account_id");
+        <ul>
+          {log.map((entry, i) => {
+            const time = entry.time;
+            const account_id = entry.account_id;
             return (
-              <div key={i}>
+              <li key={i}>
                 <>
-                  <PathLink path={entry.filename} project_id={project_id} />{" "}
-                  <User user_map={user_map} account_id={account_id} /> edited{" "}
-                  <TimeAgo date={time} />
+                  <PathLink
+                    trunc={32}
+                    full={true}
+                    style={{ fontWeight: "bold" }}
+                    path={entry.filename}
+                    project_id={project_id}
+                  />{" "}
+                  <Text type="secondary">
+                    by <User user_map={user_map} account_id={account_id} />{" "}
+                    <TimeAgo date={time} />
+                  </Text>
                 </>
-              </div>
+              </li>
             );
           })}
+        </ul>
       </Paragraph>
     );
   }
