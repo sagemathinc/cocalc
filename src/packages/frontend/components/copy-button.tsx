5f4937ba8ebd5b2551347bc35ae57485e234d7bc
William Stein <wstein@sagemath.com>
2023-04-03 03:33:48 +0000

    copied --> copy if change (button)

--- src/packages/frontend/components/copy-button.tsx
+++ src/packages/frontend/components/copy-button.tsx
@@ -1,4 +1,4 @@
-import { CSSProperties, useState } from "react";
+import { CSSProperties, useEffect, useState } from "react";
 import { Button } from "antd";
 import { Icon } from "@cocalc/frontend/components/icon";
 import { CopyToClipboard } from "react-copy-to-clipboard";
@@ -10,6 +10,9 @@ interface Props {
 
 export default function CopyButton({ style, value }: Props) {
   const [copied, setCopied] = useState<boolean>(false);
+  useEffect(() => {
+    setCopied(false);
+  }, [value]);
   return (
     <CopyToClipboard text={value} onCopy={() => setCopied(true)}>
       <Button size="small" type="text" style={style}>
