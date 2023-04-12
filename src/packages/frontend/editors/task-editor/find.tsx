81e3eca830ce63b769e1232acf36ccc1e33420a1
William Stein <wstein@sagemath.com>
2023-04-12 05:02:23 +0000

    tasks: modernize the find input

--- src/packages/frontend/editors/task-editor/find.tsx
+++ src/packages/frontend/editors/task-editor/find.tsx
@@ -7,15 +7,8 @@
 Searching for tasks by full text search and done/deleted status.
 */
 
-import { React, ReactDOM, useEffect, useRef } from "../../app-framework";
-
-import { Icon } from "../../components";
-import {
-  Button,
-  FormControl,
-  FormGroup,
-  InputGroup,
-} from "../../antd-bootstrap";
+import { useEffect, useRef } from "../../app-framework";
+import { Input } from "antd";
 import { ShowToggle } from "./show-toggle";
 import { EmptyTrash } from "./empty-trash";
 import { TaskActions } from "./actions";
@@ -28,82 +21,67 @@ interface Props {
   focus_find_box?: boolean;
 }
 
-export const Find: React.FC<Props> = React.memo(
-  ({ actions, local_view_state, counts, focus_find_box }) => {
-    useEffect(() => {
-      if (focus_find_box) {
-        ReactDOM.findDOMNode(search_ref.current)?.focus();
-      }
-    }, [focus_find_box]);
-
-    const search_ref = useRef(null);
-
-    function render_toggle(type: "deleted" | "done") {
-      if (counts == null) return null;
-      const count = counts.get(type);
-      const show = local_view_state.get(`show_${type}`);
-      return (
-        <div style={{ minWidth: "150px", padding: "2px 5px" }}>
-          <ShowToggle actions={actions} type={type} show={show} count={count} />
-          {show && type === "deleted" && count > 0 && (
-            <EmptyTrash actions={actions} count={count} />
-          )}
-        </div>
-      );
+export function Find({
+  actions,
+  local_view_state,
+  counts,
+  focus_find_box,
+}: Props) {
+  const inputRef = useRef<any>(null);
+  useEffect(() => {
+    if (focus_find_box) {
+      inputRef.current?.focus();
     }
+  }, [focus_find_box]);
 
-    function key_down(evt) {
-      if (evt.which === 27) {
-        actions.set_local_view_state({ search: "" });
-        ReactDOM.findDOMNode(search_ref.current).blur();
-        return false;
-      }
-    }
+  return (
+    <div style={{ display: "flex", marginLeft: "5px" }}>
+      <Input.Search
+        ref={inputRef}
+        allowClear
+        value={local_view_state.get("search") ?? ""}
+        placeholder={"Search for tasks..."}
+        onChange={(e) =>
+          actions.set_local_view_state({
+            search: e.target.value,
+          })
+        }
+        onBlur={() => actions.blur_find_box()}
+        onFocus={() => actions.disable_key_handler()}
+        onKeyDown={(evt) => {
+          if (evt.which === 27) {
+            actions.set_local_view_state({ search: "" });
+            inputRef.current?.blur();
+            return false;
+          }
+        }}
+      />
+      <Toggle
+        type="done"
+        counts={counts}
+        local_view_state={local_view_state}
+        actions={actions}
+      />
+      <Toggle
+        type="deleted"
+        counts={counts}
+        local_view_state={local_view_state}
+        actions={actions}
+      />
+    </div>
+  );
+}
 
-    function clear_and_focus_search_input() {
-      actions.set_local_view_state({ search: "" });
-      ReactDOM.findDOMNode(search_ref.current)?.focus();
-    }
-
-    function render_search() {
-      const value = local_view_state.get("search") ?? "";
-      return (
-        <FormGroup style={{ marginBottom: 0, marginRight: "20px" }}>
-          <InputGroup>
-            <FormControl
-              type="text"
-              ref={search_ref}
-              componentClass="input"
-              placeholder={"Search for tasks..."}
-              value={value}
-              onChange={() =>
-                actions.set_local_view_state({
-                  search: ReactDOM.findDOMNode(search_ref.current).value,
-                })
-              }
-              onBlur={() => actions.blur_find_box()}
-              onFocus={() => actions.disable_key_handler()}
-              onKeyDown={key_down}
-            />
-            <InputGroup.Button>
-              <Button
-                onClick={clear_and_focus_search_input}
-                bsStyle={value.length > 0 ? "warning" : "default"}
-              >
-                <Icon name="times-circle" />
-              </Button>
-            </InputGroup.Button>
-          </InputGroup>
-        </FormGroup>
-      );
-    }
-
-    return (
-      <div style={{ display: "flex", marginLeft: "5px" }}>
-        {render_search()}
-        {render_toggle("done")}
-        {render_toggle("deleted")}
-      </div>
-    );
-  }
-);
+function Toggle({ type, counts, local_view_state, actions }) {
+  if (counts == null) return null;
+  const count = counts.get(type);
+  const show = local_view_state.get(`show_${type}`);
+  return (
+    <div style={{ minWidth: "150px", padding: "2px 5px" }}>
+      <ShowToggle actions={actions} type={type} show={show} count={count} />
+      {show && type === "deleted" && count > 0 && (
+        <EmptyTrash actions={actions} count={count} />
+      )}
+    </div>
+  );
+}
