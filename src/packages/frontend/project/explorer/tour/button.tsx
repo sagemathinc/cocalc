e63ce237fe67aff1db164841d087fc3d8ced7714
William Stein <wstein@sagemath.com>
2023-05-03 21:49:09 +0000

    tours: remove them on mobile
    - basically the antd Tour component is completely broken on mobile...
    
    - right now the mobile version of antd doesn't seem to have tours: https://mobile.ant.design/

--- src/packages/frontend/project/explorer/tour/button.tsx
+++ src/packages/frontend/project/explorer/tour/button.tsx
@@ -2,10 +2,11 @@ import { Button } from "antd";
 import { Icon } from "@cocalc/frontend/components/icon";
 import { redux, useRedux } from "@cocalc/frontend/app-framework";
 import track from "@cocalc/frontend/user-tracking";
+import { IS_MOBILE } from "@cocalc/frontend/feature";
 
 export default function ProjectTourButton({ project_id }) {
   const tours = useRedux("account", "tours");
-  if (tours?.includes("all") || tours?.includes("explorer")) {
+  if (IS_MOBILE || tours?.includes("all") || tours?.includes("explorer")) {
     return null;
   }
   return (
