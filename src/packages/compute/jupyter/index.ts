34850829cff53ea8b5bef27ffc2bc509966b25f6
William Stein <wstein@sagemath.com>
2023-05-21 23:00:41 +0000

    @cocalc/compute -- little bit of clean up and org

---
+++ src/packages/compute/jupyter/index.ts
@@ -0,0 +1,53 @@
+import SyncClient from "@cocalc/sync-client";
+import { meta_file } from "@cocalc/util/misc";
+import { initJupyterRedux } from "@cocalc/jupyter/kernel";
+import { redux } from "@cocalc/jupyter/redux/app";
+import getLogger from "@cocalc/backend/logger";
+const logger = getLogger("compute");
+import { COMPUTE_THRESH_MS } from "@cocalc/jupyter/redux/project-actions";
+
+// path should be something like "foo/bar.ipynb"
+export function jupyter({
+  project_id,
+  path,
+}: {
+  project_id: string;
+  path: string;
+}) {
+  const log = (...args) => logger.debug(path, ...args);
+  log();
+  const syncdb_path = meta_file(path, "jupyter2");
+  const client = new SyncClient();
+  const syncdb = client.sync_client.sync_db({
+    project_id,
+    path: syncdb_path,
+    change_throttle: 50, // our UI/React can handle more rapid updates; plus we want output FAST.
+    patch_interval: 50,
+    primary_keys: ["type", "id"],
+    string_cols: ["input"],
+    cursors: false,
+    persistent: true,
+  });
+
+  syncdb.once("ready", () => {
+    const f = () => {
+      syncdb.set({
+        type: "compute",
+        id: client.client_id(),
+        time: Date.now(),
+      });
+      syncdb.commit();
+    };
+    const i = setInterval(f, COMPUTE_THRESH_MS / 2);
+    f();
+    syncdb.once("closed", () => {
+      clearInterval(i);
+    });
+  });
+
+  log("initializing jupyter notebook redux...");
+  initJupyterRedux(syncdb, client);
+  const actions = redux.getEditorActions(project_id, path);
+  const store = redux.getEditorStore(project_id, path);
+  return { syncdb, client, actions, store, redux };
+}
--- src/packages/compute/lib/index.ts
+++ src/packages/compute/lib/index.ts
@@ -1,11 +1,5 @@
 import SyncClient from "@cocalc/sync-client";
 import { SyncDB } from "@cocalc/sync/editor/db";
-import { meta_file } from "@cocalc/util/misc";
-import { initJupyterRedux } from "@cocalc/jupyter/kernel";
-import { redux } from "@cocalc/jupyter/redux/app";
-import getLogger from "@cocalc/backend/logger";
-const logger = getLogger("compute");
-import { COMPUTE_THRESH_MS } from "@cocalc/jupyter/redux/project-actions";
 
 export function tasks({
   project_id,
@@ -22,50 +16,4 @@ export function tasks({
     string_cols: ["desc"],
   });
   return s;
-}
-
-// path should be something like "foo/bar.ipynb"
-export function jupyter({
-  project_id,
-  path,
-}: {
-  project_id: string;
-  path: string;
-}) {
-  const log = (...args) => logger.debug(path, ...args);
-  log();
-  const syncdb_path = meta_file(path, "jupyter2");
-  const client = new SyncClient();
-  const syncdb = client.sync_client.sync_db({
-    project_id,
-    path: syncdb_path,
-    change_throttle: 50, // our UI/React can handle more rapid updates; plus we want output FAST.
-    patch_interval: 50,
-    primary_keys: ["type", "id"],
-    string_cols: ["input"],
-    cursors: false,
-    persistent: true,
-  });
-
-  syncdb.once("ready", () => {
-    const f = () => {
-      syncdb.set({
-        type: "compute",
-        id: client.client_id(),
-        time: Date.now(),
-      });
-      syncdb.commit();
-    };
-    const i = setInterval(f, COMPUTE_THRESH_MS / 2);
-    f();
-    syncdb.once("closed", () => {
-      clearInterval(i);
-    });
-  });
-
-  log("initializing jupyter notebook redux...");
-  initJupyterRedux(syncdb, client);
-  const actions = redux.getEditorActions(project_id, path);
-  const store = redux.getEditorStore(project_id, path);
-  return { syncdb, client, actions, store, redux };
 }
