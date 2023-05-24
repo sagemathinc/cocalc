550540650c0116bce9d427c0f6ad404102d0d7c0
William Stein <wstein@sagemath.com>
2023-05-24 23:38:56 +0000

    make sync-client slightly more general

--- src/packages/sync-client/lib/connect-to-project.ts
+++ src/packages/sync-client/lib/connect-to-project.ts
@@ -18,16 +18,11 @@ export default async function connectToProject(
   const log = debug("cocalc:compute:sync:connect");
 
   // temporary for a proof of concept!
-  if (!process.env.PROJECT_PORT) {
-    throw Error("you MUST set the env variable PROJECT_PORT right now");
-  }
-  const port = parseInt(process.env.PROJECT_PORT);
-  const appBasePath =
-    process.env.PROJECT_BASE_PATH ??
-    "/10f0e544-313c-4efe-8718-2142ac97ad11/port/5000";
+  const port = parseInt(process.env.PROJECT_PORT ?? "0");
+  const appBasePath = process.env.PROJECT_BASE_PATH ?? "/";
   const server = process.env.PROJECT_SERVER ?? "http://localhost";
 
-  const url = `${server}:${port}`;
+  const url = `${server}${port ? `:${port}` : ""}`;
   const pathname = join(appBasePath, project_id, "raw/.smc/ws");
   const target = `${url}${pathname}`;
   log("connecting to ", target);
