c92b4e807e4cb5247576ac349dfa2689212e5a0b
Harald Schilly <harald.schilly@gmail.com>
2023-06-30 16:54:07 +0200

    frontend/flyout/files: fix adhoc file creation in subdir -- #6807

--- src/packages/frontend/project/page/flyouts/files.tsx
+++ src/packages/frontend/project/page/flyouts/files.tsx
@@ -150,12 +150,20 @@ export function FilesFlyout({ project_id }): JSX.Element {
     };
   }, [project_id, current_path]);
 
-  const [directoryFiles, fileMap, activeFile] = useMemo((): [
+  // active file: current editor is the file in the listing
+  // empty: either no files, or just the ".." for the parent dir
+  const [directoryFiles, fileMap, activeFile, isEmpty] = useMemo((): [
     DirectoryListing,
     FileMap,
-    DirectoryListingEntry | null
+    DirectoryListingEntry | null,
+    boolean
   ] => {
-    const empty: [DirectoryListing, FileMap, null] = [[], {}, null];
+    const empty: [DirectoryListing, FileMap, null, boolean] = [
+      [],
+      {},
+      null,
+      true,
+    ];
     if (directoryListings == null) return empty;
     const filesStore = directoryListings.get(current_path);
     if (filesStore == null) return empty;
@@ -235,6 +243,9 @@ export function FilesFlyout({ project_id }): JSX.Element {
       procFiles.reverse(); // inplace op
     }
 
+    const isEmpty = procFiles.length === 0;
+
+    // the ".." dir does not change the isEmpty state
     if (current_path != "") {
       procFiles.unshift({
         name: "..",
@@ -245,7 +256,7 @@ export function FilesFlyout({ project_id }): JSX.Element {
     // map each filename to it's entry in the directory listing
     const fileMap = fromPairs(procFiles.map((file) => [file.name, file]));
 
-    return [procFiles, fileMap, activeFile];
+    return [procFiles, fileMap, activeFile, isEmpty];
   }, [
     directoryListings,
     activeFileSort,
@@ -487,7 +498,7 @@ export function FilesFlyout({ project_id }): JSX.Element {
         setScrollIdx(null);
       } else if (search != "") {
         setSearch("");
-        if (directoryFiles.length > 0) {
+        if (!isEmpty) {
           open(e, 0);
         } else {
           createFileOrFolder();
@@ -734,7 +745,7 @@ export function FilesFlyout({ project_id }): JSX.Element {
   }
 
   function createFileIfNotExists() {
-    if (search === "" || directoryFiles.length > 0) return;
+    if (search === "" || !isEmpty) return;
 
     const what = search.trim().endsWith("/") ? "directory" : "file";
     return (
