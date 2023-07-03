69f3679b494d7ce12b1de12f8acbc52f4dcd0519
William Stein <wstein@sagemath.com>
2023-07-03 00:34:38 +0000

    purchases page: further organize

--- src/packages/frontend/purchases/account-page.tsx
+++ src/packages/frontend/purchases/account-page.tsx
@@ -1,15 +1,36 @@
+import { Collapse } from "antd";
 import Purchases from "./purchases";
 import AccountStatus from "./account-status";
 import Quotas from "./all-quotas-config";
 import CostBarChart from "./cost-bar-chart";
+import { useState } from "react";
 
-export default function PayAsYouGoAccountPage({}) {
+type Key = string[] | string | number[] | number;
+
+const cache: { activeKey: Key } = { activeKey: [] };
+
+export default function PurchasesPage() {
+  const [activeKey, setActiveKey] = useState<Key>(cache.activeKey);
   return (
     <div>
       <AccountStatus />
-      <Purchases />
-      <Quotas />
-      <CostBarChart />
+      <Collapse
+        activeKey={activeKey}
+        onChange={(x) => {
+          cache.activeKey = x;
+          setActiveKey(x);
+        }}
+      >
+        <Collapse.Panel key="transactions" header="Transactions">
+          <Purchases />
+        </Collapse.Panel>
+        <Collapse.Panel key="limits" header="Limits">
+          <Quotas />
+        </Collapse.Panel>
+        <Collapse.Panel key="spend" header="Spend">
+          <CostBarChart />
+        </Collapse.Panel>
+      </Collapse>
     </div>
   );
 }
