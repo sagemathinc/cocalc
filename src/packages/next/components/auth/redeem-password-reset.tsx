38133b53783f8bdc477ec9885c008ce53e15c308
William A. Stein <wstein@sagemath.com>
2023-07-20 17:37:05 +0000

    make password reset work properly even if you are already signed in

--- src/packages/next/components/auth/redeem-password-reset.tsx
+++ src/packages/next/components/auth/redeem-password-reset.tsx
@@ -16,7 +16,7 @@ import { Icon } from "@cocalc/frontend/components/icon";
 import Contact from "components/landing/contact";
 
 export default function PasswordReset({ passwordResetId }) {
-  const { account, siteName } = useCustomize();
+  const { siteName } = useCustomize();
   const [password, setPassword] = useState<string>("");
   const [resetting, setResetting] = useState<boolean>(false);
   const [error, setError] = useState<string>("");
@@ -33,8 +33,8 @@ export default function PasswordReset({ passwordResetId }) {
         password,
         passwordResetId,
       });
-      // if no error got signed in, so do a page refresh to see this
-      router.reload();
+      // if no error got signed in, so go to success page.
+      router.push("/auth/password-reset-done");
     } catch (err) {
       setError(`${err}`);
     } finally {
@@ -46,114 +46,93 @@ export default function PasswordReset({ passwordResetId }) {
     <div style={{ padding: "15px" }}>
       <div style={{ textAlign: "center", marginBottom: "15px" }}>
         <Logo type="icon" style={{ width: "100px", height: "100px" }} />
-        <h1>
-          {account ? "Successfully " : ""}Reset Your {siteName} Password
-        </h1>
+        <h1>Reset Your {siteName} Password</h1>
       </div>
-      {account && (
+      <>
         <div style={LOGIN_STYLE}>
-          <Alert
-            style={{ marginTop: "20px" }}
-            message={<b>Success</b>}
-            description={
-              <div style={{ fontSize: "12pt" }}>
-                Password successfully set. You are now signed in using your new
-                password.
-              </div>
-            }
-            type="success"
-            showIcon
-          />
+          <div style={{ margin: "10px 0" }}>
+            Choose a new {siteName} password:
+          </div>
+          <form>
+            <Input.Password
+              style={{ fontSize: "13pt" }}
+              autoFocus
+              placeholder="New Password"
+              autoComplete="username"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+                setError("");
+                setSuccess("");
+              }}
+              onPressEnter={(e) => {
+                e.preventDefault();
+                if (resetting || !isValidPassword(password)) return;
+                resetPassword();
+              }}
+            />
+            {password && (
+              <Button
+                disabled={resetting || !isValidPassword(password)}
+                shape="round"
+                size="large"
+                type="primary"
+                style={{ width: "100%", marginTop: "20px" }}
+                onClick={resetPassword}
+              >
+                {resetting ? (
+                  <>
+                    <Icon name="spinner" spin /> Changing password...
+                  </>
+                ) : !isValidPassword(password) ? (
+                  "Enter password (at least 6 characters)."
+                ) : (
+                  "Change Password"
+                )}
+              </Button>
+            )}
+          </form>
+          {error && (
+            <Alert
+              style={{ marginTop: "20px" }}
+              message="Error"
+              description={
+                <div style={{ fontSize: "12pt" }}>
+                  <b>{error}</b>
+                  <br /> If you are stuck <Contact lower />.
+                </div>
+              }
+              type="error"
+              showIcon
+            />
+          )}
+          {success && (
+            <Alert
+              style={{ marginTop: "20px" }}
+              message={<b>Success</b>}
+              description={<div style={{ fontSize: "12pt" }}>{success}</div>}
+              type="success"
+              showIcon
+            />
+          )}
         </div>
-      )}
 
-      {!account && (
-        <>
-          <div style={LOGIN_STYLE}>
-            <div style={{ margin: "10px 0" }}>
-              Choose a new {siteName} password:
-            </div>
-            <form>
-              <Input.Password
-                style={{ fontSize: "13pt" }}
-                autoFocus
-                placeholder="New Password"
-                autoComplete="username"
-                value={password}
-                onChange={(e) => {
-                  setPassword(e.target.value);
-                  setError("");
-                  setSuccess("");
-                }}
-                onPressEnter={(e) => {
-                  e.preventDefault();
-                  if (resetting || !isValidPassword(password)) return;
-                  resetPassword();
-                }}
-              />
-              {password && (
-                <Button
-                  disabled={resetting || !isValidPassword(password)}
-                  shape="round"
-                  size="large"
-                  type="primary"
-                  style={{ width: "100%", marginTop: "20px" }}
-                  onClick={resetPassword}
-                >
-                  {resetting ? (
-                    <>
-                      <Icon name="spinner" spin /> Changing password...
-                    </>
-                  ) : !isValidPassword(password) ? (
-                    "Enter password (at least 6 characters)."
-                  ) : (
-                    "Change Password"
-                  )}
-                </Button>
-              )}
-            </form>
-            {error && (
-              <Alert
-                style={{ marginTop: "20px" }}
-                message="Error"
-                description={
-                  <div style={{ fontSize: "12pt" }}>
-                    <b>{error}</b>
-                    <br /> If you are stuck <Contact lower />.
-                  </div>
-                }
-                type="error"
-                showIcon
-              />
-            )}
-            {success && (
-              <Alert
-                style={{ marginTop: "20px" }}
-                message={<b>Success</b>}
-                description={<div style={{ fontSize: "12pt" }}>{success}</div>}
-                type="success"
-                showIcon
-              />
-            )}
-          </div>
-
-          <div
-            style={{
-              ...LOGIN_STYLE,
-              backgroundColor: "white",
-              marginTop: "30px",
-              marginBottom: "30px",
-              paddingTop: "15px",
-            }}
-          >
-            <p>
-              Remember your password? <A href="/auth/sign-in">Sign In</A>
-            </p>
-            You can also{" "}
-            <A href="/auth/try">use {siteName} without creating an account</A>
-          </div>
-        </>
-      )}
+        <div
+          style={{
+            ...LOGIN_STYLE,
+            backgroundColor: "white",
+            marginTop: "30px",
+            marginBottom: "30px",
+            paddingTop: "15px",
+          }}
+        >
+          <p>
+            Remember your password? <A href="/auth/sign-in">Sign In</A>
+          </p>
+          You can also{" "}
+          <A href="/auth/try">use {siteName} without creating an account</A>
+        </div>
+      </>
     </div>
   );
 }
---
+++ src/packages/next/pages/auth/password-reset-done.tsx
@@ -0,0 +1,44 @@
+/*
+ *  This file is part of CoCalc: Copyright © 2021 Sagemath, Inc.
+ *  License: AGPLv3 s.t. "Commons Clause" – see LICENSE.md for details
+ */
+
+import { Alert, Layout } from "antd";
+import Footer from "components/landing/footer";
+import Head from "components/landing/head";
+import Header from "components/landing/header";
+import { Customize } from "lib/customize";
+import withCustomize from "lib/with-customize";
+import { LOGIN_STYLE } from "components/auth/shared";
+
+export default function Home({ customize }) {
+  return (
+    <Customize value={customize}>
+      <Head title={"Forgot your Password?"} />
+      <Layout>
+        <Header page="sign-in" subPage="password-reset" />
+        <Layout.Content style={{ backgroundColor: "white" }}>
+          <div style={LOGIN_STYLE}>
+            <Alert
+              style={{ marginTop: "20px" }}
+              message={<b>Success</b>}
+              description={
+                <div style={{ fontSize: "12pt" }}>
+                  Password successfully set. You are now signed in using your
+                  new password.
+                </div>
+              }
+              type="success"
+              showIcon
+            />
+          </div>
+          <Footer />
+        </Layout.Content>
+      </Layout>
+    </Customize>
+  );
+}
+
+export async function getServerSideProps(context) {
+  return await withCustomize({ context });
+}
