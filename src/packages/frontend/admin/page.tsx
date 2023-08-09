31304888e8d8fbdaa90af458f910298a135617c8
William Stein <wstein@sagemath.com>
2023-08-09 16:29:45 +0000

    add icons to admin panels

--- src/packages/frontend/admin/page.tsx
+++ src/packages/frontend/admin/page.tsx
@@ -12,6 +12,7 @@ import { SiteLicenses } from "../site-licenses/admin/component";
 import { UsageStatistics } from "./stats/page";
 import { SystemNotifications } from "./system-notifications";
 import { UserSearch } from "./users/user-search";
+import { Icon } from "@cocalc/frontend/components";
 
 const { Panel } = Collapse;
 
@@ -34,26 +35,59 @@ export function AdminPage() {
           setActiveKey(activeKey as string[]);
         }}
       >
-        <Panel key="user-search" header="User Search">
+        <Panel
+          key="user-search"
+          header=<>
+            <Icon name="users" style={{ marginRight: "8px" }} /> User Search
+          </>
+        >
           <UserSearch />
         </Panel>
-        <Panel key="site-licenses" header="Site Licenses">
+        <Panel
+          key="site-licenses"
+          header=<>
+            <Icon name="key" style={{ marginRight: "8px" }} /> Licenses
+          </>
+        >
           <SiteLicenses />
         </Panel>
-        <Panel key="site-settings" header="Site Settings">
+        <Panel
+          key="site-settings"
+          header=<>
+            <Icon name="gears" style={{ marginRight: "8px" }} /> Site Settings
+          </>
+        >
           <SiteSettings
             close={() => {
               setActiveKey(activeKey.filter((key) => key != "site-settings"));
             }}
           />
         </Panel>
-        <Panel key="registration-tokens" header="Registration Tokens">
+        <Panel
+          key="registration-tokens"
+          header=<>
+            <Icon name="sign-in" style={{ marginRight: "8px" }} /> Registration
+            Tokens
+          </>
+        >
           <RegistrationToken />
         </Panel>
-        <Panel key="system-notifications" header="System Notifications">
+        <Panel
+          key="system-notifications"
+          header=<>
+            <Icon name="comment" style={{ marginRight: "8px" }} /> System
+            Notifications
+          </>
+        >
           <SystemNotifications />
         </Panel>
-        <Panel key="usage-stats" header="Usage Statistics">
+        <Panel
+          key="usage-stats"
+          header=<>
+            <Icon name="line-chart" style={{ marginRight: "8px" }} /> Usage
+            Statistics
+          </>
+        >
           <UsageStatistics />
         </Panel>
       </Collapse>
