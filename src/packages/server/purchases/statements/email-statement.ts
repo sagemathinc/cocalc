0a6f3cc85b1a0dfaf4ab5c580ad24342d2a5cf5d
William Stein <wstein@sagemath.com>
2023-08-12 00:47:37 +0000

    fix confusing sign error in statement

--- src/packages/server/purchases/statements/email-statement.ts
+++ src/packages/server/purchases/statements/email-statement.ts
@@ -76,13 +76,11 @@ export default async function emailStatement(opts: {
         pay = `<b><a href="${payUrl}">Click here to pay ${toPay}</a>, since you do NOT have automatic payments setup and your statement balance is negative.  You can also sign in and add money on <a href="${await siteURL()}/settings/purchases">the purchases page</a>.</b>`;
       }
     } else {
-      pay = `You have automatic payments setup and your balance is currently ${toPay}. `;
+      pay = "You have automatic payments setup. ";
       if (statement.automatic_payment) {
         pay += ` ${siteName} initiated a payment of ${toPay}.`;
       } else {
-        pay += ` ${siteName} will soon initiate a payment of ${currency(
-          -statement.balance
-        )}.`;
+        pay += ` ${siteName} will soon initiate a payment of ${toPay}.`;
       }
     }
   }
