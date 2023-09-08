7eaa96ed6ddbe5851765b0f08224abfd8885e65e
Harald Schilly <harald.schilly@gmail.com>
2023-09-08 12:54:49 +0200

    frontend/flyouts: make flyouts the default for newly created accounts

--- src/packages/frontend/project/page/vbar.tsx
+++ src/packages/frontend/project/page/vbar.tsx
@@ -3,6 +3,9 @@
  *  License: AGPLv3 s.t. "Commons Clause" – see LICENSE.md for details
  */
 
+import { AccountStore } from "@cocalc/frontend/account";
+import { redux } from "@cocalc/frontend/app-framework";
+
 // in the other_settings map
 export const VBAR_KEY = "vertical_fixed_bar";
 
@@ -24,12 +27,26 @@ export const VBAR_OPTIONS = {
   full: "Buttons show full pages",
 } as const;
 
-export const VBAR_OPTIONS_DEFAULT = "both";
+// New users created after this date will have the default VBAR option set to "flyout"
+function getDefaultVBAROption() {
+  const store: AccountStore = redux.getStore("account");
+  if (store == null) return "both";
+  const created = store.get("created");
+  // check that cretaed is a Date
+  if (created == null || !(created instanceof Date)) return "both";
+  // if created is after 2023-09-10 return "flyout", else "both"
+  if (created > new Date("2023-09-10")) {
+    return "flyout";
+  } else {
+    return "both";
+  }
+}
 
 export function getValidVBAROption(
   vbar_setting: any
 ): keyof typeof VBAR_OPTIONS {
-  if (typeof vbar_setting !== "string") return VBAR_OPTIONS_DEFAULT;
-  if (VBAR_OPTIONS[vbar_setting] == null) return VBAR_OPTIONS_DEFAULT;
+  if (typeof vbar_setting !== "string" || VBAR_OPTIONS[vbar_setting] == null) {
+    return getDefaultVBAROption();
+  }
   return vbar_setting as keyof typeof VBAR_OPTIONS;
 }
