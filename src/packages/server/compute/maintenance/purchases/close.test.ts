307f1f148a3976483ee350974520a1c855959925
William Stein <wstein@sagemath.com>
2023-11-02 20:18:52 +0000

    compute: get typescript and testing passing again for now

--- src/packages/server/compute/maintenance/purchases/close.test.ts
+++ src/packages/server/compute/maintenance/purchases/close.test.ts
@@ -5,9 +5,9 @@ Test functions for closing purchases in various ways.
 import {
   closePurchase,
   closeAndContinuePurchase,
-  closeAndContinueNetworkPurchase,
+  // closeAndContinueNetworkPurchase,
 } from "./close";
-import getPool, { initEphemeralDatabase } from "@cocalc/database/pool";
+import { initEphemeralDatabase } from "@cocalc/database/pool";
 import { uuid } from "@cocalc/util/misc";
 import createAccount from "@cocalc/server/accounts/create-account";
 import createProject from "@cocalc/server/projects/create";
@@ -83,6 +83,9 @@ describe("creates account, project, test compute server, and purchase, then clos
       Math.abs(Date.now() - (purchaseAfter.period_end?.valueOf() ?? 0)),
     ).toBeLessThan(30 * 1000);
     // 0.05 is about half the 0.1 above, since time is 30 minutes
+    if (purchaseAfter.cost == null) {
+      throw Error("fail");
+    }
     expect(Math.abs(purchaseAfter.cost - 0.05)).toBeLessThan(0.01);
   });
 
@@ -110,12 +113,18 @@ describe("creates account, project, test compute server, and purchase, then clos
     const purchase = await getPurchase(purchase_id);
     const newPurchaseId = await closeAndContinuePurchase({ purchase, server });
     const purchaseAfter = await getPurchase(purchase_id);
+    if (purchaseAfter.period_end == null) {
+      throw Error("fail");
+    }
     expect(
-      Math.abs(Date.now() - purchaseAfter.period_end?.valueOf() ?? 0),
+      Math.abs(Date.now() - purchaseAfter.period_end.valueOf() ?? 0),
     ).toBeLessThan(30 * 1000);
     const newPurchase = await getPurchase(newPurchaseId);
+    if (newPurchase.period_start == null) {
+      throw Error("fail");
+    }
     expect(
-      Math.abs(Date.now() - newPurchase.period_start?.valueOf() ?? 0),
+      Math.abs(Date.now() - newPurchase.period_start.valueOf() ?? 0),
     ).toBeLessThan(30 * 1000);
   });
 });
