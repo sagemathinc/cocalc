3c8a5e4bc459a55a1ec496abae7422b677e9a71d
William Stein <wstein@sagemath.com>
2023-11-18 04:11:32 +0000

    compute: make server banner a little better

--- src/packages/frontend/compute/doc-status.tsx
+++ src/packages/frontend/compute/doc-status.tsx
@@ -44,8 +44,18 @@ export default function ComputeServerTransition({
     return null;
   }
 
+  const requestedServer = computeServers.get(`${requestedId}`);
+
   const topBar = (progress) => (
-    <div style={{ display: "flex" }}>
+    <div
+      style={{
+        display: "flex",
+        borderBottom: !showDetails
+          ? `1px solid ${requestedServer.get("color")}`
+          : undefined,
+        height: "19px",
+      }}
+    >
       {progress == 100 && (
         <SyncButton
           style={{ marginTop: "-3px" }}
@@ -53,6 +63,14 @@ export default function ComputeServerTransition({
           type="text"
           compute_server_id={id}
           project_id={project_id}
+          syncing={
+            requestedServer?.get("state") == "running" &&
+            (requestedServer?.getIn([
+              "detailed_state",
+              "filesystem-sync",
+              "progress",
+            ]) ?? 100) < 100
+          }
         >
           Sync Files
         </SyncButton>
@@ -62,7 +80,7 @@ export default function ComputeServerTransition({
         title={
           <>
             {progress == 100 ? "Running on " : "Moving to "}{" "}
-            <Inline id={requestedId} />.
+            <Inline id={requestedId} computeServer={requestedServer} />.
           </>
         }
       >
@@ -75,6 +93,7 @@ export default function ComputeServerTransition({
         >
           <div style={{ marginRight: "5px", flex: 1 }}>
             <Inline
+              computeServer={requestedServer}
               colorOnly
               id={requestedId}
               style={{
