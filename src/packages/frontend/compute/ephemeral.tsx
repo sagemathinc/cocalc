007adaf18ebddec5325dcb25926b191d8b5dd6ea
William Stein <wstein@sagemath.com>
2023-12-15 06:32:16 +0000

    change ephemeral compute server description and label

--- src/packages/frontend/compute/ephemeral.tsx
+++ src/packages/frontend/compute/ephemeral.tsx
@@ -31,8 +31,8 @@ export default function Ephemeral({ setConfig, configuration, loading }) {
               <div>
                 <p>
                   Make your compute server "ephemeral" if you are only using the
-                  compute server for <i>temporary compute</i>, and want{" "}
-                  <i>maximum flexibility and minimal cost</i>.
+                  compute server for <i>temporary compute</i>, and do not want
+                  to store data on this compute server between sessions.
                 </p>
                 <p>
                   Do you plan to store data on this compute server that you
@@ -40,9 +40,9 @@ export default function Ephemeral({ setConfig, configuration, loading }) {
                   hidden folders and directories to exclude from sync (listed
                   above). Other files, e.g., in /tmp and systemwide changes,
                   exist only on the compute server's disk. If you select
-                  "ephemeral" below, the user interface makes it very easy to
-                  delete the compute server's temporary disk when you are done
-                  using it. This setting only modifies the user interface.
+                  "ephemeral" below, the disk of this server will be deleted by
+                  default when you turn it off, so that you are not charged for
+                  storage space.
                 </p>
                 <p>
                   <b>WARNING:</b> Files that are only on compute servers are NOT
@@ -63,7 +63,8 @@ export default function Ephemeral({ setConfig, configuration, loading }) {
             setEphemeral(!ephemeral);
           }}
         >
-          Ephemeral: I do not care about data on this compute server
+          Ephemeral: I do not want to store data on this compute server between
+          sessions
         </Checkbox>
       </div>
     </div>
