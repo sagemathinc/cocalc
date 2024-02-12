292d20ea2c1025b708ab2fae54c97c61176bd203
schrodingersket <schrodingersket@gmail.com>
2024-02-12 11:31:45 -0700

    Removed extraneous `jest.config.js` file.

--- src/packages/backend/jest.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  setupFiles: [], // Path to your setup file
-  testMatch: ['**/?(*.)+(spec|test).ts?(x)'],
-};
