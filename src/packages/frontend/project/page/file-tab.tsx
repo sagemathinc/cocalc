63a0bb35f017cd6d6db1ef639fffdb2548228592
William Stein <wstein@sagemath.com>
2024-02-19 22:53:25 +0000

    fix #7287 -- "Active" --> "Tabs"

--- src/packages/frontend/project/page/file-tab.tsx
+++ src/packages/frontend/project/page/file-tab.tsx
@@ -87,8 +87,8 @@ type FixedTabs = {
 
 export const FIXED_PROJECT_TABS: FixedTabs = {
   active: {
-    label: "Active",
-    flyoutTitle: "Active Files",
+    label: "Tabs",
+    flyoutTitle: "File Tabs",
     icon: "edit",
     flyout: ActiveFlyout,
     noAnonymous: false,
