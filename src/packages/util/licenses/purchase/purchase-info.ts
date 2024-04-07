d643a4653ba6dec3393506494b4a3b6b3bb84b13
William Stein <wstein@sagemath.com>
2024-04-07 16:47:35 +0000

    this is for some one off thing

--- src/packages/util/licenses/purchase/purchase-info.ts
+++ src/packages/util/licenses/purchase/purchase-info.ts
@@ -8,6 +8,7 @@ import dayjs from "dayjs";
 
 export default function getPurchaseInfo(
   conf: SiteLicenseDescriptionDB,
+  old = false,
 ): PurchaseInfo {
   conf.type = conf.type ?? "quota"; // backwards compatibility
 
@@ -33,7 +34,7 @@ export default function getPurchaseInfo(
         upgrade: "custom" as "custom",
         quantity: run_limit,
         subscription: (period == "range" ? "no" : period) as Subscription,
-        ...fixRange(conf.range, conf.period),
+        ...fixRange(conf.range, conf.period, old),
         custom_ram: ram,
         custom_dedicated_ram: 0,
         custom_cpu: cpu,
@@ -52,7 +53,7 @@ export default function getPurchaseInfo(
         quantity: 1,
         dedicated_vm: conf.dedicated_vm,
         subscription: "no",
-        ...fixRange(conf.range, conf.period),
+        ...fixRange(conf.range, conf.period, old),
         title,
         description,
       };
@@ -65,7 +66,7 @@ export default function getPurchaseInfo(
         subscription: conf.period,
         title,
         description,
-        ...fixRange(null, conf.period),
+        ...fixRange(null, conf.period, old),
       };
   }
 }
@@ -79,8 +80,9 @@ export default function getPurchaseInfo(
 export function fixRange(
   rangeOrig: readonly [Date0 | string, Date0 | string] | undefined | null,
   period: Period,
+  old = false,
 ): StartEndDates {
-  if (period != "range") {
+  if (!old && period != "range") {
     // ATTN! -- we messed up and didn't deal with this case before, and a user
     // could in theory:
     //  1. set the period to 'range', and put in a week period via start and end
@@ -117,4 +119,45 @@ function addPeriod(date: Date, period: Period): Date {
   } else {
     throw Error(`unsupported period ${period}`);
   }
+}
+
+const active = new Set([
+  "4bc5c13e-5677-4705-b492-e23511d2e911",
+  "61bc0c56-de6e-4a35-9983-639c2a5b1af9",
+  "a747301a-987c-4be7-b4f6-3390358fcd11",
+  "ea7eb026-9091-48b9-9a55-6859ca5a4dba",
+  "22d2edba-564f-446e-8975-2dac39ed0556",
+  "2c0b6c43-cc0b-475f-af02-3784968219e8",
+  "3c61c11c-7ae8-413c-bba0-5f2b3f15c8b8",
+  "26580598-f267-4cec-8d0d-72429fb4c74c",
+  "010d74be-0837-4bdb-830d-5aaaaf22d7bd",
+  "3cb74317-083e-4a85-ae1a-e7cb82b3820b",
+  "42b36bc3-3ef5-4c7a-aeb2-39566f838734",
+  "d1f8ed91-ba06-4de4-a096-901779eba1a6",
+]);
+
+export function report() {
+  const data = JSON.parse(require("fs").readFileSync("/tmp/data.json"));
+  const { compute_cost } = require("./compute-cost");
+  const v: any[] = [];
+  for (const x of data) {
+    if (new Date(x.purchased) <= new Date("2024-01-15")) continue;
+    if (!active.has(x.license_id)) continue;
+    const correctInfo = getPurchaseInfo(x.description, false);
+    const correctCost =
+      x.description.run_limit * compute_cost(correctInfo).cost_per_unit;
+    const actualInfo = getPurchaseInfo(x.description, true);
+    const actualCost =
+      x.description.run_limit * compute_cost(actualInfo).cost_per_unit;
+    if (Math.abs(actualCost - correctCost) < 10) {
+      continue;
+    }
+    v.push({
+      ...x,
+      actual: actualCost,
+      correct: correctCost,
+      sub: `select '${x.license_id}' from subscriptions where metadata#>>'{license_id}'='${x.license_id}' and status='active';`,
+    });
+  }
+  return v;
 }
