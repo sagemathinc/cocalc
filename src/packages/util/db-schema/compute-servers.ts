f75e5b8adba602928a8130a69b569af9d2c3b2de
William Stein <wstein@sagemath.com>
2024-04-09 18:10:24 +0000

    workaround chrome bug involving antd checkboxes (crashing browser)

--- src/packages/util/db-schema/compute-servers.ts
+++ src/packages/util/db-schema/compute-servers.ts
@@ -163,7 +163,7 @@ export const ACTION_INFO: {
     description:
       "Deprovisioning DELETES THE VIRTUAL MACHINE BOOT DISK, but keeps the compute server parameters.   There are no costs associated with a deprovisioned compute server, and you can move it to a different region or zone.  Any files in the home directory of your project are not affected.",
     confirm: true,
-    confirmMessage: "I understand that my compute server disk will be deleted.",
+    confirmMessage: "I understand that my compute server disks will be deleted.",
     danger: true,
     target: "deprovisioned",
   },
