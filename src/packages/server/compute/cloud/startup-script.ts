d7d1bd8654ac65f46a2deec091e38e86c074b23f
William Stein <wstein@sagemath.com>
2024-04-13 17:42:19 +0000

    compute-server: only run startup-script and configure crontab if not already done

--- src/packages/server/compute/cloud/startup-script.ts
+++ src/packages/server/compute/cloud/startup-script.ts
@@ -39,8 +39,14 @@ write_files:
 runcmd:
   - |
     #!/bin/bash
-    (crontab -l 2>/dev/null; echo "@reboot /root/cocalc-startup.sh") | crontab -
-    /root/cocalc-startup.sh
+    set -v
+    crontab -l | grep "@reboot /root/cocalc-startup.sh"
+    if [ $? -ne 0 ]; then
+      # first boot ever, and crontab not setup, so we we add /root/cocalc-start.sh to
+      # crontab and run it. Otherwise, it will already be run.
+      (crontab -l 2>/dev/null; echo "@reboot /root/cocalc-startup.sh") | crontab -
+      /root/cocalc-startup.sh
+    fi
 `;
 }
 
