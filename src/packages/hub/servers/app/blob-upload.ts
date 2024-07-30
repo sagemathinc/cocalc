493390d895f291637e0fa90c8c0c5774050aaf23
William Stein <wstein@sagemath.com>
2024-07-30 03:51:36 +0000

    starting work on file upload directly to hub (no project involved) for use in doc editing

--- src/packages/hub/servers/app/blob-upload.ts
+++ src/packages/hub/servers/app/blob-upload.ts
@@ -1,3 +1,20 @@
+/*
+Support user uploading a blob directly from their browser to the CoCalc database,
+mainly for markdown documents.  This is meant to be very similar to how GitHub
+allows for attaching files to github issue comments.
+
+
+*/
+
+// Note that github has a 10MB limit -- https://docs.github.com/en/get-started/writing-on-github/working-with-advanced-formatting/attaching-files
+const MAX_BLOB_SIZE_MB = 10;
+
+// some throttling -- note that after a bit, most blobs end up longterm
+// cloud storage and are never accessed.
+// this is just a limit to prevent abuse, and it's done on a per-hub
+// basis using an in-memory data structure.
+const MAX_BLOBS_SIZE_MB_PER_USER_PER_DAY = 250;
+
 import { Router } from "express";
 import { database } from "../database";
 import { is_valid_uuid_string } from "@cocalc/util/misc";
@@ -8,7 +25,7 @@ import { getLogger } from "@cocalc/hub/logger";
 const logger = getLogger("hub:servers:app:blobs");
 export default function init(router: Router) {
   // return uuid-indexed blobs (mainly used for graphics)
-  router.get("/blobs/*", async (req, res) => {
+  router.post("/blobs", async (req, res) => {
     logger.debug(`${JSON.stringify(req.query)}, ${req.path}`);
     const uuid = `${req.query.uuid}`;
     if (!is_valid_uuid_string(uuid)) {
