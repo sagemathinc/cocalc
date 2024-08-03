aed9474d6f9a0ea95bf8b43f6422f3b9e5c9842b
William Stein <wstein@sagemath.com>
2024-08-03 22:50:17 +0000

    timetravel modernization -- sage worksheets

--- src/packages/frontend/frame-editors/time-travel-editor/open-snapshots.tsx
+++ src/packages/frontend/frame-editors/time-travel-editor/open-snapshots.tsx
@@ -11,29 +11,21 @@
 */
 
 import { Button } from "antd";
-import { Rendered, Component } from "@cocalc/frontend/app-framework";
-import { TimeTravelActions } from "./actions";
 import { Icon } from "@cocalc/frontend/components";
 import track from "@cocalc/frontend/user-tracking";
 
-interface Props {
-  actions: TimeTravelActions;
-}
-
-export class OpenSnapshots extends Component<Props> {
-  public render(): Rendered {
-    return (
-      <Button
-        onClick={() => {
-          this.props.actions.open_snapshots();
-          track("snapshots", { action: "open", where: "time-travel" });
-        }}
-        title={
-          "Open the file system snapshots of this project, which may also be helpful in recovering past versions."
-        }
-      >
-        <Icon name={"life-ring"} /> Backups
-      </Button>
-    );
-  }
+export function OpenSnapshots({ actions }) {
+  return (
+    <Button
+      onClick={() => {
+        actions.open_snapshots();
+        track("snapshots", { action: "open", where: "time-travel" });
+      }}
+      title={
+        "Open the file system snapshots of this project, which may also be helpful in recovering past versions."
+      }
+    >
+      <Icon name={"life-ring"} /> Backups
+    </Button>
+  );
 }
--- src/packages/frontend/frame-editors/time-travel-editor/sagews-codemirror.tsx
+++ src/packages/frontend/frame-editors/time-travel-editor/sagews-codemirror.tsx
@@ -10,11 +10,11 @@ code to get this done for now.
 
 import { debounce } from "lodash";
 import * as CodeMirror from "codemirror";
-import { Component, React, Rendered } from "../../app-framework";
 import { Map } from "immutable";
-
+import { useEffect, useRef, MutableRefObject } from "react";
 const { codemirror_editor } = require("../../editor");
 const { SynchronizedWorksheet } = require("../../sagews/sagews");
+import { useFrameContext } from "@cocalc/frontend/frame-editors/frame-tree/frame-context";
 
 interface Props {
   content: string;
@@ -24,72 +24,66 @@ interface Props {
   editor_settings: Map<string, any>;
 }
 
-export class SagewsCodemirror extends Component<Props> {
-  private update: Function;
-  private view_doc: any;
-  private worksheet: any;
-  private cm: CodeMirror.Editor;
-  private div_ref: React.RefObject<HTMLDivElement> = React.createRef<
-    HTMLDivElement
-  >();
+export function SagewsCodemirror(props: Props) {
+  const { isVisible } = useFrameContext();
+  const updateRef = useRef<Function>(null) as MutableRefObject<Function>;
+  const cmRef = useRef<CodeMirror.Editor | null>(
+    null,
+  ) as MutableRefObject<CodeMirror.Editor | null>;
+  const viewDocRef = useRef<any>(null);
+  const divRef = useRef<any>(null);
 
-  private init_sagews(): void {
-    const div = this.div_ref.current;
-    if (div == null) return; // can't happen
+  const initSagews = (): void => {
+    const div = divRef.current;
+    if (div == null) {
+      // this better not happen
+      return;
+    }
 
     const opts = { mode: "sagews", read_only: true };
-    this.view_doc = codemirror_editor(
-      this.props.project_id,
-      this.props.path,
-      opts
-    );
-    this.cm = this.view_doc.codemirror;
+    viewDocRef.current = codemirror_editor(props.project_id, props.path, opts);
+    cmRef.current = viewDocRef.current.codemirror;
     // insert it into the dom.
-    $(this.view_doc.element).appendTo($(div));
+    $(viewDocRef.current.element).appendTo($(div));
     // remove the second codemirror editor
-    $(this.view_doc.codemirror1.getWrapperElement()).remove();
+    $(viewDocRef.current.codemirror1.getWrapperElement()).remove();
 
     const opts0 = {
       allow_javascript_eval: false,
       static_viewer: true,
     };
-    this.worksheet = new SynchronizedWorksheet(this.view_doc, opts0);
+    const worksheet = new SynchronizedWorksheet(viewDocRef.current, opts0);
 
     const f = (content: string): void => {
-      if (this.view_doc == null) return;
-      this.cm.setValueNoJump(content);
-      this.worksheet.process_sage_updates();
+      if (viewDocRef.current == null) {
+        return;
+      }
+      cmRef.current?.setValueNoJump(content);
+      worksheet.process_sage_updates();
     };
-    f(this.props.content);
-    this.update = debounce(f, 100);
-  }
+    f(props.content);
+    updateRef.current = debounce(f, 100);
+  };
 
-  public componentDidMount(): void {
-    this.init_sagews();
-  }
+  useEffect(() => {
+    initSagews();
+    return () => {
+      if (viewDocRef.current == null) {
+        return;
+      }
+      viewDocRef.current.remove();
+    };
+  }, []);
 
-  public componentWillUnmount(): void {
-    if (this.view_doc == null) return;
-    this.view_doc.remove();
-    delete this.view_doc;
-    delete this.worksheet;
-  }
+  useEffect(() => {
+    updateRef.current?.(props.content);
+    viewDocRef.current?.set_font_size(cmRef.current, props.font_size);
+    cmRef.current?.refresh();
+  }, [props.font_size, props.content, isVisible]);
 
-  public UNSAFE_componentWillReceiveProps(props): void {
-    if (props.content != this.props.content) {
-      this.update(props.content);
-    }
-    if (props.font_size != this.props.font_size) {
-      this.view_doc.set_font_size(this.cm, props.font_size);
-    }
-    this.cm.refresh();
-  }
-
-  public render(): Rendered {
-    return (
-      <div className="smc-vfill" style={{ overflow: "auto" }}>
-        <div ref={this.div_ref} />
-      </div>
-    );
-  }
+  return (
+    <div className="smc-vfill" style={{ overflow: "auto" }}>
+      <div ref={divRef} />
+    </div>
+  );
 }
