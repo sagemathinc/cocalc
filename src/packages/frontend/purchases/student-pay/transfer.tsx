a9979aa0530e2e22397653ed28d45b7b8c6bbb33
William Stein <wstein@sagemath.com>
2024-09-13 19:10:47 +0000

    course license transfer: implement getting the allowed licenses

--- src/packages/frontend/purchases/student-pay/transfer.tsx
+++ src/packages/frontend/purchases/student-pay/transfer.tsx
@@ -1,7 +1,27 @@
-import { Button, Spin } from "antd";
+/*
+If the user is NOT a student in another course that they *have* paid the license for, then
+this component shows nothing.
+
+If the user is a student in at least one other course that they have paid for the license 
+for, then a "Transfer License..." button shows up.  When clicked, it shows a description
+and buttons for each of the other courses that qualify.  Clicking on the button uses
+a backend api call to actually transfer the license, which makes the current course be
+paid for, but the other one is no longer paid for.
+
+The motivation for this is that students switch sections in large classes, and this
+minimizes friction.  Also, they pay for a license for the whole course, and getting a
+prorated refund and making a new purchase isn't fair.
+
+NOTE: the api call just allows for transfering and doesn't do much of a check, at least for
+the first release. That's another way a malicious user could cheat.
+*/
+
+import { Button, Card, Spin } from "antd";
 import { Icon } from "@cocalc/frontend/components";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import ShowError from "@cocalc/frontend/components/error";
+import { redux } from "@cocalc/frontend/app-framework";
+import { getCost } from "./cost";
 
 interface Props {
   project_id: string;
@@ -9,15 +29,98 @@ interface Props {
 
 export default function Transfer({ project_id }: Props) {
   const [loading, setLoading] = useState<boolean>(false);
+  const [open, setOpen] = useState<boolean>(false);
   const [error, setError] = useState<string>("");
+  const available = useMemo(() => {
+    return getAvailable(project_id);
+  }, []);
+
+  if (available.length == 0) {
+    return null;
+  }
 
   return (
     <div>
-      <Button disabled={loading}>
+      <Button disabled={loading} onClick={() => setOpen(true)}>
         <Icon name="sync" /> Transfer License...
         {loading && <Spin />}
       </Button>
+      {open && (
+        <Card
+          title={
+            <>
+              Transfer License
+              <Button style={{ float: "right" }} onClick={() => setOpen(false)}>
+                Close
+              </Button>
+            </>
+          }
+          style={{ maxWidth: "650px", margin: "30px auto" }}
+        >
+          {JSON.stringify(available)}
+        </Card>
+      )}
       <ShowError error={error} setError={setError} />
     </div>
   );
+}
+
+// returns project_id's of projects that are paid for and you can transfer the license
+function getAvailable(project_id): string[] {
+  // projects = data about all projects you collaborate on (and used recently -- this might leave off old projects, but that
+  // should be fine for the purposes of transfering a license)
+  const projects = redux.getStore("projects").get("project_map")?.toJS();
+  if (projects == null) {
+    return [];
+  }
+  // each project that is part of a course has a "course" field:
+  //   pay = when it must be paid by
+  //   paid = iso string of when they paid, if they actually paid
+  //   payInfo = info about what they have to pay for, including start and end dates (as iso strings)
+  //   cost = {cost:number, ...}
+  // We allow transferring if:
+  //   (1) start and end dates are within 7 days
+  //   (2) cost is within $5.
+  // At the end of the day we're just transferring a license and not actually giving away or taking anything, so there
+  // is no danger of some really clever attacker creating fake classes and stealing money.  The worst case is just that
+  // the student "cheats themselves" out of what the instructor recommends that they purchase for the best experience.
+
+  const { course: targetCourse } = projects[project_id] ?? {};
+  const targetStart = new Date(targetCourse.payInfo?.start ?? 0).valueOf();
+  const targetEnd = new Date(targetCourse.payInfo?.end ?? 0).valueOf();
+  const targetCost = getCost(targetCourse.payInfo);
+
+  const available: string[] = [];
+  if (targetCourse == null) {
+    // this project isn't even part of a course.
+    return available;
+  }
+  for (const id in projects) {
+    if (id == project_id) {
+      continue;
+    }
+    const { course } = projects[id] ?? {};
+    if (course == null) {
+      continue;
+    }
+    if (!course.paid) {
+      continue;
+    }
+    // do the checks:
+    const start = new Date(course.payInfo?.start ?? 0).valueOf();
+    const end = new Date(course.payInfo?.end ?? 0).valueOf();
+    const cost = getCost(course.payInfo);
+    if (Math.abs(cost - targetCost) > 5) {
+      continue;
+    }
+    if (Math.abs(start - targetStart) > 60 * 1000 * 24 * 7) {
+      continue;
+    }
+    if (Math.abs(end - targetEnd) > 60 * 1000 * 24 * 7) {
+      continue;
+    }
+    available.push(id);
+  }
+
+  return available;
 }
