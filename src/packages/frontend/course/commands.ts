e62b3ebe775fdc02e07c6673a32ecf5b3802ddf5
Harald Schilly <harald.schilly@gmail.com>
2024-10-24 12:23:56 +0200

    frontend/course: more translations

--- src/packages/frontend/course/commands.ts
+++ src/packages/frontend/course/commands.ts
@@ -6,7 +6,7 @@
 import { defineMessage } from "react-intl";
 
 import type { CourseEditorActions } from "@cocalc/frontend/frame-editors/course-editor/actions";
-import { course, IntlMessage } from "@cocalc/frontend/i18n";
+import { course, IntlMessage, labels } from "@cocalc/frontend/i18n";
 import { ENV_VARS_ICON } from "@cocalc/frontend/project/settings/environment";
 
 // ATTN: the COMMANDS configuration is not only used in the menu (using ManageCommands),
@@ -81,9 +81,17 @@ export const COMMANDS: { [name: string]: Command } = {
   },
   "title-and-description": {
     icon: "header",
-    label: "Course Title and Description",
-    button: "Title",
-    title: "Set the course title and description.",
+    label: defineMessage({
+      id: "course.commands.title-and-description.label",
+      defaultMessage: "Course Title and Description",
+      description: "title and description of a course for students.",
+    }),
+    button: labels.title,
+    title: defineMessage({
+      id: "course.commands.title-and-description.tooltip",
+      defaultMessage: "Set the course title and description.",
+      description: "title and description of a course for students.",
+    }),
     onClick: ({ props }) => {
       const { actions } = props;
       actions.setModal("title-and-description");
@@ -91,10 +99,16 @@ export const COMMANDS: { [name: string]: Command } = {
   },
   "email-invitation": {
     icon: "mail",
-    label: "Email Invitation",
-    button: "Invite",
-    title:
-      "If you add a student to this course using their email address, and they do not have a CoCalc account, then they will receive this email invitation.",
+    label: defineMessage({
+      id: "course.commands.email-invitation.label",
+      defaultMessage: "Email Invitation",
+    }),
+    button: labels.invite,
+    title: defineMessage({
+      id: "course.commands.email-invitation.tooltip",
+      defaultMessage:
+        "If you add a student to this course using their email address, and they do not have a CoCalc account, then they will receive this email invitation.",
+    }),
     onClick: ({ props }) => {
       const { actions } = props;
       actions.setModal("email-invitation");
@@ -102,9 +116,16 @@ export const COMMANDS: { [name: string]: Command } = {
   },
   "copy-limit": {
     icon: "users",
-    label: "Parallel Copy Limit",
-    button: "Limit",
-    title: "Max number of students to copy and collect files from in parallel.",
+    label: defineMessage({
+      id: "course.commands.copy-limit.label",
+      defaultMessage: "Parallel Copy Limit",
+    }),
+    button: labels.limit,
+    title: defineMessage({
+      id: "course.commands.copy-limit.tooltip",
+      defaultMessage:
+        "Max number of students to copy and collect files from in parallel.",
+    }),
     onClick: ({ props }) => {
       const { actions } = props;
       actions.setModal("copy-limit");
@@ -112,10 +133,20 @@ export const COMMANDS: { [name: string]: Command } = {
   },
   "collaborator-policy": {
     icon: "mail",
-    label: "Collaborator Policy",
-    button: "Collab",
-    title:
-      "Control if the owner and any collaborator on this student project may add collaborators to this project.",
+    label: defineMessage({
+      id: "course.commands.collaborator-policy.label",
+      defaultMessage: "Collaborator Policy",
+    }),
+    button: defineMessage({
+      id: "course.commands.collaborator-policy.button",
+      defaultMessage: "Collab",
+      description: "Short label on a button, abbrivation of 'Collaborators'",
+    }),
+    title: defineMessage({
+      id: "course.commands.collaborator-policy.tooltip",
+      defaultMessage:
+        "Control if the owner and any collaborator on this student project may add collaborators to this project.",
+    }),
     onClick: ({ props }) => {
       const { actions } = props;
       actions.setModal("collaborator-policy");
@@ -123,9 +154,15 @@ export const COMMANDS: { [name: string]: Command } = {
   },
   "restrict-student-projects": {
     icon: "lock",
-    label: "Restrict Student Projects",
-    button: "Restrict",
-    title: "Remove functionality from student projects",
+    label: defineMessage({
+      id: "course.commands.restrict-student-projects.label",
+      defaultMessage: "Restrict Student Projects",
+    }),
+    button: labels.restrict,
+    title: defineMessage({
+      id: "course.commands.restrict-student-projects.toolteip",
+      defaultMessage: "Remove functionality from student projects",
+    }),
     onClick: ({ props }) => {
       const { actions } = props;
       actions.setModal("restrict-student-projects");
@@ -133,9 +170,15 @@ export const COMMANDS: { [name: string]: Command } = {
   },
   nbgrader: {
     icon: "graduation-cap",
-    label: "Configure Nbgrader",
-    button: "Nbgrader",
-    title: "Configure how nbgrader works.",
+    label: defineMessage({
+      id: "course.commands.nbgrader.label",
+      defaultMessage: "Configure Nbgrader",
+    }),
+    button: labels.nbgrader,
+    title: defineMessage({
+      id: "course.commands.nbgrader.tooltip",
+      defaultMessage: "Configure how nbgrader works.",
+    }),
     onClick: ({ props }) => {
       const { actions } = props;
       actions.setModal("nbgrader");
@@ -143,10 +186,13 @@ export const COMMANDS: { [name: string]: Command } = {
   },
   "software-environment": {
     icon: "laptop",
-    label: "Software Environment",
-    button: "Software",
-    title:
-      "Configure the software environment that all student projects will use.",
+    label: labels.software_environment,
+    button: labels.software,
+    title: defineMessage({
+      id: "course.commands.software-environment.tooltip",
+      defaultMessage:
+        "Configure the software environment that all student projects will use.",
+    }),
     onClick: ({ props }) => {
       const { actions } = props;
       actions.setModal("software-environment");
@@ -154,10 +200,13 @@ export const COMMANDS: { [name: string]: Command } = {
   },
   "network-file-systems": {
     icon: "database",
-    label: "Cloud Storage & Remote File Systems",
+    label: labels.cloud_storage_remote_filesystems,
     button: "Nbgrader",
-    title:
-      "Give all student projects read-only access to the same cloud stores and remote file systems as this instructor project.",
+    title: defineMessage({
+      id: "course.commands.network-file-systems.tooltip",
+      defaultMessage:
+        "Give all student projects read-only access to the same cloud stores and remote file systems as this instructor project.",
+    }),
     onClick: ({ props }) => {
       const { actions } = props;
       actions.setModal("network-file-systems");
@@ -165,10 +214,16 @@ export const COMMANDS: { [name: string]: Command } = {
   },
   "env-variables": {
     icon: ENV_VARS_ICON,
-    label: "Configure Environment Variables",
-    button: "Environment",
-    title:
-      "Configure whether or not student projects inherit the environment variables of this instructor project.",
+    label: defineMessage({
+      id: "course.commands.env-variables.label",
+      defaultMessage: "Configure Environment Variables",
+    }),
+    button: labels.environment,
+    title: defineMessage({
+      id: "course.commands.env-variables.tooltip",
+      defaultMessage:
+        "Configure whether or not student projects inherit the environment variables of this instructor project.",
+    }),
     onClick: ({ props }) => {
       const { actions } = props;
       actions.setModal("env-variables");
@@ -176,9 +231,15 @@ export const COMMANDS: { [name: string]: Command } = {
   },
   "configuration-copying": {
     icon: "copy",
-    label: "Copy Course Configuration",
-    button: "Config",
-    title: "Copy configuration from this course to other courses.",
+    label: defineMessage({
+      id: "course.commands.configuration-copying.label",
+      defaultMessage: "Copy Course Configuration",
+    }),
+    button: labels.config,
+    title: defineMessage({
+      id: "course.commands.configuration-copying.tooltip",
+      defaultMessage: "Copy configuration from this course to other courses.",
+    }),
     onClick: ({ props }) => {
       const { actions } = props;
       actions.setModal("configuration-copying");
@@ -186,22 +247,33 @@ export const COMMANDS: { [name: string]: Command } = {
   },
   upgrades: {
     icon: "gears",
-    label: "Configure Upgrades (Student or Instructor Pay)",
-    button: "Upgrades",
-    title:
-      "Use a license to upgrade all projects, or require your students to purchase a specific license.",
+    label: defineMessage({
+      id: "course.commands.upgrades.label",
+      defaultMessage: "Configure Upgrades (Student or Instructor Pay)",
+    }),
+    button: labels.upgrades,
+    title: defineMessage({
+      id: "course.commands.upgrades.tooltip",
+      defaultMessage:
+        "Use a license to upgrade all projects, or require your students to purchase a specific license.",
+    }),
     onClick: ({ props }) => {
       const { actions } = props;
       actions.setModal("upgrades");
     },
   },
-
   "start-all-projects": {
     icon: "bolt",
-    label: "Start or Stop all Student Projects",
-    button: "Start All",
-    title:
-      "You can start all projects associated with this course so they are immediately ready for your students to use.",
+    label: defineMessage({
+      id: "course.commands.start-all-projects.label",
+      defaultMessage: "Start or Stop all Student Projects",
+    }),
+    button: labels.start_all,
+    title: defineMessage({
+      id: "course.commands.start-all-projects.tooltip",
+      defaultMessage:
+        "You can start all projects associated with this course so they are immediately ready for your students to use.",
+    }),
     onClick: ({ props }) => {
       const { actions } = props;
       actions.setModal("start-all-projects");
@@ -209,10 +281,16 @@ export const COMMANDS: { [name: string]: Command } = {
   },
   "terminal-command": {
     icon: "terminal",
-    label: "Run Terminal Command in all Student Projects",
-    button: "Terminal",
-    title:
-      "Run a bash terminal command in the home directory of all student projects. Up to 30 commands run in parallel, with a timeout of 1 minutes.",
+    label: defineMessage({
+      id: "course.commands.terminal-command.label",
+      defaultMessage: "Run Terminal Command in all Student Projects",
+    }),
+    button: labels.terminal,
+    title: defineMessage({
+      id: "course.commands.terminal-command.tooltip",
+      defaultMessage:
+        "Run a bash terminal command in the home directory of all student projects. Up to 30 commands run in parallel, with a timeout of 1 minutes.",
+    }),
     onClick: ({ props }) => {
       const { actions } = props;
       actions.setModal("terminal-command");
@@ -220,9 +298,13 @@ export const COMMANDS: { [name: string]: Command } = {
   },
   "reconfigure-all-projects": {
     icon: "mail",
-    label: "Reconfigure all Projects",
-    button: "Reconfigure",
-    title: "Update all projects with correct students, descriptions, etc.",
+    label: course.reconfigure_all_projects,
+    button: labels.reconfigure,
+    title: defineMessage({
+      id: "course.commands.reconfigure-all-projects.tooltip",
+      defaultMessage:
+        "Update all projects with correct students, descriptions, etc.",
+    }),
     onClick: ({ props }) => {
       const { actions } = props;
       actions.setModal("reconfigure-all-projects");
@@ -230,10 +312,13 @@ export const COMMANDS: { [name: string]: Command } = {
   },
   "export-grades": {
     icon: "table",
-    label: "Export Grades",
-    button: "Grades",
-    title:
-      "Export all the grades you have recorded for students in your course to a csv or Python file.",
+    label: course.export_grades,
+    button: course.grades,
+    title: defineMessage({
+      id: "course.commands.export-grades.tooltip",
+      defaultMessage:
+        "Export all the grades you have recorded for students in your course to a csv or Python file.",
+    }),
     onClick: ({ props }) => {
       const { actions } = props;
       actions.setModal("export-grades");
@@ -241,10 +326,13 @@ export const COMMANDS: { [name: string]: Command } = {
   },
   "resend-invites": {
     icon: "mail",
-    label: "Resend Outstanding Invites",
-    button: "Invites",
-    title:
-      "Send another email to every student who didn't sign up yet. This sends a maximum of one email every 1 day.",
+    label: course.resend_invites,
+    button: labels.invites,
+    title: defineMessage({
+      id: "course.commands.resend-invites.tooltip",
+      defaultMessage:
+        "Send another email to every student who didn't sign up yet. This sends a maximum of one email every 1 day.",
+    }),
     onClick: ({ props }) => {
       const { actions } = props;
       actions.setModal("resend-invites");
@@ -252,10 +340,16 @@ export const COMMANDS: { [name: string]: Command } = {
   },
   "copy-missing-handouts-and-assignments": {
     icon: "graph",
-    label: "Copy Missing Handouts and Assignments",
-    button: "Copy Missing",
-    title:
-      "If you add new students to your course, you can ensure they have all the assignments and handouts that you have already assigned to other students in the course.",
+    label: course.copy_missing_handouts_assignments,
+    button: defineMessage({
+      id: "course.commands.copy-missing-handouts-and-assignments.button",
+      defaultMessage: "Copy Missing",
+    }),
+    title: defineMessage({
+      id: "course.commands.copy-missing-handouts-and-assignments.tooltip",
+      defaultMessage:
+        "If you add new students to your course, you can ensure they have all the assignments and handouts that you have already assigned to other students in the course.",
+    }),
     onClick: ({ props }) => {
       const { actions } = props;
       actions.setModal("copy-missing-handouts-and-assignments");
@@ -263,10 +357,13 @@ export const COMMANDS: { [name: string]: Command } = {
   },
   "empty-trash": {
     icon: "trash",
-    label: "Empty Trash",
-    button: "Trash",
-    title:
-      "Empty trash by purging deleted students, assignments, and handouts.",
+    label: labels.empty_trash,
+    button: labels.trash,
+    title: defineMessage({
+      id: "course.commands.empty-trash.tooltip",
+      defaultMessage:
+        "Empty trash by purging deleted students, assignments, and handouts.",
+    }),
     onClick: ({ props }) => {
       const { actions } = props;
       actions.setModal("empty-trash");
@@ -274,10 +371,16 @@ export const COMMANDS: { [name: string]: Command } = {
   },
   "delete-student-projects": {
     icon: "trash",
-    label: "Delete Student Projects",
-    button: "Delete",
-    title:
-      "If for some reason you would like to delete all the student projects created for this course, you may do so by clicking above.",
+    label: defineMessage({
+      id: "course.commands.delete-student-projects.title",
+      defaultMessage: "Delete Student Projects",
+    }),
+    button: labels.delete,
+    title: defineMessage({
+      id: "course.commands.delete-student-projects.tooltip",
+      defaultMessage:
+        "If for some reason you would like to delete all the student projects created for this course, you may do so by clicking above.",
+    }),
     onClick: ({ props }) => {
       const { actions } = props;
       actions.setModal("delete-student-projects");
@@ -285,10 +388,16 @@ export const COMMANDS: { [name: string]: Command } = {
   },
   "delete-students": {
     icon: "trash",
-    label: "Delete Students",
-    button: "Delete",
-    title:
-      "Student projects will not be deleted. If you make a mistake, students can still be undeleted from the Student tab or using TimeTravel.",
+    label: defineMessage({
+      id: "course.commands.delete-students.label",
+      defaultMessage: "Delete Students",
+    }),
+    button: labels.delete,
+    title: defineMessage({
+      id: "course.commands.delete-students.tooltip",
+      defaultMessage:
+        "Student projects will not be deleted. If you make a mistake, students can still be undeleted from the Student tab or using TimeTravel.",
+    }),
     onClick: ({ props }) => {
       const { actions } = props;
       actions.setModal("delete-students");
@@ -297,9 +406,12 @@ export const COMMANDS: { [name: string]: Command } = {
   "delete-shared-project": {
     icon: "trash",
     label: course.delete_shared_project,
-    button: "Delete",
-    title:
-      "Student projects will not be deleted. If you make a mistake, students can still be undeleted from the Student tab or using TimeTravel.",
+    button: labels.delete,
+    title: defineMessage({
+      id: "course.commands.delete-shared-project.tooltip",
+      defaultMessage:
+        "If it exists, delete the common shared project, which everybody has access to.",
+    }),
     onClick: ({ props }) => {
       const { actions } = props;
       actions.setModal("delete-shared-project");
@@ -308,9 +420,15 @@ export const COMMANDS: { [name: string]: Command } = {
   "create-shared-project": {
     icon: "users",
     label: course.create_shared_project,
-    button: "Shared",
-    title:
-      "Create a single common shared project, which everybody – students and all collaborators on this project (your TAs and other instructors) – have write access to.",
+    button: defineMessage({
+      id: "course.commands.create-shared-project.button",
+      defaultMessage: "Shared",
+    }),
+    title: defineMessage({
+      id: "course.commands.create-shared-project.tooltip",
+      defaultMessage:
+        "Create a single common shared project, which everybody – students and all collaborators on this project (your TAs and other instructors) – have write access to.",
+    }),
     onClick: ({ props }) => {
       const { actions } = props;
       actions.setModal("create-shared-project");
