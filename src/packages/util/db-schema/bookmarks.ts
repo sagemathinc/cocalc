81b06be53a905b8c8a7714a73ef2fcb9211c51ea
Harald Schilly <harald.schilly@gmail.com>
2024-10-31 14:00:14 +0100

    bookmarks: setup user_query support

--- src/packages/util/db-schema/bookmarks.ts
+++ src/packages/util/db-schema/bookmarks.ts
@@ -3,8 +3,10 @@
  *  License: MS-RSL – see LICENSE.md for details
  */
 
-import { Table } from "./types";
+import { BOOKMARK_TYPES } from "@cocalc/util/consts/bookmarks";
+
 import { ID } from "./crm";
+import { Table } from "./types";
 
 // This table stores various types of bookmarks. This started with backing up starred tabs for a user in a project.
 Table({
@@ -41,6 +43,41 @@ Table({
     desc: "Table for various types of bookmarks.",
     primary_key: "id",
     pg_indexes: ["type", "project_id", "account_id"],
-    user_query: {},
+    user_query: {
+      get: {
+        pg_where: [
+          {
+            "project_id = $::UUID": "project_id",
+            "account_id = $::UUID": "account_id",
+          },
+        ],
+        fields: {
+          id: null,
+          type: null,
+          project_id: null,
+          account_id: null,
+          path: null,
+          stars: null,
+          last_edited: null,
+        },
+      },
+      set: {
+        fields: {
+          type: true,
+          project_id: "project_write",
+          account_id: "account_id",
+          path: true,
+          stars: true,
+        },
+        async check_hook(_db, obj, _account_id, _project_id, cb) {
+          const type = obj.type;
+          if (!BOOKMARK_TYPES.includes(type)) {
+            cb("type '${type} is invalid");
+            return;
+          }
+          cb();
+        },
+      },
+    },
   },
 });
