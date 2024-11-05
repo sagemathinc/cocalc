b28c71d882c093495283e73ab683be67ea2c568c
William Stein <wstein@sagemath.com>
2024-11-05 18:58:08 +0000

    fix #7993 -- jupyter notebook output causes repeated stacktrace errors: ignore

--- src/packages/static/src/webapp-error.ts
+++ src/packages/static/src/webapp-error.ts
@@ -62,7 +62,7 @@ function handleError(event) {
       url,
       stack,
       showLoadFail,
-    })
+    }),
   );
 }
 
@@ -74,7 +74,7 @@ export default function init() {
     createRoot(crashContainer).render(React.createElement(Crash));
   } else {
     throw Error(
-      "there must be a div with id cocalc-crash-container in the document!"
+      "there must be a div with id cocalc-crash-container in the document!",
     );
   }
 
@@ -91,6 +91,12 @@ export function startedUp() {
 
 function isWhitelisted({ error }): boolean {
   try {
+    if (error?.stack?.includes("jupyter/output-messages")) {
+      // see https://github.com/sagemathinc/cocalc/issues/7993
+      // we should never show a popup cocalc crash when a jupyter message results
+      // in a crash, since this is user level code.
+      return true;
+    }
     if (error?.stack?.includes("Bokeh")) {
       // see https://github.com/sagemathinc/cocalc/issues/6507
       return true;
