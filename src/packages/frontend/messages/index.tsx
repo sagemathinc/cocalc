2107fb13b254df9257e0990169260da94894849c
William Stein <wstein@sagemath.com>
2024-11-26 04:11:47 +0000

    messages: fix issue with first load of component

--- src/packages/frontend/messages/index.tsx
+++ src/packages/frontend/messages/index.tsx
@@ -2,7 +2,7 @@
 Component to show all your messages.
 */
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { init } from "./redux";
 import Main from "./main";
 import { redux, useTypedRedux } from "@cocalc/frontend/app-framework";
@@ -15,12 +15,27 @@ import SmartAnchorTag from "@cocalc/frontend/components/smart-anchor-tag";
 import { ComposeModal } from "./compose";
 import ShowError from "@cocalc/frontend/components/error";
 
+// This is a little trick so we can initialize the redux store *only* when
+// the component is first mounted... which only happens if the user actually
+// clicks to visits the notifications page, not on any load.  This reduces
+// memory usage and server and network load.
+export default function Messages(props) {
+  const [initialized, setInitialized] = useState<boolean>(false);
+  useEffect(() => {
+    // ONLY initialize the state stuff if the actual messages
+    // are displayed, to avoid  waste of resources/load
+    init();
+    setInitialized(true);
+  }, []);
+
+  return initialized ? <Messages0 {...props} /> : null;
+}
+
 interface Props {
   filter?: Filter;
   style?;
 }
-
-export default function Messages({ filter, style }: Props) {
+function Messages0({ filter, style }: Props) {
   useEffect(() => {
     // ONLY initialize the state stuff if the actual messages
     // are displayed, to avoid  waste of resources/load
@@ -35,7 +50,6 @@ export default function Messages({ filter, style }: Props) {
   return (
     <FileContext.Provider value={{ AnchorTagComponent }}>
       <ConfigProvider renderEmpty={() => <Empty description={"No messages"} />}>
-        <ComposeModal />
         <div
           style={{
             overflowY: "auto",
@@ -45,6 +59,7 @@ export default function Messages({ filter, style }: Props) {
           className="smc-vfill"
         >
           <Search filter={filter} />
+          <ComposeModal />
           <ShowError
             error={error}
             setError={(error) => redux.getActions("messages").setError(error)}
