a66e226b4d384eb574f98acc6d0594ada1f51922
William Stein <wstein@sagemath.com>
2025-01-29 19:35:27 +0000

    subscriptions: make "use balance" more visible

--- src/packages/frontend/account/balance-toward-subs.tsx
+++ src/packages/frontend/account/balance-toward-subs.tsx
@@ -1,6 +1,6 @@
 // slightly weird props since this will be used in the nextjs app
 
-import { Card, Checkbox, Tooltip } from "antd";
+import { Alert, Card, Checkbox, Tooltip } from "antd";
 import { Icon } from "@cocalc/frontend/components/icon";
 
 export default function UseBalanceTowardSubscriptions({
@@ -10,17 +10,42 @@ export default function UseBalanceTowardSubscriptions({
   minimal,
 }) {
   const body = (
-    <Tooltip title="Enable this if you do not need to maintain a positive balance for pay as you go purchases.  If you are using compute servers you probably do not want to enable this. However, if you primarily put credit on your account to pay for subscriptions, consider enabling this.  The entire amount for the subscription renewal must be available.">
-      <Checkbox
-        checked={use_balance_toward_subscriptions}
-        onChange={(e) => {
-          set_use_balance_toward_subscriptions(e.target.checked);
-        }}
-      >
-        Use Balance - pay subscription using balance on your account, if
-        possible
-      </Checkbox>
-    </Tooltip>
+    <Alert
+      style={{ marginBottom: "15px" }}
+      type="info"
+      showIcon
+      message={
+        <div>
+          <Tooltip
+            title={
+              <div>
+                Enable this if you do not need to maintain a positive balance
+                for pay as you go purchases. If you are using compute servers
+                you probably do not want to enable this. However, if you
+                primarily put credit on your account to pay for subscriptions,
+                consider enabling this. The entire amount for the subscription
+                renewal must be available.
+              </div>
+            }
+          >
+            <Checkbox
+              checked={use_balance_toward_subscriptions}
+              onChange={(e) => {
+                set_use_balance_toward_subscriptions(e.target.checked);
+              }}
+            >
+              <span style={{ fontSize: "13pt" }}>
+                Use Balance - pay subscription using balance on your account, if
+                possible.{" "}
+                {!use_balance_toward_subscriptions && (
+                  <b>(Currently Disabled)</b>
+                )}
+              </span>
+            </Checkbox>
+          </Tooltip>
+        </div>
+      }
+    />
   );
   if (minimal) {
     return body;
