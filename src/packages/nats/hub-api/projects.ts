10390faac6e66f088e19c89f0a2d968b8ba84b04
William Stein <wstein@sagemath.com>
2025-02-05 00:58:43 +0000

    nats: work in progress rewriting copy path between projects...

--- src/packages/nats/hub-api/projects.ts
+++ src/packages/nats/hub-api/projects.ts
@@ -1,13 +1,16 @@
 import { authFirstRequireAccount } from "./util";
 import { type CreateProjectOptions } from "@cocalc/util/db-schema/projects";
+import { type UserCopyOptions } from "@cocalc/util/db-schema/projects";
 
 export const projects = {
   addProjectPermission: authFirstRequireAccount,
   createProject: authFirstRequireAccount,
+  copyPathBetweenProjects: authFirstRequireAccount,
 };
 
 export interface Projects {
   // request to have NATS permissions to project subjects.
   addProjectPermission: (opts: { project_id: string }) => Promise<void>;
   createProject: (opts: CreateProjectOptions) => Promise<string>;
+  copyPathBetweenProjects: (opts: UserCopyOptions) => Promise<void>;
 }
