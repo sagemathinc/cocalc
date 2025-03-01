37b6678b7409c1d59ccfe9cd013af6e87762aa79
William Stein <wstein@sagemath.com>
2025-03-01 15:38:59 +0000

    file-server: nfs share configuration

--- src/packages/file-server/zfs/index.ts
+++ src/packages/file-server/zfs/index.ts
@@ -2,7 +2,9 @@ import { executeCode } from "@cocalc/backend/execute-code";
 import { reuseInFlight } from "@cocalc/util/reuse-in-flight";
 import { isValidUUID } from "@cocalc/util/misc";
 
-const DEFAULT_NAMESPACE = process.env.NAMESPACE ?? "default";
+const context = {
+  namespace: process.env.NAMESPACE ?? "default",
+};
 
 const DEFAULT_QUOTA = "1G";
 
@@ -55,6 +57,9 @@ interface Project {
   // optional arbitrary affinity string - we attempt if possible to put
   // projects with the same affinity in the same pool, to improve chances of dedup.
   affinity?: string;
+  // comma separated list of hosts (or range using CIDR notation) that we're
+  // granting NFS client access to.
+  nfs?: string;
 }
 
 import Database from "better-sqlite3";
@@ -63,14 +68,14 @@ export function getDb(): Database.Database {
   if (db == null) {
     db = new Database("projects.db");
     db.prepare(
-      "CREATE TABLE IF NOT EXISTS projects (namespace TEXT, project_id TEXT, pool TEXT, archived TEXT, affinity TEXT, PRIMARY KEY (namespace, project_id))",
+      "CREATE TABLE IF NOT EXISTS projects (namespace TEXT, project_id TEXT, pool TEXT, archived TEXT, affinity TEXT, nfs TEXT, PRIMARY KEY (namespace, project_id))",
     ).run();
   }
   return db!;
 }
 
 export function dbProject({
-  namespace = DEFAULT_NAMESPACE,
+  namespace = context.namespace,
   project_id,
 }: {
   namespace?: string;
@@ -91,15 +96,17 @@ export async function getProject(opts) {
 }
 
 export async function createProject({
-  namespace = DEFAULT_NAMESPACE,
+  namespace = context.namespace,
   project_id,
   affinity,
   quota = DEFAULT_QUOTA,
+  nfs,
 }: {
   namespace?: string;
   project_id: string;
   affinity?: string;
   quota?: string;
+  nfs?: string;
 }) {
   const project = dbProject({ namespace, project_id });
   if (project != null) {
@@ -205,15 +212,15 @@ export async function createProject({
 
   // update database
   db.prepare(
-    "INSERT INTO projects(namespace,project_id,pool,affinity) VALUES(?,?,?,?)",
-  ).run(namespace, project_id, pool, affinity);
+    "INSERT INTO projects(namespace,project_id,pool,affinity,nfs) VALUES(?,?,?,?,?)",
+  ).run(namespace, project_id, pool, affinity, nfs);
 
   return dbProject({ namespace, project_id });
 }
 
 export async function setQuota({
   project_id,
-  namespace = DEFAULT_NAMESPACE,
+  namespace = context.namespace,
   quota,
 }: {
   namespace?: string;
@@ -236,7 +243,7 @@ export async function setQuota({
 
 export async function deleteProject({
   project_id,
-  namespace = DEFAULT_NAMESPACE,
+  namespace = context.namespace,
 }: {
   namespace?: string;
   project_id: string;
@@ -267,7 +274,7 @@ export async function deleteProject({
 
 export async function mountProject({
   project_id,
-  namespace = DEFAULT_NAMESPACE,
+  namespace = context.namespace,
 }: {
   namespace?: string;
   project_id: string;
@@ -275,7 +282,6 @@ export async function mountProject({
   const { pool } = dbProject({ namespace, project_id });
   try {
     await executeCode({
-      verbose: true,
       command: "sudo",
       args: ["zfs", "mount", `${pool}/${namespace}/${project_id}`],
     });
@@ -290,7 +296,7 @@ export async function mountProject({
 
 export async function unmountProject({
   project_id,
-  namespace = DEFAULT_NAMESPACE,
+  namespace = context.namespace,
 }: {
   namespace?: string;
   project_id: string;
@@ -309,4 +315,127 @@ export async function unmountProject({
       throw err;
     }
   }
+}
+
+// Ensure that this project is mounted and setup so that export to the
+// given client is allowed. Returns the remote address that the client
+// should use for NFS mounting.
+// If client is not given, just sets the share at NFS level
+// to what's specified in the database.
+export async function shareNFS({
+  client,
+  project_id,
+  namespace = context.namespace,
+}: {
+  client?: string;
+  project_id: string;
+  namespace?: string;
+}): Promise<string> {
+  client = client?.trim();
+  const { pool, nfs } = dbProject({ namespace, project_id });
+  let clients;
+  let hostname;
+  if (client) {
+    hostname = await hostnameFor(client);
+    const v = nfs?.split(",") ?? [];
+    if (!v.includes(client)) {
+      v.push(client);
+      // update database which tracks what the share should be.
+      const db = getDb();
+      db.prepare(
+        "UPDATE projects SET nfs=? WHERE namespace=? AND project_id=?",
+      ).run(v.join(","), namespace, project_id);
+    }
+    clients = v;
+  } else {
+    clients = nfs?.split(",") ?? [];
+  }
+  // actually ensure share is configured.
+  const name = `${pool}/${namespace}/${project_id}`;
+  const sharenfs =
+    clients.length > 0
+      ? `${clients.map((client) => `rw=${client}`).join(",")},no_root_squash,crossmnt,no_subtree_check`
+      : "off";
+  await executeCode({
+    verbose: true,
+    command: "sudo",
+    args: ["zfs", "set", `sharenfs=${sharenfs}`, name],
+  });
+  if (client) {
+    return `${hostname}:/projects/${namespace}/${project_id}`;
+  } else {
+    // no exports configured
+    return "";
+  }
+}
+
+// remove given client from nfs sharing
+export async function unshareNFS({
+  client,
+  project_id,
+  namespace = context.namespace,
+}: {
+  client: string;
+  project_id: string;
+  namespace?: string;
+}) {
+  const { nfs } = dbProject({ namespace, project_id });
+  let v = nfs?.split(",") ?? [];
+  if (!v.includes(client)) {
+    // nothing to do
+    return;
+  }
+  v = v.filter((x) => x != client);
+  // update database which tracks what the share should be.
+  const db = getDb();
+  db.prepare(
+    "UPDATE projects SET nfs=? WHERE namespace=? AND project_id=?",
+  ).run(v.join(","), namespace, project_id);
+  await shareNFS({ project_id, namespace });
+}
+
+let serverIps: null | string[] = null;
+async function hostnameFor(client: string) {
+  if (serverIps == null) {
+    const ipRegex = /^(\d{1,3}\.){3}\d{1,3}$/;
+    const { stdout } = await executeCode({
+      verbose: true,
+      command: "ifconfig",
+    });
+    let i = stdout.indexOf("inet ");
+    const v: string[] = [];
+    while (i != -1) {
+      let j = stdout.indexOf("\n", i);
+      if (j == -1) {
+        break;
+      }
+      const x = stdout.slice(i, j).split(" ");
+      const ip = x[1];
+      if (ipRegex.test(ip)) {
+        v.push(ip);
+      }
+      i = stdout.indexOf("inet ", j);
+    }
+    if (v.length == 0) {
+      throw Error("unable to determine server ip address");
+    }
+    serverIps = v;
+  }
+  for (const ip of serverIps) {
+    if (subnetMatch(ip, client)) {
+      return ip;
+    }
+  }
+  throw Error("found no matching subdomain");
+}
+
+// a and b are ip addresses.  Return true
+// if the are on the same subnet, by which
+// we mean that the first *TWO* segments match,
+// since that's the size of our subnets usually.
+// TODO: make configurable (?).
+function subnetMatch(a, b) {
+  const v = a.split(".");
+  const w = b.split(".");
+  return v[0] == w[0] && v[1] == w[1];
 }
