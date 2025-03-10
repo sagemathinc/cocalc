6d834c3facbfe0c94c355e7e0f295264588a4397
William Stein <wstein@sagemath.com>
2025-03-10 14:07:39 +0000

    fix a broken compute server test (deprecation of old images was done incorrectly so put back)
    - also "expect(groups.includes(info.group)).toBe(true);" is really
      annoying since you can't easily see why a test breaks...  Instead do
      "expect(groups).toContain(info.group);"

--- src/packages/util/compute-images.test.ts
+++ src/packages/util/compute-images.test.ts
@@ -14,28 +14,16 @@ test("consistent naming", () => {
     const [base, ts] =
       i > 0 ? [name.slice(0, i), name.slice(i + 1)] : [name, ""];
     expect(info.title).toBeDefined();
-    expect(groups.includes(info.group)).toBe(true);
+
+    expect(groups).toContain(info.group);
     const { group, title, short } = info;
 
     expect(short).toBeDefined();
     switch (group) {
       case "Main":
-        expect(
-          ["default", "ubuntu2004", "ubuntu2204", "ubuntu1804"].includes(base),
-        ).toBe(true);
-        break;
-
-      case "Ubuntu 16.04":
-        expect(["old"].includes(base)).toBe(true);
-        expect(title?.indexOf(ts) ?? 0 > 0);
-        break;
-
-      case "Ubuntu 18.04":
-        expect(["exp", "stable", "previous"].includes(base)).toBe(true);
-        expect(title?.indexOf(ts) ?? 0 > 0);
-        if (base === "stable") {
-          expect(ts.startsWith(short ?? "")).toBe(true);
-        }
+        expect(["default", "ubuntu2004", "ubuntu2204", "ubuntu1804"]).toContain(
+          base,
+        );
         break;
 
       case "Ubuntu 20.04":
