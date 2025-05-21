2d60f9f85c8ca803ade6f64076a25a5e2450f9f7
William Stein <wstein@sagemath.com>
2025-05-21 22:30:15 +0000

    fix some version mismatches

--- src/packages/project/conat/sync.ts
+++ src/packages/project/conat/sync.ts
@@ -2,10 +2,8 @@ import {
   dstream as createDstream,
   type DStream,
 } from "@cocalc/conat/sync/dstream";
-import { kv as createKV, type KV } from "@cocalc/conat/sync/kv";
 import { dkv as createDKV, type DKV } from "@cocalc/conat/sync/dkv";
 import { dko as createDKO, type DKO } from "@cocalc/conat/sync/dko";
-import { getEnv } from "./env";
 import { project_id } from "@cocalc/project/data";
 import {
   createOpenFiles,
@@ -17,22 +15,18 @@ import {
   type Inventory,
 } from "@cocalc/conat/sync/inventory";
 
-export type { DStream, KV, DKV, OpenFiles, OpenFileEntry };
+export type { DStream, DKV, OpenFiles, OpenFileEntry };
 
 export async function dstream<T = any>(opts): Promise<DStream<T>> {
   return await createDstream<T>({ project_id, ...opts });
 }
 
-export async function kv<T = any>(opts): Promise<KV<T>> {
-  return await createKV<T>({ project_id, env: await getEnv(), ...opts });
-}
-
 export async function dkv<T = any>(opts): Promise<DKV<T>> {
-  return await createDKV<T>({ project_id, env: await getEnv(), ...opts });
+  return await createDKV<T>({ project_id, ...opts });
 }
 
 export async function dko<T = any>(opts): Promise<DKO<T>> {
-  return await createDKO<T>({ project_id, env: await getEnv(), ...opts });
+  return await createDKO<T>({ project_id, ...opts });
 }
 
 export async function openFiles(): Promise<OpenFiles> {
