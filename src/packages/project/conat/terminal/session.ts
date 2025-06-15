4e20aa4be2f5f656fb817ecd2aeb654be59f1d37
William Stein <wstein@sagemath.com>
2025-06-15 11:45:03 +0000

    terminal -- better clear

--- src/packages/project/conat/terminal/session.ts
+++ src/packages/project/conat/terminal/session.ts
@@ -21,7 +21,7 @@ const logger = getLogger("project:conat:terminal:session");
 const EXIT_MESSAGE = "\r\n\r\n[Process completed - press any key]\r\n\r\n";
 
 const SOFT_RESET =
-  "tput rmcup; printf '\e[?1000l\e[?1002l\e[?1003l\e[?1006l\e[?1l'; clear -x";
+  "tput rmcup; printf '\e[?1000l\e[?1002l\e[?1003l\e[?1006l\e[?1l'; clear -x; sleep 0.1; clear -x";
 
 const COMPUTE_SERVER_INIT = `PS1="(\\h) \\w$ "; ${SOFT_RESET}; history -d $(history 1);\n`;
 
