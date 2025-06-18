ec9168ebf03f794abc386d23cee46bc03b15acea
William Stein <wstein@sagemath.com>
2025-06-18 14:49:57 +0000

    tweak some params to make unit tests in backend more robust

--- src/packages/backend/get-port.test.ts
+++ src/packages/backend/get-port.test.ts
@@ -8,12 +8,13 @@ describe("test getting a random available port", () => {
 });
 
 describe("test getPorts -- getting many ports at once in parallel", () => {
-  const count = 5000;
+  const count = 1000;
   it(`get ${count} ports at once, thus testing it isn't too slow and also nice to see no conflicts`, async () => {
     const start = Date.now();
     const w = await getPorts(count);
     expect(new Set(w).size).toBe(count);
-    // takes ~200ms to get 5000 of them on my laptop:
-    expect(Date.now() - start).toBeLessThan(2000);
+    // takes ~200ms to get 5000 of them on my laptop, but with heavy load
+    // it can be much slower.
+    expect(Date.now() - start).toBeLessThan(4000);
   });
 });
