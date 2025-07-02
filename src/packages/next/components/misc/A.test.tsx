58f5446d27798ec77e3d5fb7613d267d271250a7
William Stein <wstein@sagemath.com>
2025-07-02 18:54:57 +0000

    build and passing tests

--- src/packages/next/components/misc/A.test.tsx
+++ src/packages/next/components/misc/A.test.tsx
@@ -5,8 +5,12 @@ test("A opens a new tab at the right url", () => {
   const { getByRole } = render(<A href="https://cocalc.com">CoCalc</A>);
   const link = getByRole("link", { name: "CoCalc" });
 
+  // @ts-ignore
   expect(link).toHaveAttribute("target", "_blank");
+  // @ts-ignore
   expect(link).toHaveAttribute("href", "https://cocalc.com");
+  // @ts-ignore
   expect(link).toHaveAttribute("rel", "noopener");
+  // @ts-ignore
   expect(link).toHaveTextContent("CoCalc");
 });
