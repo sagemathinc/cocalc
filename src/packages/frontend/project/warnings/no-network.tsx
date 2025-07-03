363206c58905cb9b62c09c793bd1e56cef9b3071
Harald Schilly <harald.schilly@gmail.com>
2025-07-03 12:44:51 +0200

    frontend/project/settings: fix "is paid student project" logic and banner

--- src/packages/frontend/project/warnings/no-network.tsx
+++ src/packages/frontend/project/warnings/no-network.tsx
@@ -6,18 +6,15 @@
 import { Alert } from "@cocalc/frontend/antd-bootstrap";
 import { Icon } from "@cocalc/frontend/components";
 import { UPGRADE_HINT } from "./non-member";
-import { NO_INTERNET } from "@cocalc/frontend/project/trial-banner";
 
 export function NoNetworkProjectWarning() {
   return (
     <Alert bsStyle="warning" style={{ margin: "15px" }}>
       <h4>
         <Icon name="exclamation-triangle" /> Warning: this project{" "}
-        <strong>does not have full internet access</strong>
+        <strong>does not have full internet access</strong>.
       </h4>
-      <p>
-        Projects without internet {NO_INTERNET}. {UPGRADE_HINT}
-      </p>
+      <p>{UPGRADE_HINT}</p>
     </Alert>
   );
 }
