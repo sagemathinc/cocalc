3b4178a0935dcacc3b96294e91a3c9d579537303
William Stein <wstein@sagemath.com>
2025-07-11 21:41:32 +0000

    modern style for client-test.ts

--- src/packages/sync/editor/string/test/client-test.ts
+++ src/packages/sync/editor/string/test/client-test.ts
@@ -8,7 +8,6 @@ Minimal client class that we use for testing.
 */
 
 import { EventEmitter } from "events";
-import { bind_methods, keys } from "@cocalc/util/misc";
 import {
   Client as Client0,
   FileWatcher as FileWatcher0,
@@ -39,62 +38,61 @@ export class Client extends EventEmitter implements Client0 {
     super();
     this._client_id = client_id;
     this.initial_get_query = initial_get_query;
-    bind_methods(this, ["query", "dbg", "query_cancel"]);
   }
 
-  public server_time(): Date {
+  server_time = (): Date => {
     return new Date();
-  }
+  };
 
   isTestClient = () => {
     return true;
   };
 
-  public is_project(): boolean {
+  is_project = (): boolean => {
     return false;
-  }
+  };
 
-  public is_browser(): boolean {
+  is_browser = (): boolean => {
     return true;
-  }
+  };
 
-  public is_compute_server(): boolean {
+  is_compute_server = (): boolean => {
     return false;
-  }
+  };
 
-  public dbg(_f: string): Function {
+  dbg = (_f: string): Function => {
     //     return (...args) => {
     //       console.log(_f, ...args);
     //     };
     return (..._) => {};
-  }
+  };
 
-  public mark_file(_opts: {
+  mark_file = (_opts: {
     project_id: string;
     path: string;
     action: string;
     ttl: number;
-  }): void {
+  }): void => {
     //console.log("mark_file", opts);
-  }
+  };
 
-  public log_error(opts: {
+  log_error = (opts: {
     project_id: string;
     path: string;
     string_id: string;
     error: any;
-  }): void {
+  }): void => {
     console.log("log_error", opts);
-  }
+  };
 
-  public query(opts): void {
+  query = (opts): void => {
     if (opts.options && opts.options.length === 1 && opts.options[0].set) {
       // set query
       this.set_queries.push(opts);
       opts.cb();
     } else {
       // get query -- returns predetermined result
-      const table = keys(opts.query)[0];
+      const table = Object.keys(opts.query)[0];
       let result = this.initial_get_query[table];
       if (result == null) {
         result = [];
@@ -102,88 +100,88 @@ export class Client extends EventEmitter implements Client0 {
       //console.log("GET QUERY ", table, result);
       opts.cb(undefined, { query: { [table]: result } });
     }
-  }
+  };
 
-  path_access(opts: { path: string; mode: string; cb: Function }): void {
+  path_access = (opts: { path: string; mode: string; cb: Function }): void => {
     console.log("path_access", opts.path, opts.mode);
     opts.cb(true);
-  }
-  path_exists(opts: { path: string; cb: Function }): void {
+  };
+  path_exists = (opts: { path: string; cb: Function }): void => {
     console.log("path_access", opts.path);
     opts.cb(true);
-  }
-  path_stat(opts: { path: string; cb: Function }): void {
+  };
+  path_stat = (opts: { path: string; cb: Function }): void => {
     console.log("path_state", opts.path);
     opts.cb(true);
-  }
-  async path_read(opts: {
+  };
+  path_read = async (opts: {
     path: string;
     maxsize_MB?: number;
     cb: Function;
-  }): Promise<void> {
+  }): Promise<void> => {
     console.log("path_ready", opts.path);
     opts.cb(true);
-  }
-  async write_file(opts: {
+  };
+  write_file = async (opts: {
     path: string;
     data: string;
     cb: Function;
-  }): Promise<void> {
+  }): Promise<void> => {
     console.log("write_file", opts.path, opts.data);
     opts.cb(true);
-  }
-  watch_file(opts: { path: string }): FileWatcher {
+  };
+  watch_file = (opts: { path: string }): FileWatcher => {
     return new FileWatcher(opts.path);
-  }
+  };
 
-  public is_connected(): boolean {
+  is_connected = (): boolean => {
     return true;
-  }
+  };
 
-  public is_signed_in(): boolean {
+  is_signed_in = (): boolean => {
     return true;
-  }
+  };
 
-  public touch_project(_): void {}
+  touch_project = (_): void => {};
 
-  public query_cancel(_): void {}
+  query_cancel = (_): void => {};
 
-  public alert_message(_): void {}
+  alert_message = (_): void => {};
 
-  public is_deleted(_filename: string, _project_id?: string): boolean {
+  is_deleted = (_filename: string, _project_id?: string): boolean => {
     return false;
-  }
+  };
 
-  public set_deleted(_filename: string, _project_id?: string): void {}
+  set_deleted = (_filename: string, _project_id?: string): void => {};
 
-  async synctable_ephemeral(
+  synctable_ephemeral = async (
     _project_id: string,
     query: any,
     options: any,
     throttle_changes?: number,
-  ): Promise<SyncTable> {
+  ): Promise<SyncTable> => {
     const s = new SyncTable(query, options, this, throttle_changes);
     await once(s, "connected");
     return s;
-  }
+  };
 
-  async synctable_conat(_query: any): Promise<SyncTable> {
+  synctable_conat = async (_query: any): Promise<SyncTable> => {
     throw Error("synctable_conat: not implemented");
-  }
-  async pubsub_conat(_query: any): Promise<SyncTable> {
+  };
+  pubsub_conat = async (_query: any): Promise<SyncTable> => {
     throw Error("pubsub_conat: not implemented");
-  }
+  };
 
   // account_id or project_id
-  public client_id(): string {
+  client_id = (): string => {
     return this._client_id;
-  }
+  };
 
-  public sage_session({ path }): void {
+  sage_session = ({ path }): void => {
     console.log(`sage_session: path=${path}`);
-  }
+  };
 
-  public shell(opts: ExecuteCodeOptionsWithCallback): void {
+  shell = (opts: ExecuteCodeOptionsWithCallback): void => {
     console.log(`shell: opts=${JSON.stringify(opts)}`);
-  }
+  };
 }
