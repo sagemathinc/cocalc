0b8b0750af4013506df9638bf953ca2cfbdb3639
Harald Schilly <harald.schilly@gmail.com>
2025-07-30 11:57:54 +0200

    frontend/activity bar: make full pages the default for everyone -- fixes #8475

--- src/packages/frontend/project/page/activity-bar.tsx
+++ src/packages/frontend/project/page/activity-bar.tsx
@@ -3,32 +3,22 @@
  *  License: MS-RSL – see LICENSE.md for details
  */
 
-import { AccountStore } from "@cocalc/frontend/account";
-import { redux } from "@cocalc/frontend/app-framework";
 import { ACTIVITY_BAR_OPTIONS } from "./activity-bar-consts";
 
-const FLYOUT_DEFAULT_DATE = new Date("2100-01-01");
-
-// New users created after this date will have the default activity bar option set to "flyout"
-function getDefaultActivityBarOption() {
-  const store: AccountStore = redux.getStore("account");
-  if (store == null) return "both";
-  const created = store.get("created");
-  // check that created is a Date
-  if (created == null || !(created instanceof Date)) return "both";
-  // if created is after this date return "flyout", else "both"
-  if (created > FLYOUT_DEFAULT_DATE) {
-    return "flyout";
-  } else {
-    return "both";
-  }
+// If not set, default is full pages
+// https://github.com/sagemathinc/cocalc/issues/8475
+function getDefaultActivityBarOption(): keyof typeof ACTIVITY_BAR_OPTIONS {
+  return "full";
 }
 
 export function getValidActivityBarOption(
-  vbar_setting: any,
+  activityBarSetting: any,
 ): keyof typeof ACTIVITY_BAR_OPTIONS {
-  if (typeof vbar_setting !== "string" || ACTIVITY_BAR_OPTIONS[vbar_setting] == null) {
+  if (
+    typeof activityBarSetting !== "string" ||
+    ACTIVITY_BAR_OPTIONS[activityBarSetting] == null
+  ) {
     return getDefaultActivityBarOption();
   }
-  return vbar_setting as keyof typeof ACTIVITY_BAR_OPTIONS;
+  return activityBarSetting as keyof typeof ACTIVITY_BAR_OPTIONS;
 }
