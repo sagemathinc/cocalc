5cf7e69c7f845156dc812b8bb93afabb201524f9
William Stein <wstein@sagemath.com>
2025-08-25 16:16:37 -0700

    fix a bunch of version incompatibilities; fix some unit tests

--- src/packages/frontend/test/setup.js
+++ src/packages/frontend/test/setup.js
@@ -1,2 +1,7 @@
 require("@testing-library/jest-dom");
 process.env.COCALC_TEST_MODE = true;
+
+// polyfill TextEncoder so we can run tests using nodej.s
+const { TextEncoder, TextDecoder } = require("util");
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
