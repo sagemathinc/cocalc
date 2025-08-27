695736785af3a9be5ade17cbb592158e16aa1f39
William Stein <wstein@sagemath.com>
2025-08-27 10:29:34 -0700

    fix so sea works on macos too

--- src/packages/lite/sea/build.sh
+++ src/packages/lite/sea/build.sh
@@ -1,8 +1,26 @@
 set -ev
 
-cp $HOME/.nvm/versions/node/v24.*/bin/node cocalc
+cp "$(command -v node)" cocalc
+
+cp ../cocalc-lite.tar.gz .
+
+if [ `uname` == "Darwin" ]; then
+  codesign --remove-signature cocalc
+fi
 
 node --experimental-sea-config sea-config.json
 
-npx postject cocalc NODE_SEA_BLOB sea-prep.blob \
-    --sentinel-fuse NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2
+if [ `uname` == "Linux" ]; then
+    npx postject hello NODE_SEA_BLOB sea-prep.blob \
+    --sentinel-fuse NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2 
+fi
+
+if [ `uname` == "Darwin" ]; then
+   # 2) Inject the SEA blob (macOS needs the Mach-O segment name)
+    npx postject cocalc NODE_SEA_BLOB sea-prep.blob \
+      --sentinel-fuse NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2 \
+      --macho-segment-name NODE_SEA
+
+    # 3) Re-sign (ad-hoc is fine for local testing)
+    codesign --sign - cocalc
+fi
