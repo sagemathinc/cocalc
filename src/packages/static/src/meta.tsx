7849864ae37a444023698ce2220351a81225d713
William Stein <wstein@sagemath.com>
2025-08-28 16:00:42 -0700

    fix #8541 - whitelist youtube (and vimeo) videos on the share server (and in timetravel)
    - I did this by switching from xss to sanitize-html, which seems to be a
      lot cleaner and easier to use, just as popular, updated more recently, etc.

--- src/packages/static/src/meta.tsx
+++ src/packages/static/src/meta.tsx
@@ -4,7 +4,7 @@ export default function Meta() {
       <meta charSet="utf-8" />
 
       <meta name="application-name" content="CoCalc" />
-      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="mobile-web-app-capable" content="yes" />
       <meta name="apple-mobile-web-app-status-bar-style" content="black" />
       <meta name="apple-mobile-web-app-title" content="CoCalc" />
       <meta name="theme-color" content="#fbb635" />
