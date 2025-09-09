10e5b31f12eb0eb9a47f950840b7d8ec2df80ff5
William Stein <wstein@sagemath.com>
2025-09-08 21:09:15 -0700

    sync api: add better structure/typing

--- src/packages/conat/hub/api/sync.ts
+++ src/packages/conat/hub/api/sync.ts
@@ -1,11 +1,25 @@
 import { authFirst } from "./util";
 
+export interface Patch {
+  seq: number;
+  time: number;
+  mesg: {
+    time: number;
+    wall: number;
+    patch: string;
+    user_id: number;
+    is_snapshot?: boolean;
+    parents: number[];
+    version?: number;
+  };
+}
+
 export interface Sync {
   history: (opts: {
     account_id?: string;
     project_id: string;
     path: string;
-  }) => Promise<any[]>;
+  }) => Promise<{ patches: Patch[] }>;
 }
 
 export const sync = {
--- src/packages/server/conat/api/sync.ts
+++ src/packages/server/conat/api/sync.ts
@@ -1,18 +1,21 @@
 import { conat } from "@cocalc/backend/conat";
 import isCollaborator from "@cocalc/server/projects/is-collaborator";
 import { patchesStreamName } from "@cocalc/conat/sync/synctable-stream";
+import { type Patch } from "@cocalc/conat/hub/api/sync";
 
 export async function history({
   account_id,
   project_id,
   path,
   start_seq = 0,
+  end_seq,
 }: {
   account_id?: string;
   project_id: string;
   path: string;
-  start_seq: number;
-}): Promise<any[]> {
+  start_seq?: number;
+  end_seq?: number;
+}): Promise<{ patches: Patch[] }> {
   if (!account_id || !(await isCollaborator({ account_id, project_id }))) {
     throw Error("user must be collaborator on source project");
   }
@@ -24,21 +27,12 @@ export async function history({
     project_id,
     noInventory: true,
   });
-  const patches: {
-    seq: number;
-    time: number;
-    mesg: {
-      time: number;
-      wall: number;
-      patch: string;
-      user_id: number;
-      is_snapshot?: boolean;
-      parents: number[];
-      version?: number;
-    };
-  }[] = [];
-  for await (const patch of await astream.getAll({ start_seq })) {
-    patches.push(patch);
+  const patches: Patch[] = [];
+  for await (const patch of await astream.getAll({
+    start_seq,
+    end_seq,
+  })) {
+    patches.push(patch as any);
   }
   return { patches };
 }
