978125efcf347699a1f031d96f3cbe7277ce15e0
William Stein <wstein@sagemath.com>
2025-09-08 21:28:21 -0700

    api: add info to history query

--- src/packages/conat/hub/api/sync.ts
+++ src/packages/conat/hub/api/sync.ts
@@ -14,6 +14,24 @@ export interface Patch {
   };
 }
 
+export interface HistoryInfo {
+  doctype: string;
+  init: { time: Date; size: number; error: string };
+  last_active: Date;
+  path: string;
+  project_id: string;
+  read_only: boolean;
+  save: {
+    state: string;
+    error: string;
+    hash: number;
+    time: number;
+    expected_hash: number;
+  };
+  string_id: string;
+  users: string[];
+}
+
 export interface Sync {
   history: (opts: {
     account_id?: string;
--- src/packages/server/conat/api/sync.ts
+++ src/packages/server/conat/api/sync.ts
@@ -1,7 +1,8 @@
 import { conat } from "@cocalc/backend/conat";
 import isCollaborator from "@cocalc/server/projects/is-collaborator";
 import { patchesStreamName } from "@cocalc/conat/sync/synctable-stream";
-import { type Patch } from "@cocalc/conat/hub/api/sync";
+import { type Patch, type HistoryInfo } from "@cocalc/conat/hub/api/sync";
+import { client_db } from "@cocalc/util/db-schema/client-db";
 
 export async function history({
   account_id,
@@ -15,7 +16,7 @@ export async function history({
   path: string;
   start_seq?: number;
   end_seq?: number;
-}): Promise<{ patches: Patch[] }> {
+}): Promise<{ patches: Patch[]; info: HistoryInfo }> {
   if (!account_id || !(await isCollaborator({ account_id, project_id }))) {
     throw Error("user must be collaborator on source project");
   }
@@ -34,5 +35,18 @@ export async function history({
   })) {
     patches.push(patch as any);
   }
-  return { patches };
+
+  const akv = client.sync.akv({
+    name: `__dko__syncstrings:${client_db.sha1(project_id, path)}`,
+    project_id,
+    noInventory: true,
+  });
+  const keys = await akv.keys();
+  const info: Partial<HistoryInfo> = {};
+  for (const key of keys) {
+    if (key[0] != "[") continue;
+    info[JSON.parse(key)[1]] = await akv.get(key);
+  }
+
+  return { patches, info: info as HistoryInfo };
 }
