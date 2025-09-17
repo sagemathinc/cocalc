e12986d89bf45a1ff7204fa6c5fff5452b7f8ba8
William Stein <wstein@sagemath.com>
2025-09-17 09:57:11 -0700

    add dbclient ssh wrapper; add sync-times script

--- src/packages/backend/bin/install.sh
+++ src/packages/backend/bin/install.sh
@@ -3,3 +3,11 @@ set -ev
 
 cp "`pwd`/dist/bin/open.js" node_modules/.bin/open
 chmod +x node_modules/.bin/open
+
+cp "`pwd`/dist/bin/sync-mtime-ssh.js" node_modules/.bin/sync-mtime-ssh
+chmod +x node_modules/.bin/sync-mtime-ssh
+
+
+mkdir -p node_modules/.bin/mutagen.bin
+cp "`pwd`/bin/dbclient-wrapper.sh" node_modules/.bin/mutagen.bin/ssh
+chmod +x  node_modules/.bin/mutagen.bin/ssh
