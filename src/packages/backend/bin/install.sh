775a6117be8b020e761910815d0c45caff71ca4c
William Stein <wstein@sagemath.com>
2025-10-02 13:46:03 -0700

    remove sync-mtime -- we will use rsync for this when we need something like this

--- src/packages/backend/bin/install.sh
+++ src/packages/backend/bin/install.sh
@@ -4,7 +4,5 @@ set -ev
 cp "`pwd`/dist/bin/open.js" node_modules/.bin/open
 chmod +x node_modules/.bin/open
 
-cp "`pwd`/dist/bin/sync-mtime-ssh.js" node_modules/.bin/sync-mtime-ssh
-chmod +x node_modules/.bin/sync-mtime-ssh
 
 
