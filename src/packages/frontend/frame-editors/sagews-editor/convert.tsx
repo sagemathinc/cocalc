17adf294e554c68f41d5e738b7718660c7d23ac3
William Stein <wstein@sagemath.com>
2025-10-08 11:56:39 -0700

    get content of sagews

--- src/packages/frontend/frame-editors/sagews-editor/convert.tsx
+++ src/packages/frontend/frame-editors/sagews-editor/convert.tsx
@@ -1,6 +1,6 @@
 import { Button } from "antd";
 
-export default function Convert({}) {
+export default function Convert({ actions }) {
   return (
     <div className="smc-vfill">
       <div style={{ textAlign: "center", marginTop: "45px" }}>
@@ -9,6 +9,18 @@ export default function Convert({}) {
         <Button size="large" type="primary">
           Convert to Jupyter Notebook
         </Button>
+        <hr />
+        <pre
+          style={{
+            textAlign: "left",
+            margin: "30px auto",
+            maxWidth: "900px",
+            border: "1px solid #aaa",
+            padding: "15px",
+          }}
+        >
+          {actions._syncstring?.to_str()}
+        </pre>
       </div>
     </div>
   );
