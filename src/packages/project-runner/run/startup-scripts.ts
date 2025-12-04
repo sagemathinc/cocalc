d20a2660a2793f5e629a6940149b87b74aad6b7e
William Stein <wstein@sagemath.com>
2025-12-03 19:26:44 -0800

    ssh config: fix permissions

--- src/packages/project-runner/run/startup-scripts.ts
+++ src/packages/project-runner/run/startup-scripts.ts
@@ -1,10 +1,13 @@
-import { SSHD_CONFIG, START_PROJECT_SSH } from "@cocalc/conat/project/runner/constants";
+import {
+  SSHD_CONFIG,
+  START_PROJECT_SSH,
+} from "@cocalc/conat/project/runner/constants";
 import { mkdir, writeFile } from "node:fs/promises";
 import { dirname, join } from "path";
 
 export async function writeStartupScripts(home: string) {
   const ssh = join(home, START_PROJECT_SSH);
-  await mkdir(dirname(ssh), { recursive: true });
+  await mkdir(dirname(ssh), { recursive: true, mode: 0o700 });
   await writeFile(ssh, START_PROJECT_SSH_SERVER_SH, {
     mode: 0o700,
   });
@@ -17,6 +20,8 @@ const START_PROJECT_SSH_SERVER_SH = `#!/usr/bin/env bash
 set -ev
 
 mkdir -p /etc/dropbear
+
+chmod og-rwx -R /root/.ssh
 
 dropbear -p \${COCALC_SSHD_PORT:=22} -e -s -a -R -D /root/${SSHD_CONFIG}
 
