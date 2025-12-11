343ec5897f3191df5fdb03a053f64ea2b3cdbdab
William Stein <wstein@sagemath.com>
2025-12-10 16:55:22 -0800

    delete test of something that we no longer need to test (and it was hangin)

--- src/packages/project/main.test.ts
+++
@@ -1,6 +0,0 @@
-export {};
-
-test("we import the main program under nodejs", async () => {
-  // This should work.  Ensures we're using nodejs code only.
-  await import("@cocalc/project/project");
-});
