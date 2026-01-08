43d74c76b986e36dbc9683ccb8429bf770963dac
William Stein <wstein@sagemath.com>
2026-01-08 08:52:05 -0800

    fix merge conflict

--- src/packages/next/pages/api/v2/software/status.ts
+++ src/packages/next/pages/api/v2/software/status.ts
@@ -3,12 +3,7 @@ import {
   isLaunchpadMode,
 } from "@cocalc/server/software-licenses/activation";
 
-<<<<<<< HEAD
 export default async function handle(_req, res) {
-||||||| f47e9a0adb
-=======
-export default async function handle(req, res) {
->>>>>>> b1e11986e362b0b55b2da835d6f7be487b670692
   try {
     if (!isLaunchpadMode()) {
       res.json({ activated: true });
